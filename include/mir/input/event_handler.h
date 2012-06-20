a49dfbb4df30f28b45c9e3f968bae651aa6d1a0a
Thomas Voß <thomas.voss@rub.de>
2012-06-20 16:16:17 +0200

    Added a first test-case for input-handling.

---
+++ include/mir/input/event_handler.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of
+ * Canonical Ltd. not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior permission.
+ * Canonical Ltd. makes no representations about the suitability of this
+ * software for any purpose. It is provided "as is" without express or
+ * implied warranty.
+ *
+ * CANONICAL, LTD. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+ * NO EVENT SHALL CANONICAL, LTD. BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Authored by: Thomas Voss <thomas.voss@canonical.com>
+ */
+
+#ifndef EVENT_HANDLER_H_
+#define EVENT_HANDLER_H_
+
+namespace mir
+{
+namespace input
+{
+
+class event;
+
+// event_handler is the interface by which input
+// devices know the input dispatcher
+class event_handler
+{
+ public:
+  virtual ~event_handler() {}
+
+  virtual void on_event(event* e) = 0;
+};
+
+}}
+
+#endif /* EVENT_HANDLER_H_ */
---
+++ tests/test_input_dispatch.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of
+ * Canonical Ltd. not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior permission.
+ * Canonical Ltd. makes no representations about the suitability of this
+ * software for any purpose. It is provided "as is" without express or
+ * implied warranty.
+ *
+ * CANONICAL, LTD. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+ * NO EVENT SHALL CANONICAL, LTD. BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Authored by: Thomas Voss <thomas.voss@canonical.com>
+ */
+
+#include "mir/input/device.h"
+#include "mir/input/dispatcher.h"
+#include "mir/input/filter.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace mi = mir::input;
+
+namespace
+{
+
+class mock_filter : public mi::filter {
+ public:
+  MOCK_METHOD1(accept, bool(mi::event*));
+};
+
+class mock_input_device : public mi::device
+{
+public:
+  mock_input_device(mi::event_handler* h) : mi::device(h)
+  {
+  }
+  
+  void trigger_event()
+  {
+    handler->on_event(nullptr);
+  }
+};
+}
+
+TEST(input_dispatch, incoming_input_triggers_filter)
+{
+	using namespace testing;
+        mi::dispatcher dispatcher;
+        
+        mock_filter filter;
+        dispatcher.register_filter(&filter);
+        
+        mock_input_device device(&dispatcher);
+       
+	EXPECT_CALL(filter, accept(_)).Times(AtLeast(1));
+        
+	device.trigger_event();
+}
