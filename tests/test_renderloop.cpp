9b88960a21e59f8da15b8cdc6544d701397b85ca
Thomas Voß <thomas.voss@rub.de>
2012-06-20 10:16:16 +0200

    Added a basic cmake setup and a first test for testing setup.

---
+++ tests/test_renderloop.cpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of
+ * Canonical Ltd. not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior permission.
+ * Canonical Ltd. makes no representations about the suitability of this
+ * software for any purpose. It is provided "as is" without express or
+ * implied warranty.
+ *
+ * CANONICAL, LTD. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+ * NO EVENT SHALL CANONICAL, LTD. BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Authored by: Thomas Voss <thomas.voss@canonical.com>
+ */
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+using ::testing::AtLeast;
+
+namespace {
+
+class Trigger {
+ public:
+  virtual ~Trigger() {}
+
+  virtual void Do() = 0;
+};
+
+class MockTrigger : public Trigger {
+ public:
+  MOCK_METHOD0(Do, void());
+};
+
+}
+
+TEST(RenderLoop, Trigger) {
+  MockTrigger trigger;
+  EXPECT_CALL(trigger, Do()).Times(AtLeast(1));
+
+  trigger.Do();
+}
+
