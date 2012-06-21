8cf173be73b310bc3e7d2c2fa9a46d1085611281
Alan Griffiths <alan@octopull.co.uk>
2012-06-21 09:54:58 +0100

    Add unit test directories

---
+++ unit-tests/compositor/test-compositor.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of
+ * Canonical Ltd. not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior permission.
+ * Canonical Ltd. makes no representations about the suitability of this
+ * software for any purpose. It is provided "as is" without express or
+ * implied warranty.
+ *
+ * CANONICAL, LTD. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+ * NO EVENT SHALL CANONICAL, LTD. BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Authored by: Alan Griffiths <alan@octopull.co.uk>
+ */
+
+#include "mir/compositor/compositor.h"
+#include "mir/compositor/buffer_manager.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace mc = mir::compositor;
+
+namespace
+{
+
+class mock_buffer_renderer : public mc::buffer_renderer
+{
+public:
+    MOCK_METHOD0(render, void ());
+};
+}
+
+
+TEST(compositor, render)
+{
+	using namespace testing;
+
+	mock_buffer_renderer buffer_renderer;
+	mc::compositor comp(nullptr, &buffer_renderer);
+
+	EXPECT_CALL(buffer_renderer, render()).Times(AtLeast(1));
+
+	comp.render(nullptr);
+}
