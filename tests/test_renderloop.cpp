92998356c35896283cfb60e83ae24e408f1a2a87
Alan Griffiths <alan@octopull.co.uk>
2012-06-25 15:30:39 +0100

    Test for scenegraph being queried for surfaces

--- tests/test_renderloop.cpp
+++ tests/test_renderloop.cpp
@@ -27,13 +27,16 @@
 #include "mir/compositor/drawer.h"
 #include "mir/compositor/compositor.h"
 #include "mir/compositor/buffer_manager.h"
+#include "mir/surfaces/scenegraph.h"
+#include "mir/geometry/rectangle.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 namespace mc = mir::compositor;
 namespace mg = mir::graphics;
-
+namespace ms = mir::surfaces;
+namespace geom = mir::geometry;
 
 namespace
 {
@@ -42,18 +45,46 @@ class mock_framebuffer_backend : public mg::framebuffer_backend
 public:
     MOCK_METHOD0(render, void ());
 };
-}
 
+class mock_scenegraph : public ms::scenegraph
+{
+public:
+    MOCK_METHOD1(get_surfaces_in, ms::surfaces_to_render (geom::rectangle const&));
+};
+}
 
 TEST(compositor_renderloop, notify_sync_and_see_paint)
 {
     using namespace testing;
 
     mock_framebuffer_backend graphics;
+    mock_scenegraph scenegraph;
+
     mc::buffer_manager buffer_manager(&graphics);
-    mc::drawer&& comp = mc::compositor(nullptr, &buffer_manager);
+    mc::drawer&& comp = mc::compositor(&scenegraph, &buffer_manager);
 
     EXPECT_CALL(graphics, render()).Times(AtLeast(1));
+
+    EXPECT_CALL(scenegraph, get_surfaces_in(_))
+    		.WillRepeatedly(Return(ms::surfaces_to_render()));
+
+    comp.render(nullptr);
+}
+
+TEST(compositor_renderloop, notify_sync_and_see_scenegraph_query)
+{
+    using namespace testing;
+
+    mock_framebuffer_backend graphics;
+    mock_scenegraph scenegraph;
+
+    mc::buffer_manager buffer_manager(&graphics);
+    mc::drawer&& comp = mc::compositor(&scenegraph, &buffer_manager);
+
+    EXPECT_CALL(graphics, render());
+
+    EXPECT_CALL(scenegraph, get_surfaces_in(_)).Times(AtLeast(1))
+    		.WillRepeatedly(Return(ms::surfaces_to_render()));
 
     comp.render(nullptr);
 }
