bbfb4d392ea4c3802178c4d71c8ecb6b6f3a7582
Thomas Voß <thomas.voss@rub.de>
2012-06-25 10:57:14 +0200

    Moved Dispatcher impl into its own implementation file.

--- src/input/dispatcher.cpp
+++ src/input/dispatcher.cpp
@@ -23,48 +23,52 @@
  * Authored by: Thomas Voss <thomas.voss@canonical.com>
  */
 
-#ifndef DISPATCHER_H_
-#define DISPATCHER_H_
-
+#include "mir/input/dispatcher.h"
 #include "mir/input/event_handler.h"
+#include "mir/input/filter.h"
 
 #include <cassert>
 
-namespace mir
+namespace mi = mir::input;
+
+mi::Dispatcher::Dispatcher(mi::Filter* shell_filter,
+                           mi::Filter* grab_filter,
+                           mi::Filter* application_filter)
+        : shell_filter(shell_filter),
+          grab_filter(grab_filter),
+          application_filter(application_filter)
 {
-namespace input
-{
-
-class Device;
-class Event;
-class Filter;
-
-class Dispatcher : public EventHandler
-{
- public:
-
-    Dispatcher(Filter* shell_filter,
-               Filter* grab_filter,
-               Filter* application_filter);
-
-    ~Dispatcher() = default;
-
-    // Implemented from EventHandler
-    void OnEvent(Event* e);
-
-    void RegisterShellFilter(Filter* f);
-
-    void RegisterGrabFilter(Filter* f);
-
-    void RegisterApplicationFilter(Filter* f);
-
- private:
-    Filter* shell_filter;
-    Filter* grab_filter;
-    Filter* application_filter;
-};
-
-}
+    assert(shell_filter);
+    assert(grab_filter);
+    assert(application_filter);
 }
 
-#endif /* DISPATCHER_H_ */
+// Implemented from EventHandler
+void mi::Dispatcher::OnEvent(mi::Event* e)
+{
+    if (shell_filter->Accept(e))
+        return;
+
+    if (grab_filter->Accept(e))
+        return;
+
+    application_filter->Accept(e);
+}
+
+void mi::Dispatcher::RegisterShellFilter(mi::Filter* f)
+{
+    assert(f);
+    shell_filter = f;
+}
+
+void mi::Dispatcher::RegisterGrabFilter(mi::Filter* f)
+{
+    assert(f);
+    grab_filter = f;
+}
+
+void mi::Dispatcher::RegisterApplicationFilter(mi::Filter* f)
+{
+    assert(f);
+    application_filter = f;
+}
