b2d8a51b06b8cd96ec5b63e55c5e7d1b6fbc2a2b
Kevin DuBois <kevin.dubois@canonical.com>
2012-06-27 14:34:16 -0700

    add graphic buffer allocation header (tests still don't compile)

---
+++ include/mir/compositor/graphic_buffer_allocator.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#ifndef MIR_COMPOSITOR_GRAPHIC_BUFFER_ALLOCATOR_H_
+#define MIR_COMPOSITOR_GRAPHIC_BUFFER_ALLOCATOR_H_
+
+namespace mir
+{
+namespace compositor
+{
+
+class GraphicBufferAllocator
+{
+    public:
+};
+
+}
+}
+#endif // MIR_COMPOSITOR_GRAPHIC_BUFFER_ALLOCATOR_H_
