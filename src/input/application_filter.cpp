7a4f71780dc993d424e10452d9257c04d80b03c9
Alan Griffiths <alan@octopull.co.uk>
2012-07-03 15:12:20 +0100

    Added ApplicationFilter

---
+++ src/input/application_filter.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alan Griffiths <alan@octopull.co.uk>
+ */
+
+#include "mir/input/application_filter.h"
+
+namespace mi = mir::input;
+
+mi::ApplicationFilter::ApplicationFilter(std::shared_ptr<Filter> const& next_link) :
+    ChainingFilter(next_link)
+{
+}
+
+void mi::ApplicationFilter::accept(Event* e) const
+{
+    ChainingFilter::accept(e);
+}
+
