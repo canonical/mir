67a6ce309f1146acf37ef10a0d64cb0a46b53fd4
Alan Griffiths <alan@octopull.co.uk>
2012-07-04 17:04:23 +0100

    compiz => mir

--- cmake/CompizCommon.cmake
+++ cmake/CompizCommon.cmake
@@ -1,13 +1,13 @@
 cmake_minimum_required (VERSION 2.6)
 # Create target to discover tests
-function (compiz_discover_tests EXECUTABLE)
+function (mir_discover_tests EXECUTABLE)
 
     add_dependencies (${EXECUTABLE}
-              compiz_discover_gtest_tests)
+              mir_discover_gtest_tests)
 
     add_custom_command (TARGET ${EXECUTABLE}
 			POST_BUILD
-			COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE} --gtest_list_tests | ${CMAKE_BINARY_DIR}/compiz_gtest/compiz_discover_gtest_tests ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE}
+			COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE} --gtest_list_tests | ${CMAKE_BINARY_DIR}/mir_gtest/mir_discover_gtest_tests ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE}
 			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 			COMMENT "Discovering Tests in ${EXECUTABLE}"
 			DEPENDS 
