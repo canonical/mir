e8bba8e016f910a925d620882717dcee05c3e289
Kevin DuBois <kevin.dubois@canonical.com>
2012-07-10 09:36:22 -0700

    refactor tests. too many expectations in one test. 2 tests contained 6 expectations, so now there are 6 tests

--- unit-tests/compositor/test_buffer_swapper.cpp
+++ unit-tests/compositor/test_buffer_swapper.cpp
@@ -27,79 +27,120 @@
 namespace mc = mir::compositor;
 namespace geom = mir::geometry;
 
-TEST(buffer_swapper_double, simple_swaps)
+geom::Width w{1024};
+geom::Height h{768};
+geom::Stride s{1024};
+mc::PixelFormat pf{mc::PixelFormat::rgba_8888};
+
+TEST(buffer_swapper_double, simple_swaps0)
 {
     using namespace testing;
-
-    geom::Width w{1024};
-    geom::Height h{768};
-    geom::Stride s{1024};
-    mc::PixelFormat pf{mc::PixelFormat::rgba_8888};
     
     std::shared_ptr<mc::Buffer> buf_a(new mc::MockBuffer(w, h, s, pf));
     std::shared_ptr<mc::Buffer> buf_b(new mc::MockBuffer(w, h, s, pf));
-    mc::Buffer* buf_tmp_a;
-    mc::Buffer* buf_tmp_b;
+    mc::Buffer* buf_tmp;
 
     /* BufferSwapperDouble implements the BufferSwapper interface */
     mc::BufferSwapperDouble swapper_double(buf_a, buf_b);
-
     mc::BufferSwapper * swapper = &swapper_double;
 
     /* normal, one in, one out, pattern */
     /* first buffer is requested and returned */
-    swapper->dequeue_free_buffer(buf_tmp_a);
-    EXPECT_TRUE((buf_tmp_a == buf_a.get()) || (buf_tmp_a == buf_b.get())); /* we should get valid buffer we supplied in constructor */
-    swapper->queue_finished_buffer(buf_tmp_a);
+    swapper->dequeue_free_buffer(buf_tmp);
+    EXPECT_TRUE((buf_tmp == buf_a.get()) || (buf_tmp == buf_b.get())); /* we should get valid buffer we supplied in constructor */
+    swapper->queue_finished_buffer(buf_tmp);
     
-    /* second buffer is requested and returned */
-    swapper->dequeue_free_buffer(buf_tmp_b);
-    EXPECT_TRUE((buf_tmp_a == buf_a.get()) || (buf_tmp_a == buf_b.get())); /* we should get valid buffer we supplied in constructor */
-    EXPECT_NE(buf_tmp_a, buf_tmp_b); /* we should have gotten a different buffer back this time */
-    swapper->queue_finished_buffer(buf_tmp_b);
+}
 
-    /* third buffer is requested and returned */
-    swapper->dequeue_free_buffer(buf_tmp_a);
-    EXPECT_TRUE((buf_tmp_a == buf_a.get()) || (buf_tmp_a == buf_b.get())); /* we should get valid buffer we supplied in constructor */
-    EXPECT_NE(buf_tmp_a, buf_tmp_b); /* we should have gotten a different buffer back this time */
-    swapper->queue_finished_buffer(buf_tmp_a);
-} 
-
-#ifdef MIR_TODO
-TEST(buffer_swapper_double, simple_grabs)
+ 
+TEST(buffer_swapper_double, simple_swaps1)
 {
-    geom::Width w{1024};
-    geom::Height h{768};
-    geom::Stride s{1024};
-    mc::PixelFormat pf{mc::PixelFormat::rgba_8888};
+    using namespace testing;
     
     std::shared_ptr<mc::Buffer> buf_a(new mc::MockBuffer(w, h, s, pf));
     std::shared_ptr<mc::Buffer> buf_b(new mc::MockBuffer(w, h, s, pf));
     mc::Buffer* buf_tmp_a;
     mc::Buffer* buf_tmp_b;
-    mc::Buffer* buf_tmp_c;
 
     /* BufferSwapperDouble implements the BufferSwapper interface */
     mc::BufferSwapperDouble swapper_double(buf_a, buf_b);
     mc::BufferSwapper * swapper = &swapper_double;
 
-    /* first buffer is requested and returned (this gives us a valid last posted buffer ) */
     swapper->dequeue_free_buffer(buf_tmp_a);
-    EXPECT_TRUE((buf_tmp_a == buf_a.get()) || (buf_tmp_a == buf_b.get())); /* we should get valid buffer we supplied in constructor */
+    swapper->queue_finished_buffer(buf_tmp_a);
+    swapper->dequeue_free_buffer(buf_tmp_b);
+    swapper->queue_finished_buffer(buf_tmp_b);
+
+    EXPECT_TRUE((buf_tmp_a == buf_a.get()) || (buf_tmp_a == buf_b.get()));
+    EXPECT_TRUE((buf_tmp_b == buf_a.get()) || (buf_tmp_b == buf_b.get()));
+    EXPECT_NE(buf_tmp_a, buf_tmp_b);
+}
+
+TEST(buffer_swapper_double, simple_grabs0)
+{
+    using namespace testing;
+
+    std::shared_ptr<mc::Buffer> buf_a(new mc::MockBuffer(w, h, s, pf));
+    std::shared_ptr<mc::Buffer> buf_b(new mc::MockBuffer(w, h, s, pf));
+    mc::Buffer* buf_tmp_a;
+    mc::Buffer* buf_tmp_b;
+
+    /* BufferSwapperDouble implements the BufferSwapper interface */
+    mc::BufferSwapperDouble swapper_double(buf_a, buf_b);
+    mc::BufferSwapper * swapper = &swapper_double;
+
+    swapper->dequeue_free_buffer(buf_tmp_a);
     swapper->queue_finished_buffer(buf_tmp_a);
 
     swapper->grab_last_posted(buf_tmp_b);
-    EXPECT_EQ(buf_tmp_a, buf_tmp_b); /* whatever buf_tmp_a was, this was the last posted buffer */
-    swapper->ungrab(buf_tmp_b);
-
-    swapper->dequeue_free_buffer(buf_tmp_c);
     EXPECT_TRUE((buf_tmp_a == buf_a.get()) || (buf_tmp_a == buf_b.get())); /* we should get valid buffer we supplied in constructor */
-    EXPECT_NE(buf_tmp_c, buf_tmp_a); /* we should have gotten a different buffer back this time */
+}
+
+TEST(buffer_swapper_double, simple_grabs1)
+{ 
+    using namespace testing;
+
+    std::shared_ptr<mc::Buffer> buf_a(new mc::MockBuffer(w, h, s, pf));
+    std::shared_ptr<mc::Buffer> buf_b(new mc::MockBuffer(w, h, s, pf));
+    mc::Buffer* buf_tmp_a;
+    mc::Buffer* buf_tmp_b;
+
+    /* BufferSwapperDouble implements the BufferSwapper interface */
+    mc::BufferSwapperDouble swapper_double(buf_a, buf_b);
+    mc::BufferSwapper * swapper = &swapper_double;
+
+    swapper->dequeue_free_buffer(buf_tmp_a);
     swapper->queue_finished_buffer(buf_tmp_a);
 
-    swapper->grab_last_posted(buf_tmp_a);
-    EXPECT_EQ(buf_tmp_a, buf_tmp_c); /* whatever buf_tmp_c was, this was the last posted buffer */
+    swapper->grab_last_posted(buf_tmp_b);
     swapper->ungrab(buf_tmp_b);
+
+    EXPECT_EQ(buf_tmp_a, buf_tmp_b); /* whatever buf_tmp_a was, this was the last posted buffer */
+
+
+}
+
+TEST(buffer_swapper_double, simple_grabs2)
+{ 
+    using namespace testing;
+
+    std::shared_ptr<mc::Buffer> buf_a(new mc::MockBuffer(w, h, s, pf));
+    std::shared_ptr<mc::Buffer> buf_b(new mc::MockBuffer(w, h, s, pf));
+    mc::Buffer* buf_tmp_a;
+    mc::Buffer* buf_tmp_b;
+
+    /* BufferSwapperDouble implements the BufferSwapper interface */
+    mc::BufferSwapperDouble swapper_double(buf_a, buf_b);
+    mc::BufferSwapper * swapper = &swapper_double;
+
+    swapper->dequeue_free_buffer(buf_tmp_a);
+    swapper->queue_finished_buffer(buf_tmp_a);
+
+    swapper->grab_last_posted(buf_tmp_b);
+    swapper->ungrab(buf_tmp_b);
+
+    swapper->grab_last_posted(buf_tmp_a);
+    EXPECT_EQ(buf_tmp_a, buf_tmp_b);
 
 }
 
@@ -123,8 +164,6 @@ TEST(buffer_swapper_double, init_test)
     mc::BufferSwapper * swapper = &swapper_double;
 
     swapper->grab_last_posted(buf_tmp);
-    /* note: kdub, the grab_last_posted has something of undefined behavior if nothing has ever been posted */
     EXPECT_EQ(buf_tmp, nullptr); /* no one has posted yet, so we should be returning nothing (or error) */
 
 }
-#endif
