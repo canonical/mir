53880a864055732ebff18960f7915eebfb589e5b
Alan Griffiths <alan@octopull.co.uk>
2012-07-12 16:05:07 +0100

    Restore test

--- unit-tests/surfaces/test_surface_stack.cpp
+++ unit-tests/surfaces/test_surface_stack.cpp
@@ -16,21 +16,22 @@
  * Authored by: Thomas Voss <thomas.voss@canonical.com>
  */
 
+#include "mir/surfaces/surface_stack.h"
 #include "mir/compositor/buffer_bundle.h"
 #include "mir/compositor/buffer_bundle_factory.h"
-#include "mir/surfaces/surface_stack.h"
+#include "mir/compositor/buffer_swapper.h"
 #include "mir/surfaces/surface.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include "mir_test/gmock_fixes.h"
 
 #include <memory>
 
 namespace mc = mir::compositor;
 namespace ms = mir::surfaces;
-namespace mg = mir::geometry;
+namespace geom = mir::geometry;
 
-#if 0
 namespace
 {
 struct MockBufferBundleFactory : public mc::BufferBundleFactory
@@ -38,8 +39,8 @@ struct MockBufferBundleFactory : public mc::BufferBundleFactory
     MOCK_METHOD3(
         create_buffer_bundle,
         std::shared_ptr<mc::BufferBundle>(
-            mg::Width width,
-            mg::Height height,
+            geom::Width width,
+            geom::Height height,
             mc::PixelFormat pf));
 
 };
@@ -55,18 +56,16 @@ TEST(
     std::unique_ptr<mc::BufferSwapper> swapper_handle(nullptr);
     mc::BufferBundle buffer_bundle(std::move(swapper_handle));
     MockBufferBundleFactory buffer_bundle_factory;
-/*
+
     EXPECT_CALL(
         buffer_bundle_factory,
         create_buffer_bundle(_, _, _))
             .Times(AtLeast(1))
             .WillRepeatedly(Return(std::shared_ptr<mc::BufferBundle>(new mc::BufferBundle(std::move(swapper_handle)))));
-*/
-     
+
     ms::SurfaceStack stack(&buffer_bundle_factory);
     std::weak_ptr<ms::Surface> surface = stack.create_surface(
-        ms::a_surface().of_size(mg::Width(1024), mg::Height(768)));
-    
+        ms::a_surface().of_size(geom::Width(1024), geom::Height(768)));
+
     stack.destroy_surface(surface);
 }
-#endif
