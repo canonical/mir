42a950485cce36b44b78178db407b2d0279c3078
Kevin DuBois <kevin.dubois@canonical.com>
2012-07-18 13:30:00 -0700

    remove some dead code

--- include/mir_test/multithread_harness.h
+++ include/mir_test/multithread_harness.h
@@ -26,12 +26,11 @@ namespace mir
 {
 namespace testing
 {
-
 template< typename S, typename T>
 class SynchronizedThread {
     public:
         SynchronizedThread (std::chrono::time_point<std::chrono::system_clock> timeout,
-                            std::function<void(SynchronizedThread*, std::shared_ptr<S>, T* )> function,
+                            std::function<void(SynchronizedThread<S,T>*, std::shared_ptr<S>, T* )> function,
                             std::shared_ptr<S> test_object,
                             T* data )
          : abs_timeout(timeout),
@@ -39,7 +38,7 @@ class SynchronizedThread {
            pause_request(false),
            kill(false)
         {
-            thread = new std::thread(function, this, test_object, data);
+            thread = new std::thread(function, this, test_object, data); 
         };
 
         ~SynchronizedThread ()
@@ -116,51 +115,6 @@ class SynchronizedThread {
         bool kill;
 };
 
-#if 0 
-template <typename S>
-void test_thread(std::function<void(std::shared_ptr<S>, Synchronizer*, int)> function,
-                       std::condition_variable *cv1, std::mutex* exec_lock, 
-                       std::shared_ptr<S> test_object, int tid ,
-                       Synchronizer* synchronizer)
-{
-    std::unique_lock<std::mutex> lk(*exec_lock);
-    /* once we acquire lock, we know that the parent thread's timer is running, so we can release it.
-        if its not released, then a deadlock situation would prevent the wait_until() from ever waking */
-    lk.unlock();
-
-    function(test_object, synchronizer, tid);
-
-    /* notify that we are done working */
-    cv1->notify_one();
-    return;
-}
-
-template <typename S>
-void manager_thread(std::function<void(std::shared_ptr<S>, Synchronizer*, int)> function,
-                   std::shared_ptr<S> test_object , int tid, Synchronizer* synchronizer,
-                   std::chrono::milliseconds timeout)
-{
-    std::condition_variable cv1;
-    std::mutex exec_lock;
-
-    std::unique_lock<std::mutex> lk(exec_lock);
-    std::thread(test_thread<S>, function, &cv1, &exec_lock, test_object, tid, synchronizer).detach();
-
-    /* set timeout as absolute time from this point */
-    auto timeout_time = std::chrono::system_clock::now() + timeout;
-
-    /* once the cv starts waiting, it releases the lock and triggers the timeout-protected 
-       thread that it can start the execution of the timeout function. this way the tiemout
-       thread cannot send a cv notify_one before we are actually waiting (as with a very short
-       target function */
-    if (std::cv_status::timeout == cv1.wait_until(lk, timeout_time ))
-    {
-        FAIL();
-    }
-
-    return;
-}
-#endif
 }
 }
 #endif /* MIR_TEST_MULTITHREAD_HARNESS_ */
