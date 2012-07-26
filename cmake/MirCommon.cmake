809ecff1ef54e4a0dbace35ef41f896c58fe9e99
Thomas Voß <thomas.voss@rub.de>
2012-07-26 09:03:35 +0200

    * Enabled coverage reporting.  * Added optional creation of memcheck targets (OPTION_ENABLE_MEMCHECK)  * Added flag --gtest_death_test_use_fork to prevent death tests from failing in valgrind (see https://groups.google.com/forum/?fromgroups#!topic/googletestframework/qS4aySHaM5o)

--- cmake/MirCommon.cmake
+++ cmake/MirCommon.cmake
@@ -1,16 +1,36 @@
 cmake_minimum_required (VERSION 2.6)
 # Create target to discover tests
+
+option(
+  ENABLE_MEMCHECK_OPTION
+  "If set to ON, enables automatic creation of memcheck targets"
+  OFF
+)
+
+
 function (mir_discover_tests EXECUTABLE)
 
-    add_dependencies (${EXECUTABLE}
-              mir_discover_gtest_tests)
+  if(ENABLE_MEMCHECK_OPTION)
+    find_program(
+      VALGRIND_EXECUTABLE
+      valgrind)
 
-    add_custom_command (TARGET ${EXECUTABLE}
-			POST_BUILD
-			COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE} --gtest_list_tests | ${CMAKE_BINARY_DIR}/mir_gtest/mir_discover_gtest_tests ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE}
-			WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-			COMMENT "Discovering Tests in ${EXECUTABLE}"
-			DEPENDS 
-			VERBATIM)
+    if(VALGRIND_EXECUTABLE)
+      set(ENABLE_MEMCHECK_FLAG "--enable-memcheck")
+    else(VALGRIND_EXECUTABLE)
+      message("Not enabling memcheck as valgrind is missing on your system")
+    endif(VALGRIND_EXECUTABLE)
+  endif(ENABLE_MEMCHECK_OPTION)
+
+  add_dependencies (${EXECUTABLE}
+    mir_discover_gtest_tests)
+  
+  add_custom_command (TARGET ${EXECUTABLE}
+    POST_BUILD
+    COMMAND ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE} --gtest_list_tests | ${CMAKE_BINARY_DIR}/mir_gtest/mir_discover_gtest_tests ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE} ${ENABLE_MEMCHECK_FLAG}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMENT "Discovering Tests in ${EXECUTABLE}"
+    DEPENDS 
+    VERBATIM)
 endfunction ()
 
