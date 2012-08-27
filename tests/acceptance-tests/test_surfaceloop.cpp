0b0070e4490fe6904da2a8f20573e0b06ab2b891
Alan Griffiths <alan@octopull.co.uk>
2012-08-27 16:09:20 +0100

    Preserve size info in Surface

--- tests/acceptance-tests/test_surfaceloop.cpp
+++ tests/acceptance-tests/test_surfaceloop.cpp
@@ -143,12 +143,18 @@ TEST_F(DefaultDisplayServerTestFixture, creates_surface_of_correct_size)
 
             Surface mysurface;
 
-            // connect surface
-            EXPECT_NO_THROW(mysurface =
-                Surface(mir::test_socket_file(), 640, 480, 0, std::make_shared<ConsoleLogger>()));
+            mysurface =
+                Surface(mir::test_socket_file(), 640, 480, 0, std::make_shared<ConsoleLogger>());
 
             EXPECT_EQ(640, mysurface.width());
-        }
+            EXPECT_EQ(480, mysurface.height());
+
+            mysurface =
+                Surface(mir::test_socket_file(), 1600, 1200, 0, std::make_shared<ConsoleLogger>());
+
+            EXPECT_EQ(1600, mysurface.width());
+            EXPECT_EQ(1200, mysurface.height());
+}
     } client_connects_and_disconnects;
 
     launch_client_process(client_connects_and_disconnects);
