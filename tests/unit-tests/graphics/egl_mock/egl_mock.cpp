6dd7e41a7572392500783d41584efc93c96bc38c
Kevin DuBois <kevin.dubois@canonical.com>
2012-08-28 11:51:38 -0700

    get egl mock and egl mock test to compile

--- tests/unit-tests/graphics/egl_mock/egl_mock.cpp
+++ tests/unit-tests/graphics/egl_mock/egl_mock.cpp
@@ -1,5 +1,25 @@
-#include "egl_mock.h"
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by:
+ * Thomas Voss <thomas.voss@canonical.com>
+ * Kevin DuBois <kevin.dubois@canonical.com>
+ */
 
+#include "mir_test/egl_mock.h"
+#include <gtest/gtest.h>
 namespace
 {
 mir::EglMock* global_mock = NULL;
@@ -17,26 +37,30 @@ mir::EglMock::~EglMock()
     global_mock = NULL;
 }
 
+#define CHECK_GLOBAL_MOCK(rettype)  \
+    if (!global_mock)               \
+    {                               \
+        using namespace ::testing;  \
+        ADD_FAILURE_AT("__FILE__",__LINE__); \
+        rettype type = (rettype) 0;       \
+        return type;        \
+    }
 
 EGLint eglGetError (void)
 {
-    if (global_mock)
-    {
-        return global_mock->eglGetError();
-    }
+    CHECK_GLOBAL_MOCK(EGLint)
+    return global_mock->eglGetError();
 }
 
 EGLDisplay eglGetDisplay (NativeDisplayType display)
 {
-    if (global_mock)
-    {
-        return global_mock->eglGetDisplay(display);
-    }
+    CHECK_GLOBAL_MOCK(EGLDisplay);
+    return global_mock->eglGetDisplay(display);
 }
 
 EGLBoolean eglInitialize (EGLDisplay dpy, EGLint *major, EGLint *minor)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
     {
         return global_mock->eglInitialize(dpy, major, minor);
     }
@@ -44,7 +68,7 @@ EGLBoolean eglInitialize (EGLDisplay dpy, EGLint *major, EGLint *minor)
 
 EGLBoolean eglTerminate (EGLDisplay dpy)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
     {
         return global_mock->eglTerminate(dpy);
     }
@@ -52,7 +76,7 @@ EGLBoolean eglTerminate (EGLDisplay dpy)
 
 const char * eglQueryString (EGLDisplay dpy, EGLint name)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(const char *)
     {
         return global_mock->eglQueryString(dpy, name);
     }
@@ -60,7 +84,7 @@ const char * eglQueryString (EGLDisplay dpy, EGLint name)
 
 EGLBoolean eglGetConfigs (EGLDisplay dpy, EGLConfig *configs, EGLint config_size, EGLint *num_config)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
     {
         return global_mock->eglGetConfigs(dpy, configs, config_size, num_config);
     }
@@ -68,7 +92,7 @@ EGLBoolean eglGetConfigs (EGLDisplay dpy, EGLConfig *configs, EGLint config_size
 
 EGLBoolean eglChooseConfig (EGLDisplay dpy, const EGLint *attrib_list, EGLConfig *configs, EGLint config_size, EGLint *num_config)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
     {
         return global_mock->eglChooseConfig(dpy, attrib_list, configs, config_size, num_config);
     }
@@ -76,129 +100,129 @@ EGLBoolean eglChooseConfig (EGLDisplay dpy, const EGLint *attrib_list, EGLConfig
 
 EGLBoolean eglGetConfigAttrib (EGLDisplay dpy, EGLConfig config, EGLint attribute, EGLint *value)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglGetConfigAttrib(dpy, config, attribute, value);
 }
 
 EGLSurface eglCreateWindowSurface (EGLDisplay dpy, EGLConfig config, NativeWindowType window, const EGLint *attrib_list)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLSurface)
         return global_mock->eglCreateWindowSurface(dpy, config, window, attrib_list);
 }
 
 EGLSurface eglCreatePixmapSurface (EGLDisplay dpy, EGLConfig config, NativePixmapType pixmap, const EGLint *attrib_list)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLSurface)
         return global_mock->eglCreatePixmapSurface(dpy, config, pixmap, attrib_list);
 }
 
 EGLSurface eglCreatePbufferSurface (EGLDisplay dpy, EGLConfig config, const EGLint *attrib_list)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLSurface)
         return global_mock->eglCreatePbufferSurface(dpy, config, attrib_list);
 }
 
 EGLBoolean eglDestroySurface (EGLDisplay dpy, EGLSurface surface)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglDestroySurface(dpy, surface);
 }
 
 EGLBoolean eglQuerySurface (EGLDisplay dpy, EGLSurface surface, EGLint attribute, EGLint *value)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglQuerySurface(dpy, surface, attribute, value);
 }
 
 /* EGL 1.1 render-to-texture APIs */
 EGLBoolean eglSurfaceAttrib (EGLDisplay dpy, EGLSurface surface, EGLint attribute, EGLint value)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglSurfaceAttrib(dpy, surface, attribute, value);
 }
 
 EGLBoolean eglBindTexImage(EGLDisplay dpy, EGLSurface surface, EGLint buffer)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglBindTexImage(dpy, surface, buffer);
 }
 
 EGLBoolean eglReleaseTexImage(EGLDisplay dpy, EGLSurface surface, EGLint buffer)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglReleaseTexImage(dpy, surface, buffer);
 }
 
 /* EGL 1.1 swap control API */
 EGLBoolean eglSwapInterval(EGLDisplay dpy, EGLint interval)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglSwapInterval(dpy, interval);
 }
 
 EGLContext eglCreateContext (EGLDisplay dpy, EGLConfig config, EGLContext share_list, const EGLint *attrib_list)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLContext)
         return global_mock->eglCreateContext(dpy, config, share_list, attrib_list);
 }
 
 EGLBoolean eglDestroyContext (EGLDisplay dpy, EGLContext ctx)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglDestroyContext(dpy, ctx);
 }
 
 EGLBoolean eglMakeCurrent (EGLDisplay dpy, EGLSurface draw, EGLSurface read, EGLContext ctx)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglMakeCurrent(dpy, draw, read, ctx);
 }
 
 EGLContext eglGetCurrentContext (void)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLContext)
         return global_mock->eglGetCurrentContext();
 }
 
 EGLSurface eglGetCurrentSurface (EGLint readdraw)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLSurface)
         return global_mock->eglGetCurrentSurface(readdraw);
 }
 
 EGLDisplay eglGetCurrentDisplay (void)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLDisplay)
         return global_mock->eglGetCurrentDisplay();
 }
 
 EGLBoolean eglQueryContext (EGLDisplay dpy, EGLContext ctx, EGLint attribute, EGLint *value)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglQueryContext(dpy, ctx, attribute, value);
 }
 
 EGLBoolean eglWaitGL (void)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglWaitGL();
 }
 
 EGLBoolean eglWaitNative (EGLint engine)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglWaitNative(engine);
 }
 
 EGLBoolean eglSwapBuffers (EGLDisplay dpy, EGLSurface draw)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglSwapBuffers(dpy, draw);
 }
 
 EGLBoolean eglCopyBuffers (EGLDisplay dpy, EGLSurface surface, NativePixmapType target)
 {
-    if (global_mock)
+    CHECK_GLOBAL_MOCK(EGLBoolean)
         return global_mock->eglCopyBuffers(dpy, surface, target);
 }
 
--- tests/unit-tests/graphics/egl_mock/egl_mock_test.cpp
+++ tests/unit-tests/graphics/egl_mock/egl_mock_test.cpp
@@ -1,4 +1,24 @@
-#include <egl_mock.h>
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by:
+ * Thomas Voss <thomas.voss@canonical.com>
+ */
+
+#include "mir_test/egl_mock.h"
+#include "gtest/gtest.h"
 
 TEST(EglMock, demo)
 {
@@ -19,5 +39,5 @@ TEST(EglMock, demo)
     egl_call;                                                          \
     }                                                                  \
 
-EGL_MOCK_TEST(eglGetError, eglGetError(), eglGetError());
-EGL_MOCK_TEST(eglGetDisplay, eglGetDisplay(NULL), eglGetDisplay(NULL));
+EGL_MOCK_TEST(eglGetError, eglGetError(), eglGetError())
+EGL_MOCK_TEST(eglGetDisplay, eglGetDisplay(NULL), eglGetDisplay(NULL))
