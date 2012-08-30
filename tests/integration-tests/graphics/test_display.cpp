0d16fd7c31c9cd87a3923153faec4ea948f7ef14
Kevin DuBois <kevin.dubois@canonical.com>
2012-08-30 08:34:10 -0700

    add infrastructure for display integration test

---
+++ tests/integration-tests/graphics/test_display.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#include <gtest/gtest.h>
+
+class AndroidFramebufferIntegration : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        using namespace testing;
+    }
+
+};
+
+
+TEST_F(AndroidFramebufferIntegration, init_does_not_throw)
+{
+    using namespace testing;
+
+}
