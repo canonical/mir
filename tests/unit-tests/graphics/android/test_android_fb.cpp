28dd53a7da8688aa82a60b3abbf95a765cf1e761
Kevin DuBois <kevin.dubois@canonical.com>
2012-08-30 11:40:35 -0700

    add a test for notify_update

--- tests/unit-tests/graphics/android/test_android_fb.cpp
+++ tests/unit-tests/graphics/android/test_android_fb.cpp
@@ -402,3 +402,11 @@ TEST_F(AndroidTestFramebufferInit, eglDisplay_is_terminated)
     });
     
 }
+
+TEST_F(AndroidTestFramebufferInit, eglDisplay_is_terminated)
+{
+    using namespace testing;
+    std::shared_ptr<mg::Display> display(new mga::AndroidDisplay(native_win));
+    bool success = display->notify_update();
+    EXPECT_TRUE(success); 
+}
