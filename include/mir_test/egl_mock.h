1e59e9f529efb2a4e0ae47d5e7ca8fd1d3ce5b9f
Kevin DuBois <kevin.dubois@canonical.com>
2012-08-31 10:36:27 -0700

    add missing copyright notice to egl_mock

--- include/mir_test/egl_mock.h
+++ include/mir_test/egl_mock.h
@@ -1,5 +1,23 @@
-#ifndef EGL_MOCK_H_
-#define EGL_MOCK_H_
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Thomas Voss <thomas.voss@canonical.com>
+ */
+
+#ifndef MIR_MIR_TEST_EGL_MOCK_H_
+#define MIR_MIR_TEST_EGL_MOCK_H_
 
 #include <gmock/gmock.h>
 
@@ -63,4 +81,4 @@ public:
 };
 }
 
-#endif // EGL_MOCK_H_
+#endif /* MIR_MIR_TEST_EGL_MOCK_H_ */
