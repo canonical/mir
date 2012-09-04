15471bd7923bb6cb73e6b4de3f79c1731344f9a7
Kevin DuBois <kevin.dubois@canonical.com>
2012-09-04 15:33:43 -0700

    add two forgotten files

---
+++ include/mir/graphics/android/android_buffer_handle.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by:
+ *   Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#ifndef MIR_GRAPHICS_ANDROID_ANDROID_BUFFER_HANDLE_H_
+#define MIR_GRAPHICS_ANDROID_ANDROID_BUFFER_HANDLE_H_
+
+#define GL_GLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+#include <system/window.h>
+
+namespace mir
+{
+namespace graphics
+{
+namespace android
+{
+
+class BufferHandle 
+{
+public:
+    virtual ~BufferHandle() {}
+
+protected:
+    BufferHandle() = default;
+};
+
+
+class AndroidBufferHandle: public BufferHandle
+{
+public:
+    explicit AndroidBufferHandle(buffer_handle_t han);
+    EGLClientBuffer get_egl_client_buffer();
+
+    buffer_handle_t handle;
+};
+
+}
+}
+}
+
+#endif /*MIR_GRAPHICS_ANDROID_ANDROID_BUFFER_HANDLE_H_ */
