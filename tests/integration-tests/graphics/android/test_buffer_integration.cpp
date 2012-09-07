1cf04964eeb816c16d465422e7f7125473dc464e
Kevin DuBois <kevin.dubois@canonical.com>
2012-09-07 14:20:51 -0700

    add new files

---
+++ tests/integration-tests/graphics/android/test_buffer_integration.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#include "mir/graphics/android/android_buffer.h"
+#include "mir/graphics/android/android_alloc_adaptor.h"
+
+#include "mir_test/mock_android_alloc_adaptor.h"
+
+#include <gtest/gtest.h>
+
+
