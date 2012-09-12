029b48445be3fbcd778b47ef02b1fe451a4b0058
Kevin DuBois <kevin.dubois@canonical.com>
2012-09-12 10:14:13 -0700

    add initial test for buffer ipc package

---
+++ tests/unit-tests/compositor/test_buffer_ipc.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#include "mir/compositor/buffer_ipc_package.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+namespace mc=mir::compositor;
+
+class BufferIPCPackageTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        std::vector<int> data = {443, 421, 493};
+    }
+
+public:
+    std::vector<int> data;
+};
+
+TEST_F(BufferIPCPackageTest, test_int_acquisiton_length)
+{
+    using namespace testing;
+
+    mc::BufferIPCPackage package(data);
+    auto test_vector = package.get_ipc_data();
+   
+    EXPECT_EQ(data.length(), test_vector.length()); 
+}
+
