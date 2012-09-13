2742edfc8d9a8b9bcc91d5d9011567e017775d4e
Kevin DuBois <kevin.dubois@canonical.com>
2012-09-13 14:35:06 -0700

    modify test to render from texture (displays black, correct color, but want to find way to fill color into buffer)

--- src/graphics/android/android_buffer.cpp
+++ src/graphics/android/android_buffer.cpp
@@ -86,11 +86,9 @@ void mga::AndroidBuffer::unlock()
 #include <system/window.h>
 static void inc(struct android_native_base_t*)
 {
-    printf("INCREF\n");
 }
 static void dec(struct android_native_base_t*)
 {
-    printf("DECREF\n");
 }
 
 void mga::AndroidBuffer::bind_to_texture()
@@ -110,22 +108,9 @@ void mga::AndroidBuffer::bind_to_texture()
     it = egl_image_map.find(disp);
     if (it == egl_image_map.end())
     {
-printf("throw BIND new im\n");
         ANativeWindowBuffer *buf = (ANativeWindowBuffer*) native_window_buffer_handle->get_egl_client_buffer();
-        printf("width is %i\n", buf->width);
-        printf("incref is %i\n", (int) buf->common.incRef);
         buf->common.incRef = &inc;
         buf->common.decRef = &dec;
-        printf("handle is %X\n", (int) buf->handle);
-        printf("handle data is %X\n", (int) buf->handle->numFds);
-        printf("handle data is %X\n", (int) buf->handle->numInts);
-        printf("handle data is %X\n", (int) buf->handle->data);
-    for(int i=0; i< buf->handle->numFds + buf->handle->numInts; i++)
-    {
-        printf("[%i]: %i\n",i, (int) buf->handle->data[i]);
-    }
-    
-printf("submitting...\n");
         image = eglCreateImageKHR(disp, EGL_NO_CONTEXT, EGL_NATIVE_BUFFER_ANDROID,
                                   buf, image_attrs);
 
--- tests/integration-tests/graphics/android/test_buffer_integration.cpp
+++ tests/integration-tests/graphics/android/test_buffer_integration.cpp
@@ -22,6 +22,8 @@
 #include "mir/compositor/double_buffer_allocation_strategy.h"
 #include "mir/compositor/buffer_swapper.h"
 
+#include "../tools/mir_image.h"
+#include <GLES2/gl2.h>
 #include <stdexcept>
 #include <gtest/gtest.h>
 
@@ -103,6 +105,103 @@ TEST_F(AndroidBufferIntegration, buffer_throws_with_no_egl_context)
 
 }
 
+namespace mg=mir::graphics;
+
+static const GLchar *vtex_shader_src =
+{
+    "attribute vec4 vPosition;\n"
+    "attribute vec4 uvCoord;\n"
+    "varying vec2 texcoord;\n"
+    "uniform float slide;\n"
+    "void main() {\n"
+    "   gl_Position = vPosition;\n"
+    "   texcoord = uvCoord.xy + vec2(slide);\n"
+    "}\n"
+};
+
+static const GLchar *frag_shader_src =
+{
+    "precision mediump float;\n"
+    "uniform sampler2D tex;\n"
+    "varying vec2 texcoord;\n"
+    "void main() {\n"
+    "   gl_FragColor = texture2D(tex, texcoord);\n"
+    "}\n"
+};
+
+const GLint num_vertex = 4;
+GLfloat vertex_data[] =
+{
+    -1.0f, -1.0f, 0.0f, 1.0f,
+    -1.0f,  1.0f, 0.0f, 1.0f,
+    1.0f, -1.0f, 0.0f, 1.0f,
+    1.0f,  1.0f, 0.0f, 1.0f,
+};
+
+GLfloat uv_data[] =
+{
+    1.0f, 1.0f, 0.0f, 0.0f,
+    1.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f
+
+};
+
+void setup_gl(GLuint& program, GLuint& vPositionAttr, GLuint& uvCoord, GLuint& slideUniform)
+{
+    glClearColor(0.0, 1.0, 0.0, 1.0);
+
+    GLuint vtex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vtex_shader, 1, &vtex_shader_src, 0);
+    glCompileShader(vtex_shader);
+
+    GLuint frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag_shader, 1, &frag_shader_src, 0);
+    glCompileShader(frag_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vtex_shader);
+    glAttachShader(program, frag_shader);
+    glLinkProgram(program);
+
+    vPositionAttr = glGetAttribLocation(program, "vPosition");
+    glVertexAttribPointer(vPositionAttr, num_vertex, GL_FLOAT, GL_FALSE, 0, vertex_data);
+    uvCoord = glGetAttribLocation(program, "uvCoord");
+    glVertexAttribPointer(uvCoord, num_vertex, GL_FLOAT, GL_FALSE, 0, uv_data);
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(
+        GL_TEXTURE_2D, 0, GL_RGBA,
+        mir_image.width, mir_image.height, 0,
+        GL_RGBA, GL_UNSIGNED_BYTE,
+        mir_image.pixel_data);
+    slideUniform = glGetUniformLocation(program, "slide");
+}
+
+void gl_render(GLuint program, GLuint vPosition, GLuint uvCoord,
+               GLuint slideUniform, float slide)
+{
+    glUseProgram(program);
+
+    glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
+
+    glUniform1fv(slideUniform, 1, &slide);
+
+    glActiveTexture(GL_TEXTURE0);
+    glEnableVertexAttribArray(vPosition);
+    glEnableVertexAttribArray(uvCoord);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, num_vertex);
+    glDisableVertexAttribArray(uvCoord);
+    glDisableVertexAttribArray(vPosition);
+}
+
 TEST_F(AndroidBufferIntegration, buffer_ok_with_egl_context)
 {
     using namespace testing;
@@ -119,17 +218,24 @@ TEST_F(AndroidBufferIntegration, buffer_ok_with_egl_context)
     std::shared_ptr<mg::Display> display;
     display = mg::create_display();
 
-    printf("HERE\n"); 
+    GLuint program, vPositionAttr, uvCoord, slideUniform;
+    setup_gl(program, vPositionAttr, uvCoord, slideUniform);
     /* add to swapper to surface */
     /* add to surface to surface stack */
 
     /* add swapper to ipc mechanism thing */
 
+    float slide =1.0;
     auto buffer = swapper->compositor_acquire();
     EXPECT_NO_THROW({
-    buffer->bind_to_texture();
+        buffer->bind_to_texture();
     });
-    display->post_update();
+
+    for(;;)
+    {
+        gl_render(program, vPositionAttr, uvCoord, slideUniform, slide);
+        display->post_update();
+    }
 }
 
 
