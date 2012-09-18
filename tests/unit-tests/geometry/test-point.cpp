06986a9624f99856463dbf9565397f6209ee4e88
Alexandros Frantzis <alexandros.frantzis@canonical.com>
2012-09-18 13:35:52 +0300

    geometry: Implement Point type

---
+++ tests/unit-tests/geometry/test-point.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alexandros Frantzis <alexandros.frantzis@canonical.com>
+ */
+
+#include "mir/geometry/point.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace geom = mir::geometry;
+
+TEST(geometry, point)
+{
+    using namespace geom;
+    Point const pointx2y4{X(2), Y(4)};
+
+    EXPECT_EQ(X(2), pointx2y4.x);
+    EXPECT_EQ(Y(4), pointx2y4.y);
+
+    Point const copy = pointx2y4;
+    EXPECT_EQ(X(2), copy.x);
+    EXPECT_EQ(Y(4), copy.y);
+    EXPECT_EQ(pointx2y4, copy);
+
+    Point const defaultValue;
+    EXPECT_EQ(X(0), defaultValue.x);
+    EXPECT_EQ(Y(0), defaultValue.y);
+    EXPECT_NE(pointx2y4, defaultValue);
+}
