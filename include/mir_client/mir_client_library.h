e108f07098cd5e1834ce6443bb941e283a31fade
Alan Griffiths <alan@octopull.co.uk>
2012-09-26 14:24:39 +0100

    Formatting changes

--- include/mir_client/mir_client_library.h
+++ include/mir_client/mir_client_library.h
@@ -33,25 +33,10 @@ extern "C" {
 /* Display server connection API */
 typedef struct MirConnection MirConnection;
 typedef struct MirWaitHandle MirWaitHandle;
+typedef struct MirSurface MirSurface;
 
-typedef void (* mir_connected_callback)(MirConnection * connection, void * client_context);
-typedef void (* mir_disconnected_callback)(void * client_context);
-
-/* Request a connection to the MIR server.
-   The supplied callback is called when the connection is established, or fails.
-*/
-MirWaitHandle* mir_connect(char const* socket_file, char const* app_name, mir_connected_callback callback, void * client_context);
-
-/* Return a non-zero value if the supplied connection is valid, 0 otherwise. */
-int mir_connection_is_valid(MirConnection * connection);
-
-/* Returns a text description of any error resulting in an invalid connection,
-   or the empty string "" if the connection is valid. */
-char const * mir_connection_get_error_message(MirConnection * connection);
-
-/* Release a connection to the MIR server.
- */
-void mir_connection_release(MirConnection * connection);
+typedef void (*mir_connected_callback)(MirConnection *connection, void *client_context);
+typedef void (*mir_surface_lifecycle_callback)(MirSurface *surface, void *client_context);
 
 /* Surface API */
 typedef enum MirPixelFormat
@@ -61,7 +46,7 @@ typedef enum MirPixelFormat
 
 typedef struct MirSurfaceParameters
 {
-    char const* name;
+    char const *name;
     int width;
     int height;
     MirPixelFormat pixel_format;
@@ -76,40 +61,65 @@ typedef struct MirGraphicsRegion
 
 } MirGraphicsRegion;
 
-typedef struct MirSurface MirSurface;
-
-typedef void (*mir_surface_lifecycle_callback)(MirSurface *surface, void *client_context);
-
-/* Request a new MIR surface on the supplied connection with the supplied parameters. */
-MirWaitHandle* mir_surface_create(MirConnection * connection,
-                        MirSurfaceParameters const * surface_parameters,
-                        mir_surface_lifecycle_callback callback,
-                        void * client_context);
+/* Request a connection to the MIR server.
+   The supplied callback is called when the connection is established, or fails.
+*/
+MirWaitHandle* mir_connect(
+    char const *socket_file,
+    char const *app_name,
+    mir_connected_callback callback,
+    void *client_context);
 
 /* Return a non-zero value if the supplied connection is valid, 0 otherwise. */
-int mir_surface_is_valid(MirSurface * surface);
+int mir_connection_is_valid(MirConnection *connection);
+
+/* Returns a text description of any error resulting in an invalid connection,
+   or the empty string "" if the connection is valid. */
+char const *mir_connection_get_error_message(MirConnection *connection);
+
+/* Release a connection to the MIR server.
+ */
+void mir_connection_release(MirConnection *connection);
+
+/* Request a new MIR surface on the supplied connection with the supplied parameters. */
+MirWaitHandle* mir_surface_create(
+    MirConnection *connection,
+    MirSurfaceParameters const *surface_parameters,
+    mir_surface_lifecycle_callback callback,
+    void *client_context);
+
+/* Return a non-zero value if the supplied connection is valid, 0 otherwise. */
+int mir_surface_is_valid(MirSurface *surface);
 
 /* Returns a text description of any error resulting in an invalid surface,
    or the empty string "" if the surface is valid. */
-char const * mir_surface_get_error_message(MirSurface * surface);
+char const *mir_surface_get_error_message(MirSurface *surface);
 
 /* Get a valid surface's parameters. */
-void mir_surface_get_parameters(MirSurface * surface, MirSurfaceParameters *parameters);
+void mir_surface_get_parameters(MirSurface *surface, MirSurfaceParameters *parameters);
 
 /* Get a surface's buffer. */
-void mir_surface_get_current_buffer(MirSurface *surface, MirGraphicsRegion *buffer_package);
+void mir_surface_get_current_buffer(
+    MirSurface *surface,
+    MirGraphicsRegion *buffer_package);
 
 /* Advance a surface's buffer. */
-MirWaitHandle* mir_surface_next_buffer(MirSurface *surface, mir_surface_lifecycle_callback callback, void * context);
+MirWaitHandle* mir_surface_next_buffer(
+    MirSurface *surface,
+    mir_surface_lifecycle_callback callback,
+    void *context);
 
 /* Release the supplied surface and any associated buffer. */
-MirWaitHandle* mir_surface_release(MirSurface * surface, mir_surface_lifecycle_callback callback, void * context);
+MirWaitHandle* mir_surface_release(
+    MirSurface *surface,
+    mir_surface_lifecycle_callback callback,
+    void *context);
 
 void mir_wait_for(MirWaitHandle* wait_handle);
 
 
 /* Return the id of the surface. (Only useful for debug output.) */
-int mir_debug_surface_id(MirSurface * surface);
+int mir_debug_surface_id(MirSurface *surface);
 
 
 #ifdef __cplusplus
