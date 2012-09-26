715a5765014a04549f6f8b160c573d6ac5b7d1e4
Alexandros Frantzis <alexandros.frantzis@canonical.com>
2012-09-26 10:23:29 +0300

    graphics: Add GL renderer

---
+++ src/graphics/gl_renderer.cpp
@@ -0,0 +1,243 @@
+#include "mir/graphics/gl_renderer.h"
+#include "mir/graphics/renderable.h"
+#include "mir_test/mir_image.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <stdexcept>
+
+namespace mg=mir::graphics;
+namespace geom=mir::geometry;
+
+namespace
+{
+
+const GLchar* vertex_shader_src =
+{
+    "attribute vec3 position;\n"
+    "attribute vec2 texcoord;\n"
+    "uniform mat4 screen_to_gl_coords;\n"
+    "uniform mat4 transform;\n"
+    "varying vec2 v_texcoord;\n"
+    "void main() {\n"
+    "   gl_Position = screen_to_gl_coords * transform * vec4(position, 1.0);\n"
+    "   v_texcoord = texcoord;\n"
+    "}\n"
+};
+
+const GLchar* fragment_shader_src =
+{
+    "precision mediump float;\n"
+    "uniform sampler2D tex;\n"
+    "uniform float alpha;\n"
+    "varying vec2 v_texcoord;\n"
+    "void main() {\n"
+    "   vec4 frag = texture2D(tex, v_texcoord);\n"
+    "   gl_FragColor = vec4(frag.xyz, frag.a * alpha);\n"
+    "}\n"
+};
+
+struct VertexAttributes
+{
+    glm::vec3 position;
+    glm::vec2 texcoord;
+};
+
+VertexAttributes vertex_attribs[4] =
+{
+    {
+        glm::vec3{-0.5f, -0.5f, 0.0f},
+        glm::vec2{0.0f, 1.0f}
+    },
+    {
+        glm::vec3{-0.5f, 0.5f, 0.0f},
+        glm::vec2{0.0f, 0.0f},
+    },
+    {
+        glm::vec3{0.5f, -0.5f, 0.0f},
+        glm::vec2{1.0f, 1.0f},
+    },
+    {
+        glm::vec3{0.5f, 0.5f, 0.0f},
+        glm::vec2{1.0f, 0.0f}
+    }
+};
+
+}
+
+mg::GLRenderer::Resources::~Resources()
+{
+    if (vertex_shader)
+        glDeleteShader(vertex_shader);
+    if (fragment_shader)
+        glDeleteShader(fragment_shader);
+    if (program)
+        glDeleteProgram(program);
+    if (vertex_attribs_vbo)
+        glDeleteBuffers(1, &vertex_attribs_vbo);
+    if (texture)
+        glDeleteTextures(1, &texture);
+}
+
+void mg::GLRenderer::Resources::setup(const geometry::Size& display_size)
+{
+    GLint param = 0;
+
+    /* Create shaders and program */
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_src, 0);
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &param);
+    if (param == GL_FALSE)
+    {
+        glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &param);
+        auto info_log = std::unique_ptr<GLchar>(new GLchar[param + 1]);
+        glGetShaderInfoLog(vertex_shader, param + 1, NULL, info_log.get());
+        std::string info_str{"Failed to compile vertex shader:"};
+        info_str += info_log.get();
+        throw new std::runtime_error(info_str);
+    }
+
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_src, 0);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &param);
+    if (param == GL_FALSE)
+    {
+        glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &param);
+        auto info_log = std::unique_ptr<GLchar>(new GLchar[param + 1]);
+        glGetShaderInfoLog(vertex_shader, param + 1, NULL, info_log.get());
+        std::string info_str{"Failed to compile fragment shader:"};
+        info_str += info_log.get();
+        throw new std::runtime_error(info_str);
+    }
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &param);
+    if (param == GL_FALSE)
+    {
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &param);
+        auto info_log = std::unique_ptr<GLchar>(new GLchar[param + 1]);
+        glGetProgramInfoLog(program, param + 1, NULL, info_log.get());
+        std::string info_str{"Failed to compile fragment shader:"};
+        info_str += info_log.get();
+        throw new std::runtime_error(info_str);
+    }
+
+    glUseProgram(program);
+
+    /* Set up program variables */
+    GLint mat_loc = glGetUniformLocation(program, "screen_to_gl_coords");
+    GLint tex_loc = glGetUniformLocation(program, "tex");
+    transform_uniform_loc = glGetUniformLocation(program, "transform");
+    alpha_uniform_loc = glGetUniformLocation(program, "alpha");
+    position_attr_loc = glGetAttribLocation(program, "position");
+    texcoord_attr_loc = glGetAttribLocation(program, "texcoord");
+
+    /* 
+     * Create and set screen_to_gl_coords transformation matrix.
+     * The screen_to_gl_coords matrix transforms from the screen coordinate system
+     * (top-left is (0,0), bottom-right is (W,H)) to the normalized GL coordinate system
+     * (top-left is (-1,1), bottom-right is (1,-1))
+     */
+    glm::mat4 screen_to_gl_coords = glm::translate(glm::mat4{1.0f}, glm::vec3{-1.0f, 1.0f, 0.0f});
+    screen_to_gl_coords = glm::scale(screen_to_gl_coords,
+            glm::vec3{2.0f / display_size.width.as_uint32_t(),
+            -2.0f / display_size.height.as_uint32_t(),
+            1.0f});
+    glUniformMatrix4fv(mat_loc, 1, GL_FALSE, glm::value_ptr(screen_to_gl_coords));
+
+    /* Create the texture (temporary workaround until we can use the Renderable's texture) */
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
+                 mir_image.width, mir_image.height, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE,
+                 mir_image.pixel_data);
+
+    glUniform1i(tex_loc, 0);
+
+    /* Create VBO */
+    glGenBuffers(1, &vertex_attribs_vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_attribs_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_attribs),
+            glm::value_ptr(vertex_attribs[0].position), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glUseProgram(0);
+}
+
+mg::GLRenderer::GLRenderer(const geom::Size& display_size)
+{
+    resources.setup(display_size);
+}
+
+void mg::GLRenderer::render(Renderable& renderable)
+{
+    const geom::Point top_left = renderable.top_left();
+    const geom::Size size = renderable.size();
+
+    const glm::vec3 top_left_vec{top_left.x.as_uint32_t(),
+                                 top_left.y.as_uint32_t(),
+                                 0.0f};
+    const glm::vec3 size_vec{size.width.as_uint32_t(),
+                             size.height.as_uint32_t(),
+                             0.0f};
+
+    /* Get the center of the renderable's area */
+    const glm::vec3 center_vec{top_left_vec + 0.5f * size_vec};
+
+    /* 
+     * Every renderable is drawn using a 1x1 quad centered at 0,0.
+     * We need to transform and scale that quad to get to its final position
+     * and size.
+     *
+     * 1. We scale the quad vertices (from 1x1 to wxh)
+     * 2. We move the quad to its final position. Note that because the quad
+     *    is centered at (0,0), we need to translate by center_vec, not
+     *    top_left_vec.
+     */
+    glm::mat4 pos_size_matrix;
+    pos_size_matrix = glm::translate(pos_size_matrix, center_vec);
+    pos_size_matrix = glm::scale(pos_size_matrix, size_vec);
+
+    /* Apply the renderable's custom transformation */
+    const glm::mat4 transformation = pos_size_matrix * renderable.transformation();
+
+    glUseProgram(resources.program);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glActiveTexture(GL_TEXTURE0);
+
+    glUniformMatrix4fv(resources.transform_uniform_loc, 1, GL_FALSE, glm::value_ptr(transformation));
+    glUniform1f(resources.alpha_uniform_loc, renderable.alpha());
+
+    /* Set up vertex attribute data */
+    glBindBuffer(GL_ARRAY_BUFFER, resources.vertex_attribs_vbo);
+    glVertexAttribPointer(resources.position_attr_loc, 3, GL_FLOAT, GL_FALSE, sizeof(VertexAttributes), 0);
+    glVertexAttribPointer(resources.texcoord_attr_loc, 2, GL_FLOAT, GL_FALSE, sizeof(VertexAttributes),
+                          reinterpret_cast<void*>(sizeof(glm::vec3)));
+
+    /* TODO: Bind renderable texture */
+    //glBindTexture(GL_TEXTURE_2D, texture);
+    //renderable->texture().map_texture();
+
+    /* Draw */
+    glEnableVertexAttribArray(resources.position_attr_loc);
+    glEnableVertexAttribArray(resources.texcoord_attr_loc);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glDisableVertexAttribArray(resources.texcoord_attr_loc);
+    glDisableVertexAttribArray(resources.position_attr_loc);
+}
