b46e00e191b448618526ea0c23e6258e50a9e7da
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2012-09-28 18:00:34 +0200

    Add native-compile.sh script
    This script builds mir with the native gcc-4.4 toolchain. In the future it
    could be generalized to build for both cross and native targets,
    cross-toolchain configuration files permitting. Currently it serves as an
    example on how to take advantage of the cmake toolchain file feature.

---
+++ native-compile.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Native build script for Mir
+#
+# Builds the project with gcc-4.4, as specified by the toolchain file
+set -e
+
+CMAKE_TOOLCHAIN_FILE=Toolchain-Ubuntu-GCC44.cmake
+
+if [ ! -e build-native ]; then
+    mkdir build-native
+    ( cd build-native && cmake -DCMAKE_TOOLCHAIN_FILE=../$CMAKE_TOOLCHAIN_FILE ..)
+fi
+
+cmake --build build-native
