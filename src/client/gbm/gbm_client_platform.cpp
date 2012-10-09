90f3ae61ae8ae9bd6fd938b223e15a23a8cfeaf2
Kevin DuBois <kevin.dubois@canonical.com>
2012-10-08 17:16:12 -0700

    update comments

--- src/client/gbm/gbm_client_platform.cpp
+++ src/client/gbm/gbm_client_platform.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois<kevin.dubois@canonical.com>
+ */
+
+/* note: (kdub) this whole file is pretty much a TODO. if we do client side allocations
+         in gbm, the factory can actually allocate buffers here */
+
 #include "mir_client/mir_client_library.h"
 #include "mir_connection.h"
 #include "client_buffer_factory.h"
