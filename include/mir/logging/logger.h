9430a84055b4e994762b2a8d0f31d70414493d08
Thomas Voß <thomas.voss@canonical.com>
2012-10-09 18:12:17 +0200

    mir/logging: Add a server-side logging facade.

---
+++ include/mir/logging/logger.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Thomas Voß <thomas.voss@canonical.com>
+ */
+
+#ifndef MIR_LOGGING_LOGGER_H_
+#define MIR_LOGGING_LOGGER_H_
+
+#include <string>
+
+namespace mir
+{
+namespace logging
+{
+
+// A facade to shield the inner core of mir. It's a facade that
+// prevents an actual logging framework from trickling into the mir
+// core.
+class Logger
+{
+public:
+    
+    enum Severity
+    {
+        critical = 0,
+        error = 1,
+        warning = 2,
+        informational = 3,
+        debug = 4
+    };
+
+    static const char* unknown_component_name()
+    {
+        static const char* name = "UnknownComponent";
+        return name;
+    }
+    
+    virtual ~Logger() {}
+
+    template<Severity severity>
+    void log(const std::string& message, const std::string& component = Logger::unknown_component_name())
+    {
+        log(severity, message, component);
+    }
+    
+  protected:
+    Logger() {}
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+    
+    virtual void log(Severity severity,
+                     const std::string& message,
+                     const std::string& component) = 0;
+};
+}
+}
+
+#endif // MIR_LOGGING_LOGGER_H_
---
+++ src/logging/dumb_console_logger.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Thomas Voß <thomas.voss@canonical.com>
+ */
+
+#include "mir/logging/dumb_console_logger.h"
+
+#include <iostream>
+
+namespace ml = mir::logging;
+
+ml::DumbConsoleLogger::DumbConsoleLogger()
+{
+}
+
+void ml::DumbConsoleLogger::log(ml::Logger::Severity severity,
+                                const std::string& message,
+                                const std::string& component)
+{
+
+    static const char* lut[8] = 
+            {
+                "CC",
+                "EE",
+                "WW",
+                "II",
+                "DD"
+            };
+
+    std::ostream& out = std::cout;
+
+    switch(severity)
+    {
+        case ml::Logger::critical:
+        case ml::Logger::error:
+            //out = std::cerr;
+            break;
+        case ml::Logger::warning:
+        case ml::Logger::informational:
+            //out = std::cout;
+            break;
+        case ml::Logger::debug:
+            //out = std::clog;
+            break;
+        default:
+            return;
+    }
+
+    out << "[" << lut[severity] << ", " << component << "] " 
+        << message << "\n"; 
+}
