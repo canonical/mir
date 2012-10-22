b976dfa26ff082142975a9cdf52f478f2d5e2cac
Alan Griffiths <alan@octopull.co.uk>
2012-10-22 16:43:15 +0100

    Just enough to compile & link

---
+++ include/mir/choice/option.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alan Griffiths <alan@octopull.co.uk>
+ */
+
+#ifndef MIR_CHOICE_OPTION_H_
+#define MIR_CHOICE_OPTION_H_
+
+namespace mir
+{
+namespace choice
+{
+class Option
+{
+public:
+    virtual bool get(char const* name, bool default_) const = 0;
+    virtual char const* get(char const* name, char const* default_) const = 0;
+
+protected:
+    Option() = default;
+    ~Option() = default;
+    Option(Option const&) = delete;
+    Option& operator=(Option const&) = delete;
+};
+}
+}
+
+
+#endif /* MIR_CHOICE_OPTION_H_ */
---
+++ include/mir/choice/program_option.h
@@ -0,0 +1,60 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alan Griffiths <alan@octopull.co.uk>
+ */
+
+
+#ifndef PROGRAM_OPTION_H_
+#define PROGRAM_OPTION_H_
+
+#include "option.h"
+
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/options_description.hpp>
+
+namespace mir
+{
+namespace choice
+{
+class ProgramOption : public Option
+{
+public:
+    ProgramOption();
+
+    void parse_arguments(
+        boost::program_options::options_description const& options,
+        int argc,
+        char const* argv[]);
+
+    void parse_environment(
+        boost::program_options::options_description const& options,
+        char const* prefix);
+
+    void parse_file(
+        boost::program_options::options_description const& options,
+        std::string const& filename);
+
+    bool get(char const* name, bool default_) const;
+    char const* get(char const*, char const* default_) const;
+
+private:
+    boost::program_options::variables_map options;
+};
+}
+}
+
+
+#endif /* PROGRAM_OPTION_H_ */
