ccb4fef3e341938df72e30223403a1863609ee32
Alan Griffiths <alan@octopull.co.uk>
2012-10-26 11:57:04 +0200

    test ApplicationListener::application_next_buffer

--- tests/integration-tests/frontend/test_application_listener.cpp
+++ tests/integration-tests/frontend/test_application_listener.cpp
@@ -38,10 +38,22 @@ struct MockApplicationListener : mf::NullApplicationListener
 
         EXPECT_CALL(*this, application_create_surface_called(testing::_)).
             Times(testing::AtLeast(0));
+
+        EXPECT_CALL(*this, application_next_buffer_called(testing::_)).
+            Times(testing::AtLeast(0));
+
+        EXPECT_CALL(*this, application_release_surface_called(testing::_)).
+            Times(testing::AtLeast(0));
+
+        EXPECT_CALL(*this, application_disconnect_called(testing::_)).
+            Times(testing::AtLeast(0));
     }
 
     MOCK_METHOD1(application_connect_called, void (std::string const&));
     MOCK_METHOD1(application_create_surface_called, void (std::string const&));
+    MOCK_METHOD1(application_next_buffer_called, void (std::string const&));
+    MOCK_METHOD1(application_release_surface_called, void (std::string const&));
+    MOCK_METHOD1(application_disconnect_called, void (std::string const&));
 };
 }
 
@@ -130,6 +142,65 @@ TEST_F(BespokeDisplayServerTestFixture, application_listener_create_surface_is_n
                 &client.surface,
                 google::protobuf::NewCallback(&client, &mt::TestClient::create_surface_done));
             client.wait_for_create_surface();
+
+        }
+    } client_process;
+
+    launch_client_process(client_process);
+}
+
+TEST_F(BespokeDisplayServerTestFixture, application_listener_next_buffer_is_notified)
+{
+    struct Server : TestingServerConfiguration
+    {
+        std::shared_ptr<mf::ApplicationListener>
+        make_application_listener()
+        {
+            auto result = std::make_shared<MockApplicationListener>();
+
+            EXPECT_CALL(*result, application_next_buffer_called(testing::_)).
+                Times(1);
+
+            return result;
+        }
+    } server_processing;
+
+    launch_server_process(server_processing);
+
+    struct Client: TestingClientConfiguration
+    {
+        void exec()
+        {
+            mt::TestClient client(mir::test_socket_file());
+
+            client.connect_parameters.set_application_name(__PRETTY_FUNCTION__);
+            EXPECT_CALL(client, connect_done()).Times(testing::AtLeast(0));
+            EXPECT_CALL(client, create_surface_done()).Times(testing::AtLeast(0));
+            EXPECT_CALL(client, next_buffer_done()).Times(testing::AtLeast(0));
+
+            client.display_server.connect(
+                0,
+                &client.connect_parameters,
+                &client.connection,
+                google::protobuf::NewCallback(&client, &mt::TestClient::connect_done));
+
+            client.wait_for_connect_done();
+
+            client.display_server.create_surface(
+                0,
+                &client.surface_parameters,
+                &client.surface,
+                google::protobuf::NewCallback(&client, &mt::TestClient::create_surface_done));
+            client.wait_for_create_surface();
+
+            client.display_server.next_buffer(
+                0,
+                &client.surface.id(),
+                client.surface.mutable_buffer(),
+                google::protobuf::NewCallback(&client, &mt::TestClient::next_buffer_done));
+
+            client.wait_for_next_buffer();
+            EXPECT_TRUE(client.surface.has_buffer());
 
         }
     } client_process;
