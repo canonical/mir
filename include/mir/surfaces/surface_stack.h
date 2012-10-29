242b8fa8725f5770590e65e7c6dd47b159a17309
Robert Carr <robert.carr@canonical.com>
2012-10-29 07:55:59 +0100

    Apply

--- include/mir/surfaces/surface_stack.h
+++ include/mir/surfaces/surface_stack.h
@@ -46,7 +46,7 @@ public:
     virtual ~SurfaceStack() {}
 
     // From Renderview
-	virtual void apply(mc::RenderableFilter &filter, mc::RenderableOperator &renderable_operator);
+    virtual void apply(mc::RenderableFilter &filter, mc::RenderableOperator &renderable_operator);
 
     // From SurfaceStackModel
     virtual std::weak_ptr<Surface> create_surface(const SurfaceCreationParameters& params);
