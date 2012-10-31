a530b5f0ddaea41fcb7486830a2b00329d7891ee
Kevin DuBois <kevin.dubois@canonical.com>
2012-10-31 09:07:46 -0700

    get android to pass the platform test

---
+++ include/mir_client/android/android_client_platform.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+#ifndef MIR_CLIENT_ANDROID_ANDROID_CLIENT_PLATFORM_H_
+#define MIR_CLIENT_ANDROID_ANDROID_CLIENT_PLATFORM_H_
+
+#include "mir_client/client_platform.h"
+
+namespace mir
+{
+namespace client
+{
+class ClientBufferDepository;
+
+class AndroidClientPlatform : public ClientPlatform
+{
+public:
+    std::shared_ptr<ClientBufferDepository> create_platform_depository ();
+};
+
+}
+}
+
+#endif /* MIR_CLIENT_ANDROID_ANDROID_CLIENT_PLATFORM_H_ */
