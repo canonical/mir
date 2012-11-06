279ba320d6054f49b8d2af1ab8ac6c1fa70f7b86
Robert Carr <robert.carr@canonical.com>
2012-11-06 13:14:34 -0800

    Add event_filter definition

---
+++ include/mir/input/event_filter.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Robert Carr <robert.carr@canonical.com>
+ */
+
+#ifndef MIR_INPUT_EVENT_FILTER_H_
+#define MIR_INPUT_EVENT_FILTER_H_
+
+#include <memory>
+
+namespace android
+{
+    class InputEvent;
+}
+
+namespace mir
+{
+namespace input
+{
+
+class EventFilter
+{
+public:
+    virtual ~EventFilter() {}
+
+    virtual bool filter_event(android::InputEvent *event) = 0;
+
+protected:
+    EventFilter() = default;
+    EventFilter(const EventFilter&) = delete;
+    EventFilter& operator=(const EventFilter&) = delete;
+};
+
+}
+}
+
+#endif // MIR_INPUT_EVENT_FILTER_H_
