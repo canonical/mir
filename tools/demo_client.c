ef1fb00ad74edaf988604ca088c2238790e62388
Kevin DuBois <kevin.dubois@canonical.com>
2012-11-09 12:48:21 -0800

    add -std=c99 for c flags instead of reverting demo_client.c to the older syntax

--- tools/demo_client.c
+++ tools/demo_client.c
@@ -122,8 +122,7 @@ int main(int argc, char* argv[])
     assert(0 <= buffer_package.data_items);
     assert(0 <= buffer_package.fd_items);
 
-    int i;
-    for(i = 0; i < buffer_swap_count; i++)
+    for(int i = 0; i < buffer_swap_count; i++)
     {
         mir_wait_for(mir_surface_next_buffer(surface, surface_next_buffer_callback, 0));
     }
