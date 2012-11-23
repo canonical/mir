081571489e2eeb6812833947d1935aae61002731
Alan Griffiths <alan@octopull.co.uk>
2012-11-23 09:18:10 +0100

    Longer sleep to help jenkins run tests (maybe)

--- tests/integration-tests/test_shutdown.cpp
+++ tests/integration-tests/test_shutdown.cpp
@@ -138,7 +138,7 @@ struct FrontendShutdown : BespokeDisplayServerTestFixture
     {
         // How do we detect that client has started a session?
         // Also, only need this delay in testing process.
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(400));
     }
 };
 }
