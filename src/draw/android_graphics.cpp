715669e86fcd56522520e3ee4543e849cd545459
Kevin DuBois <kevin.dubois@canonical.com>
2012-12-04 14:21:55 -0800

    cleanups. add the draw pattern tests to run on native as well

--- src/draw/android_graphics.cpp
+++ src/draw/android_graphics.cpp
@@ -29,6 +29,50 @@ namespace md=mir::draw;
 namespace mc=mir::compositor;
 namespace geom=mir::geometry;
 
+namespace
+{
+struct RegionDeleter
+{
+    RegionDeleter(gralloc_module_t* grmod, native_handle_t* handle)
+     : grmod(grmod),
+       handle(handle)
+    {
+    }
+
+    void operator()(MirGraphicsRegion* region)
+    {
+        grmod->unlock(grmod, handle);
+        free(handle);
+        delete region;
+    }
+
+    gralloc_module_t *grmod;
+    native_handle_t *handle;
+};
+
+static const char* proc_dir = "/proc";
+static const char* surface_flinger_executable_name = "surfaceflinger";
+int surface_flinger_filter(const struct dirent* d)
+{
+    if (fnmatch("[1-9]*", d->d_name, 0))
+        return 0;
+
+    char path[256];
+    snprintf(path, sizeof(path), "%s/%s/cmdline", proc_dir, d->d_name);
+
+    std::ifstream in(path);
+    std::string line;
+
+    while(std::getline(in, line))
+    {
+        if (line.find(surface_flinger_executable_name) != std::string::npos)
+            return 1;
+    }
+
+    return 0;
+}
+}
+
 md::grallocRenderSW::grallocRenderSW()
  : gralloc_ownership(true)
 {
@@ -51,28 +95,6 @@ md::grallocRenderSW::~grallocRenderSW()
 {
     if (gralloc_ownership)
         gralloc_close(alloc_dev);
-}
-
-namespace
-{
-struct RegionDeleter
-{
-    RegionDeleter(gralloc_module_t* grmod, native_handle_t* handle)
-     : grmod(grmod),
-       handle(handle)
-    {
-    }
-
-    void operator()(MirGraphicsRegion* region)
-    {
-        grmod->unlock(grmod, handle);
-        free(handle);
-        delete region;
-    }
-
-    gralloc_module_t *grmod;
-    native_handle_t *handle;
-};
 }
 
 std::shared_ptr<MirGraphicsRegion> md::grallocRenderSW::get_graphic_region_from_package(
@@ -102,34 +124,6 @@ std::shared_ptr<MirGraphicsRegion> md::grallocRenderSW::get_graphic_region_from_
     region->pixel_format = mir_pixel_format_rgba_8888;
 
     return std::shared_ptr<MirGraphicsRegion>(region, del);
-}
-
-namespace
-{
-
-static const char* proc_dir = "/proc";
-static const char* surface_flinger_executable_name = "surfaceflinger";
-
-int surface_flinger_filter(const struct dirent* d)
-{
-    if (fnmatch("[1-9]*", d->d_name, 0))
-        return 0;
-
-    char path[256];
-    snprintf(path, sizeof(path), "%s/%s/cmdline", proc_dir, d->d_name);
-
-    std::ifstream in(path);
-    std::string line;
-
-    while(std::getline(in, line))
-    {
-        if (line.find(surface_flinger_executable_name) != std::string::npos)
-            return 1;
-    }
-
-    return 0;
-}
-
 }
 
 bool md::is_surface_flinger_running()
