1fea81374930dd202f1cfbfb8cb88937954c2151
Kevin DuBois <kevin.dubois@canonical.com>
2013-01-02 16:31:27 -0800

    add a missing file (the x-compile file for native linux)

---
+++ cmake/LinuxCrossCompile.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+
+set(MIR_NDK_PATH $ENV{MIR_BUNDLE_PATH} CACHE STRING "path of mir android bundle")
+
+set(CMAKE_C_COMPILER   /usr/bin/arm-linux-gnueabihf-gcc-4.6)
+set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++-4.6)
+
+# where too look to find dependencies in the target environment 
+set(CMAKE_FIND_ROOT_PATH  "/opt/bundle")
+
+#use only the cross compile system
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
