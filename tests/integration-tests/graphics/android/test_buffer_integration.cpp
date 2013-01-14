221b65d3e578a6161bff76d55e266c1d45802ccf
Kevin DuBois <kevin.dubois@canonical.com>
2013-01-14 12:40:05 -0800

    get android tests to compile as well

--- tests/integration-tests/graphics/android/test_buffer_integration.cpp
+++ tests/integration-tests/graphics/android/test_buffer_integration.cpp
@@ -125,13 +125,13 @@ TEST_F(AndroidBufferIntegration, swapper_returns_non_null)
     using namespace testing;
 
     mc::BufferProperties actual;
-    std::weak_ptr<mc::Buffer> returned_buffer;
+    std::shared_ptr<mc::Buffer> returned_buffer;
     mc::BufferID id{34};
 
     std::unique_ptr<mc::BufferSwapper> swapper = strategy->create_swapper(actual, buffer_properties);
 
     swapper->client_acquire(returned_buffer, id);
-    EXPECT_NE((int)returned_buffer.lock().get(), NULL);
+    EXPECT_NE((int)returned_buffer.get(), NULL);
 }
 
 TEST_F(AndroidBufferIntegration, buffer_ok_with_egl_context)
