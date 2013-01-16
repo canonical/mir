f14ab5d0f8a3b71f9dfa7f1395b4985a076b2e90
Alan Griffiths <alan@octopull.co.uk>
2013-01-16 10:56:11 +0000

    Change dependencies to the SessionStore interface, not the SessionManager implementation

--- include/mir/server_configuration.h
+++ include/mir/server_configuration.h
@@ -40,7 +40,7 @@ class ApplicationListener;
 
 namespace sessions
 {
-class SessionManager;
+class SessionStore;
 class SurfaceFactory;
 }
 namespace graphics
@@ -68,10 +68,10 @@ public:
     virtual std::shared_ptr<graphics::Renderer> make_renderer(
         std::shared_ptr<graphics::Display> const& display) = 0;
     virtual std::shared_ptr<frontend::Communicator> make_communicator(
-        std::shared_ptr<sessions::SessionManager> const& session_manager,
+        std::shared_ptr<sessions::SessionStore> const& session_store,
         std::shared_ptr<graphics::Display> const& display,
         std::shared_ptr<compositor::GraphicBufferAllocator> const& allocator) = 0;
-    virtual std::shared_ptr<sessions::SessionManager> make_session_manager(
+    virtual std::shared_ptr<sessions::SessionStore> make_session_store(
         std::shared_ptr<sessions::SurfaceFactory> const& surface_factory) = 0;
     virtual std::shared_ptr<input::InputManager> make_input_manager(
         const std::initializer_list<std::shared_ptr<input::EventFilter> const>& event_filters,
@@ -98,10 +98,10 @@ public:
     virtual std::shared_ptr<graphics::Renderer> make_renderer(
         std::shared_ptr<graphics::Display> const& display);
     virtual std::shared_ptr<frontend::Communicator> make_communicator(
-        std::shared_ptr<sessions::SessionManager> const& session_manager,
+        std::shared_ptr<sessions::SessionStore> const& session_store,
         std::shared_ptr<graphics::Display> const& display,
         std::shared_ptr<compositor::GraphicBufferAllocator> const& allocator);
-    virtual std::shared_ptr<sessions::SessionManager> make_session_manager(
+    virtual std::shared_ptr<sessions::SessionStore> make_session_store(
         std::shared_ptr<sessions::SurfaceFactory> const& surface_factory);
     virtual std::shared_ptr<input::InputManager> make_input_manager(
         const std::initializer_list<std::shared_ptr<input::EventFilter> const>& event_filters,
@@ -115,7 +115,7 @@ private:
 
     // the communications interface to use
     virtual std::shared_ptr<frontend::ProtobufIpcFactory> make_ipc_factory(
-        std::shared_ptr<sessions::SessionManager> const& session_manager,
+        std::shared_ptr<sessions::SessionStore> const& session_store,
         std::shared_ptr<graphics::Display> const& display,
         std::shared_ptr<compositor::GraphicBufferAllocator> const& allocator);
 
