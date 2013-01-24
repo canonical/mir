40e1824ad1182479eabb10ab57c7a3e045728778
Kevin DuBois <kevin.dubois@canonical.com>
2013-01-24 10:01:41 -0800

    rewrite android_framebuffer_window.cpp to use more c++11 logic. (instead of tacking on [] to delete)

--- src/graphics/android/android_framebuffer_window.cpp
+++ src/graphics/android/android_framebuffer_window.cpp
@@ -20,6 +20,8 @@
 
 #include <boost/throw_exception.hpp>
 
+#include <vector>
+#include <algorithm>
 #include <stdexcept>
 
 namespace mga = mir::graphics::android;
@@ -46,38 +48,31 @@ EGLNativeWindowType mga::AndroidFramebufferWindow::android_native_window_type() 
 
 EGLConfig mga::AndroidFramebufferWindow::android_display_egl_config(EGLDisplay egl_display) const
 {
+    int num_potential_configs, android_native_id;
     EGLint num_match_configs;
-    int num_potential_configs;
-    EGLConfig* config_slots;
-    EGLConfig egl_config;
+
     eglGetConfigs(egl_display, NULL, 0, &num_potential_configs);
-    config_slots = new EGLConfig[num_potential_configs];
+    std::vector<EGLConfig> config_slots(num_potential_configs);
 
     /* upon return, this will fill config_slots[0:num_match_configs] with the matching */
-    eglChooseConfig(egl_display, default_egl_config_attr, config_slots, num_potential_configs, &num_match_configs );
-
-    int android_native_id;
-    native_window->query(native_window.get(), NATIVE_WINDOW_FORMAT, &android_native_id);
+    eglChooseConfig(egl_display, default_egl_config_attr,
+                    config_slots.data(), num_potential_configs, &num_match_configs);
+    config_slots.resize(num_match_configs);
 
     /* why check manually for EGL_NATIVE_VISUAL_ID instead of using eglChooseConfig? the egl
      * specification does not list EGL_NATIVE_VISUAL_ID as something it will check for in
      * eglChooseConfig */
-    bool found=false;
-    for(int i=0; i < num_match_configs; i++)
-    {
-        int visual_id;
-        eglGetConfigAttrib(egl_display, config_slots[i], EGL_NATIVE_VISUAL_ID, &visual_id);
-        if(visual_id == android_native_id)
+    native_window->query(native_window.get(), NATIVE_WINDOW_FORMAT, &android_native_id);
+    auto const pegl_config = std::find_if(begin(config_slots), end(config_slots),
+        [&](EGLConfig& current) -> bool
         {
-            egl_config = config_slots[i];
-            found = true;
-            break;
-        }
-    }
-    delete[] config_slots;
+            int visual_id;
+            eglGetConfigAttrib(egl_display, current, EGL_NATIVE_VISUAL_ID, &visual_id);
+            return (visual_id == android_native_id);
+        });
+    
+    if (pegl_config == end(config_slots))
+        BOOST_THROW_EXCEPTION(std::runtime_error("could not select EGL config for use with framebuffer"));
 
-    if (!found)
-        BOOST_THROW_EXCEPTION(std::runtime_error("could not select EGL config"));
-
-    return egl_config;
+    return *pegl_config;
 }
