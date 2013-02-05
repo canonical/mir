bcf602cb0c42d37cbced3ff0117bf5fcaa9eaca9
Alan Griffiths <alan@octopull.co.uk>
2013-02-05 15:06:44 +0000

    logic_error is correct for precondition failures

--- tests/unit-tests/sessions/test_registration_order_focus_sequence.cpp
+++ tests/unit-tests/sessions/test_registration_order_focus_sequence.cpp
@@ -131,14 +131,14 @@ TEST_F(RegistrationOrderFocusSequenceSetup, invalid_session_throw_behavior)
 
     EXPECT_THROW({
             focus_sequence.successor_of(null_session);
-    }, std::runtime_error);
+    }, std::logic_error);
     EXPECT_THROW({
             focus_sequence.predecessor_of(null_session);
-    }, std::runtime_error);
+    }, std::logic_error);
     EXPECT_THROW({
             focus_sequence.successor_of(invalid_session);
-    }, std::runtime_error);
+    }, std::logic_error);
     EXPECT_THROW({
             focus_sequence.predecessor_of(invalid_session);
-    }, std::runtime_error);
+    }, std::logic_error);
 }
