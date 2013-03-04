ea3852fe88d6e4c85bbc2752420071522f861129
Didier Roche <didrocks@ubuntu.com>
2013-03-04 16:26:57 +0100

    change licenses so that the library part is LGPL 3

--- src/client/android/mir_native_window.h
+++ src/client/android/mir_native_window.h
@@ -1,9 +1,9 @@
 /*
  * Copyright © 2012 Canonical Ltd.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
