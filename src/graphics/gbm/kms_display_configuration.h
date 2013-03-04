404adfe773a4412ddc90b5700130b004e2ede95a
Didier Roche <didrocks@ubuntu.com>
2013-03-04 17:02:02 +0100

    more licensing fix and remove the *cmake license as well

--- src/graphics/gbm/kms_display_configuration.h
+++ src/graphics/gbm/kms_display_configuration.h
@@ -10,7 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by: Alexandros Frantzis <alexandros.frantzis@canonical.com>
