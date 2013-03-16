b4ba431bbe8016344d4804689cbb6d8e4b0e209f
Alexandros Frantzis <alexandros.frantzis@canonical.com>
2013-03-16 17:08:08 +0200

    compositor: Introduce the new Compositor class

---
+++ include/server/mir/compositor/compositor.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alexandros Frantzis <alexandros.frantzis@canonical.com>
+ */
+
+#ifndef MIR_COMPOSITOR_COMPOSITOR_H_
+#define MIR_COMPOSITOR_COMPOSITOR_H_
+
+namespace mir
+{
+namespace compositor
+{
+
+class Compositor
+{
+public:
+    virtual ~Compositor() {}
+
+    virtual void start() = 0;
+    virtual void stop() = 0;
+
+protected:
+    Compositor() = default;
+    Compositor(Compositor const&) = delete;
+    Compositor& operator=(Compositor const&) = delete;
+};
+
+}
+}
+
+#endif // MIR_COMPOSITOR_COMPOSITOR_H_
