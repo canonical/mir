282dc5f096d6d8da5c93cfeff737cc18c12c2ac6
Kevin DuBois <kevin.dubois@canonical.com>
2013-03-15 17:33:18 -0700

    clean up gralloc module mock a bit

--- include/test/mir_test/hw_mock.h
+++ include/test/mir_test/hw_mock.h
@@ -18,13 +18,48 @@
 #ifndef MIR_TEST_DOUBLES_MOCK_ANDROID_HWC_DEVICE_H_
 #define MIR_TEST_DOUBLES_MOCK_ANDROID_HWC_DEVICE_H_
 
+#include <memory>
 #include <hardware/hardware.h>
 #include <gmock/gmock.h>
+#include "mir_test_doubles/mock_android_alloc_device.h"
 
 namespace mir
 {
 namespace test
 {
+
+
+typedef struct hw_module_t hw_module;
+class gralloc_module_mock : public hw_module
+{
+public:
+    gralloc_module_mock(hw_device_t* const& device)
+        : mock_ad(device)
+    { 
+        gr_methods.open = hw_open;
+        methods = &gr_methods; 
+    }
+    ~gralloc_module_mock()
+    {
+    } 
+
+    static int hw_open(const struct hw_module_t* module, const char*, struct hw_device_t** device)
+    {
+        gralloc_module_mock* self = (gralloc_module_mock*) module;
+        self->mock_ad->close = hw_close;
+        *device = (hw_device_t*) self->mock_ad;
+        return 0;
+    }
+
+    static int hw_close(struct hw_device_t*)
+    {
+        return 0;
+    }
+
+    hw_module_methods_t gr_methods;
+    hw_device_t* mock_ad;
+};
+
 
 class HardwareAccessMock
 {
@@ -34,12 +69,10 @@ public:
 
     MOCK_METHOD2(hw_get_module, int(const char *id, const struct hw_module_t**));
 
+    std::shared_ptr<alloc_device_t> ad; 
+    std::shared_ptr<gralloc_module_mock> grmock;
+
     struct hw_device_t *fake_hwc_device;
-    struct hw_device_t *fake_gr_device;
-
-    hw_module_t fake_hw_gr_module;
-    hw_module_methods_t gr_methods;
-
     hw_module_t fake_hw_hwc_module;
     hw_module_methods_t hwc_methods; 
 };
--- tests/unit-tests/graphics/egl_mock/hw_mock.cpp
+++ tests/unit-tests/graphics/egl_mock/hw_mock.cpp
@@ -4,6 +4,7 @@
 #include <hardware/hwcomposer.h> 
 
 namespace mt = mir::test;
+namespace mtd = mir::test::doubles;
 
 #include <system/window.h>
 #include <memory>
@@ -40,46 +41,6 @@ public:
 mt::HardwareAccessMock* global_hw_mock = NULL;
 //std::shared_ptr<StubANativeWindow> global_android_fb_win;
 
-namespace gralloc_mock
-{
-
-
-//todo: use the mock for the alloc device
-struct alloc_device_t fake_device;
-static struct native_handle fake_handle;
-
-int hw_gralloc_alloc(struct alloc_device_t*,
-            int, int, int, int,
-            buffer_handle_t* handle, int* stride)
-{
-    *handle = (buffer_handle_t) &fake_handle;
-    *stride = 44; 
-    return 0;
-}
-
-int hw_gralloc_free(struct alloc_device_t*,
-        buffer_handle_t)
-{
-    return 0;
-}
-
-int hw_close(struct hw_device_t*)
-{
-    return 0;
-}
-
-static int hw_open(const struct hw_module_t*, const char*, struct hw_device_t** device)
-{
-    fake_device.common.close = hw_close;
-    fake_device.alloc = hw_gralloc_alloc;    
-    fake_device.free = hw_gralloc_free;    
- 
-    *device = (hw_device_t*) &fake_device;
-    return 0;
-}
-
-}
-
 
 namespace hwc_mock
 {
@@ -108,17 +69,17 @@ mt::HardwareAccessMock::HardwareAccessMock()
     assert(global_hw_mock == NULL && "Only one mock object per process is allowed");
     global_hw_mock = this;
 
-    gr_methods.open = gralloc_mock::hw_open; 
-    fake_hw_gr_module.methods = &gr_methods;
-    fake_gr_device = (hw_device_t*) &gralloc_mock::fake_device;
+    ad = std::make_shared<doubles::MockAllocDevice>();
+    grmock = std::make_shared<mt::gralloc_module_mock>(&ad->common);
+
 
     hwc_methods.open = hwc_mock::hw_open;
     fake_hw_hwc_module.methods = &hwc_methods;
-    fake_hwc_device = &hwc_mock::fake_device;
+    fake_hwc_device = (hw_device_t*) &hwc_mock::fake_device;
     fake_hwc_device->version = HWC_DEVICE_API_VERSION_1_1;
 
     ON_CALL(*this, hw_get_module(StrEq(GRALLOC_HARDWARE_MODULE_ID),_))
-        .WillByDefault(DoAll(SetArgPointee<1>(&fake_hw_gr_module), Return(0)));
+        .WillByDefault(DoAll(SetArgPointee<1>(grmock.get()), Return(0)));
     ON_CALL(*this, hw_get_module(StrEq(HWC_HARDWARE_MODULE_ID),_))
         .WillByDefault(DoAll(SetArgPointee<1>(&fake_hw_hwc_module), Return(0)));
 }
@@ -137,14 +98,4 @@ int hw_get_module(const char *id, const struct hw_module_t **module)
     }
     int rc =  global_hw_mock->hw_get_module(id, module);
     return rc;
-}
-
-extern "C"
-{
-ANativeWindow* android_createDisplaySurface()
-{
-    return new StubANativeWindow();
-}
-
-
 }
