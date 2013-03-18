c9ec1e0558f57b4d8b602c726e555c657e600234
Robert Carr <robert.carr@canonical.com>
2013-03-17 20:50:16 -0700

    Add shared stub surface

---
+++ include/test/mir_test_doubles/stub_surface.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Robert Carr <robert.carr@canonical.com>
+ */
+
+#ifndef MIR_TEST_DOUBLES_STUB_SURFACE_H_
+#define MIR_TEST_DOUBLES_STUB_SURFACE_H_
+
+#include "mir/shell/surface.h"
+
+namespace mir
+{
+namespace test
+{
+namespace doubles
+{
+
+class StubSurface
+{
+public:
+
+    virtual ~StubSurface() = default;
+    
+    void hide() {}
+    void show() {}
+    void destroy() {}
+    void shutdown() {}
+    
+    geometry::Size size() const
+    {
+        return geometry::Size();
+    }
+    geometry::PixelFormat pixel_format() const
+    {
+        return geometry::PixelFormat();
+    }
+    void advance_client_buffer()
+    {
+    }
+    std::shared_ptr<compositor::Buffer> client_buffer() const
+    {
+        return std::shared_ptr<compositor::Buffer>();
+    }
+};
+
+}
+}
+} // namespace mir
+
+#endif // MIR_TEST_DOUBLES_STUB_SURFACE_H_
