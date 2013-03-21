ecef3df01b8ccf01d59b6d7c681962e77ea51b70
bryce <bryce@canonical.com>
2013-03-20 20:10:03 -0700

    apport: Include versions for libdrm and mesa as well

--- debian/source_mir.py
+++ debian/source_mir.py
@@ -1,5 +1,8 @@
 from apport.hookutils import *
 
 def add_info(report, ui=None):
-    attach_related_packages(report, ['lightdm'])
     attach_file_if_exists(report, '/var/log/boot.log', 'BootLog')
+
+    report['version.libdrm'] = package_versions('libdrm2')
+    report['version.lightdm'] = package_versions('lightdm')
+    report['version.mesa'] = package_versions('libegl1-mesa-dev')
