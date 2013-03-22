03f0191c0c9a4eabde57ed5b0be72b05cfbe80c5
Kevin DuBois <kevin.dubois@canonical.com>
2013-03-22 14:24:13 -0700

    add a stub HWC11Device cpp (tests now compile/fail)

---
+++ src/server/graphics/android/hwc11_device.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by:
+ *   Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#include "hwc11_device.h"
+
+namespace mga=mir::graphics::android;
+
+mga::HWC11Device::HWC11Device(std::shared_ptr<hwc_composer_device_1> /*hwc_device*/)
+{
+}
+
+void mga::HWC11Device::wait_for_vsync()
+{
+}
