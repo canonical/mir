351d4ceef27f93016619de81342ecd749d39385d
Daniel van Vugt <daniel.van.vugt@canonical.com>
2013-03-22 15:03:08 +0800

    Initial definition of MirSurfaceState, with some etymological analysis too.

--- include/shared/mir_toolkit/common.h
+++ include/shared/mir_toolkit/common.h
@@ -30,6 +30,7 @@
 typedef enum MirSurfaceAttrib
 {
     mir_surface_attrib_type,
+    mir_surface_attrib_state,
     mir_surface_attrib_arraysize_
 } MirSurfaceAttrib;
 
@@ -44,6 +45,29 @@ typedef enum MirSurfaceType
     mir_surface_type_arraysize_
 } MirSurfaceType;
 
-/* TODO: Surface states here */
+/*
+ * MirSurfaceState: "minimized" or "minimised"?
+ * A review of 7 leading dictionaries shows "z" is the most common spelling
+ * (listed in all dictionaries), and "s" only appears in 4. And those that list
+ * both "z" and "s" usually point out that "z" is the main spelling. Only 2
+ * dictionaries say that "s" is preferred, and even still all dictionaries seem
+ * to agree that "s" is only used in the UK.
+ * The same is true for "maximized" vs "maximised".
+ */
+typedef enum MirSurfaceState
+{
+    mir_surface_state_restored,
+    mir_surface_state_minimized,
+    mir_surface_state_minimised = mir_surface_state_minimized,
+    mir_surface_state_maximized,
+    mir_surface_state_maximised = mir_surface_state_maximized,
+    mir_surface_state_vertmaximized,
+    mir_surface_state_vertmaximised = mir_surface_state_vertmaximized,
+    /* mir_surface_state_semimaximized,
+       Omitted for now, since it's functionally a subset of vertmaximized and
+       differs only in the X coordinate. */
+    mir_surface_state_fullscreen,
+    mir_surface_state_arraysize_
+} MirSurfaceState;
 
 #endif
