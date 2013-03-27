a7d22c2636b2703238c05e97ae197bbf01f7f9ec
Kevin DuBois <kevin.dubois@canonical.com>
2013-03-27 11:03:18 -0700

    get blank turn-on test to work

--- src/server/graphics/android/hwc11_device.cpp
+++ src/server/graphics/android/hwc11_device.cpp
@@ -55,6 +55,8 @@ mga::HWC11Device::HWC11Device(std::shared_ptr<hwc_composer_device_1> const& hwc_
     {
         BOOST_THROW_EXCEPTION(std::runtime_error("could not enable hwc vsync notifications"));
     }
+
+    hwc_device->blank(hwc_device.get(), HWC_DISPLAY_PRIMARY, 0);
 }
 
 void mga::HWC11Device::notify_vsync()
