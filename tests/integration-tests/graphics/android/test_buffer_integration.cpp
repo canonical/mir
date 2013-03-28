77d3207312fb251417f59d3b9fa7f7d430262687
Kevin DuBois <kevin.dubois@canonical.com>
2013-03-27 17:25:34 -0700

    split out the integration test into buffer-related tests and display related ones

--- tests/integration-tests/graphics/android/test_buffer_integration.cpp
+++ tests/integration-tests/graphics/android/test_buffer_integration.cpp
@@ -43,193 +43,53 @@ namespace mtd=mir::test::draw;
 namespace
 {
 
-class AndroidBufferIntegration : public ::testing::Test
+class AndroidGraphicsPlatform : public ::testing::Test
 {
 protected:
-    static void SetUpTestCase()
-    {
-        ASSERT_FALSE(mtd::is_surface_flinger_running());
-        ASSERT_NO_THROW(
-        {
-            platform = mg::create_platform(std::make_shared<mg::NullDisplayReport>());
-            display = platform->create_display();
-        });
-    }
-
     virtual void SetUp()
     {
+        platform = mg::create_platform(std::make_shared<mg::NullDisplayReport>());
         ASSERT_TRUE(platform != NULL);
-        ASSERT_TRUE(display != NULL);
 
-        null_buffer_initializer = std::make_shared<mg::NullBufferInitializer>();
-        allocator = platform->create_buffer_allocator(null_buffer_initializer);
-        strategy = std::make_shared<mc::SwapperFactory>(allocator);
-        size = geom::Size{geom::Width{gl_animation.texture_width()},
-                          geom::Height{gl_animation.texture_height()}};
+        size = geom::Size{geom::Width{334},
+                          geom::Height{122}};
         pf  = geom::PixelFormat::abgr_8888;
         buffer_properties = mc::BufferProperties{size, pf, mc::BufferUsage::software};
+        null_buffer_initializer = std::make_shared<mg::NullBufferInitializer>();
     }
 
-    md::glAnimationBasic gl_animation;
     std::shared_ptr<mg::BufferInitializer> null_buffer_initializer;
-    std::shared_ptr<mc::GraphicBufferAllocator> allocator;
-    std::shared_ptr<mc::SwapperFactory> strategy;
     geom::Size size;
     geom::PixelFormat pf;
     mc::BufferProperties buffer_properties;
-    mtd::TestGrallocMapper sw_renderer;
 
-    /* note about display: android drivers seem to only be able to open fb once
-       per process (gralloc's framebuffer_close() doesn't seem to work). once we
-       figure out why, we can put display in the test fixture */
-    static std::shared_ptr<mg::Platform> platform;
-    static std::shared_ptr<mg::Display> display;
+    std::shared_ptr<mg::Platform> platform;
 };
 
-std::shared_ptr<mg::Display> AndroidBufferIntegration::display;
-std::shared_ptr<mg::Platform> AndroidBufferIntegration::platform;
-
 }
 
-TEST_F(AndroidBufferIntegration, post_does_not_throw)
+TEST_F(AndroidGraphicsPlatform, alloc_does_not_throw)
 {
     using namespace testing;
 
-    EXPECT_NO_THROW(
-    {
-        display->for_each_display_buffer([](mg::DisplayBuffer& buffer)
-        {
-            buffer.post_update();
-        });
-    });
-}
-
-TEST(AndroidBufferIntegrationBasic, alloc_does_not_throw)
-{
-    using namespace testing;
-
-    auto null_buffer_initializer = std::make_shared<mg::NullBufferInitializer>();
     EXPECT_NO_THROW({
+        auto allocator = std::make_shared<mga::AndroidBufferAllocator>(null_buffer_initializer);
+        auto strategy = std::make_shared<mc::SwapperFactory>(allocator);
+    });
+
+}
+
+TEST_F(AndroidGraphicsPlatform, swapper_creation_is_sane)
+{
+    using namespace testing;
+
     auto allocator = std::make_shared<mga::AndroidBufferAllocator>(null_buffer_initializer);
     auto strategy = std::make_shared<mc::SwapperFactory>(allocator);
-    });
-
-}
-TEST_F(AndroidBufferIntegration, swapper_creation_ok)
-{
-    using namespace testing;
-
     mc::BufferProperties actual;
-
-    EXPECT_NO_THROW({
-    std::unique_ptr<mc::BufferSwapper> swapper = strategy->create_swapper(actual, buffer_properties);
-    });
-}
-
-TEST_F(AndroidBufferIntegration, swapper_returns_non_null)
-{
-    using namespace testing;
-
-    mc::BufferProperties actual;
-    std::shared_ptr<mc::Buffer> returned_buffer;
     mc::BufferID id{34};
-
-    std::unique_ptr<mc::BufferSwapper> swapper = strategy->create_swapper(actual, buffer_properties);
-
+    auto swapper = strategy->create_swapper(actual, buffer_properties);
+    std::shared_ptr<mc::Buffer> returned_buffer;
     swapper->client_acquire(returned_buffer, id);
+
     EXPECT_NE(nullptr, returned_buffer);
-}
-
-TEST_F(AndroidBufferIntegration, buffer_ok_with_egl_context)
-{
-    using namespace testing;
-
-    mtd::DrawPatternSolid red_pattern(0xFF0000FF);
-    auto allocator = std::make_shared<mga::AndroidBufferAllocator>(null_buffer_initializer);
-    auto strategy = std::make_shared<mc::SwapperFactory>(allocator);
-
-    geom::PixelFormat pf(geom::PixelFormat::abgr_8888);
-    mc::BufferProperties buffer_properties{size, pf, mc::BufferUsage::software};
-    mc::BufferProperties actual;
-    std::unique_ptr<mc::BufferSwapper> swapper = strategy->create_swapper(actual, buffer_properties);
-    auto bundle = std::make_shared<mc::BufferBundleSurfaces>(std::move(swapper));
-
-    gl_animation.init_gl();
-
-    auto client_buffer = bundle->secure_client_buffer();
-    auto ipc_package = client_buffer->get_ipc_package();
-    auto region = sw_renderer.get_graphic_region_from_package(ipc_package, size);
-    red_pattern.draw(region);
-    client_buffer.reset();
-
-    auto texture_res = bundle->lock_back_buffer();
-    gl_animation.render_gl();
-    display->for_each_display_buffer([](mg::DisplayBuffer& buffer)
-    {
-        buffer.post_update();
-    });
-    texture_res.reset();
-}
-
-TEST_F(AndroidBufferIntegration, DISABLED_buffer_ok_with_egl_context_repeat)
-{
-    using namespace testing;
-
-    mtd::DrawPatternSolid red_pattern(0xFF0000FF);
-    mtd::DrawPatternSolid green_pattern(0xFF00FF00);
-
-    auto allocator = std::make_shared<mga::AndroidBufferAllocator>(null_buffer_initializer);
-    auto strategy = std::make_shared<mc::SwapperFactory>(allocator);
-
-    geom::PixelFormat pf(geom::PixelFormat::abgr_8888);
-    mc::BufferProperties buffer_properties{size, pf, mc::BufferUsage::software};
-    mc::BufferProperties actual;
-    std::unique_ptr<mc::BufferSwapper> swapper = strategy->create_swapper(actual, buffer_properties);
-    auto bundle = std::make_shared<mc::BufferBundleSurfaces>(std::move(swapper));
-
-    gl_animation.init_gl();
-
-    for(;;)
-    {
-        /* buffer 0 */
-        auto client_buffer = bundle->secure_client_buffer();
-        auto ipc_package = client_buffer->get_ipc_package();
-        auto region = sw_renderer.get_graphic_region_from_package(ipc_package, size);
-        red_pattern.draw(region);
-        client_buffer.reset();
-
-        auto texture_res = bundle->lock_back_buffer();
-        gl_animation.render_gl();
-        display->for_each_display_buffer([](mg::DisplayBuffer& buffer)
-        {
-            buffer.post_update();
-        });
-        texture_res.reset();
-        sleep(1);
-
-        /* buffer 1 */
-        client_buffer = bundle->secure_client_buffer();
-        ipc_package = client_buffer->get_ipc_package();
-        region = sw_renderer.get_graphic_region_from_package(ipc_package, size);
-        green_pattern.draw(region);
-        client_buffer.reset();
-
-        texture_res = bundle->lock_back_buffer();
-        gl_animation.render_gl();
-        display->for_each_display_buffer([](mg::DisplayBuffer& buffer)
-        {
-            buffer.post_update();
-        });
-        texture_res.reset();
-        sleep(1);
-    }
-
-}
-
-TEST_F(AndroidBufferIntegration, display_cleanup_ok)
-{
-    EXPECT_NO_THROW(
-    {
-        display.reset();
-    });
 }
