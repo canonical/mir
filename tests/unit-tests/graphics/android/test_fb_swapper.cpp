4d13c40c00d0c2d980942c9627b8110b24dfdb36
Kevin DuBois <kevin.dubois@canonical.com>
2013-04-04 13:37:34 -0700

    get the fbswapper test to compile/not link

--- tests/unit-tests/graphics/android/test_fb_swapper.cpp
+++ tests/unit-tests/graphics/android/test_fb_swapper.cpp
@@ -16,60 +16,74 @@
  * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
  */
 
+#include "src/server/graphics/android/fb_swapper.h"
+#include "mir_test_doubles/mock_buffer.h"
+
+#include <stdexcept>
 #include <gtest/gtest.h>
+
+namespace mc=mir::compositor;
+namespace mga=mir::graphics::android;
+namespace mtd=mir::test::doubles;
 
 class FBSwapperTest : public ::testing::Test
 {
 public:
     virtual void SetUp()
     {
+        buffer1 = std::make_shared<mtd::MockBuffer>();
+        buffer2 = std::make_shared<mtd::MockBuffer>();
+        buffer3 = std::make_shared<mtd::MockBuffer>();
     }
-}
+
+    std::shared_ptr<mc::Buffer> buffer1;
+    std::shared_ptr<mc::Buffer> buffer2;
+    std::shared_ptr<mc::Buffer> buffer3;
+};
 
 TEST_F(FBSwapperTest, simple_swaps_returns_valid)
 {
-    FBSwapper fb_swapper({buffer1, buffer2});
+    mga::FBSwapper fb_swapper({buffer1, buffer2});
 
-    auto test_buffer = fb_swapper.compositor_acquire()
+    auto test_buffer = fb_swapper.compositor_acquire();
 
     EXPECT_TRUE((test_buffer == buffer1) || (test_buffer == buffer2));
 } 
 
-#if 0
 TEST_F(FBSwapperTest, simple_swaps_return_aba_pattern)
 {
-    FBSwapper fb_swapper({buffer1, buffer2});
+    mga::FBSwapper fb_swapper({buffer1, buffer2});
 
-    auto test_buffer_1 = fb_swapper.compositor_acquire()
+    auto test_buffer_1 = fb_swapper.compositor_acquire();
     fb_swapper.compositor_release(test_buffer_1);
 
-    auto test_buffer_2 = fb_swapper.compositor_acquire()
+    auto test_buffer_2 = fb_swapper.compositor_acquire();
     fb_swapper.compositor_release(test_buffer_2);
 
-    auto test_buffer_3 = fb_swapper.compositor_acquire()
+    auto test_buffer_3 = fb_swapper.compositor_acquire();
     fb_swapper.compositor_release(test_buffer_3);
 
     EXPECT_EQ(test_buffer_1, test_buffer_3);
     EXPECT_NE(test_buffer_1, test_buffer_2);
 } 
 
-TEST_F(FBSwapperTest, triple_swaps_return_abca_pattern)
+TEST_F(FBSwapperTest, triple_swaps_return_abcab_pattern)
 {
-    FBSwapper fb_swapper({buffer1, buffer2, buffer3});
+    mga::FBSwapper fb_swapper({buffer1, buffer2, buffer3});
 
-    auto test_buffer_1 = fb_swapper.compositor_acquire()
+    auto test_buffer_1 = fb_swapper.compositor_acquire();
     fb_swapper.compositor_release(test_buffer_1);
 
-    auto test_buffer_2 = fb_swapper.compositor_acquire()
+    auto test_buffer_2 = fb_swapper.compositor_acquire();
     fb_swapper.compositor_release(test_buffer_2);
 
-    auto test_buffer_3 = fb_swapper.compositor_acquire()
+    auto test_buffer_3 = fb_swapper.compositor_acquire();
     fb_swapper.compositor_release(test_buffer_3);
 
-    auto test_buffer_4 = fb_swapper.compositor_acquire()
+    auto test_buffer_4 = fb_swapper.compositor_acquire();
     fb_swapper.compositor_release(test_buffer_4);
 
-    auto test_buffer_5 = fb_swapper.compositor_acquire()
+    auto test_buffer_5 = fb_swapper.compositor_acquire();
     fb_swapper.compositor_release(test_buffer_5);
 
     EXPECT_EQ(test_buffer_1, test_buffer_4);
@@ -84,14 +98,14 @@ TEST_F(FBSwapperTest, triple_swaps_return_abca_pattern)
 //not supported yet. we will support this for composition bypass
 TEST_F(FBSwapperTest, client_acquires_throw)
 {
-    FBSwapper fb_swapper({buffer1, buffer2});
+    mga::FBSwapper fb_swapper({buffer1, buffer2});
 
     EXPECT_THROW({
         fb_swapper.client_acquire();
-    });
+    }, std::runtime_error);
 
     EXPECT_THROW({
-        fb_swapper.client_release();
-    });
+        std::shared_ptr<mc::Buffer> empty;
+        fb_swapper.client_release(empty);
+    }, std::runtime_error);
 }
-#endif 
