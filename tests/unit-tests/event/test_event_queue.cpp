5213893315892e5b7425321bbf3f6c4f1d6e2f51
Daniel van Vugt <daniel.van.vugt@canonical.com>
2013-04-09 16:28:10 +0800

    More unit tests for EventQueue/EventSink.

--- tests/unit-tests/event/test_event_queue.cpp
+++ tests/unit-tests/event/test_event_queue.cpp
@@ -64,7 +64,7 @@ namespace
     private:
         MirEvent last_event;
     };
-} // anonymous namespace
+}
 
 TEST(EventQueue, events_get_handled)
 {
@@ -91,4 +91,36 @@ TEST(EventQueue, events_get_handled)
     e.type = mir_event_type_surface_change;
     q.post(e);
     EXPECT_EQ(mir_event_type_surface_change, s->last_event_handled().type);
+}
+
+TEST(EventQueue, sink_is_changeable)
+{
+    mir::EventQueue q;
+    std::shared_ptr<TestSink> a(new TestSink);
+    std::shared_ptr<TestSink> b(new TestSink);
+
+    q.set_sink(a);
+
+    MirEvent e;
+    memset(&e, 0, sizeof e);
+
+    e.type = mir_event_type_motion;
+    q.post(e);
+    EXPECT_EQ(mir_event_type_motion, a->last_event_handled().type);
+
+    e.type = mir_event_type_hw_switch;
+    q.post(e);
+    EXPECT_EQ(mir_event_type_hw_switch, a->last_event_handled().type);
+
+    q.set_sink(b);
+
+    e.type = mir_event_type_surface_change;
+    q.post(e);
+    EXPECT_EQ(mir_event_type_surface_change, b->last_event_handled().type);
+    EXPECT_EQ(mir_event_type_hw_switch, a->last_event_handled().type);
+
+    e.type = mir_event_type_key;
+    q.post(e);
+    EXPECT_EQ(mir_event_type_key, b->last_event_handled().type);
+    EXPECT_EQ(mir_event_type_hw_switch, a->last_event_handled().type);
 }
