b6530ded6c6b673d8882884680909cec43756235
Robert Ancell <robert.ancell@canonical.com>
2013-04-12 16:55:11 +1200

    Correct license in server code and tests incorrectly marked as LGPL

--- src/server/surfaces/surface_stack.cpp
+++ src/server/surfaces/surface_stack.cpp
@@ -2,7 +2,7 @@
  * Copyright © 2012 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License version 3,
+ * under the terms of the GNU General Public License version 3,
  * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
@@ -10,7 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by:
