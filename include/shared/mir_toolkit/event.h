f36b9d523e2fbe082bf7e60fd50575b61a818de5
Brandon Schaefer <brandon.schaefer@canonical.com>
2013-04-29 13:21:12 -0700

    * Mir needs to keep vscroll/hscroll event data from android.

--- include/shared/mir_toolkit/event.h
+++ include/shared/mir_toolkit/event.h
@@ -157,6 +157,8 @@ typedef struct
         float size;
         float pressure;
         float orientation;
+        float vscroll;
+        float hscroll;
     } pointer_coordinates[MIR_INPUT_EVENT_MAX_POINTER_COUNT];
 } MirMotionEvent;
 
