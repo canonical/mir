e706769470460f6168fe01339dcab2eef7d9d656
Alan Griffiths <alan@octopull.co.uk>
2013-04-30 16:46:25 -0700

    Don't spread ownership of the cursor around

--- src/server/graphics/android/android_display.h
+++ src/server/graphics/android/android_display.h
@@ -63,7 +63,7 @@ public:
     void make_current();
     void release_current();
 
-    std::shared_ptr<Cursor> the_cursor();
+    std::weak_ptr<Cursor> the_cursor();
 
 protected:
     EGLDisplay egl_display;
