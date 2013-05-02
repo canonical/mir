306a1f1c66ca078ab9a954726cc414fb4fe851e7
Robert Carr <robert.carr@canonical.com>
2013-05-02 11:26:29 -0700

    Cleanup

--- include/server/mir/surfaces/surface_stack_model.h
+++ include/server/mir/surfaces/surface_stack_model.h
@@ -40,7 +40,7 @@ class SurfaceStackModel
 public:
     virtual ~SurfaceStackModel() {}
 
-    virtual std::weak_ptr<Surface> create_surface(frontend::SurfaceCreationParameters const& params, DepthId layer) = 0;
+    virtual std::weak_ptr<Surface> create_surface(frontend::SurfaceCreationParameters const& params, DepthId depth) = 0;
 
     virtual void destroy_surface(std::weak_ptr<Surface> const& surface) = 0;
 
