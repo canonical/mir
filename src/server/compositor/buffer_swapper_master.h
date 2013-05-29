2cb9df7868f194dbd5a3412624162e8d03db53d8
Kevin DuBois <kevin.dubois@canonical.com>
2013-05-29 15:50:00 -0700

    add missing file

---
+++ src/server/compositor/buffer_swapper_master.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by:
+ * Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#ifndef MIR_COMPOSITOR_BUFFER_SWAPPER_MASTER_H_
+#define MIR_COMPOSITOR_BUFFER_SWAPPER_MASTER_H_
+
+#include "mir/compositor/buffer_swapper.h"
+
+namespace mir
+{
+namespace compositor
+{
+
+class BufferSwapperMaster : public BufferSwapper
+{
+public:
+    virtual void change_swapper(std::function<std::shared_ptr<BufferSwapper>
+                                         (std::vector<std::shared_ptr<Buffer>>&, size_t&)>) = 0;
+    virtual ~BufferSwapperMaster() {}
+
+protected:
+    BufferSwapperMaster() = default;
+    BufferSwapperMaster(BufferSwapperMaster const&) = delete;
+    BufferSwapperMaster& operator=(BufferSwapperMaster const&) = delete;
+};
+
+}
+}
+
+#endif /* MIR_COMPOSITOR_BUFFER_SWAPPER_H_ */
