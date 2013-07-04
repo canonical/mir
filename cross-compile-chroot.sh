cbf20495d93613de747a6ede6bd2e6531da70f98
Alan Griffiths <alan@octopull.co.uk>
2013-07-04 15:13:57 +0100

    Fix PKG_CONFIG_PATH

--- cross-compile-chroot.sh
+++ cross-compile-chroot.sh
@@ -23,6 +23,8 @@ rm -rf ${BUILD_DIR}
 mkdir ${BUILD_DIR}
 pushd ${BUILD_DIR} > /dev/null 
 
+#    export PKG_CONFIG_PATH=${MIR_NDK_PATH}/usr/lib/arm-linux-gnueabihf/pkgconfig
+    export PKG_CONFIG_PATH="${MIR_NDK_PATH}/usr/lib/pkgconfig:${MIR_NDK_PATH}/usr/lib/arm-linux-gnueabihf/pkgconfig"
     cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/LinuxCrossCompile.cmake \
       -DBoost_COMPILER=-gcc \
       -DMIR_PLATFORM=android \
