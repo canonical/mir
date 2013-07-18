efc9c657f7d7eca96940c982efb10d9a93c57d3e
Alan Griffiths <alan@octopull.co.uk>
2013-07-18 15:08:13 +0100

    Add missing header

--- src/server/graphics/gl_renderer.cpp
+++ src/server/graphics/gl_renderer.cpp
@@ -1,3 +1,20 @@
+/* Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored By: Alexandros Frantzis <alexandros.frantzis@canonical.com>
+ */
+
 #include "mir/graphics/gl_renderer.h"
 #include "mir/graphics/compositing_criteria.h"
 #include "mir/surfaces/buffer_stream.h"
