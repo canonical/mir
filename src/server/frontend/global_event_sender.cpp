da189aebb6cd1c943a00466bbe37eedf3ac14818
Kevin DuBois <kevin.dubois@canonical.com>
2013-08-08 12:47:31 -0700

    missing files

---
+++ src/server/frontend/global_event_sender.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#include "mir/frontend/global_event_sender.h"
+
+namespace mf=mir::frontend;
+namespace mg=mir::graphics;
+
+mf::GlobalEventSender::GlobalEventSender(std::shared_ptr<shell::SessionContainer> const&)
+{
+}
+
+void mf::GlobalEventSender::handle_event(MirEvent const& e)
+{
+    (void) e;
+}
+
+void mf::GlobalEventSender::handle_display_config_change(mg::DisplayConfiguration const& config)
+{
+    (void) config;
+}
