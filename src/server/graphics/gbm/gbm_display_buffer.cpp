b9eeb08aefd1cd8727aecfedef4291fb3c40b163
Daniel van Vugt <daniel.van.vugt@canonical.com>
2013-08-14 11:14:34 +0800

    Remove nouveau debug changes

--- src/server/graphics/gbm/gbm_display_buffer.cpp
+++ src/server/graphics/gbm/gbm_display_buffer.cpp
@@ -290,7 +290,7 @@ bool mgg::GBMDisplayBuffer::schedule_and_wait_for_page_flip(BufferObject* bufobj
     }
 
     if (page_flips_pending == 0)
-        return true;  // FIXME: nouveau needs this
+        return false;
 
     for (auto& output : outputs)
     {
