1622cf382b73fb9cbccaef5ed3d85a62cd6aa228
Alan Griffiths <alan@octopull.co.uk>
2013-08-21 17:41:27 +0100

    Refactoring

--- src/server/graphics/nested/nested_display_configuration.cpp
+++ src/server/graphics/nested/nested_display_configuration.cpp
@@ -18,6 +18,8 @@
 
 #include "nested_display_configuration.h"
 
+#include <algorithm>
+
 namespace mgn = mir::graphics::nested;
 
 mgn::NestedDisplayConfiguration::NestedDisplayConfiguration(MirConnection* connection) :
@@ -36,35 +38,38 @@ void mgn::NestedDisplayConfiguration::for_each_card(std::function<void(DisplayCo
 
 void mgn::NestedDisplayConfiguration::for_each_output(std::function<void(DisplayConfigurationOutput const&)> f) const
 {
-    for (auto egl_display_info = display_config->outputs; egl_display_info != display_config->outputs+display_config->num_outputs; ++egl_display_info)
-    {
-        std::vector<geometry::PixelFormat> formats;
-        formats.reserve(egl_display_info->num_output_formats);
-        for (auto p = egl_display_info->output_formats; p != egl_display_info->output_formats+egl_display_info->num_output_formats; ++p)
-            formats.push_back(geometry::PixelFormat(*p));
+    std::for_each(
+        display_config->outputs,
+        display_config->outputs+display_config->num_outputs,
+        [&f](MirDisplayOutput const& mir_output)
+        {
+            std::vector<geometry::PixelFormat> formats;
+            formats.reserve(mir_output.num_output_formats);
+            for (auto p = mir_output.output_formats; p != mir_output.output_formats+mir_output.num_output_formats; ++p)
+                formats.push_back(geometry::PixelFormat(*p));
 
-        std::vector<DisplayConfigurationMode> modes;
-        modes.reserve(egl_display_info->num_modes);
-        for (auto p = egl_display_info->modes; p != egl_display_info->modes+egl_display_info->num_modes; ++p)
-            modes.push_back(DisplayConfigurationMode{{p->horizontal_resolution, p->vertical_resolution}, p->refresh_rate});
+            std::vector<DisplayConfigurationMode> modes;
+            modes.reserve(mir_output.num_modes);
+            for (auto p = mir_output.modes; p != mir_output.modes+mir_output.num_modes; ++p)
+                modes.push_back(DisplayConfigurationMode{{p->horizontal_resolution, p->vertical_resolution}, p->refresh_rate});
 
-        DisplayConfigurationOutput const output{
-            DisplayConfigurationOutputId(egl_display_info->output_id),
-            DisplayConfigurationCardId(egl_display_info->card_id),
-            DisplayConfigurationOutputType(egl_display_info->type),
-            std::move(formats),
-            std::move(modes),
-            egl_display_info->preferred_mode,
-            geometry::Size{egl_display_info->physical_width_mm, egl_display_info->physical_height_mm},
-            !!egl_display_info->connected,
-            !!egl_display_info->used,
-            geometry::Point{egl_display_info->position_x, egl_display_info->position_y},
-            egl_display_info->current_mode,
-            egl_display_info->current_output_format
-        };
+            DisplayConfigurationOutput const output{
+                DisplayConfigurationOutputId(mir_output.output_id),
+                DisplayConfigurationCardId(mir_output.card_id),
+                DisplayConfigurationOutputType(mir_output.type),
+                std::move(formats),
+                std::move(modes),
+                mir_output.preferred_mode,
+                geometry::Size{mir_output.physical_width_mm, mir_output.physical_height_mm},
+                !!mir_output.connected,
+                !!mir_output.used,
+                geometry::Point{mir_output.position_x, mir_output.position_y},
+                mir_output.current_mode,
+                mir_output.current_output_format
+            };
 
-        f(output);
-    }
+            f(output);
+        });
 }
 
 void mgn::NestedDisplayConfiguration::configure_output(DisplayConfigurationOutputId /*id*/, bool /*used*/, geometry::Point /*top_left*/, size_t /*mode_index*/)
