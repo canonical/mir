d4801aea0060861bccb7bc3779ca748a2a72f8bf
Robert Carr <robert.carr@canonical.com>
2013-09-18 07:18:06 -0700

    Improve ensure display powered logic

--- src/server/shell/mediating_display_changer.cpp
+++ src/server/shell/mediating_display_changer.cpp
@@ -118,16 +118,23 @@ msh::MediatingDisplayChanger::MediatingDisplayChanger(
 
 void msh::MediatingDisplayChanger::ensure_display_powered(std::shared_ptr<mf::Session> const& session)
 {
+    bool switched = false;
+
     auto conf = active_configuration();
     conf->for_each_output([&](mg::DisplayConfigurationOutput const& output) -> void
     {
         if (!output.used) return;
-        conf->configure_output(output.id, output.used,
-                               output.top_left, 
-                               output.current_mode_index,
-                               mir_power_mode_on);
+        
+        if (output.power_mode != mir_power_mode_on)
+        {
+            switched = true;
+            conf->configure_output(output.id, output.used,
+                                   output.top_left, 
+                                   output.current_mode_index,
+                                   mir_power_mode_on);
+        }
     });
-    if (conf != active_configuration())
+    if (switched)
         configure(session, conf);
 }
 
