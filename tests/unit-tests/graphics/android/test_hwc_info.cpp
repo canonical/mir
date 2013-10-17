95877756a1f6925ce9dcfeea197595d6610c6c9d
Kevin DuBois <kevin.dubois@canonical.com>
2013-10-17 15:21:16 -0700

    split out the if0ed HWCInfo tests into their own test file

---
+++ tests/unit-tests/graphics/android/test_hwc_info.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#include "src/server/graphics/android/hwc11_device.h"
+#include "mir_test_doubles/mock_hwc_composer_device_1.h"
+#include <gtest/gtest.h>
+#include <stdexcept>
+
+namespace mg=mir::graphics;
+namespace mga=mir::graphics::android;
+namespace mtd=mir::test::doubles;
+
+class HWCInfoTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp()
+    {
+        mock_device = std::make_shared<testing::NiceMock<mtd::MockHWCComposerDevice1>>();
+    }
+
+    std::shared_ptr<mtd::MockHWCComposerDevice1> mock_device;
+};
+
+TEST_F(HWCInfoTest, test_hwc_device_display_config)
+{
+    using namespace testing;
+
+    unsigned int hwc_configs = 0xA1;
+    EXPECT_CALL(*mock_device, getDisplayConfigs_interface(mock_device.get(),HWC_DISPLAY_PRIMARY,_,Pointee(1)))
+        .Times(1)
+        .WillOnce(DoAll(SetArgPointee<2>(hwc_configs), Return(0)));
+
+    mga::HWCInfo device(mock_device);
+}
+
+//apparently this can happen if the display is in the 'unplugged state'
+TEST_F(HWCInfoTest, test_hwc_device_display_config_failure_throws)
+{
+    using namespace testing;
+
+    EXPECT_CALL(*mock_device, getDisplayConfigs_interface(mock_device.get(),HWC_DISPLAY_PRIMARY,_,_))
+        .Times(1)
+        .WillOnce(Return(-1));
+
+    EXPECT_THROW({
+        mga::HWCInfo device(mock_device);
+    }, std::runtime_error);
+}
+
+namespace
+{
+static int const display_width = 180;
+static int const display_height = 1010101;
+
+static int display_attribute_handler(struct hwc_composer_device_1*, int, uint32_t,
+                                     const uint32_t* attribute_list, int32_t* values)
+{
+    EXPECT_EQ(attribute_list[0], HWC_DISPLAY_WIDTH);
+    EXPECT_EQ(attribute_list[1], HWC_DISPLAY_HEIGHT);
+    EXPECT_EQ(attribute_list[2], HWC_DISPLAY_NO_ATTRIBUTE);
+
+    values[0] = display_width;
+    values[1] = display_height;
+    return 0;
+}
+}
+
+TEST_F(HWCInfoTest, test_hwc_device_display_width_height)
+{
+    using namespace testing;
+
+    int hwc_configs = 0xA1;
+    EXPECT_CALL(*mock_device, getDisplayConfigs_interface(mock_device.get(),HWC_DISPLAY_PRIMARY,_,_))
+        .Times(1)
+        .WillOnce(DoAll(SetArgPointee<2>(hwc_configs), Return(0)));
+
+    mga::HWCInfo device(mock_device);
+ 
+    EXPECT_CALL(*mock_device, getDisplayAttributes_interface(mock_device.get(), HWC_DISPLAY_PRIMARY,hwc_configs,_,_))
+        .Times(1)
+        .WillOnce(Invoke(display_attribute_handler));
+
+    auto size = device.display_size();
+    EXPECT_EQ(size.width.as_uint32_t(),  static_cast<unsigned int>(display_width));
+    EXPECT_EQ(size.height.as_uint32_t(), static_cast<unsigned int>(display_height));
+}
