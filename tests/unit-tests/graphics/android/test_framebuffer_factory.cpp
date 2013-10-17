b4e3f92d4217a917378d4b2db0625c4c7bd1ad38
Kevin DuBois <kevin.dubois@canonical.com>
2013-10-17 16:42:15 -0700

    one more test!

--- tests/unit-tests/graphics/android/test_framebuffer_factory.cpp
+++ tests/unit-tests/graphics/android/test_framebuffer_factory.cpp
@@ -48,7 +48,7 @@ public:
 };
 }
 
-class FBFactory  : public ::testing::Test
+class ResourceFactoryTest  : public ::testing::Test
 {
 public:
     void SetUp()
@@ -77,20 +77,116 @@ public:
     mtd::HardwareAccessMock hw_access_mock;
 };
 
+TEST_F(ResourceFactoryTest, test_native_window_creation_figures_out_fb_number)
+{
+    using namespace testing; 
+    EXPECT_CALL(*mock_display_info, number_of_framebuffers_available())
+        .Times(1)
+        .WillOnce(Return(fake_fb_num));
+    EXPECT_CALL(*mock_buffer_allocator, alloc_buffer_platform(_,_,_))
+        .Times(fake_fb_num);
+
+    mga::ResourceFactory factory(mock_buffer_allocator);
+    factory.create_fb_buffers(mock_display_info);
+}
 #if 0
-TEST_F(FBFactory, test_native_window_creation_figures_out_fb_number)
+TEST_F(ResourceFactoryTest, test_native_window_creation_uses_size)
 {
     using namespace testing;
 
     mga::ResourceFactory factory(mock_buffer_allocator);
+
+    geom::Width disp_width{44};
+    geom::Height disp_height{4567654};
+    geom::Size disp_size{disp_width, disp_height};   
  
-    EXPECT_CALL(*mock_display_info, number_of_framebuffers_available())
-        .Times(1);
-    EXPECT_CALL(*mock_buffer_allocator, alloc_buffer_platform(_,_,_))
+    EXPECT_CALL(*mock_display_info_provider, display_size())
+        .Times(1)
+        .WillOnce(Return(disp_size));
+    EXPECT_CALL(*mock_buffer_allocator, alloc_buffer_platform(disp_size,_,_))
         .Times(fake_fb_num);
  
-    factory.create_display(mock_display_info, mock_report);
+    factory.create_display(mock_display_info_provider, mock_report);
+} 
+
+TEST_F(ResourceFactoryTest, test_native_window_creation_specifies_buffer_type)
+{
+    using namespace testing;
+
+    mga::ResourceFactory factory(mock_buffer_allocator);
+
+    EXPECT_CALL(*mock_buffer_allocator, alloc_buffer_platform(_,_,mga::BufferUsage::use_framebuffer_gles))
+        .Times(fake_fb_num);
+ 
+    factory.create_display(mock_display_info_provider, mock_report);
+} 
+
+//note: @kdub imo, the hwc api has a hole in it that it doesn't allow query for format. surfaceflinger code
+//            makes note of this api hole in its comments too. It always uses rgba8888, which we will do too.
+TEST_F(ResourceFactoryTest, test_native_window_creation_uses_rgba8888)
+{
+    using namespace testing;
+
+    mga::ResourceFactory factory(mock_buffer_allocator);
+    geom::PixelFormat pf = geom::PixelFormat::abgr_8888; 
+ 
+    EXPECT_CALL(*mock_display_info_provider, display_format())
+        .Times(AtLeast(1))
+        .WillRepeatedly(Return(pf));
+    EXPECT_CALL(*mock_buffer_allocator, alloc_buffer_platform(_,pf,_))
+        .Times(fake_fb_num);
+ 
+    factory.create_display(mock_display_info_provider, mock_report);
 }
+
+TEST_F(ResourceFactoryTest, test_device_creation_accesses_gralloc)
+{
+    using namespace testing;
+    EXPECT_CALL(hw_access_mock, hw_get_module(StrEq(GRALLOC_HARDWARE_MODULE_ID), _))
+        .Times(1);
+
+    mga::ResourceFactory factory(mock_buffer_allocator);
+    factory.create_fb_device();
+}
+
+TEST_F(ResourceFactoryTest, test_device_creation_throws_on_failure)
+{
+    using namespace testing;
+    mga::ResourceFactory factory(mock_buffer_allocator);
+
+    /* failure because of rc */
+    EXPECT_CALL(hw_access_mock, hw_get_module(StrEq(GRALLOC_HARDWARE_MODULE_ID), _))
+        .Times(1)
+        .WillOnce(Return(-1));
+
+    EXPECT_THROW({
+        factory.create_fb_device();
+    }, std::runtime_error);
+
+    /* failure because of nullptr returned */
+    EXPECT_CALL(hw_access_mock, hw_get_module(StrEq(GRALLOC_HARDWARE_MODULE_ID), _))
+        .Times(1)
+        .WillOnce(DoAll(SetArgPointee<1>(nullptr),Return(-1)));
+
+    EXPECT_THROW({
+        factory.create_fb_device();
+    }, std::runtime_error);
+
+}
+
+TEST_F(ResourceFactoryTest, test_device_creation_resource_has_fb_close_on_destruct)
+{
+    using namespace testing;
+    EXPECT_CALL(hw_access_mock, hw_get_module(StrEq(GRALLOC_HARDWARE_MODULE_ID), _))
+        .Times(1);
+
+    mga::ResourceFactory factory(mock_buffer_allocator);
+    factory.create_fb_device();
+
+    EXPECT_TRUE(hw_access_mock.open_count_matches_close());
+}
+#endif
+#if 0
 
 #include "src/server/graphics/android/display_support_provider.h"
 #include "src/server/graphics/android/android_display_factory.h"
@@ -332,100 +428,3 @@ TEST_F(AndroidDisplayFactoryTest, hwc_with_hwc_device_failure_because_hwc_versio
 }
 #endif
 
-#if 0
-TEST_F(FBFactory, test_native_window_creation_uses_size)
-{
-    using namespace testing;
-
-    mga::ResourceFactory factory(mock_buffer_allocator);
-
-    geom::Width disp_width{44};
-    geom::Height disp_height{4567654};
-    geom::Size disp_size{disp_width, disp_height};   
- 
-    EXPECT_CALL(*mock_display_info_provider, display_size())
-        .Times(1)
-        .WillOnce(Return(disp_size));
-    EXPECT_CALL(*mock_buffer_allocator, alloc_buffer_platform(disp_size,_,_))
-        .Times(fake_fb_num);
- 
-    factory.create_display(mock_display_info_provider, mock_report);
-} 
-
-TEST_F(FBFactory, test_native_window_creation_specifies_buffer_type)
-{
-    using namespace testing;
-
-    mga::ResourceFactory factory(mock_buffer_allocator);
-
-    EXPECT_CALL(*mock_buffer_allocator, alloc_buffer_platform(_,_,mga::BufferUsage::use_framebuffer_gles))
-        .Times(fake_fb_num);
- 
-    factory.create_display(mock_display_info_provider, mock_report);
-} 
-
-//note: @kdub imo, the hwc api has a hole in it that it doesn't allow query for format. surfaceflinger code
-//            makes note of this api hole in its comments too. It always uses rgba8888, which we will do too.
-TEST_F(FBFactory, test_native_window_creation_uses_rgba8888)
-{
-    using namespace testing;
-
-    mga::ResourceFactory factory(mock_buffer_allocator);
-    geom::PixelFormat pf = geom::PixelFormat::abgr_8888; 
- 
-    EXPECT_CALL(*mock_display_info_provider, display_format())
-        .Times(AtLeast(1))
-        .WillRepeatedly(Return(pf));
-    EXPECT_CALL(*mock_buffer_allocator, alloc_buffer_platform(_,pf,_))
-        .Times(fake_fb_num);
- 
-    factory.create_display(mock_display_info_provider, mock_report);
-}
-
-TEST_F(FBFactory, test_device_creation_accesses_gralloc)
-{
-    using namespace testing;
-    EXPECT_CALL(hw_access_mock, hw_get_module(StrEq(GRALLOC_HARDWARE_MODULE_ID), _))
-        .Times(1);
-
-    mga::ResourceFactory factory(mock_buffer_allocator);
-    factory.create_fb_device();
-}
-
-TEST_F(FBFactory, test_device_creation_throws_on_failure)
-{
-    using namespace testing;
-    mga::ResourceFactory factory(mock_buffer_allocator);
-
-    /* failure because of rc */
-    EXPECT_CALL(hw_access_mock, hw_get_module(StrEq(GRALLOC_HARDWARE_MODULE_ID), _))
-        .Times(1)
-        .WillOnce(Return(-1));
-
-    EXPECT_THROW({
-        factory.create_fb_device();
-    }, std::runtime_error);
-
-    /* failure because of nullptr returned */
-    EXPECT_CALL(hw_access_mock, hw_get_module(StrEq(GRALLOC_HARDWARE_MODULE_ID), _))
-        .Times(1)
-        .WillOnce(DoAll(SetArgPointee<1>(nullptr),Return(-1)));
-
-    EXPECT_THROW({
-        factory.create_fb_device();
-    }, std::runtime_error);
-
-}
-
-TEST_F(FBFactory, test_device_creation_resource_has_fb_close_on_destruct)
-{
-    using namespace testing;
-    EXPECT_CALL(hw_access_mock, hw_get_module(StrEq(GRALLOC_HARDWARE_MODULE_ID), _))
-        .Times(1);
-
-    mga::ResourceFactory factory(mock_buffer_allocator);
-    factory.create_fb_device();
-
-    EXPECT_TRUE(hw_access_mock.open_count_matches_close());
-}
-#endif
