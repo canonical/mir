f5ab9f01174f7bee734489de4ae470086cf68076
Alan Griffiths <alan@octopull.co.uk>
2013-11-15 16:33:02 +0000

    Move files bzr lost this time around

--- src/server/surfaces/application_session.h
+++ src/server/surfaces/application_session.h
@@ -16,8 +16,8 @@
  * Authored By: Robert Carr <racarr@canonical.com>
  */
 
-#ifndef MIR_SHELL_APPLICATION_SESSION_H_
-#define MIR_SHELL_APPLICATION_SESSION_H_
+#ifndef MIR_SURFACES_APPLICATION_SESSION_H_
+#define MIR_SURFACES_APPLICATION_SESSION_H_
 
 #include "mir/shell/session.h"
 
@@ -33,20 +33,21 @@ namespace shell
 {
 class SurfaceFactory;
 class Surface;
-class SnapshotStrategy;
 class SessionListener;
+}
 
-// TODO make private to shell
-// This first needs unity-mir and unit-system_compositor to be updated to use Session
-// and that first needs -c 1167 of development-branch to land on lp:mir
-class ApplicationSession : public Session
+namespace surfaces
+{
+class SnapshotStrategy;
+
+class ApplicationSession : public shell::Session
 {
 public:
     ApplicationSession(
-        std::shared_ptr<SurfaceFactory> const& surface_factory,
+        std::shared_ptr<shell::SurfaceFactory> const& surface_factory,
         std::string const& session_name,
         std::shared_ptr<SnapshotStrategy> const& snapshot_strategy,
-        std::shared_ptr<SessionListener> const& session_listener,
+        std::shared_ptr<shell::SessionListener> const& session_listener,
         std::shared_ptr<frontend::EventSink> const& sink);
 
     ~ApplicationSession();
@@ -55,8 +56,8 @@ public:
     void destroy_surface(frontend::SurfaceId surface);
     std::shared_ptr<frontend::Surface> get_surface(frontend::SurfaceId surface) const;
 
-    void take_snapshot(SnapshotCallback const& snapshot_taken);
-    std::shared_ptr<Surface> default_surface() const;
+    void take_snapshot(shell::SnapshotCallback const& snapshot_taken);
+    std::shared_ptr<shell::Surface> default_surface() const;
 
     std::string name() const;
 
@@ -75,17 +76,17 @@ protected:
     ApplicationSession& operator=(ApplicationSession const&) = delete;
 
 private:
-    std::shared_ptr<SurfaceFactory> const surface_factory;
+    std::shared_ptr<shell::SurfaceFactory> const surface_factory;
     std::string const session_name;
     std::shared_ptr<SnapshotStrategy> const snapshot_strategy;
-    std::shared_ptr<SessionListener> const session_listener;
+    std::shared_ptr<shell::SessionListener> const session_listener;
     std::shared_ptr<frontend::EventSink> const event_sink;
 
     frontend::SurfaceId next_id();
 
     std::atomic<int> next_surface_id;
 
-    typedef std::map<frontend::SurfaceId, std::shared_ptr<Surface>> Surfaces;
+    typedef std::map<frontend::SurfaceId, std::shared_ptr<shell::Surface>> Surfaces;
     Surfaces::const_iterator checked_find(frontend::SurfaceId id) const;
     std::mutex mutable surfaces_mutex;
     Surfaces surfaces;
@@ -94,4 +95,4 @@ private:
 }
 } // namespace mir
 
-#endif // MIR_SHELL_APPLICATION_SESSION_H_
+#endif // MIR_SURFACES_APPLICATION_SESSION_H_
