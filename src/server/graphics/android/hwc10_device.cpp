212e6d8cc558225a981ec483918feb4af2ca6ea6
Kevin DuBois <kevin.dubois@canonical.com>
2013-11-19 09:28:57 -0800

    improve comment noting how hwc 1.0 set is differnt from the other versions set

--- src/server/graphics/android/hwc10_device.cpp
+++ src/server/graphics/android/hwc10_device.cpp
@@ -71,7 +71,8 @@ void mga::HWC10Device::commit_frame(EGLDisplay dpy, EGLSurface sur)
         BOOST_THROW_EXCEPTION(std::runtime_error("error during hwc prepare()"));
     }
 
-    //with hwc 1.0 only, set() goes and calls eglSwapBuffers
+    //set() may affect EGL state by calling eglSwapBuffers.
+    //HWC 1.0 is the only version of HWC that can do this.
     rc = hwc_device->set(hwc_device.get(), 1, &display_list);
     if (rc != 0)
     {
