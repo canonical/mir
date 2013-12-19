f4d65bdd0c2f82f6c935dd0cc287a7460be43ce2
Alan Griffiths <alan@octopull.co.uk>
2013-12-19 15:37:25 +0000

    POC code to demonstrate that the cross-process sync hooks break InProcessServer

--- tests/acceptance-tests/test_test_framework.cpp
+++ tests/acceptance-tests/test_test_framework.cpp
@@ -69,11 +69,20 @@ TEST_F(DefaultDisplayServerTestFixture, demonstrate_multiple_clients)
 
 namespace
 {
+struct InProcessTestingServerConfiguration : TestingServerConfiguration
+{
+    // Disable the TestingServerConfiguration listener that tries to sync across processes
+    std::shared_ptr<mir::ServerStatusListener> the_server_status_listener() override
+    {
+        return mir::DefaultServerConfiguration::the_server_status_listener();
+    }
+};
+
 struct DemoInProcessServer : mir_test_framework::InProcessServer
 {
     virtual mir::DefaultServerConfiguration& server_config() { return server_config_; }
 
-    TestingServerConfiguration server_config_;
+    InProcessTestingServerConfiguration server_config_;
 };
 }
 
