aa8a92958fd174c1bdb4bd67b978f7bdefbb2972
Daniel van Vugt <daniel.van.vugt@canonical.com>
2013-12-23 15:05:31 +0800

    Add frame time metrics too

--- src/server/logging/compositor_report.cpp
+++ src/server/logging/compositor_report.cpp
@@ -53,33 +53,43 @@ void logging::CompositorReport::end_frame(Id id)
     auto& inst = instance[id];
 
     auto t = now();
-    inst.total_time_sum +=
-        std::chrono::duration_cast<std::chrono::microseconds>(
+    auto interval = std::chrono::duration_cast<std::chrono::microseconds>(
             t - inst.end_of_frame).count();
-    inst.frame_time_sum +=
-        std::chrono::duration_cast<std::chrono::microseconds>(
+    auto frame_time = std::chrono::duration_cast<std::chrono::microseconds>(
             t - inst.start_of_frame).count();
+
+    inst.total_time_sum += interval;
+    inst.frame_time_sum += frame_time;
     inst.end_of_frame = t;
     inst.nframes++;
 
+    /*
+     * The exact reporting interval doesn't matter because we count everything
+     * as a Reimann sum. Results will simply be the average over the interval.
+     */
     if ((t - last_report) >= min_report_interval)
     {
         last_report = t;
 
-        int n = 0;
         for (auto& ip : instance)
         {
             auto &i = ip.second;
 
             if (i.last_reported_total_time_sum)
             {
-                long fps1000 =
-                    ((i.nframes - i.last_reported_nframes) * 1000000000LL) /
-                    (i.total_time_sum - i.last_reported_total_time_sum);
+                auto dt = i.total_time_sum - i.last_reported_total_time_sum;
+                auto dn = i.nframes - i.last_reported_nframes;
+                auto df = i.frame_time_sum - i.last_reported_frame_time_sum;
+
+                long fps1000 = dn * 1000000000LL / dt;
+                long frametime1000 = df / dn;
 
                 char msg[128];
-                snprintf(msg, sizeof msg, "[%d] %ld.%03ld FPS",
-                         n, fps1000 / 1000, fps1000 % 1000);
+                snprintf(msg, sizeof msg, "[%p] averaged %ld.%03ld FPS, "
+                         "%ld.%03ld ms/frame",
+                         ip.first, fps1000 / 1000, fps1000 % 1000,
+                         frametime1000 / 1000, frametime1000 % 1000
+                         );
 
                 logger->log(Logger::informational, msg, component);
             }
@@ -87,7 +97,6 @@ void logging::CompositorReport::end_frame(Id id)
             i.last_reported_total_time_sum = i.total_time_sum;
             i.last_reported_frame_time_sum = i.frame_time_sum;
             i.last_reported_nframes = i.nframes;
-            n++;
         }
     }
 }
