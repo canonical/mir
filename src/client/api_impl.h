815a018bfc8f1c4a4d4cfd7468c31a0b5ecad1e2
Alexandros Frantzis <alexandros.frantzis@canonical.com>
2014-01-28 20:21:14 +0200

    tests: Provide a helper for running clients with a stub ClientPlatform

---
+++ src/client/api_impl.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2014 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Alexandros Frantzis <alexandros.frantzis@canonical.com>
+ */
+
+#ifndef MIR_CLIENT_API_IMPL_H_
+#define MIR_CLIENT_API_IMPL_H_
+
+#include "api_impl_types.h"
+
+extern mir_connect_impl_func mir_connect_impl;
+extern mir_connection_release_impl_func mir_connection_release_impl;
+
+#endif /* MIR_CLIENT_API_IMPL_H_ */
