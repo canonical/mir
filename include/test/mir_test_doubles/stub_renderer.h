96dbe6c424fa14eeeea38858168cf86855640fa7
Daniel van Vugt <daniel.van.vugt@canonical.com>
2014-02-14 16:23:29 +0800

    Fix conflict from previous merge

--- include/test/mir_test_doubles/stub_renderer.h
+++ include/test/mir_test_doubles/stub_renderer.h
@@ -19,7 +19,7 @@
 #ifndef MIR_TEST_DOUBLES_STUB_RENDERER_H_
 #define MIR_TEST_DOUBLES_STUB_RENDERER_H_
 
-#include "src/server/compositor/renderer.h"
+#include "mir/compositor/renderer.h"
 
 namespace mir
 {
