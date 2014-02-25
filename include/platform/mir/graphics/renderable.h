3aa12fb3a737f023afc6ade73f60fbf2993384b7
Daniel van Vugt <daniel.van.vugt@canonical.com>
2014-02-25 16:07:33 +0800

    Add detailed documentation for buffer(unsigned long frameno)

--- include/platform/mir/graphics/renderable.h
+++ include/platform/mir/graphics/renderable.h
@@ -32,7 +32,23 @@ class Buffer;
 class Renderable
 {
 public:
+    /**
+     * Return the next buffer that should be composited/rendered.
+     *
+     * \param [in] frameno The frameno parameter is important for
+     *                     multi-monitor platforms. Calls with the same frameno
+     *                     will get the same buffer returned. This ensures that
+     *                     a surface visible on multiple outputs does not get
+     *                     its buffers consumed any faster than the refresh
+     *                     rate of a single monitor. Implementations may ignore
+     *                     the value of frameno on single-monitor platforms
+     *                     only. The caller should always ensure the frameno
+     *                     is different to the previous frame. The exact value
+     *                     of frameno is not important but a large range of
+     *                     values is recommended.
+     */
     virtual std::shared_ptr<Buffer> buffer(unsigned long frameno) const = 0;
+
     virtual bool alpha_enabled() const = 0;
     virtual geometry::Rectangle screen_position() const = 0;
 
