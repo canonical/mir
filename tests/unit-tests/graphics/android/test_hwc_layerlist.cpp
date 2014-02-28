97b236c32ffa70d7b83b905713549448e2c93c54
Kevin DuBois <kevin.dubois@canonical.com>
2014-02-28 09:16:39 -0800

    improve layerlist test to drive the layerlist giving out iterators

--- tests/unit-tests/graphics/android/test_hwc_layerlist.cpp
+++ tests/unit-tests/graphics/android/test_hwc_layerlist.cpp
@@ -16,26 +16,66 @@
  * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
  */
 
+#include "mir_test_doubles/stub_renderable.h"
 #include "src/platform/graphics/android/hwc_layerlist.h"
 #include "hwc_struct_helpers.h"
 #include <gtest/gtest.h>
 
 namespace mga=mir::graphics::android;
 
-TEST(LayerListTest, list_defaults)
+namespace
 {
-    mga::LayerList layerlist;
+struct LayerListTest
+{
+    LayerListTest()
+        : renderables{std::make_shared<mtd::StubRenderable>(),
+                      std::make_shared<mtd::StubRenderable>(),
+                      std::make_shared<mtd::StubRenderable>()}
+    {}
+
+    std::list<std::shared_ptr<mg::Renderable>> renderables;
+};
+}
+
+TEST_F(LayerListTest, list_defaults)
+{
+    mga::LayerList layerlist{0};
 
     auto list = layerlist.native_list().lock();
     EXPECT_EQ(-1, list->retireFenceFd);
     EXPECT_EQ(HWC_GEOMETRY_CHANGED, list->flags);
     EXPECT_NE(nullptr, list->dpy);
     EXPECT_NE(nullptr, list->sur);
+    EXPECT_EQ(layerlist.begin(), layerlist.end());
+}
+
+TEST_F(LayerListTest, list_defaults)
+{
+    mga::LayerList layerlist{0};
+    size_t additional_layers = 2;
+    auto changed = update_list_and_check_if_changed(renderables, additional_layers);
+
+    auto additional_ct = 0;
+    auto composed_ct = 0; 
+    auto in_additional_layers = false;
+    for(auto it = layerlist.begin(); it != layerlist.end(); it++)
+    {
+        if (it == layerlist.last_composition_layer())
+            in_additional_layers = true;
+
+        if (in_additional_layers)
+            additional_ct++;
+        else
+            composed_ct++;
+    }
+
+    EXPECT_EQ(composed_ct, renderables.size());
+    EXPECT_EQ(addional_ct, additional_layers);
 }
 
 /* Tests without HWC_FRAMEBUFFER_TARGET */
 /* an empty list should have a skipped layer. This will force a GL render on hwc set */
-TEST(LayerListTest, hwc10_list_defaults)
+TEST_F(LayerListTest, hwc10_list_defaults)
 {
     mga::LayerList layerlist;
 
