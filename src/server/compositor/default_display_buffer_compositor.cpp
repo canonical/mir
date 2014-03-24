6a5b15c0b2d6796d213e0c24d132b436e24da307
Kevin DuBois <kevin.dubois@canonical.com>
2014-03-24 13:14:12 -0700

    remove production code that checks bypass the old way, see test failure

--- src/server/compositor/default_display_buffer_compositor.cpp
+++ src/server/compositor/default_display_buffer_compositor.cpp
@@ -120,14 +120,14 @@ bool mc::DefaultDisplayBufferCompositor::composite()
     {
         display_buffer.make_current();
 
-        //scene.generate_renderables_list()
+//        auto renderable_list = scene.generate_renderables_list()
         //filter_occlusions(list, area);
         //FilterForVisibleSceneInRegion selector(occluded_surfaces);
 
-        auto const& view_area = display_buffer.view_area();
-        mc::OcclusionFilter occlusion_search(view_area);
-        mc::OcclusionMatch occlusion_match;
-        scene->reverse_for_each_if(occlusion_search, occlusion_match);
+ //       auto const& view_area = display_buffer.view_area();
+ //       mc::OcclusionFilter occlusion_search(view_area);
+       mc::OcclusionMatch occlusion_match;
+ //       scene->reverse_for_each_if(occlusion_search, occlusion_match);
 
         renderer->set_rotation(display_buffer.orientation());
         renderer->begin();
