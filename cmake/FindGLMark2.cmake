83404186e95a9ac47344c120e8f0cc7eef9e7cfc
Josh Arenson <joshua.arenson@canonical.com>
2014-04-14 12:52:24 -0700

    Added CMake functionality to detect if GLMark2 and, specifically, glmark2-es2-mir is found. This information can be used for future testing.

---
+++ cmake/FindGLMark2.cmake
@@ -0,0 +1,7 @@
+# - Try to find GLMakrk2
+# Once done this will define
+#  GLMARK2_FOUND - System has GLMark2
+#  GLMARK2_ES2_MIR - System has glmark2-es2-mir
+
+find_program(GLMARK2_FOUND glmark2)
+find_program(GLMARK2_ES2_MIR glmark2-es2-mir)
