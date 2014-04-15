71a8d58451d78a5e2690de45572cda620c63f942
Kevin DuBois <kevin.dubois@canonical.com>
2014-04-14 17:02:46 -0700

    add a test that catches the lp:1286190 frig

--- tests/integration-tests/test_surface_stack_with_compositor.cpp
+++ tests/integration-tests/test_surface_stack_with_compositor.cpp
@@ -16,32 +16,23 @@
  * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
  */
 
-#include "mir_test_doubles/stub_input_registrar.h"
+#include "mir/scene/surface_creation_parameters.h"
 #include "src/server/report/null_report_factory.h"
 #include "src/server/scene/surface_stack.h"
 #include "src/server/compositor/gl_renderer_factory.h"
-#include "mir/shell/surface_creation_parameters.h"
+#include "src/server/scene/basic_surface.h"
 #include "src/server/compositor/default_display_buffer_compositor_factory.h"
 #include "src/server/compositor/multi_threaded_compositor.h"
+#include "mir_test/fake_shared.h"
+#include "mir_test_doubles/mock_buffer_stream.h"
 #include "mir_test_doubles/null_display.h"
+#include "mir_test_doubles/stub_input_registrar.h"
 #include "mir_test_doubles/stub_renderer.h"
 #include "mir_test_doubles/stub_display_buffer.h"
 #include "mir_test_doubles/stub_buffer.h"
-#include "mir_test/fake_shared.h"
 
-
-#include "mir_test_doubles/mock_buffer_stream.h"
-#include "src/server/scene/basic_surface.h"
-//#include "mir/input/input_channel_factory.h"
-//#include "mir_test_doubles/stub_input_registrar.h"
-//#include "mir_test_doubles/stub_input_channel.h"
-//#include "mir_test_doubles/mock_input_registrar.h"
-
-
-
-#include <mutex>
 #include <condition_variable>
-
+#include <mutex>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -52,7 +43,6 @@ namespace mr = mir::report;
 namespace mc = mir::compositor;
 namespace mg = mir::graphics;
 namespace geom = mir::geometry;
-namespace msh = mir::shell;
 
 namespace
 {
@@ -67,9 +57,8 @@ public:
 
 struct CountingDisplayBuffer : public mtd::StubDisplayBuffer
 {
-    CountingDisplayBuffer(unsigned int expected_count)
-     : StubDisplayBuffer({{0,0}, {10, 10}}),
-       expected(expected_count)
+    CountingDisplayBuffer()
+     : StubDisplayBuffer({{0,0}, {10, 10}})
     {
     }
 
@@ -90,13 +79,12 @@ struct CountingDisplayBuffer : public mtd::StubDisplayBuffer
         increment_post_count();
     }
 
-    bool reaches_expected_count_before(std::chrono::system_clock::time_point& timeout)
+    bool has_posted_at_least(unsigned int count, std::chrono::system_clock::time_point& timeout)
     {
         std::unique_lock<decltype(mutex)> lk(mutex);
-
         return count_cv.wait_until(lk, timeout,
-        [this](){
-            return post_count_ == expected;  
+        [this, &count](){
+            return post_count_ >= count;  
         });
     }
 
@@ -104,14 +92,12 @@ private:
     void increment_post_count()
     {
         std::unique_lock<decltype(mutex)> lk(mutex);
-        if (++post_count_ > expected)
-            throw std::logic_error("post was called more times than expected.");;
+        ++post_count_;
         count_cv.notify_all();
     }
+
     std::mutex mutex;
     std::condition_variable count_cv;
-
-    unsigned int const expected;
     unsigned int post_count_{0};
 };
 
@@ -132,10 +118,10 @@ private:
     mg::DisplayBuffer& secondary;
 };
 
-struct SurfaceStackCompositorInteractions : public testing::Test
+struct SurfaceStackCompositor : public testing::Test
 {
-    SurfaceStackCompositorInteractions()
-        : timeout{std::chrono::system_clock::now() + std::chrono::seconds(1)},
+    SurfaceStackCompositor()
+        : timeout{std::chrono::system_clock::now() + std::chrono::seconds(5)},
           mock_buffer_stream(std::make_shared<testing::NiceMock<mtd::MockBufferStream>>()),
           stub_surface{std::make_shared<ms::BasicSurface>(
             std::string("stub"),
@@ -158,16 +144,15 @@ struct SurfaceStackCompositorInteractions : public testing::Test
     std::chrono::system_clock::time_point timeout;
     std::shared_ptr<mtd::MockBufferStream> mock_buffer_stream;
     std::shared_ptr<ms::BasicSurface> stub_surface; 
-    msh::SurfaceCreationParameters default_params;
+    ms::SurfaceCreationParameters default_params;
     mtd::StubBuffer stubbuf;
 };
 }
 
-TEST_F(SurfaceStackCompositorInteractions, composes_on_start_if_told_to_in_constructor)
+TEST_F(SurfaceStackCompositor, composes_on_start_if_told_to_in_constructor)
 {
-    unsigned int const expected_post_count{1};
-    CountingDisplayBuffer stub_primary_db{expected_post_count};
-    CountingDisplayBuffer stub_secondary_db{expected_post_count};
+    CountingDisplayBuffer stub_primary_db;
+    CountingDisplayBuffer stub_secondary_db;
     StubDisplay stub_display{stub_primary_db, stub_secondary_db};
     ms::SurfaceStack stack(mt::fake_shared(stub_input_registrar), null_scene_report);
     mc::DefaultDisplayBufferCompositorFactory dbc_factory{
@@ -182,15 +167,14 @@ TEST_F(SurfaceStackCompositorInteractions, composes_on_start_if_told_to_in_const
         null_comp_report, true);
     mt_compositor.start();
 
-    EXPECT_TRUE(stub_primary_db.reaches_expected_count_before(timeout));
-    EXPECT_TRUE(stub_secondary_db.reaches_expected_count_before(timeout));
+    EXPECT_TRUE(stub_primary_db.has_posted_at_least(1, timeout));
+    EXPECT_TRUE(stub_secondary_db.has_posted_at_least(1, timeout));
 }
 
-TEST_F(SurfaceStackCompositorInteractions, does_not_composes_on_start_if_told_not_to_in_constructor)
+TEST_F(SurfaceStackCompositor, does_not_composes_on_start_if_told_not_to_in_constructor)
 {
-    unsigned int const expected_post_count{0};
-    CountingDisplayBuffer stub_primary_db{expected_post_count};
-    CountingDisplayBuffer stub_secondary_db{expected_post_count};
+    CountingDisplayBuffer stub_primary_db;
+    CountingDisplayBuffer stub_secondary_db;
     StubDisplay stub_display{stub_primary_db, stub_secondary_db};
     ms::SurfaceStack stack(mt::fake_shared(stub_input_registrar), null_scene_report);
     mc::DefaultDisplayBufferCompositorFactory dbc_factory{
@@ -205,15 +189,14 @@ TEST_F(SurfaceStackCompositorInteractions, does_not_composes_on_start_if_told_no
         null_comp_report, false);
     mt_compositor.start();
 
-    EXPECT_TRUE(stub_primary_db.reaches_expected_count_before(timeout));
-    EXPECT_TRUE(stub_secondary_db.reaches_expected_count_before(timeout));
+    EXPECT_TRUE(stub_primary_db.has_posted_at_least(0, timeout));
+    EXPECT_TRUE(stub_secondary_db.has_posted_at_least(0, timeout));
 }
 
-TEST_F(SurfaceStackCompositorInteractions, adding_a_surface_that_has_been_swapped_triggers_a_composition)
+TEST_F(SurfaceStackCompositor, adding_a_surface_that_has_been_swapped_triggers_a_composition)
 {
-    unsigned int const expected_post_count{1};
-    CountingDisplayBuffer stub_primary_db{expected_post_count};
-    CountingDisplayBuffer stub_secondary_db{expected_post_count};
+    CountingDisplayBuffer stub_primary_db;
+    CountingDisplayBuffer stub_secondary_db;
     StubDisplay stub_display{stub_primary_db, stub_secondary_db};
     ms::SurfaceStack stack(mt::fake_shared(stub_input_registrar), null_scene_report);
     mc::DefaultDisplayBufferCompositorFactory dbc_factory{
@@ -231,8 +214,8 @@ TEST_F(SurfaceStackCompositorInteractions, adding_a_surface_that_has_been_swappe
     stub_surface->swap_buffers(&stubbuf, [](mg::Buffer*){});
     stack.add_surface(stub_surface, default_params.depth, default_params.input_mode);
 
-    EXPECT_TRUE(stub_primary_db.reaches_expected_count_before(timeout));
-    EXPECT_TRUE(stub_secondary_db.reaches_expected_count_before(timeout));
+    EXPECT_TRUE(stub_primary_db.has_posted_at_least(1, timeout));
+    EXPECT_TRUE(stub_secondary_db.has_posted_at_least(1, timeout));
 }
 
 namespace
@@ -245,15 +228,14 @@ int thread_distinguishing_buffers_ready_for_compositor()
     return sequence[sequence_idx++];
 }
 }
-TEST_F(SurfaceStackCompositorInteractions, compositor_runs_until_all_surfaces_buffers_are_consumed)
+TEST_F(SurfaceStackCompositor, compositor_runs_until_all_surfaces_buffers_are_consumed)
 {
     using namespace testing;
     ON_CALL(*mock_buffer_stream, buffers_ready_for_compositor())
         .WillByDefault(Invoke(thread_distinguishing_buffers_ready_for_compositor));
 
-    unsigned int const expected_post_count{5};
-    CountingDisplayBuffer stub_primary_db{expected_post_count};
-    CountingDisplayBuffer stub_secondary_db{expected_post_count};
+    CountingDisplayBuffer stub_primary_db;
+    CountingDisplayBuffer stub_secondary_db;
     StubDisplay stub_display{stub_primary_db, stub_secondary_db};
     ms::SurfaceStack stack(mt::fake_shared(stub_input_registrar), null_scene_report);
     mc::DefaultDisplayBufferCompositorFactory dbc_factory{
@@ -271,15 +253,50 @@ TEST_F(SurfaceStackCompositorInteractions, compositor_runs_until_all_surfaces_bu
     stub_surface->swap_buffers(&stubbuf, [](mg::Buffer*){});
     stack.add_surface(stub_surface, default_params.depth, default_params.input_mode);
 
-    EXPECT_TRUE(stub_primary_db.reaches_expected_count_before(timeout));
-    EXPECT_TRUE(stub_secondary_db.reaches_expected_count_before(timeout));
+    EXPECT_TRUE(stub_primary_db.has_posted_at_least(5, timeout));
+    EXPECT_TRUE(stub_secondary_db.has_posted_at_least(5, timeout));
 }
 
-TEST_F(SurfaceStackCompositorInteractions, moving_a_surface_triggers_composition)
+TEST_F(SurfaceStackCompositor, an_empty_scene_retriggers)
 {
-    unsigned int const expected_post_count{1};
-    CountingDisplayBuffer stub_primary_db{expected_post_count};
-    CountingDisplayBuffer stub_secondary_db{expected_post_count};
+    using namespace testing;
+    ON_CALL(*mock_buffer_stream, buffers_ready_for_compositor())
+        .WillByDefault(Return(1));
+
+    CountingDisplayBuffer stub_primary_db;
+    CountingDisplayBuffer stub_secondary_db;
+    StubDisplay stub_display{stub_primary_db, stub_secondary_db};
+    ms::SurfaceStack stack(mt::fake_shared(stub_input_registrar), null_scene_report);
+    mc::DefaultDisplayBufferCompositorFactory dbc_factory{
+        mt::fake_shared(stack),
+        mt::fake_shared(renderer_factory),
+        null_comp_report};
+
+    mc::MultiThreadedCompositor mt_compositor(
+        mt::fake_shared(stub_display),
+        mt::fake_shared(stack),
+        mt::fake_shared(dbc_factory),
+        null_comp_report, false);
+    mt_compositor.start();
+
+    stub_surface->swap_buffers(&stubbuf, [](mg::Buffer*){});
+    stack.add_surface(stub_surface, default_params.depth, default_params.input_mode);
+
+    EXPECT_TRUE(stub_primary_db.has_posted_at_least(1, timeout));
+    EXPECT_TRUE(stub_secondary_db.has_posted_at_least(1, timeout));
+
+    stack.remove_surface(stub_surface);
+    
+    //The second render would be the removal of the surface. The 3rd is the frig
+    //implemented to avoid lp:1286190
+    EXPECT_TRUE(stub_primary_db.has_posted_at_least(3, timeout));
+    EXPECT_TRUE(stub_secondary_db.has_posted_at_least(3, timeout));
+}
+
+TEST_F(SurfaceStackCompositor, moving_a_surface_triggers_composition)
+{
+    CountingDisplayBuffer stub_primary_db;
+    CountingDisplayBuffer stub_secondary_db;
     StubDisplay stub_display{stub_primary_db, stub_secondary_db};
     ms::SurfaceStack stack(mt::fake_shared(stub_input_registrar), null_scene_report);
     mc::DefaultDisplayBufferCompositorFactory dbc_factory{
@@ -299,15 +316,14 @@ TEST_F(SurfaceStackCompositorInteractions, moving_a_surface_triggers_composition
     mt_compositor.start();
     stub_surface->move_to(geom::Point{1,1});
 
-    EXPECT_TRUE(stub_primary_db.reaches_expected_count_before(timeout));
-    EXPECT_TRUE(stub_secondary_db.reaches_expected_count_before(timeout));
+    EXPECT_TRUE(stub_primary_db.has_posted_at_least(1, timeout));
+    EXPECT_TRUE(stub_secondary_db.has_posted_at_least(1, timeout));
 }
 
-TEST_F(SurfaceStackCompositorInteractions, removing_a_surface_triggers_composition)
+TEST_F(SurfaceStackCompositor, removing_a_surface_triggers_composition)
 {
-    unsigned int const expected_post_count{1};
-    CountingDisplayBuffer stub_primary_db{expected_post_count};
-    CountingDisplayBuffer stub_secondary_db{expected_post_count};
+    CountingDisplayBuffer stub_primary_db;
+    CountingDisplayBuffer stub_secondary_db;
     StubDisplay stub_display{stub_primary_db, stub_secondary_db};
     ms::SurfaceStack stack(mt::fake_shared(stub_input_registrar), null_scene_report);
     mc::DefaultDisplayBufferCompositorFactory dbc_factory{
@@ -327,15 +343,14 @@ TEST_F(SurfaceStackCompositorInteractions, removing_a_surface_triggers_compositi
     mt_compositor.start();
     stack.remove_surface(stub_surface);
 
-    EXPECT_TRUE(stub_primary_db.reaches_expected_count_before(timeout));
-    EXPECT_TRUE(stub_secondary_db.reaches_expected_count_before(timeout));
+    EXPECT_TRUE(stub_primary_db.has_posted_at_least(1, timeout));
+    EXPECT_TRUE(stub_secondary_db.has_posted_at_least(1, timeout));
 }
 
-TEST_F(SurfaceStackCompositorInteractions, buffer_updates_trigger_composition)
+TEST_F(SurfaceStackCompositor, buffer_updates_trigger_composition)
 {
-    unsigned int const expected_post_count{1};
-    CountingDisplayBuffer stub_primary_db{expected_post_count};
-    CountingDisplayBuffer stub_secondary_db{expected_post_count};
+    CountingDisplayBuffer stub_primary_db;
+    CountingDisplayBuffer stub_secondary_db;
     StubDisplay stub_display{stub_primary_db, stub_secondary_db};
     ms::SurfaceStack stack(mt::fake_shared(stub_input_registrar), null_scene_report);
     mc::DefaultDisplayBufferCompositorFactory dbc_factory{
@@ -355,10 +370,6 @@ TEST_F(SurfaceStackCompositorInteractions, buffer_updates_trigger_composition)
     mt_compositor.start();
     stub_surface->swap_buffers(&stubbuf, [](mg::Buffer*){});
 
-    EXPECT_TRUE(stub_primary_db.reaches_expected_count_before(timeout));
-    EXPECT_TRUE(stub_secondary_db.reaches_expected_count_before(timeout));
+    EXPECT_TRUE(stub_primary_db.has_posted_at_least(1, timeout));
+    EXPECT_TRUE(stub_secondary_db.has_posted_at_least(1, timeout));
 }
-
-//TODO: future tests:
-//TEST_F(SurfaceStackCompositorInteractions, surface_buffer_updates_triggers_composition)
-//TEST_F(SurfaceStackCompositorInteractions, surface_buffer_updates_triggers_composition_until_all_consumed)
