7eff92a71f0f4ecf88cf99a7bceafebceb5c2775
Josh Arenson <joshua.arenson@canonical.com>
2014-04-17 14:09:54 -0700

    Reverted last commit as the functionality already exists in Jenkins

---
+++ tests/performance-tests/performance_test_helper.cpp
@@ -0,0 +1,20 @@
+
+#include "performance_test_helper.h"
+
+bool package_is_installed(std::string package_name)
+{
+  std::cout << package_name;
+  return true;
+}
+
+bool add_ppa(std::string ppa_name)
+{
+  std::cout << ppa_name;
+  return true;
+}
+
+bool install_package(std::string package_name)
+{
+  std::cout << package_name;
+  return true;
+}
