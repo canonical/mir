219085b38231599111e3c4a31cb8c79fd8d5021a
Josh Arenson <joshua.arenson@canonical.com>
2014-04-17 09:57:35 -0700

    Removed option to enable/disable mesa bypass

--- tests/performance-tests/test_glmark2-es2-mir.cpp
+++ tests/performance-tests/test_glmark2-es2-mir.cpp
@@ -9,18 +9,12 @@
 class GLMark2Test : public ::testing::Test
 {
 protected:
-  virtual void RunGLMark2(const char * output_filename, bool bypass_enabled)
+  virtual void RunGLMark2(const char * output_filename)
   {
     FILE * in;
     char buf[256];
     const char *cmd = "glmark2-es2-mir --fullscreen";
-    const char *initial_bypass_state = getenv("MIR_BYPASS");
     std::ofstream glmark2_output;
-
-    if(bypass_enabled)
-        putenv(strdup("MIR_BYPASS=1")); 
-    else
-        putenv(strdup("MIR_BYPASS=0"));
 
     ASSERT_TRUE((in = popen(cmd, "r")));
     
@@ -30,16 +24,10 @@ protected:
         glmark2_output << buf;
     }
     glmark2_output.close();
-    setenv("MIR_BYPASS", initial_bypass_state, 1);
   }
 };
 
-TEST_F(GLMark2Test, benchmark_with_bypass_enabled)
+TEST_F(GLMark2Test, benchmark_fullscreen_default)
 {
-    RunGLMark2("glmark2_output_bypass_enabled.results", true);
-}
-
-TEST_F(GLMark2Test, benchmark_with_bypass_disabled)
-{
-    RunGLMark2("glmark2_output_bypass_disabled.results", false);
+    RunGLMark2("glmark2_fullscreen_default.results");
 }
