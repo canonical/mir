be391c0d1ee6d36d36f122fcbf36466353cdb9c0
Josh Arenson <joshua.arenson@canonical.com>
2014-04-17 18:46:48 -0700

    [CHECKPOINT COMMIT] glmark2 test can successfuly parse score and print to stdout

--- tests/performance-tests/test_glmark2-es2-mir.cpp
+++ tests/performance-tests/test_glmark2-es2-mir.cpp
@@ -3,31 +3,47 @@
 
 #include <iostream>
 #include <fstream>
-#include <regex>
+#include <boost/regex.hpp>
 #include <string>
 
 class GLMark2Test : public ::testing::Test
 {
 protected:
-  virtual void RunGLMark2(const char * output_filename)
+  enum ResultFileType {RAW, JSON};
+  virtual void RunGLMark2(const char *output_filename, ResultFileType file_type)
   {
-    FILE * in;
-    char buf[256];
-    const char *cmd = "glmark2-es2-mir --fullscreen";
+    boost::cmatch matches;
+    boost::regex re_glmark2_score(".*glmark2\\s+Score:\\s(\\d+).*");
+    FILE *in;
+    const char *cmd = "glmark2-es2-mir -b terrain --fullscreen";
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
     std::ofstream glmark2_output;
 
     ASSERT_TRUE((in = popen(cmd, "r")));
     
     glmark2_output.open(output_filename);
-    while(fgets(buf, sizeof(buf), in)!=NULL)
+    while((read = getline(&line, &len, in)) != -1)
     {
-        glmark2_output << buf;
+      if(file_type == RAW)
+        glmark2_output << line; 
+      else if(file_type == JSON)
+      {
+        if(boost::regex_match(line, matches, re_glmark2_score))
+        {
+          std::cout << "GLMARK SCORE: " << matches[1] << std::endl;
+        }
+      }
     }
+    
+    free(line);
+    fclose(in);
     glmark2_output.close();
   }
 };
 
 TEST_F(GLMark2Test, benchmark_fullscreen_default)
 {
-    RunGLMark2("glmark2_fullscreen_default.results");
+  RunGLMark2("glmark2_fullscreen_default.json", JSON);
 }
