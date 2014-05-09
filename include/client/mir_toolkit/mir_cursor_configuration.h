2275c4c718e1bdf19c8ebac03bae3a74bfb2e4d1
Robert Carr <robert.carr@canonical.com>
2014-05-09 12:41:15 -0700

    Update cursor conf document

--- include/client/mir_toolkit/mir_cursor_configuration.h
+++ include/client/mir_toolkit/mir_cursor_configuration.h
@@ -51,8 +51,9 @@ void mir_cursor_configuration_destroy(MirCursorConfiguration *parameters);
 
 /**
  * Returns a new MirCursorConfiguration representing a named cursor
- * from the system cursor theme. Currently only the default cursor 
- * and a disabled cursor are supported.
+ * from the system cursor theme. Currently only the symbolic values,
+ * mir_default_cursor_name and mir_disabled_cursor_name are supported
+ * as input.
  *    \param [in] name The cursor name
  *    \return A cursor parameters object which must be passed
  *            to_mir_cursor_configuration_destroy
