b122ef7810ecc1e1bece64a700945893b5d945e9
Josh Arenson <joshua.arenson@canonical.com>
2014-05-09 18:00:07 -0700

    Added command_line_server_configuration.h

---
+++ include/test/mir_test_framework/command_line_server_configuration.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright © 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Josh Arenson <joshua.arenson@canonical.com>
+ */
+
+#ifndef COMMAND_LINE_SERVER_CONFIGURATION
+#define COMMAND_LINE_SERVER_CONFIGURATION
+
+int main()
+{
+	std::cout << "I AM IN YOUR HEAD";
+	return RUN_ALL_TESTS();
+}
+
+#endif /* COMMAND_LINE_SERVER_CONFIGURATION */
