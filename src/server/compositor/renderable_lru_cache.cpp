c40c82e371ffa1da56666ef6d4d7ad24bd37aeb7
Kevin DuBois <kevin.dubois@canonical.com>
2014-05-12 16:51:47 -0700

    split renderable lru cache to its own file

---
+++ src/server/compositor/renderable_lru_cache.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2014 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "renderable_lru_cache.h"
+#include "mir/graphics/buffer.h"
+#include "mir/geometry/rectangle.h"
+
+namespace mg = mir::graphics;
+namespace mc = mir::compositor;
+namespace geom = mir::geometry;
+
+void mc::RenderableLRUCache::bind_texture_from(mg::Renderable const& renderable)
+{
+    auto const& buffer = renderable.buffer();
+    auto const& id = renderable.id();
+
+    auto buffer_id = buffer->id();
+    auto& texture = textures[id];
+    texture.texture.gl_bind();
+
+    if (texture.last_bound_buffer != buffer_id)
+        buffer->bind_to_texture();
+
+    texture.resource = buffer;
+    texture.last_bound_buffer = buffer_id;
+    texture.used = true;
+}
+
+void mc::RenderableLRUCache::invalidate()
+{
+    mg::BufferID invalid_id;
+    for(auto &t : textures)
+        t.second.last_bound_buffer = invalid_id;
+}
+
+void mc::RenderableLRUCache::release_live_texture_resources()
+{
+    auto t = textures.begin();
+    while (t != textures.end())
+    {
+        auto& tex = t->second;
+        tex.resource.reset();
+        if (tex.used)
+        {
+            tex.used = false;
+            ++t;
+        }
+        else
+        {
+            t = textures.erase(t);
+        }
+    }
+}
