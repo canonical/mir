158944603f082f62af0f26867762f1281c20d497
Alan Griffiths <alan@octopull.co.uk>
2014-05-12 18:01:37 +0100

    Enough test infrastructure to create a test SessionMediator

--- tests/acceptance-tests/test_trust_session_helper.cpp
+++ tests/acceptance-tests/test_trust_session_helper.cpp
@@ -18,8 +18,14 @@
 
 #include "mir_toolkit/mir_client_library.h"
 
+#include "src/server/frontend/session_mediator.h"
+
+#include "mir/frontend/connection_context.h"
+#include "mir/graphics/graphic_buffer_allocator.h"
+
 #include "mir_test_framework/stubbed_server_configuration.h"
 #include "mir_test_framework/in_process_server.h"
+#include "mir_test_doubles/fake_ipc_factory.h"
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
@@ -28,13 +34,75 @@
 #include <condition_variable>
 #include <mutex>
 
+using namespace testing;
+
 namespace
 {
-using DemoServerConfiguration = mir_test_framework::StubbedServerConfiguration;
+struct MySessionMediator : mir::frontend::SessionMediator
+{
+    using mir::frontend::SessionMediator::SessionMediator;
+};
+
+struct FakeIpcFactory : mir::test::doubles::FakeIpcFactory
+{
+    using mir::test::doubles::FakeIpcFactory::FakeIpcFactory;
+
+    std::shared_ptr<mir::frontend::detail::DisplayServer> make_my_mediator(
+        std::shared_ptr<mir::frontend::Shell> const& shell,
+        std::shared_ptr<mir::graphics::Platform> const& graphics_platform,
+        std::shared_ptr<mir::frontend::DisplayChanger> const& changer,
+        std::shared_ptr<mir::graphics::GraphicBufferAllocator> const& buffer_allocator,
+        std::shared_ptr<mir::frontend::SessionMediatorReport> const& sm_report,
+        std::shared_ptr<mir::frontend::EventSink> const& sink,
+        std::shared_ptr<mir::frontend::Screencast> const& effective_screencast,
+        mir::frontend::ConnectionContext const& connection_context)
+    {
+        return std::make_shared<MySessionMediator>(
+            shell,
+            graphics_platform,
+            changer,
+            buffer_allocator->supported_pixel_formats(),
+            sm_report,
+            sink,
+            resource_cache(),
+            effective_screencast,
+            connection_context);
+    }
+};
+
+struct MyServerConfiguration : mir_test_framework::StubbedServerConfiguration
+{
+    std::shared_ptr<FakeIpcFactory> mediator_factory;
+
+    auto the_ipc_factory(
+        std::shared_ptr<mir::frontend::Shell> const& shell,
+        std::shared_ptr<mir::graphics::GraphicBufferAllocator> const& allocator)
+    -> std::shared_ptr<mir::frontend::ProtobufIpcFactory> override
+    {
+        return ipc_factory(
+            [&]()
+            {
+                mediator_factory = std::make_shared<
+                    NiceMock<FakeIpcFactory>>(
+                    shell,
+                    the_session_mediator_report(),
+                    the_graphics_platform(),
+                    the_frontend_display_changer(),
+                    allocator,
+                    the_screencast(),
+                    the_session_authorizer());
+
+                ON_CALL(*mediator_factory, make_mediator(_, _, _, _, _, _, _, _))
+                    .WillByDefault(Invoke(mediator_factory.get(), &FakeIpcFactory::default_make_mediator));
+
+                return mediator_factory;
+            });
+    }
+};
 
 struct TrustSessionHelper : mir_test_framework::InProcessServer
 {
-    DemoServerConfiguration my_server_config;
+    MyServerConfiguration my_server_config;
 
     mir::DefaultServerConfiguration& server_config() override { return my_server_config; }
 
@@ -80,12 +148,25 @@ void client_fd_callback(MirConnection*, size_t count, int const* fds, void* cont
 }
 }
 
-using namespace testing;
-
 TEST_F(TrustSessionHelper, gets_fds_for_trusted_clients)
 {
     mir_connection_new_fds_for_trusted_clients(connection, arbritary_fd_request_count, &client_fd_callback, this);
     EXPECT_TRUE(wait_for_callback(std::chrono::milliseconds(500)));
 
     EXPECT_THAT(actual_fd_count, Eq(arbritary_fd_request_count));
+}
+
+TEST_F(TrustSessionHelper, gets_pid_for_clients)
+{
+    EXPECT_CALL(*my_server_config.mediator_factory,
+        make_mediator(_, _, _, _, _, _, _, _)).Times(1)
+            .WillOnce(Invoke(my_server_config.mediator_factory.get(), &FakeIpcFactory::make_my_mediator));
+
+    mir_connection_new_fds_for_trusted_clients(connection, 1, &client_fd_callback, this);
+    wait_for_callback(std::chrono::milliseconds(500));
+
+    char buffer[128] = {0};
+    sprintf(buffer, "fd://%d", actual_fds[0]);
+
+    mir_connect_sync(buffer, __PRETTY_FUNCTION__);
 }
