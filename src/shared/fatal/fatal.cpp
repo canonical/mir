0a80b545f078a0e776e5b910e3ff1586b6a7a324
Daniel van Vugt <daniel.van.vugt@canonical.com>
2014-05-13 18:05:42 +0800

    Fix copyright headers

--- src/shared/fatal/fatal.cpp
+++ src/shared/fatal/fatal.cpp
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
- * Authored by: Daniel van Vugt <daniel.van.vugt@canonical.com>
+ * Author: Daniel van Vugt <daniel.van.vugt@canonical.com>
  */
 
 #include "mir/fatal.h"
