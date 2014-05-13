111bb7d9a2d56321b1a70c0cf3871b5062be2d97
Andreas Pokorny <andreas.pokorny@canonical.com>
2014-05-13 15:39:26 +0200

    improving a bit

--- tests/unit-tests/android_input/input_reader.cpp
+++ tests/unit-tests/android_input/input_reader.cpp
@@ -558,9 +558,9 @@ protected:
 
 class InputReaderTest : public testing::Test {
 protected:
-    std::shared_ptr<FakeInputListener> mFakeListener = std::make_shared<FakeInputListener>();
-    std::shared_ptr<FakeInputReaderPolicy> mFakePolicy = std::make_shared<FakeInputReaderPolicy>();
-    std::shared_ptr<FakeEventHub> mFakeEventHub = std::make_shared<FakeEventHub>();
+    std::shared_ptr<FakeEventHub> const mFakeEventHub = std::make_shared<FakeEventHub>();
+    std::shared_ptr<FakeInputReaderPolicy> const mFakePolicy = std::make_shared<FakeInputReaderPolicy>();
+    std::shared_ptr<FakeInputListener> const mFakeListener = std::make_shared<FakeInputListener>();
     sp<InstrumentedInputReader> mReader;
 
     virtual void SetUp() {
