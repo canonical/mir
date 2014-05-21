81ce69da5db08988b937a66f932b15f2b8348a16
Alan Griffiths <alan@octopull.co.uk>
2014-05-21 14:33:27 +0100

    Tidy up of test fixture

--- tests/unit-tests/scene/test_trust_session_manager.cpp
+++ tests/unit-tests/scene/test_trust_session_manager.cpp
@@ -74,16 +74,18 @@ struct StubSessionContainer : ms::SessionContainer
 
 struct TrustSessionManager : public testing::Test
 {
+    pid_t const helper_pid = __LINE__;
+    pid_t const arbitrary_pid = __LINE__;
+    std::shared_ptr<ms::Session> const helper{std::make_shared<StubSceneSession>(helper_pid)};
+    std::shared_ptr<ms::Session> const arbitrary_session{std::make_shared<StubSceneSession>(arbitrary_pid)};
+    ms::TrustSessionCreationParameters parameters;
+    StubSessionContainer existing_sessions;
+
     NiceMock<mtd::MockTrustSessionListener> trust_session_listener;
 
     ms::TrustSessionManager session_manager{mt::fake_shared(trust_session_listener)};
 
-    pid_t const arbitrary_pid = __LINE__;
-    pid_t const helper_pid = __LINE__;
-
-    std::shared_ptr<ms::Session> const helper{std::make_shared<StubSceneSession>(helper_pid)};
-    ms::TrustSessionCreationParameters parameters;
-    StubSessionContainer existing_sessions;
+    std::shared_ptr<ms::TrustSession> const trust_session{session_manager.start_trust_session_for(helper, parameters, existing_sessions)};
 };
 }
 
@@ -98,38 +100,32 @@ TEST_F(TrustSessionManager, listener_is_notified_of_trust_session_start_and_stop
     session_manager.stop_trust_session(trust_session);
 }
 
-TEST_F(TrustSessionManager, when_session_added_listener_is_notified_of_session_beginning)
+TEST_F(TrustSessionManager, when_session_is_not_in_existing_sessions_listener_is_notified_of_session_beginning)
 {
-    auto const trust_session = session_manager.start_trust_session_for(helper, parameters, existing_sessions);
     session_manager.add_trusted_process_for(trust_session, arbitrary_pid, existing_sessions);
-    std::shared_ptr<ms::Session> const session{std::make_shared<StubSceneSession>(arbitrary_pid)};
 
-    EXPECT_CALL(trust_session_listener, trusted_session_beginning(Ref(*trust_session), Eq(session))).Times(1);
+    EXPECT_CALL(trust_session_listener, trusted_session_beginning(Ref(*trust_session), Eq(arbitrary_session))).Times(1);
 
-    session_manager.add_to_waiting_trust_sessions(session);
+    session_manager.add_to_waiting_trust_sessions(arbitrary_session);
 }
 
-TEST_F(TrustSessionManager, when_session_exists_listener_is_notified_of_session_beginning)
+TEST_F(TrustSessionManager, when_session_is_in_existing_sessions_listener_is_notified_of_session_beginning)
 {
-    auto const trust_session = session_manager.start_trust_session_for(helper, parameters, existing_sessions);
-    std::shared_ptr<ms::Session> const session{std::make_shared<StubSceneSession>(arbitrary_pid)};
-    existing_sessions.insert_session(session);
+    existing_sessions.insert_session(arbitrary_session);
 
-    EXPECT_CALL(trust_session_listener, trusted_session_beginning(Ref(*trust_session), Eq(session))).Times(1);
+    EXPECT_CALL(trust_session_listener, trusted_session_beginning(Ref(*trust_session), Eq(arbitrary_session))).Times(1);
 
     session_manager.add_trusted_process_for(trust_session, arbitrary_pid, existing_sessions);
 }
 
 TEST_F(TrustSessionManager, when_session_added_listener_is_notified_of_session_beginning_and_ending)
 {
-    auto const trust_session = session_manager.start_trust_session_for(helper, parameters, existing_sessions);
     session_manager.add_trusted_process_for(trust_session, arbitrary_pid, existing_sessions);
-    std::shared_ptr<ms::Session> const session{std::make_shared<StubSceneSession>(arbitrary_pid)};
 
     InSequence seq;
-    EXPECT_CALL(trust_session_listener, trusted_session_beginning(Ref(*trust_session), Eq(session))).Times(1);
-    EXPECT_CALL(trust_session_listener, trusted_session_ending(Ref(*trust_session), Eq(session))).Times(1);
+    EXPECT_CALL(trust_session_listener, trusted_session_beginning(Ref(*trust_session), Eq(arbitrary_session))).Times(1);
+    EXPECT_CALL(trust_session_listener, trusted_session_ending(Ref(*trust_session), Eq(arbitrary_session))).Times(1);
 
-    session_manager.add_to_waiting_trust_sessions(session);
+    session_manager.add_to_waiting_trust_sessions(arbitrary_session);
     session_manager.stop_trust_session(trust_session);
 }
