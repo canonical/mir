da5ee3f11193e399aa6bba7cdd84fc181bc621fb
Alan Griffiths <alan@octopull.co.uk>
2014-05-29 09:47:41 +0200

    Add mocks to TrustSessionClientAPI.can_start_and_stop_a_trust_session to check server events happen

--- tests/acceptance-tests/test_trust_session_client_api.cpp
+++ tests/acceptance-tests/test_trust_session_client_api.cpp
@@ -17,6 +17,8 @@
  */
 
 #include "mir_toolkit/mir_trust_session.h"
+#include "mir/scene/trust_session_listener.h"
+#include "mir/scene/trust_session.h"
 
 #include "mir_test_framework/stubbed_server_configuration.h"
 #include "mir_test_framework/basic_client_server_fixture.h"
@@ -25,10 +27,55 @@
 #include <gmock/gmock.h>
 
 namespace mtf = mir_test_framework;
+namespace ms = mir::scene;
+
+using namespace testing;
 
 namespace
 {
-struct TrustSessionClientAPI : mtf::BasicClientServerFixture<mtf::StubbedServerConfiguration>
+struct MockTrustSessionListener : ms::TrustSessionListener
+{
+    MockTrustSessionListener(std::shared_ptr<ms::TrustSessionListener> const& wrapped) :
+        wrapped(wrapped)
+    {
+        ON_CALL(*this, starting(_)).WillByDefault(Invoke(wrapped.get(), &ms::TrustSessionListener::starting));
+        ON_CALL(*this, stopping(_)).WillByDefault(Invoke(wrapped.get(), &ms::TrustSessionListener::stopping));
+        ON_CALL(*this, trusted_session_beginning(_, _)).WillByDefault(Invoke(wrapped.get(), &ms::TrustSessionListener::trusted_session_beginning));
+        ON_CALL(*this, trusted_session_ending(_, _)).WillByDefault(Invoke(wrapped.get(), &ms::TrustSessionListener::trusted_session_ending));
+    }
+
+    MOCK_METHOD1(starting, void(std::shared_ptr<ms::TrustSession> const& trust_session));
+    MOCK_METHOD1(stopping, void(std::shared_ptr<ms::TrustSession> const& trust_session));
+
+    MOCK_METHOD2(trusted_session_beginning, void(ms::TrustSession const& trust_session, std::shared_ptr<ms::Session> const& session));
+    MOCK_METHOD2(trusted_session_ending, void(ms::TrustSession const& trust_session, std::shared_ptr<ms::Session> const& session));
+
+    std::shared_ptr<ms::TrustSessionListener> const wrapped;
+};
+
+struct TrustSessionListenerConfiguration : mtf::StubbedServerConfiguration
+{
+    std::shared_ptr<ms::TrustSessionListener> the_trust_session_listener()
+    {
+        return trust_session_listener([this]()
+           ->std::shared_ptr<ms::TrustSessionListener>
+           {
+               return the_mock_trust_session_listener();
+           });
+    }
+
+    std::shared_ptr<MockTrustSessionListener> the_mock_trust_session_listener()
+    {
+        return mock_trust_session_listener([this]
+            {
+                return std::make_shared<MockTrustSessionListener>(mtf::StubbedServerConfiguration::the_trust_session_listener());
+            });
+    }
+
+    mir::CachedPtr<MockTrustSessionListener> mock_trust_session_listener;
+};
+
+struct TrustSessionClientAPI : mtf::BasicClientServerFixture<TrustSessionListenerConfiguration>
 {
     static constexpr int arbitrary_base_session_id = __LINE__;
     static constexpr mir_trust_session_event_callback null_event_callback = nullptr;
@@ -52,10 +99,14 @@ void trust_session_stop_callback(MirTrustSession* /* session */, void* context)
 }
 }
 
-using namespace testing;
-
 TEST_F(TrustSessionClientAPI, can_start_and_stop_a_trust_session)
 {
+    {
+        InSequence server_seq;
+        EXPECT_CALL(*server_configuration.the_mock_trust_session_listener(), starting(_));
+        EXPECT_CALL(*server_configuration.the_mock_trust_session_listener(), stopping(_));
+    }
+
     mir_wait_for(mir_connection_start_trust_session(connection, arbitrary_base_session_id, trust_session_start_callback, null_event_callback, this));
     ASSERT_THAT(trust_session, Ne(nullptr));
     EXPECT_THAT(started, Eq(1));
@@ -67,3 +118,4 @@ TEST_F(TrustSessionClientAPI, can_start_and_stop_a_trust_session)
 }
 
 // TODO there should be testing with a non-null mir_trust_session_event_callback
+// TODO there should be test that clients can be added to a trust session
