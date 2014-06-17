3206cd95fd70c3d1c89497ccc02f1bbcbb2f7c60
Nick Dedekind <nicholas.dedekind@gmail.com>
2014-06-17 16:38:56 +0100

    Added tests for callback deadlocks

--- tests/unit-tests/scene/test_prompt_session_manager.cpp
+++ tests/unit-tests/scene/test_prompt_session_manager.cpp
@@ -101,6 +101,19 @@ struct PromptSessionManager : public testing::Test
 
         return results;
     }
+
+    void HelperFor() {
+        session_manager.helper_for(prompt_session);
+    }
+
+    void ApplicationFor() {
+        session_manager.application_for(prompt_session);
+    }
+
+    void IterateProviders() {
+        session_manager.for_each_provider_in(prompt_session,
+                                             [](std::shared_ptr<ms::Session> const&) {});
+    }
 };
 }
 
@@ -214,4 +227,43 @@ TEST_F(PromptSessionManager, can_iterate_over_prompt_providers_in_a_prompt_sessi
         prompt_session,
         [&](std::shared_ptr<ms::Session> const& prompt_provider)
             { mock.enumerate(prompt_provider); });
+}
+
+TEST_F(PromptSessionManager, can_fetch_application_during_listener_notifications)
+{
+    ON_CALL(prompt_session_listener, starting(_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::ApplicationFor));
+    ON_CALL(prompt_session_listener, stopping(_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::ApplicationFor));
+    ON_CALL(prompt_session_listener, prompt_provider_added(_,_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::ApplicationFor));
+    ON_CALL(prompt_session_listener, prompt_provider_removed(_,_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::ApplicationFor));
+
+    auto const prompt_session = session_manager.start_prompt_session_for(helper, parameters);
+    session_manager.add_prompt_provider(prompt_session, provider_session);
+    session_manager.remove_session(provider_session);
+    session_manager.stop_prompt_session(prompt_session);
+}
+
+TEST_F(PromptSessionManager, can_fetch_helper_during_listener_notifications)
+{
+    ON_CALL(prompt_session_listener, starting(_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::HelperFor));
+    ON_CALL(prompt_session_listener, stopping(_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::HelperFor));
+    ON_CALL(prompt_session_listener, prompt_provider_added(_,_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::HelperFor));
+    ON_CALL(prompt_session_listener, prompt_provider_removed(_,_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::HelperFor));
+
+    auto const prompt_session = session_manager.start_prompt_session_for(helper, parameters);
+    session_manager.add_prompt_provider(prompt_session, provider_session);
+    session_manager.remove_session(provider_session);
+    session_manager.stop_prompt_session(prompt_session);
+}
+
+TEST_F(PromptSessionManager, can_iterating_over_prompt_providers_during_listener_notifications)
+{
+    ON_CALL(prompt_session_listener, starting(_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::IterateProviders));
+    ON_CALL(prompt_session_listener, stopping(_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::IterateProviders));
+    ON_CALL(prompt_session_listener, prompt_provider_added(_,_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::IterateProviders));
+    ON_CALL(prompt_session_listener, prompt_provider_removed(_,_)).WillByDefault(InvokeWithoutArgs(this, &PromptSessionManager::IterateProviders));
+
+    auto const prompt_session = session_manager.start_prompt_session_for(helper, parameters);
+    session_manager.add_prompt_provider(prompt_session, provider_session);
+    session_manager.remove_session(provider_session);
+    session_manager.stop_prompt_session(prompt_session);
 }
