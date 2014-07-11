544c9d5694fcc93836019be2f674ef0e7836850d
Kevin DuBois <kevin.dubois@canonical.com>
2014-07-11 09:38:29 -0400

    add missing file

---
+++ src/shared/fd/fd.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright © 2014 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+#include "mir/fd.h"
+#include <unistd.h>
+
+mir::Fd::Fd(int&& other_fd)
+    : fd{other_fd}
+{
+    other_fd = -1;
+}
+
+mir::Fd::Fd(int const& fd)
+    : fd{dup(fd)}
+{
+}
+
+mir::Fd::Fd(Fd&& other)
+    : fd{other.fd}
+{
+    other.fd = -1;
+}
+
+mir::Fd::~Fd() noexcept
+{
+    if (fd >= 0)
+        close(fd);
+}
+
+mir::Fd& mir::Fd::operator=(Fd&& other)
+{
+    this->fd = other.fd;
+    other.fd = -1;
+    return *this;
+}
+
+mir::Fd::operator int() const
+{
+    return fd;
+}
--- src/shared/graphics/android/syncfence.cpp
+++ src/shared/graphics/android/syncfence.cpp
@@ -32,10 +32,6 @@ mga::SyncFence::SyncFence(std::shared_ptr<mga::SyncFileOps> const& ops, int fd)
 
 mga::SyncFence::~SyncFence() noexcept
 {
-    if (fence_fd > 0)
-    {
-        ops->close(fence_fd);
-    }
 }
 
 void mga::SyncFence::wait()
@@ -44,8 +40,6 @@ void mga::SyncFence::wait()
     {
         int timeout = infinite_timeout;
         ops->ioctl(fence_fd, SYNC_IOC_WAIT, &timeout);
-        //ops->close(fence_fd);
-        //fence_fd = -1;
     }
 }
 
@@ -67,7 +61,6 @@ void mga::SyncFence::merge_with(NativeFence&& merge_fd)
         //both fences were valid, must merge
         struct sync_merge_data data { merge_fd, "mirfence", infinite_timeout };
         ops->ioctl(fence_fd, static_cast<int>(SYNC_IOC_MERGE), &data);
-//        ops->close(fence_fd);
         ops->close(merge_fd);
         fence_fd = mir::Fd(std::move(data.fence));
     }
