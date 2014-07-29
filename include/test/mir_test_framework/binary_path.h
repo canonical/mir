1321c975f2d16114813e41eed7205eec1784b15d
Kevin DuBois <kevin.dubois@canonical.com>
2014-07-29 08:11:37 -0400

    correct copyright, headers

--- include/test/mir_test_framework/binary_path.h
+++ include/test/mir_test_framework/binary_path.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 Canonical Ltd.
+ * Copyright © 2014 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -14,11 +14,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by:
- * Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
+ *  Kevin DuBois <kevin.dubois@canonical.com>
  */
+
+#ifndef MIR_TEST_FRAMEWORK_BINARY_PATH_H_
+#define MIR_TEST_FRAMEWORK_BINARY_PATH_H_
 
 #include <string>
 namespace mir_test_framework
 {
 std::string binary_path();
 }
+#endif /* MIR_TEST_FRAMEWORK_BINARY_PATH_H_ */
--- tests/mir_test_framework/binary_path.cpp
+++ tests/mir_test_framework/binary_path.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 Canonical Ltd.
+ * Copyright © 2013,2014 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
@@ -14,7 +14,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by:
- * Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
+ *  Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
+ *  Kevin DuBois <kevin.dubois@canonical.com>
  */
 
 #include "mir_test_framework/binary_path.h"
