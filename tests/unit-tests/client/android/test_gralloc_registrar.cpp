456b82654a578b6f140e6edf4462e443f0955f15
Kevin DuBois <kevin.dubois@canonical.com>
2014-08-01 15:39:24 -0400

    update test for the client first unpacking

--- tests/unit-tests/client/android/test_gralloc_registrar.cpp
+++ tests/unit-tests/client/android/test_gralloc_registrar.cpp
@@ -93,7 +93,8 @@ struct GrallocRegistrar : public ::testing::Test
         stub_package.data_items = 21;
         for(auto i=0; i < stub_package.fd_items; i++)
             stub_package.fd[i] = (i*4);
-        for(auto i=0; i < stub_package.data_items; i++)
+        stub_package.data[0] = static_cast<int>(mir::graphics::android::BufferFlag::unfenced);
+        for(auto i=1; i < stub_package.data_items; i++)
             stub_package.data[i] = (i*3);
     }
 
@@ -112,6 +113,7 @@ struct GrallocRegistrar : public ::testing::Test
 
 TEST_F(GrallocRegistrar, client_buffer_converts_stub_package)
 {
+    int const flag_offset{1};
     mcla::GrallocRegistrar registrar(mock_module);
     auto buffer = registrar.register_buffer(stub_package, pf);
 
@@ -122,7 +124,7 @@ TEST_F(GrallocRegistrar, client_buffer_converts_stub_package)
     for(auto i = 0; i < stub_package.fd_items; i++)
         EXPECT_EQ(stub_package.fd[i], handle->data[i]);
     for(auto i = 0; i < stub_package.data_items; i++)
-        EXPECT_EQ(stub_package.data[i], handle->data[i + stub_package.fd_items]);
+        EXPECT_EQ(stub_package.data[i], handle->data[i + flag_offset + stub_package.fd_items]);
 }
 
 TEST_F(GrallocRegistrar, client_sets_correct_version)
