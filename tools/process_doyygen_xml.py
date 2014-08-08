ae5985e998f89e0b4ccef0f99811051a7f6bc25a
Alan Griffiths <alan@octopull.co.uk>
2014-08-08 17:44:13 +0100

    Spike processing doxygen output to identify symbols to publish

---
+++ tools/process_doyygen_xml.py
@@ -0,0 +1,81 @@
+#! /usr/bin/python
+from xml.dom import minidom
+from sys import argv
+
+def getText(node):
+    rc = []
+    for node in node.childNodes:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
+
+def getTextForElement(parent, tagname):
+    rc = []
+    nodes = parent.getElementsByTagName(tagname);
+    for node in nodes : rc.append(getText(node))
+    return ''.join(rc)
+
+def getLocationFile(node):
+    rc = []
+    for node in node.childNodes:
+        if node.nodeType == node.ELEMENT_NODE and node.tagName == 'location':
+            rc.append(node.attributes['file'].value)
+    return ''.join(rc)
+
+def printAttribs(node, attribs):
+    for attrib in attribs : print ' ', attrib, '=', node.attributes[attrib].value
+
+def printTextFromTags(parent, tagnames):
+    tmp = []
+    for tag in tagnames : tmp.append(getTextForElement(parent, tag))
+    print ''.join(tmp)
+    
+def printLocation(node):
+    print ' ', 'location', '=', getLocationFile(node)
+
+def getAttribs(node):
+    kind = node.attributes['kind'].value
+    static = node.attributes['static'].value
+    prot =  node.attributes['prot'].value
+    return (kind, static, prot)
+
+def parseMemberDef(node):
+    (kind, static, prot) = getAttribs(node)
+    texttags = ['definition']
+    if kind == 'function': texttags.append('argsstring')
+    printTextFromTags(node, texttags)
+    printAttribs(node, ['kind', 'prot', 'static'])
+    printLocation(node)
+    publish = '/include/' in getLocationFile(node)
+    if publish: publish = prot != 'private'
+    if publish: publish = kind == 'function' or static == 'yes'
+    print "  PUBLISH:", publish
+    print
+
+def parseMemberDefs(parent):
+    for member in parent.getElementsByTagName('memberdef') : 
+        parseMemberDef(member)
+
+def parseCompoundDefs(xmldoc):
+    compounddefs = xmldoc.getElementsByTagName('compounddef') 
+    for node in compounddefs :
+        kind = node.attributes['kind'].value
+        if kind in ['page', 'file']: continue
+        if kind in ['class', 'struct']:
+            prot =  node.attributes['prot'].value
+            printTextFromTags(node, ['compoundname'])
+            printAttribs(node, ['kind', 'prot'])
+            printLocation(node)
+            publish = '/include/' in getLocationFile(node)
+            if publish: publish = prot != 'private'
+            print "  PUBLISH:", publish
+            print
+        parseMemberDefs(node)
+
+if __name__ == "__main__":
+    for arg in argv[1:]:
+        try:
+            xmldoc = minidom.parse(arg)
+            parseCompoundDefs(xmldoc)
+        except Exception as error:
+            print 'Error:', arg, error
