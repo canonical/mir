7611ed4f032e63453af041534357cd759c177540
Robert Carr <robert.carr@canonical.com>
2014-08-12 14:43:02 -0700

    Stuff

--- examples/demo-shell/demo_renderer.h
+++ examples/demo-shell/demo_renderer.h
@@ -21,6 +21,8 @@
 
 #include "mir/compositor/gl_renderer.h"
 
+#include <unordered_set>
+
 namespace mir
 {
 namespace examples
@@ -36,8 +38,9 @@ public:
         float const titlebar_height,
         float const shadow_radius);
     ~DemoRenderer();
-
-    void begin() const override;
+    
+    void begin(std::unordered_set<graphics::Renderable::ID> &&renderables_not_to_decorate) const;
+    
     void tessellate(
         std::vector<graphics::GLPrimitive>& primitives,
         graphics::Renderable const& renderable) const override;
@@ -59,6 +62,8 @@ private:
     float const corner_radius;
     GLuint shadow_corner_tex;
     GLuint titlebar_corner_tex;
+    
+    mutable std::unordered_set<graphics::Renderable::ID> decoration_skip_list;
 };
 
 } // namespace examples
