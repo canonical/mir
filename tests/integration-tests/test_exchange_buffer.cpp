86fa53300e67c6b6892b7e2cc696093c8e77b9c4
Kevin DuBois <kevin.dubois@canonical.com>
2014-09-23 14:52:30 -0400

    start carving out new integration test

--- tests/integration-tests/test_exchange_buffer.cpp
+++ tests/integration-tests/test_exchange_buffer.cpp
@@ -32,6 +32,8 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "mir/fd.h"
+
 namespace mtf = mir_test_framework;
 namespace mtd = mir::test::doubles;
 namespace mg = mir::graphics;
@@ -42,114 +44,114 @@ namespace mp = mir::protobuf;
 
 namespace
 {
-MATCHER(DidNotTimeOut, "did not time out")
-{
-    return arg;
-}
-
-struct StubStream : public mc::BufferStream
-{
-    StubStream(std::vector<mg::BufferID> const& ids) :
-        buffer_id_seq(ids)
+    MATCHER(DidNotTimeOut, "did not time out")
     {
+        return arg;
     }
 
-    void acquire_client_buffer(std::function<void(mg::Buffer* buffer)> complete)
+    struct StubStream : public mc::BufferStream
     {
-        std::shared_ptr<mg::Buffer> stub_buffer;
-        if (buffers_acquired < buffer_id_seq.size())
-            stub_buffer = std::make_shared<mtd::StubBuffer>(buffer_id_seq.at(buffers_acquired++));
-        else
-            stub_buffer = std::make_shared<mtd::StubBuffer>(buffer_id_seq.back());
+        StubStream(std::vector<mg::BufferID> const& ids) :
+            buffer_id_seq(ids)
+        {
+        }
 
-        client_buffers.push_back(stub_buffer);
-        complete(stub_buffer.get());
-    }
+        void acquire_client_buffer(std::function<void(mg::Buffer* buffer)> complete)
+        {
+            std::shared_ptr<mg::Buffer> stub_buffer;
+            if (buffers_acquired < buffer_id_seq.size())
+                stub_buffer = std::make_shared<mtd::StubBuffer>(buffer_id_seq.at(buffers_acquired++));
+            else
+                stub_buffer = std::make_shared<mtd::StubBuffer>(buffer_id_seq.back());
 
-    void release_client_buffer(mg::Buffer*) {}
-    std::shared_ptr<mg::Buffer> lock_compositor_buffer(void const*)
-    { return std::make_shared<mtd::StubBuffer>(); }
-    std::shared_ptr<mg::Buffer> lock_snapshot_buffer()
-    { return std::make_shared<mtd::StubBuffer>(); }
-    MirPixelFormat get_stream_pixel_format() { return mir_pixel_format_abgr_8888; }
-    geom::Size stream_size() { return geom::Size{1,212121}; }
-    void resize(geom::Size const&) {};
-    void allow_framedropping(bool) {};
-    void force_requests_to_complete() {};
-    int buffers_ready_for_compositor() const { return -5; }
-    void drop_old_buffers() {}
+            client_buffers.push_back(stub_buffer);
+            complete(stub_buffer.get());
+        }
 
-    std::vector<std::shared_ptr<mg::Buffer>> client_buffers;
-    std::vector<mg::BufferID> const buffer_id_seq;
-    unsigned int buffers_acquired{0};
-};
+        void release_client_buffer(mg::Buffer*) {}
+        std::shared_ptr<mg::Buffer> lock_compositor_buffer(void const*)
+        { return std::make_shared<mtd::StubBuffer>(); }
+        std::shared_ptr<mg::Buffer> lock_snapshot_buffer()
+        { return std::make_shared<mtd::StubBuffer>(); }
+        MirPixelFormat get_stream_pixel_format() { return mir_pixel_format_abgr_8888; }
+        geom::Size stream_size() { return geom::Size{1,212121}; }
+        void resize(geom::Size const&) {};
+        void allow_framedropping(bool) {};
+        void force_requests_to_complete() {};
+        int buffers_ready_for_compositor() const { return -5; }
+        void drop_old_buffers() {}
 
-struct StubStreamFactory : public msc::BufferStreamFactory
-{
-    StubStreamFactory(std::vector<mg::BufferID> const& ids) :
-        buffer_id_seq(ids)
-    {}
+        std::vector<std::shared_ptr<mg::Buffer>> client_buffers;
+        std::vector<mg::BufferID> const buffer_id_seq;
+        unsigned int buffers_acquired{0};
+    };
 
-    std::shared_ptr<mc::BufferStream> create_buffer_stream(mg::BufferProperties const&) override
-    { return std::make_shared<StubStream>(buffer_id_seq); }
-    std::vector<mg::BufferID> const buffer_id_seq;
-};
-
-struct ExchangeServerConfiguration : mtf::StubbedServerConfiguration
-{
-    ExchangeServerConfiguration(std::vector<mg::BufferID> const& id_seq) :
-       stream_factory{std::make_shared<StubStreamFactory>(id_seq)}
+    struct StubStreamFactory : public msc::BufferStreamFactory
     {
-    }
+        StubStreamFactory(std::vector<mg::BufferID> const& ids) :
+            buffer_id_seq(ids)
+        {}
 
-    std::shared_ptr<msc::BufferStreamFactory> the_buffer_stream_factory() override
+        std::shared_ptr<mc::BufferStream> create_buffer_stream(mg::BufferProperties const&) override
+        { return std::make_shared<StubStream>(buffer_id_seq); }
+        std::vector<mg::BufferID> const buffer_id_seq;
+    };
+
+    struct ExchangeServerConfiguration : mtf::StubbedServerConfiguration
     {
-        return stream_factory;
-    }
+        ExchangeServerConfiguration(std::vector<mg::BufferID> const& id_seq) :
+            stream_factory{std::make_shared<StubStreamFactory>(id_seq)}
+        {
+        }
 
-    std::shared_ptr<msc::BufferStreamFactory> const stream_factory;
-};
+        std::shared_ptr<msc::BufferStreamFactory> the_buffer_stream_factory() override
+        {
+            return stream_factory;
+        }
 
-struct ExchangeBufferTest : mir_test_framework::InProcessServer
-{
-    std::vector<mg::BufferID> const buffer_id_exchange_seq{
-        mg::BufferID{4}, mg::BufferID{8}, mg::BufferID{9}, mg::BufferID{3}, mg::BufferID{4}};
-    ExchangeServerConfiguration server_configuration{buffer_id_exchange_seq};
-    mir::DefaultServerConfiguration& server_config() override { return server_configuration; }
-    mtf::UsingStubClientPlatform using_stub_client_platform;
+        std::shared_ptr<msc::BufferStreamFactory> const stream_factory;
+    };
 
-    void buffer_arrival()
+    struct ExchangeBufferTest : mir_test_framework::InProcessServer
     {
-        std::unique_lock<decltype(mutex)> lk(mutex);
-        arrived = true;
-        cv.notify_all();
-    }
+        std::vector<mg::BufferID> const buffer_id_exchange_seq{
+            mg::BufferID{4}, mg::BufferID{8}, mg::BufferID{9}, mg::BufferID{3}, mg::BufferID{4}};
+        ExchangeServerConfiguration server_configuration{buffer_id_exchange_seq};
+        mir::DefaultServerConfiguration& server_config() override { return server_configuration; }
+        mtf::UsingStubClientPlatform using_stub_client_platform;
 
-    //TODO: once the next_buffer rpc is deprecated, change this code out for the
-    //      mir_surface_next_buffer() api call
-    bool exchange_buffer(mp::DisplayServer& server)
-    {
-        std::unique_lock<decltype(mutex)> lk(mutex);
-        mp::Buffer next;
-        server.exchange_buffer(0, &buffer_request, &next,
-                       google::protobuf::NewCallback(this, &ExchangeBufferTest::buffer_arrival));
+        void buffer_arrival()
+        {
+            std::unique_lock<decltype(mutex)> lk(mutex);
+            arrived = true;
+            cv.notify_all();
+        }
+
+        //TODO: once the next_buffer rpc is deprecated, change this code out for the
+        //      mir_surface_next_buffer() api call
+        bool exchange_buffer(mp::DisplayServer& server)
+        {
+            std::unique_lock<decltype(mutex)> lk(mutex);
+            mp::Buffer next;
+            server.exchange_buffer(0, &buffer_request, &next,
+                    google::protobuf::NewCallback(this, &ExchangeBufferTest::buffer_arrival));
 
 
-        arrived = false;
-        auto completed = cv.wait_for(lk, std::chrono::seconds(5), [this]() {return arrived;});
-        for(auto i = 0; i < next.fd().size(); i++)
-            ::close(next.fd(i));
-        next.set_fds_on_side_channel(0);
+            arrived = false;
+            auto completed = cv.wait_for(lk, std::chrono::seconds(5), [this]() {return arrived;});
+            for(auto i = 0; i < next.fd().size(); i++)
+                ::close(next.fd(i));
+            next.set_fds_on_side_channel(0);
 
-        *buffer_request.mutable_buffer() = next;
-        return completed;
-    }
+            *buffer_request.mutable_buffer() = next;
+            return completed;
+        }
 
-    std::mutex mutex;
-    std::condition_variable cv;
-    bool arrived{false};
-    mp::BufferRequest buffer_request; 
-};
+        std::mutex mutex;
+        std::condition_variable cv;
+        bool arrived{false};
+        mp::BufferRequest buffer_request; 
+    };
 }
 
 TEST_F(ExchangeBufferTest, exchanges_happen)
@@ -166,7 +168,7 @@ TEST_F(ExchangeBufferTest, exchanges_happen)
     auto surface = mir_connection_create_surface_sync(connection, &request_params);
     auto rpc_channel = connection->rpc_channel();
     mp::DisplayServer::Stub server(
-        rpc_channel.get(), ::google::protobuf::Service::STUB_DOESNT_OWN_CHANNEL);
+            rpc_channel.get(), ::google::protobuf::Service::STUB_DOESNT_OWN_CHANNEL);
     buffer_request.mutable_buffer()->set_buffer_id(buffer_id_exchange_seq.begin()->as_value());
     for(auto i = 0; i < buffer_request.buffer().fd().size(); i++)
         ::close(buffer_request.buffer().fd(i));
@@ -176,6 +178,39 @@ TEST_F(ExchangeBufferTest, exchanges_happen)
         EXPECT_THAT(buffer_request.buffer().buffer_id(), testing::Eq(id.as_value()));
         ASSERT_THAT(exchange_buffer(server), DidNotTimeOut());
     }
+
+    mir_surface_release_sync(surface);
+    mir_connection_release(connection);
+}
+
+TEST_F(ExchangeBufferTest, fds_can_be_sent_back)
+{
+    std::string test_string{"test string"};
+    mir::Fd file(fileno(fopen("/tmp/mir-test-file.txt", "w+")));
+    unlink("/tmp/mir-test-file.txt");
+    ::write(file, test_string.c_str(), test_string.size());
+
+    auto connection = mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__);
+    MirSurfaceParameters const request_params =
+    {
+        __PRETTY_FUNCTION__,
+        640, 480,
+        mir_pixel_format_abgr_8888,
+        mir_buffer_usage_hardware,
+        mir_display_output_id_invalid
+    };
+    auto surface = mir_connection_create_surface_sync(connection, &request_params);
+    auto rpc_channel = connection->rpc_channel();
+    mp::DisplayServer::Stub server(
+            rpc_channel.get(), ::google::protobuf::Service::STUB_DOESNT_OWN_CHANNEL);
+    buffer_request.mutable_buffer()->set_buffer_id(buffer_id_exchange_seq.begin()->as_value());
+    buffer_request.mutable_buffer()->set_fds_on_side_channel(1);
+    buffer_request.mutable_buffer()->add_fd(file);
+
+    for(auto i = 0; i < buffer_request.buffer().fd().size(); i++)
+        ::close(buffer_request.buffer().fd(i));
+
+    ASSERT_THAT(exchange_buffer(server), DidNotTimeOut());
 
     mir_surface_release_sync(surface);
     mir_connection_release(connection);
