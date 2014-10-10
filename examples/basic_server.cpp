63a975f7c02905a55860cf6b4e591b1f37299ee9
Alan Griffiths <alan@octopull.co.uk>
2014-10-10 17:59:41 +0100

    Extract example display configs

--- examples/basic_server.cpp
+++ examples/basic_server.cpp
@@ -18,8 +18,8 @@
 
 #include "mir/server.h"
 
-#include "mir/graphics/display_configuration_policy.h"
-#include "mir/graphics/display_configuration.h"
+#include "example_display_configuration_policy.h"
+
 #include "mir/input/composite_event_filter.h"
 #include "mir/options/option.h"
 
@@ -27,79 +27,11 @@
 
 #include <linux/input.h>
 
-#include <unordered_map>
-
+namespace me = mir::examples;
 namespace mg = mir::graphics;
-namespace geom = mir::geometry;
 
 namespace
 {
-class SideBySideDisplayConfigurationPolicy : public mg::DisplayConfigurationPolicy
-{
-public:
-    void apply_to(mg::DisplayConfiguration& conf)
-    {
-        size_t const preferred_mode_index{0};
-        int max_x = 0;
-        std::unordered_map<mg::DisplayConfigurationCardId, size_t> available_outputs_for_card;
-
-        conf.for_each_card(
-            [&](mg::DisplayConfigurationCard const& card)
-            {
-                available_outputs_for_card[card.id] = card.max_simultaneous_outputs;
-            });
-
-        conf.for_each_output(
-            [&](mg::UserDisplayConfigurationOutput& conf_output)
-            {
-                if (conf_output.connected && conf_output.modes.size() > 0 &&
-                    available_outputs_for_card[conf_output.card_id] > 0)
-                {
-                    conf_output.used = true;
-                    conf_output.top_left = geom::Point{max_x, 0};
-                    conf_output.current_mode_index = preferred_mode_index;
-                    conf_output.power_mode = mir_power_mode_on;
-                    conf_output.orientation = mir_orientation_normal;
-                    max_x += conf_output.modes[preferred_mode_index].size.width.as_int();
-                    --available_outputs_for_card[conf_output.card_id];
-                }
-                else
-                {
-                    conf_output.used = false;
-                    conf_output.power_mode = mir_power_mode_off;
-                }
-            });
-    }
-};
-
-class SingleDisplayConfigurationPolicy : public mg::DisplayConfigurationPolicy
-{
-public:
-    void apply_to(mg::DisplayConfiguration& conf)
-    {
-        size_t const preferred_mode_index{0};
-        bool done{false};
-
-        conf.for_each_output(
-            [&](mg::UserDisplayConfigurationOutput& conf_output)
-            {
-                if (!done && conf_output.connected && conf_output.modes.size() > 0)
-                {
-                    conf_output.used = true;
-                    conf_output.top_left = geom::Point{0, 0};
-                    conf_output.current_mode_index = preferred_mode_index;
-                    conf_output.power_mode = mir_power_mode_on;
-                    done = true;
-                }
-                else
-                {
-                    conf_output.used = false;
-                    conf_output.power_mode = mir_power_mode_off;
-                }
-            });
-    }
-};
-
 class QuitFilter : public mir::input::EventFilter
 {
 public:
@@ -131,10 +63,6 @@ private:
 int main(int argc, char const* argv[])
 {
     static char const* const launch_child_opt = "launch-client";
-    static char const* const display_config_opt = "display-config";
-    static char const* const clone_opt_val = "clone";
-    static char const* const sidebyside_opt_val = "sidebyside";
-    static char const* const single_opt_val = "single";
 
     mir::Server server;
 
@@ -142,17 +70,17 @@ int main(int argc, char const* argv[])
 
     server.add_configuration_option(
         launch_child_opt, "system() command to launch client", mir::OptionType::string)
-       (display_config_opt, "Display configuration [{clone,sidebyside,single}]", clone_opt_val);
+       (me::display_config_opt, "Display configuration [{clone,sidebyside,single}]", me::clone_opt_val);
 
     server.wrap_display_configuration_policy([&](std::shared_ptr<mg::DisplayConfigurationPolicy> const& wrapped)
         -> std::shared_ptr<mg::DisplayConfigurationPolicy>
         {
-            auto display_config = server.get_options()->get<std::string>(display_config_opt);
+            auto display_config = server.get_options()->get<std::string>(me::display_config_opt);
 
-            if (display_config == sidebyside_opt_val)
-                return std::make_shared<SideBySideDisplayConfigurationPolicy>();
-            else if (display_config == single_opt_val)
-                return std::make_shared<SingleDisplayConfigurationPolicy>();
+            if (display_config == me::sidebyside_opt_val)
+                return std::make_shared<me::SideBySideDisplayConfigurationPolicy>();
+            else if (display_config == me::single_opt_val)
+                return std::make_shared<me::SingleDisplayConfigurationPolicy>();
             else
                 return wrapped;
         });
---
+++ examples/example_display_configuration_policy.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2014 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored By: Alan Griffiths <alan@octopull.co.uk>
+ */
+
+#ifndef MIR_EXAMPLE_DISPLAY_CONFIGURATION_POLICY_H_
+#define MIR_EXAMPLE_DISPLAY_CONFIGURATION_POLICY_H_
+
+#include "mir/graphics/display_configuration_policy.h"
+
+namespace mir
+{
+namespace examples
+{
+extern char const* const display_config_opt;
+extern char const* const clone_opt_val;
+extern char const* const sidebyside_opt_val;
+extern char const* const single_opt_val;
+
+class SideBySideDisplayConfigurationPolicy : public graphics::DisplayConfigurationPolicy
+{
+public:
+    void apply_to(graphics::DisplayConfiguration& conf);
+};
+
+class SingleDisplayConfigurationPolicy : public graphics::DisplayConfigurationPolicy
+{
+public:
+    void apply_to(graphics::DisplayConfiguration& conf);
+};
+}
+}
+
+#endif /* MIR_EXAMPLE_DISPLAY_CONFIGURATION_POLICY_H_ */
