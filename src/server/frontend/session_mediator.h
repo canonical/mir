92d5c0ee3bbf7f3528c9e237166f03ace8fa4422
Kevin DuBois <kevin.dubois@canonical.com>
2014-10-15 15:30:58 -0400

    clean up and fix SessionMediator

--- src/server/frontend/session_mediator.h
+++ src/server/frontend/session_mediator.h
@@ -183,7 +183,6 @@ private:
     std::shared_ptr<Shell> const shell;
     std::shared_ptr<graphics::Platform> const graphics_platform;
     std::shared_ptr<graphics::PlatformIpcOperations> const ipc_operations;
-    mir::protobuf::Buffer brq;
 
     std::vector<MirPixelFormat> const surface_pixel_formats;
 
