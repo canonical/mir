5b3e96434439d725a67f916dc2c7d58416736ebe
Alan Griffiths <alan@octopull.co.uk>
2014-11-14 11:54:58 +0000

    Migrate ClientWithCustomDisplayConfiguration

--- tests/mir_test_framework/connected_client_headless_server.cpp
+++ tests/mir_test_framework/connected_client_headless_server.cpp
@@ -18,12 +18,21 @@
 
 #include "mir_test_framework/connected_client_headless_server.h"
 
+#include "boost/throw_exception.hpp"
+
 namespace mtf = mir_test_framework;
 
 void mtf::ConnectedClientHeadlessServer::SetUp()
 {
     HeadlessInProcessServer::SetUp();
     connection = mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__);
+
+    if (!mir_connection_is_valid(connection))
+    {
+        BOOST_THROW_EXCEPTION(
+            std::runtime_error{std::string{"Failed to connect to test server: "} +
+                               mir_connection_get_error_message(connection)});
+    }
 }
 
 void mtf::ConnectedClientHeadlessServer::TearDown()
