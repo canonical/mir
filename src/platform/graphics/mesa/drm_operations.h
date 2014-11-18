64f0a3184f306a584ade41573cbec0cc504f3e75
Kevin DuBois <kevin.dubois@canonical.com>
2014-11-18 14:40:37 -0500

    add two tests, see them fail (also add some files i forgot tobzr add)

---
+++ src/platform/graphics/mesa/drm_operations.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright © 2014 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#ifndef MIR_GRAPHICS_MESA_DRM_AUTHENTICATION_H_
+#define MIR_GRAPHICS_MESA_DRM_AUTHENTICATION_H_
+
+#include "mir/fd.h"
+
+namespace mir
+{
+namespace graphics
+{
+namespace mesa
+{
+
+class DRMAuthentication
+{
+public:
+    DRMAuthentication() = default;
+    virtual ~DRMAuthentication() = default;
+    DRMAuthentication(DRMAuthentication const&) = delete;
+    DRMAuthentication& operator=(DRMAuthentication const&) = delete;
+
+};
+
+}
+}
+}
+
+#endif /* MIR_GRAPHICS_MESA_DRM_AUTHENTICATION_H_ */
