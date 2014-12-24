5bfd7ce2d2de528bd579f9fb832d3c05099a47e3
Robert Carr <racarr@ocelot>
2014-12-24 09:43:45 -0500

    Correct merge error

--- include/common/mir/log.h
+++ include/common/mir/log.h
@@ -61,11 +61,19 @@ void log_info(char const* fmt, Args... args)
                MIR_LOG_COMPONENT, fmt, args...);
 }
 
+template<typename... Args>
+void log_error(char const* fmt, Args... args)
+{
+    ::mir::log(::mir::logging::Severity::error,
+               MIR_LOG_COMPONENT, fmt, args...);
+}
+
 inline void log_critical(std::string const& message)
 {
     ::mir::log(::mir::logging::Severity::critical,
                MIR_LOG_COMPONENT, message);
 }
+
 
 inline void log_error(std::string const& message)
 {
