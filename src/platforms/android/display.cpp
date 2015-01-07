1fe1f8bb38daa78711d21e2affd1fca0d424dfa3
Kevin DuBois <kevin.dubois@canonical.com>
2015-01-07 15:56:46 -0500

    fix clang

--- src/platforms/android/display.cpp
+++ src/platforms/android/display.cpp
@@ -62,7 +62,7 @@ mga::Display::Display(
     display_report->report_successful_display_construction();
 }
 
-mga::Display::~Display()
+mga::Display::~Display() noexcept
 {
     if (display_buffer->configuration().power_mode != mir_power_mode_off)
         safe_power_mode(*hwc_config, mir_power_mode_off);
