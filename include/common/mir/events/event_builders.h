997821dd06984f23c836d33e5fd8cc8c5cc25c71
Robert Carr <robert.carr@canonical.com>
2015-01-21 16:22:45 -0800

    Add input event builders

--- include/common/mir/events/event_builders.h
+++ include/common/mir/events/event_builders.h
@@ -26,6 +26,7 @@
 #include "mir/geometry/size.h"
 
 #include <memory>
+#include <vector>
 
 namespace mir
 {
@@ -36,6 +37,24 @@ std::shared_ptr<MirEvent> make_prompt_session_event(MirPromptSessionState state)
 std::shared_ptr<MirEvent> make_resize_event(int surface_id, geometry::Size const& size);
 std::shared_ptr<MirEvent> make_surface_event(int surface_id, MirSurfaceAttrib attribute, int value);
 std::shared_ptr<MirEvent> make_close_surface_event(int surface_id);
+
+// For QtMir
+std::shared_ptr<MirEvent> make_key_event(MirInputDeviceId device_id, int64_t timestamp,
+    MirKeyInputEventAction action, xkb_keysym_t key_code,
+    int scan_code, MirInputEventModifiers modifiers);
+
+std::shared_ptr<MirEvent> make_touch_event(MirInputDeviceId device_id, int64_t timestamp,
+    MirInputEventModifiers modifiers);
+void add_touch(MirEvent &event, MirTouchInputEventTouchId touch_id, MirTouchInputEventTouchAction action,
+    MirTouchInputEventTouchTooltype tooltype, float x_axis_value, float y_axis_value,
+    float pressure_value, float touch_major_value, float touch_minor_value, float size_value);
+
+std::shared_ptr<MirEvent> make_pointer_event(MirInputDeviceId device_id, int64_t timestamp,
+    MirInputEventModifiers modifiers, MirPointerInputEventAction action,
+    std::vector<MirPointerInputEventButton> const& buttons_pressed,
+    float x_axis_value, float y_axis_value,
+    float hscroll_value, float vscroll_value);
+
 }
 }
 
