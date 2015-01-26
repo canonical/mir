c65d08830af3f44d7991f734b747b3ea2846fec8
Andreas Pokorny <andreas.pokorny@canonical.com>
2015-01-26 10:05:21 +0100

    Adds mir::Flags to common headers.
    mir::Flags is a utility to allow treating the type safe scoped enums as a set of masks or bit flags that can be combined in a type safe way. mir::Flags also works with normal enumerations. Additionally it provides utilities to print enumeration values as strings. For that purpose a variadic macro is provided.

---
+++ include/common/mir/flags.h
@@ -0,0 +1,244 @@
+/*
+ * Copyright © 2015 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored By: Andreas Pokorny <andreas.pokorny@canonical.com>
+ */
+
+#ifndef MIR_FLAGS_H_
+#define MIR_FLAGS_H_
+
+#include "mir/pp_foreach.h"
+
+#include <type_traits>
+#include <string>
+#include <ostream>
+
+namespace mir
+{
+
+#define MIR_FLAGS_DECLARE_VALUE(x,y) x = y,
+#define MIR_FLAGS_APPEND_VALUE(x,y)                                                              \
+    if (((y) == 0 && flags.value == 0) ||                                                        \
+        ((y) != 0 && (y) == (flags.value&(y))))                                                  \
+    {                                                                                            \
+        if(first)                                                                                \
+        {                                                                                        \
+            first = false;                                                                       \
+            str = #x;                                                                            \
+        }                                                                                        \
+        else str += "|" #x;                                                                      \
+    }
+
+
+/*!
+ * Declare a scoped enum the underlying type and let the macro add utilities to allow type safe
+ * bit wise operations for that enum and pretty printing to a std::string using \
+ * mir::to_string(Flags<EnumType>);
+ *
+ * Usage example:
+ * \code
+ *     MIR_FLAGS(Proc,uint32_t,(Zero,1),(Carry,1),(Interrupt,2));
+ *     using ProcFlags = mir::Flags<Proc>;
+ * \endcode
+ *
+ * \note The bitwise (Enum,Enum) operators are declared inside mir namespace to make collisions
+ * with other customer provided operators impossible.
+ */
+#define MIR_FLAGS(Type,UnderlyingType,...)                                                       \
+enum class Type : UnderlyingType { MIR_FOR_EACH_TUPLE(MIR_FLAGS_DECLARE_VALUE,__VA_ARGS__) };    \
+namespace detail                                                                                 \
+{                                                                                                \
+   struct TypePrinter ##  Type                                                                   \
+   {                                                                                             \
+       inline std::string operator()(mir::Flags<Type> const& flags)                              \
+       {                                                                                         \
+           bool first = true;                                                                    \
+           std::string str("Empty");                                                             \
+           MIR_FOR_EACH_TUPLE(MIR_FLAGS_APPEND_VALUE,__VA_ARGS__);                               \
+           return str;                                                                           \
+       }                                                                                         \
+   };                                                                                            \
+}                                                                                                \
+inline std::true_type is_bit_enum_flag_helper(Type);                                             \
+inline std::true_type has_type_printer_helper(Type);                                             \
+inline detail::TypePrinter ## Type get_type_printer_helper(Type);                                \
+
+
+inline std::false_type get_type_printer_helper(...);
+inline std::false_type is_bit_enum_flag_helper(...);
+
+/*!
+ * Treat an enumeration, scoped and unscoped, like a set of flags.
+ *
+ * If Enum | Enum and similar operations should yield Flags<Enum> declare
+ * a function within the namespace of Enum:
+ *   std::true_type is_bit_enum_flag(Enum);
+ * and the respective operators in namespace mir will be enabled.
+ *
+ * This will be done auotmatially if the enumeration is declared with
+ * the MIR_FLAGS macro.
+ */
+template<typename Enum>
+struct Flags
+{
+    using value_type = typename std::underlying_type<Enum>::type;
+    value_type value;
+
+    explicit constexpr Flags(value_type value = 0) noexcept
+        : value{value} {}
+    constexpr Flags(Enum value) noexcept
+        : value{static_cast<value_type>(value)} {}
+
+    constexpr Flags<Enum> operator|(Flags<Enum> other) const noexcept
+    {
+        return Flags<Enum>(value|other.value);
+    }
+
+    constexpr Flags<Enum> operator&(Flags<Enum> other) const noexcept
+    {
+        return Flags<Enum>(value & other.value);
+    }
+
+    constexpr Flags<Enum> operator^(Flags<Enum> other) const noexcept
+    {
+        return Flags<Enum>(value ^ other.value);
+    }
+
+    constexpr Flags<Enum> operator~() const noexcept
+    {
+        return Flags<Enum>(~value);
+    }
+
+    // those mutating operators could be trated as constexpr with c++14
+    Flags<Enum>& operator|=(Flags<Enum> other) noexcept
+    {
+        value |= other.value;
+        return *this;
+    }
+
+    Flags<Enum> operator&=(Flags<Enum> other) noexcept
+    {
+        value &= other.value;
+        return *this;
+    }
+
+    Flags<Enum> operator^=(Flags<Enum> other) noexcept
+    {
+        value ^= other.value;
+        return *this;
+    }
+
+    constexpr bool operator==(Flags<Enum> other) const noexcept
+    {
+        return value == other.value;
+    }
+};
+
+template<typename Enum>
+typename std::enable_if<
+     decltype(has_type_printer_helper(static_cast<Enum>(0)))::value,
+     std::string
+     >::type
+     to_string(Flags<Enum> flags)
+{
+    decltype(get_type_printer_helper(static_cast<Enum>(0))) printer;
+    return printer(flags);
+}
+
+template<typename Enum>
+typename std::enable_if<
+     decltype(has_type_printer_helper(static_cast<Enum>(0)))::value,
+     std::ostream
+     >::type
+     operator<<(std::ostream& out, Flags<Enum> flags)
+{
+    return out << to_string(flags);
+}
+
+template<typename Enum>
+constexpr Flags<Enum> operator|(Flags<Enum> flags, Enum e) noexcept
+{
+    return Flags<Enum>(flags.value | static_cast<decltype(flags.value)>(e));
+}
+
+template<typename Enum>
+constexpr Flags<Enum> operator|(Enum e, Flags<Enum> flags) noexcept
+{
+    return Flags<Enum>(flags.value | static_cast<decltype(flags.value)>(e));
+}
+
+template<typename Enum>
+constexpr Enum operator&(Enum e, Flags<Enum> flags) noexcept
+{
+    return static_cast<Enum>(flags.value & static_cast<decltype(flags.value)>(e));
+}
+
+template<typename Enum>
+constexpr Enum operator&(Flags<Enum> flags, Enum e) noexcept
+{
+    return static_cast<Enum>(flags.value & static_cast<decltype(flags.value)>(e));
+}
+
+template<typename Enum>
+constexpr bool operator==(Flags<Enum> flags, Enum e) noexcept
+{
+    return e == static_cast<Enum>(flags.value);
+}
+
+template<typename Enum>
+constexpr bool operator==(Enum e, Flags<Enum> flags) noexcept
+{
+    return e == static_cast<Enum>(flags.value);
+}
+
+template<typename Enum>
+constexpr bool contains(Flags<Enum> flags, Enum e) noexcept
+{
+    return e == static_cast<Enum>(flags.value & static_cast<decltype(flags.value)>(e));
+}
+
+template<typename T>
+struct is_bit_flag_enum
+{
+    using type = decltype(is_bit_enum_flag_helper(static_cast<T>(0)));
+};
+
+template<typename T>
+using is_bit_flag_enum_t = typename is_bit_flag_enum<T>::type;
+
+template<typename Enum>
+constexpr typename std::enable_if<is_bit_flag_enum_t<Enum>::value,Flags<Enum>>::type
+operator|(Enum lhs, Enum rhs) noexcept
+{
+    return Flags<Enum>(lhs) | Flags<Enum>(rhs);
+}
+
+template<typename Enum>
+constexpr typename std::enable_if<is_bit_flag_enum_t<Enum>::value,Flags<Enum>>::type
+operator&(Enum lhs, Enum rhs) noexcept
+{
+    return Flags<Enum>(lhs) & Flags<Enum>(rhs);
+}
+
+template<typename Enum>
+constexpr typename std::enable_if<is_bit_flag_enum_t<Enum>::value,Flags<Enum>>::type
+operator^(Enum lhs, Enum rhs) noexcept
+{
+    return Flags<Enum>(lhs) ^ Flags<Enum>(rhs);
+}
+
+}
+
+#endif
---
+++ tests/unit-tests/test_flags.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright © 2015 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored By: Andreas Pokorny <andreas.pokorny@canonical.com>
+ */
+
+#include "mir/flags.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#if 0
+#endif
+namespace arbitrary_namespace
+{
+MIR_FLAGS(Character, uint32_t,
+          (Empty, 0),
+          (Logical, 1<<0),
+          (Idealistic, 1<<1),
+          (Englihtened, 1<<2),
+          (Curious, 1<<3),
+          (Paranoid, 1<<16),
+          (Unstable, 1<<17),
+          (Reckless, 1<<18),
+          (Positive, (1<<4) - 1),
+          (Negative, 0xFFFF0000))
+using Profile = mir::Flags<Character>;
+}
+
+namespace mir
+{
+namespace ns_inside_mir
+{
+
+MIR_FLAGS(Capability, uint8_t, (Pointer, 1<<4),(Touchpad, 1<<3))
+using Capabilities = mir::Flags<Capability>;
+}
+}
+
+namespace nested = mir::ns_inside_mir;
+namespace arb = arbitrary_namespace;
+
+TEST(MirFlags,lookup_rules_work_in_mir_nested_namespace)
+{
+    using namespace testing;
+    using namespace mir;
+    nested::Capabilities cap = nested::Capability::Pointer | nested::Capability::Touchpad;
+
+    EXPECT_THAT(mir::to_string(cap),Eq("Pointer|Touchpad"));
+}
+
+TEST(MirFlags,lookup_rules_work_in_arbitrary_namespace)
+{
+    using namespace testing;
+    using namespace mir;
+    arb::Profile empty = arb::Character::Curious & arb::Character::Reckless;
+
+    EXPECT_THAT(mir::to_string(empty),Eq("Empty"));
+}
+
+namespace mir
+{
+TEST(MirFlags,using_namespace_mir_not_needed_inside_mir)
+{
+    using namespace testing;
+    arb::Profile mostly_positive = arb::Character::Curious | arb::Character::Logical;
+
+    EXPECT_THAT(mir::to_string(mostly_positive),Eq("Logical|Curious"));
+}
+}
+
+TEST(MirFlags,contains_check_works_for_masks)
+{
+    using namespace testing;
+    using namespace mir;
+    arb::Profile mostly_positive;
+    mostly_positive |= arb::Character::Curious | arb::Character::Logical;
+
+    EXPECT_THAT(contains(mostly_positive,arb::Character::Positive),Eq(false));
+    mostly_positive = mostly_positive | arb::Character::Idealistic | arb::Character::Englihtened;
+
+    EXPECT_THAT(contains(mostly_positive,arb::Character::Positive),Eq(true));
+}
+
+TEST(MirFlags,toggling_bits)
+{
+    using namespace testing;
+    using namespace mir;
+    arb::Profile negative{arb::Character::Negative};
+
+    EXPECT_THAT(contains(negative,arb::Character::Paranoid),Eq(true));
+    EXPECT_THAT(contains(negative^arb::Character::Paranoid,arb::Character::Paranoid),Eq(false));
+    EXPECT_THAT(contains(~negative,arb::Character::Positive),Eq(true));
+}
