fe090de4211559b35c01b35b38b074baab7b3d6e
Marco Trevisan (Treviño) <mail@3v1n0.net>
2015-02-06 16:13:39 +0100

    Mir: Fix compilation when using clang/g++ on trusty

--- examples/server_example_window_management.h
+++ examples/server_example_window_management.h
@@ -55,7 +55,7 @@ class EventTracker;
 class WindowManagmentFactory
 {
 public:
-    explicit WindowManagmentFactory(Server& server) : server{server} {}
+    explicit WindowManagmentFactory(Server& server) : server(server) {}
 
     auto window_manager() -> std::shared_ptr<WindowManager>;
 
