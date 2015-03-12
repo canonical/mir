6b7586c39f56658a74e89298887cdc88ab72f0b9
Kevin DuBois <kevin.dubois@canonical.com>
2015-03-12 13:45:16 -0400

    another round of cleanups

--- examples/eglsquare.cpp
+++ examples/eglsquare.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright © 2015 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
 #include "mir_toolkit/mir_client_library.h"
 #include <EGL/egl.h>
 #include <GLES2/gl2.h>
@@ -5,7 +23,7 @@
 #include <thread>
 #include <memory>
 #include <chrono>
-#include <tuple>
+#include <iostream>
 #include <cstring>
 #include <unistd.h>
 #include <signal.h>
@@ -15,8 +33,8 @@ namespace
 class Connection
 {
 public:
-    Connection(char const* socket_file)
-    : connection(mir_connect_sync(socket_file, __PRETTY_FUNCTION__))
+    Connection(char const* socket_file) :
+        connection(mir_connect_sync(socket_file, __PRETTY_FUNCTION__))
     {
         if (!mir_connection_is_valid(connection)) throw std::runtime_error("could not connect to server");
     }
@@ -32,9 +50,9 @@ private:
 class Context
 {
 public:
-    Context(Connection* connection, MirSurface* surface) :
+    Context(Connection& connection, MirSurface* surface) :
         native_display(reinterpret_cast<EGLNativeDisplayType>(
-            mir_connection_get_egl_native_display(*connection))),
+            mir_connection_get_egl_native_display(connection))),
         native_window(reinterpret_cast<EGLNativeWindowType>(
             mir_buffer_stream_get_egl_native_window(mir_surface_get_buffer_stream(surface)))),
         display(native_display),
@@ -81,7 +99,7 @@ private:
     struct Display
     {
         Display(EGLNativeDisplayType native) :
-        disp(eglGetDisplay(native))
+            disp(eglGetDisplay(native))
         {
             int major{0}, minor{0};
             if (disp == EGL_NO_DISPLAY) throw std::runtime_error("no egl display");
@@ -110,10 +128,10 @@ private:
     struct EglContext
     {
         EglContext(EGLDisplay disp, EGLConfig config) :
-        disp(disp),
-        context(eglCreateContext(disp, config, EGL_NO_CONTEXT, context_attribs))
+            disp(disp),
+            context(eglCreateContext(disp, config, EGL_NO_CONTEXT, context_attribs))
         {
-            if(context == EGL_NO_CONTEXT) throw std::runtime_error("could not create egl context");
+            if (context == EGL_NO_CONTEXT) throw std::runtime_error("could not create egl context");
         }
         ~EglContext()
         {
@@ -128,11 +146,13 @@ private:
 class RenderProgram
 {
 public:
-    RenderProgram(Context& context) :
-    vertex(&vtex_shader_src, GL_VERTEX_SHADER),
-    fragment(&frag_shader_src, GL_FRAGMENT_SHADER),
-    program(vertex, fragment)
+    RenderProgram(Context& context, unsigned int width, unsigned int height) :
+        vertex(&vtex_shader_src, GL_VERTEX_SHADER),
+        fragment(&frag_shader_src, GL_FRAGMENT_SHADER),
+        program(vertex, fragment)
     {
+        float square_side = 400.0f;
+        float scale[2] {square_side/width, square_side/height};
         glUseProgram(program.program);
         vPositionAttr = glGetAttribLocation(program.program, "vPosition");
         glVertexAttribPointer(vPositionAttr, 4, GL_FLOAT, GL_FALSE, 0, vertex_data);
@@ -142,8 +162,11 @@ public:
         glGenTextures(1, &texture);
         glBindTexture(GL_TEXTURE_2D, texture);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, &blue_pixel);
-        glClearColor(0.1, 0.1, 0.4, 1.0);
+        glClearColor(0.1, 0.1, 0.4, 1.0); //light blue
         glClear(GL_COLOR_BUFFER_BIT);
+        auto scaleUniform = glGetUniformLocation(program.program, "scale");
+        glUniform2fv(scaleUniform, 1, scale);
+ 
         context.swapbuffers();
         context.release_current();
     }
@@ -165,33 +188,34 @@ public:
 private:
     GLchar const*const frag_shader_src =
     {
-        "precision mediump float;\n"
-        "uniform sampler2D tex;\n"
-        "varying vec2 texcoord;\n"
-        "void main() {\n"
-        "   gl_FragColor = texture2D(tex, texcoord);\n"
-        "}\n"
+        "precision mediump float;"
+        "uniform sampler2D tex;"
+        "varying vec2 texcoord;"
+        "void main() {"
+        "   gl_FragColor = texture2D(tex, texcoord);"
+        "}"
     };
     GLchar const*const vtex_shader_src =
     {
-        "attribute vec4 vPosition;\n"
-        "attribute vec4 uvCoord;\n"
-        "varying vec2 texcoord;\n"
-        "uniform vec2 pos;\n"
-        "void main() {\n"
-        "   mat4 m;\n"
-        "    m[0] = vec4(1,0,0,pos[0]);               \n"
-        "    m[1] = vec4(0,1,0,pos[1]);              \n"
-        "    m[2] = vec4(0,0,1,0);               \n"
-        "    m[3] = vec4(0,0,0,1);              \n"
-        "   gl_Position = vPosition * m;\n"
-        "   texcoord = uvCoord.xy;\n"
-        "}\n"
+        "attribute vec4 vPosition;"
+        "attribute vec4 uvCoord;"
+        "varying vec2 texcoord;"
+        "uniform vec2 pos;"
+        "uniform vec2 scale;"
+        "void main() {"
+        "   mat4 m;"
+        "    m[0] = vec4(scale.x,     0.0, 0.0, pos.x);"
+        "    m[1] = vec4(    0.0, scale.y, 0.0, pos.y);"
+        "    m[2] = vec4(    0.0,     0.0, 1.0, 0.0);"
+        "    m[3] = vec4(    0.0,     0.0, 0.0, 1.0);"
+        "   gl_Position = vPosition * m;"
+        "   texcoord = uvCoord.xy;"
+        "}"
     };
     struct Shader
     {
         Shader(GLchar const* const* src, GLuint type) :
-        shader(glCreateShader(type))
+            shader(glCreateShader(type))
         {
             glShaderSource(shader, 1, src, 0);
             glCompileShader(shader);
@@ -205,7 +229,7 @@ private:
     struct Program
     {
         Program(Shader& vertex, Shader& fragment) :
-        program(glCreateProgram())
+            program(glCreateProgram())
         {
             glAttachShader(program, vertex.shader);
             glAttachShader(program, fragment.shader);
@@ -220,9 +244,9 @@ private:
     GLfloat vertex_data[16]
     {
         -1.0, -1.0f, 0.0f, 1.0f,
-        -1.0f,  0.0f, 0.0f, 1.0f,
-         0.0f, -1.0f, 0.0f, 1.0f,
-         0.0f,  0.0f, 0.0f, 1.0f,
+        -1.0f,  1.0f, 0.0f, 1.0f,
+         1.0f, -1.0f, 0.0f, 1.0f,
+         1.0f,  1.0f, 0.0f, 1.0f,
     };
     GLfloat uv_data[16]
     {
@@ -242,12 +266,11 @@ private:
 class Surface
 {
 public:
-    Surface(std::shared_ptr<Connection> const& connection, int w, int h) :
-    width(w),
-    height(h),
-    surface(create_surface(connection)),
-    context{connection.get(), surface},
-    program{context}
+    Surface(Connection& connection) :
+        dimensions(active_output_dimensions(connection)),
+        surface(create_surface(connection, dimensions)),
+        context{connection, surface},
+        program{context, dimensions.width, dimensions.height}
     {
     }
 
@@ -265,8 +288,8 @@ public:
             mir_touch_input_axis_y);
         context.make_current();
         program.draw(
-            x/(float)width,
-            y/(-1.0*(float)height)*2.0 + 1.5);
+            x/static_cast<float>(dimensions.width)*2.0 - 1.0,
+            y/static_cast<float>(dimensions.height)*-2.0 + 1.0);
         context.swapbuffers();
     }
 
@@ -275,22 +298,48 @@ public:
         mir_surface_release_sync(surface);
     }
 private:
-    int width, height;
+    struct OutputDimensions
+    {
+        unsigned int const width;
+        unsigned int const height;
+    } const dimensions;
     MirSurface* surface;
     Context context;
     RenderProgram program;
 
-    MirSurface* create_surface(std::shared_ptr<Connection> const& connection)
+    OutputDimensions active_output_dimensions(MirConnection* connection)
+    {
+        unsigned int width{0};
+        unsigned int height{0};
+        auto display_config = mir_connection_create_display_config(connection);
+        for (auto i = 0u; i < display_config->num_outputs; i++)
+        {
+            MirDisplayOutput const* out = display_config->outputs + i;
+            if (out->used &&
+                out->connected &&
+                out->num_modes &&
+                out->current_mode < out->num_modes)
+            {
+                width = out->modes[out->current_mode].horizontal_resolution;
+                height = out->modes[out->current_mode].vertical_resolution;
+                break;
+            }
+        }
+        mir_display_config_destroy(display_config);
+        if (width == 0 || height == 0)
+            throw std::logic_error("could not determine display size");
+        return {width, height};
+    }
+
+    MirSurface* create_surface(MirConnection* connection, OutputDimensions const& dim)
     {
         MirPixelFormat pixel_format;
         unsigned int valid_formats;
-        mir_connection_get_available_surface_formats(*connection, &pixel_format, 1, &valid_formats);
+        mir_connection_get_available_surface_formats(connection, &pixel_format, 1, &valid_formats);
 
         auto deleter = [](MirSurfaceSpec *spec) { mir_surface_spec_release(spec); };
         std::unique_ptr<MirSurfaceSpec, decltype(deleter)> spec{
-            mir_connection_create_spec_for_normal_surface(*connection, width, height, pixel_format),
-            deleter
-        };
+            mir_connection_create_spec_for_normal_surface(connection, dim.width, dim.height, pixel_format), deleter };
 
         mir_surface_spec_set_name(spec.get(), __PRETTY_FUNCTION__);
         mir_surface_spec_set_buffer_usage(spec.get(), mir_buffer_usage_hardware);
@@ -305,36 +354,12 @@ private:
         if (surface) surface->on_event(event);
     }
 };
-}
-
-std::tuple<unsigned int, unsigned int> active_output_width_and_height(MirConnection* connection)
-{
-    unsigned int width{0};
-    unsigned int height{0};
-    auto display_config = mir_connection_create_display_config(connection);
-    for (auto i = 0u; i < display_config->num_outputs; i++)
-    {
-        MirDisplayOutput const* out = display_config->outputs + i;
-        if (out->used &&
-            out->connected &&
-            out->num_modes &&
-            out->current_mode < out->num_modes)
-        {
-            width = out->modes[out->current_mode].horizontal_resolution;
-            height = out->modes[out->current_mode].vertical_resolution;
-            break;
-        }
-    }
-    mir_display_config_destroy(display_config);
-    if (width == 0 || height == 0)
-        throw std::logic_error("could not determine display size");
-    return std::tuple<unsigned int, unsigned int>{width, height};
-}
 
 std::atomic<bool> running{true};
-void signal_handler(int)
+static void signal_handler(int)
 {
     running = false;
+}
 }
 
 int main(int argc, char *argv[])
@@ -359,15 +384,13 @@ try
         }
     }
 
-    auto connection = std::make_shared<Connection>(socket_file);
-    auto width_height = active_output_width_and_height(*connection);
-    Surface surface(connection, std::get<0>(width_height), std::get<1>(width_height));
-
+    Connection connection(socket_file);
+    Surface surface(connection);
     while (running){ std::this_thread::sleep_for(std::chrono::seconds(1)); }
     return 0;
 }
 catch(std::runtime_error& e)
 {
-    printf("error: %s\n", e.what());
+    std::cerr << "error : " << e.what() << std::endl;
     return 1;
 }
