4d4f29be79879137fb1679a328d46dd1ff67d188
Kevin DuBois <kevin.dubois@canonical.com>
2015-03-12 11:03:04 -0400

    actually add the file

---
+++ examples/eglsquare.cpp
@@ -0,0 +1,316 @@
+#include "mir_toolkit/mir_client_library.h"
+#include <memory>
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+#include <assert.h>
+#include <thread>
+#include <chrono>
+
+namespace
+{
+class Connection
+{
+public:
+    Connection()
+    : connection(mir_connect_sync(nullptr, __PRETTY_FUNCTION__))
+    {
+    }
+    ~Connection()
+    {
+        mir_connection_release(connection);
+    }
+    operator MirConnection*() { return connection; }
+private:
+    MirConnection* connection;
+};
+
+class Context
+{
+public:
+    Context(Connection* connection, MirSurface* surface) :
+        native_display(reinterpret_cast<EGLNativeDisplayType>(
+            mir_connection_get_egl_native_display(*connection))),
+        native_window(reinterpret_cast<EGLNativeWindowType>(
+            mir_buffer_stream_get_egl_native_window(mir_surface_get_buffer_stream(surface)))),
+        display(native_display),
+        config(chooseconfig(display.disp)),
+        surface(display.disp, config, native_window),
+        context(display.disp, config)
+    {
+        make_current();
+    }
+    void make_current()
+    {
+        if (eglMakeCurrent(display.disp, surface.surface, surface.surface, context.context) == EGL_FALSE)
+            throw(std::runtime_error("could not makecurrent"));
+    }
+    void release_current()
+    {
+        if (eglMakeCurrent(display.disp, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT) == EGL_FALSE)
+            throw(std::runtime_error("could not makecurrent"));
+    }
+    void swapbuffers()
+    {
+        if (eglSwapBuffers(display.disp, surface.surface) == EGL_FALSE)
+            throw(std::runtime_error("could not swapbuffers"));
+    }
+private:
+    EGLConfig chooseconfig(EGLDisplay disp)
+    {
+        int n{0};
+        EGLConfig egl_config;
+        EGLint attribs[] = {
+            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+            EGL_RED_SIZE, 8,
+            EGL_GREEN_SIZE, 8,
+            EGL_BLUE_SIZE, 8,
+            EGL_ALPHA_SIZE, 8,
+            EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+            EGL_NONE };
+        if (eglChooseConfig(disp, attribs, &egl_config, 1, &n) != EGL_TRUE || n != 1)
+            throw std::runtime_error("could not find egl config");
+        return egl_config;
+    }
+    EGLNativeDisplayType native_display;
+    EGLNativeWindowType native_window;
+    struct Display
+    {
+        Display(EGLNativeDisplayType native) :
+        disp(eglGetDisplay(native))
+        {
+            int major{0}, minor{0};
+            if (disp == EGL_NO_DISPLAY) throw std::runtime_error("no egl display");
+            if (eglInitialize(disp, &major, &minor) != EGL_TRUE || major != 1 || minor != 4)
+                throw std::runtime_error("could not init egl");
+        }
+        ~Display()
+        {
+            eglTerminate(disp);
+        }
+        EGLDisplay disp;
+    } display;
+    EGLConfig config;
+    struct Surface
+    {
+        Surface(EGLDisplay display, EGLConfig config, EGLNativeWindowType native_window) :
+            disp(display),
+            surface(eglCreateWindowSurface(disp, config, native_window, NULL))
+        {
+            if (surface == EGL_NO_SURFACE) throw std::runtime_error("could not create egl surface");
+        }
+        ~Surface() { eglDestroySurface(disp, surface); }
+        EGLDisplay disp;
+        EGLSurface surface;
+    } surface;
+    struct EglContext
+    {
+        EglContext(EGLDisplay disp, EGLConfig config) :
+        disp(disp),
+        context(eglCreateContext(disp, config, EGL_NO_CONTEXT, context_attribs))
+        {
+            if(context == EGL_NO_CONTEXT) throw std::runtime_error("could not create egl context");
+        }
+        ~EglContext()
+        {
+            eglDestroyContext(disp, context);
+        }
+        EGLint context_attribs[3] = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE };
+        EGLDisplay disp;
+        EGLContext context;
+    } context;
+};
+
+class RenderProgram
+{
+public:
+    RenderProgram(Context& context) :
+    vertex(&vtex_shader_src, GL_VERTEX_SHADER),
+    fragment(&frag_shader_src, GL_FRAGMENT_SHADER),
+    program(vertex, fragment)
+    {
+        glUseProgram(program.program);
+        vPositionAttr = glGetAttribLocation(program.program, "vPosition");
+        glVertexAttribPointer(vPositionAttr, 4, GL_FLOAT, GL_FALSE, 0, vertex_data);
+        uvCoord = glGetAttribLocation(program.program, "uvCoord");
+        glVertexAttribPointer(uvCoord, 4, GL_FLOAT, GL_FALSE, 0, uv_data);
+        posUniform = glGetUniformLocation(program.program, "pos");
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, &blue_pixel);
+        glClearColor(0.1, 0.1, 0.4, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        context.swapbuffers();
+        context.release_current();
+    }
+
+    void draw(float x, float y)
+    {
+        float pos[2] = {x, y}; 
+        glUseProgram(program.program);
+        glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT );
+        glUniform2fv(posUniform, 1, pos);
+        glActiveTexture(GL_TEXTURE0);
+        glEnableVertexAttribArray(vPositionAttr);
+        glEnableVertexAttribArray(uvCoord);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        glDisableVertexAttribArray(uvCoord);
+        glDisableVertexAttribArray(vPositionAttr);
+    }
+
+private:
+    GLchar const*const frag_shader_src =
+    {
+        "precision mediump float;\n"
+        "uniform sampler2D tex;\n"
+        "varying vec2 texcoord;\n"
+        "void main() {\n"
+        "   gl_FragColor = texture2D(tex, texcoord);\n"
+        "}\n"
+    };
+    GLchar const*const vtex_shader_src =
+    {
+        "attribute vec4 vPosition;\n"
+        "attribute vec4 uvCoord;\n"
+        "varying vec2 texcoord;\n"
+        "uniform vec2 pos;\n"
+        "void main() {\n"
+        "   mat4 m;\n"
+        "    m[0] = vec4(1,0,0,pos[0]);               \n"
+        "    m[1] = vec4(0,1,0,pos[1]);              \n"
+        "    m[2] = vec4(0,0,1,0);               \n"
+        "    m[3] = vec4(0,0,0,1);              \n"
+        "   gl_Position = vPosition * m;\n"
+        "   texcoord = uvCoord.xy;\n"
+        "}\n"
+    };
+    struct Shader
+    {
+        Shader(GLchar const* const* src, GLuint type) :
+        shader(glCreateShader(type))
+        {
+            glShaderSource(shader, 1, src, 0);
+            glCompileShader(shader);
+        }
+        ~Shader()
+        {
+            glDeleteShader(shader);
+        }
+        GLuint shader;
+    } vertex, fragment;
+    struct Program
+    {
+        Program(Shader& vertex, Shader& fragment) :
+        program(glCreateProgram())
+        {
+            glAttachShader(program, vertex.shader);
+            glAttachShader(program, fragment.shader);
+            glLinkProgram(program);
+        }
+        ~Program()
+        {
+            glDeleteProgram(program);
+        }
+        GLuint program;
+    } program;
+    GLfloat vertex_data[16]
+    {
+        -1.0, -1.0f, 0.0f, 1.0f,
+        -1.0f,  0.0f, 0.0f, 1.0f,
+         0.0f, -1.0f, 0.0f, 1.0f,
+         0.0f,  0.0f, 0.0f, 1.0f,
+    };
+    GLfloat uv_data[16]
+    {
+        1.0f, 1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        0.0f, 0.0f, 0.0f, 0.0f
+
+    };
+    unsigned int const blue_pixel{0xFFFF0000};
+    GLuint vPositionAttr;
+    GLuint uvCoord; 
+    GLuint posUniform; 
+    GLuint texture;
+};
+
+class Surface
+{
+public:
+    Surface(std::shared_ptr<Connection> const& connection, int w, int h) :
+    width(w),
+    height(h),
+    surface(create_surface(connection)),
+    context{connection.get(), surface},
+    program{context}
+    {
+    }
+
+    void on_event(MirEvent const* ev)
+    {
+        if (mir_event_get_type(ev) != mir_event_type_input)
+            return;
+        auto ievent = mir_event_get_input_event(ev);
+        if (mir_input_event_get_type(ievent) != mir_input_event_type_touch)
+            return;
+        auto tev = mir_input_event_get_touch_input_event(ievent);
+        auto x = mir_touch_input_event_get_touch_axis_value(tev, 0,
+            mir_touch_input_axis_x);
+        auto y = mir_touch_input_event_get_touch_axis_value(tev, 0,
+            mir_touch_input_axis_y);
+        context.make_current();
+        program.draw(
+            x/(float)width,
+            y/(-1.0*(float)height)*2.0 + 1.5);
+        context.swapbuffers();
+    }
+
+    ~Surface()
+    {
+        mir_surface_release_sync(surface);
+    }
+private:
+    int width, height;
+    MirSurface* surface;
+    Context context;
+    RenderProgram program;
+
+    MirSurface* create_surface(std::shared_ptr<Connection> const& connection)
+    {
+        MirPixelFormat pixel_format;
+        unsigned int valid_formats;
+        mir_connection_get_available_surface_formats(*connection, &pixel_format, 1, &valid_formats);
+
+        auto deleter = [](MirSurfaceSpec *spec) { mir_surface_spec_release(spec); };
+        std::unique_ptr<MirSurfaceSpec, decltype(deleter)> spec{
+            mir_connection_create_spec_for_normal_surface(*connection, width, height, pixel_format),
+            deleter
+        };
+
+        mir_surface_spec_set_name(spec.get(), __PRETTY_FUNCTION__);
+        mir_surface_spec_set_buffer_usage(spec.get(), mir_buffer_usage_hardware);
+        auto surface = mir_surface_create_sync(spec.get());
+        MirEventDelegate delegate = {&on_event, this};
+        mir_surface_set_event_handler(surface, &delegate);
+        return surface;
+    }
+    static void on_event(MirSurface*, const MirEvent *event, void *context)
+    {
+        auto surface = reinterpret_cast<Surface*>(context);
+        if (surface) surface->on_event(event);
+    }
+};
+}
+
+int main(int argc, char *argv[])
+try
+{
+    (void) argc; (void) argv;
+    auto connection = std::make_shared<Connection>();
+    Surface surface(connection, 768, 1280);
+    while (true){ std::this_thread::sleep_for(std::chrono::seconds(1)); }
+}
+catch(std::runtime_error& e)
+{
+    printf("ERR. %s\n", e.what());
+}
