6f52e96f0eb5766835672b104874d0cae9fd168d
Alexandros Frantzis <alexandros.frantzis@canonical.com>
2015-03-23 13:50:35 +0200

    Revert commit 2297 'Always use double buffers instead of triple. (LP: #1240909)'
    Various applications on the phone (notably the Apps scope) are not fast enough
    to work smoothly with double buffering.

--- src/server/compositor/buffer_stream_factory.cpp
+++ src/server/compositor/buffer_stream_factory.cpp
@@ -47,6 +47,7 @@ mc::BufferStreamFactory::BufferStreamFactory(std::shared_ptr<mg::GraphicBufferAl
 std::shared_ptr<mc::BufferStream> mc::BufferStreamFactory::create_buffer_stream(
     mg::BufferProperties const& buffer_properties)
 {
-    auto switching_bundle = std::make_shared<mc::BufferQueue>(2, gralloc, buffer_properties, *policy_factory);
+    // Note: Framedropping requires a minimum 3 buffers
+    auto switching_bundle = std::make_shared<mc::BufferQueue>(3, gralloc, buffer_properties, *policy_factory);
     return std::make_shared<mc::BufferStreamSurfaces>(switching_bundle);
 }
