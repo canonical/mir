964b366950c049ff4d4a712b8427059814119223
Daniel van Vugt <daniel.van.vugt@canonical.com>
2015-03-31 17:14:49 +0800

    Prototype texturing.
    Starting again from scratch since yesterday's work was lost by accident.

---
+++ examples/target.c
@@ -0,0 +1,157 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Daniel van Vugt <daniel.van.vugt@canonical.com>
+ */
+
+#include "eglapp.h"
+#include <assert.h>
+#include <stdio.h>
+#include <GLES2/gl2.h>
+
+static GLuint load_shader(const char *src, GLenum type)
+{
+    GLuint shader = glCreateShader(type);
+    if (shader)
+    {
+        GLint compiled;
+        glShaderSource(shader, 1, &src, NULL);
+        glCompileShader(shader);
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+        if (!compiled)
+        {
+            GLchar log[1024];
+            glGetShaderInfoLog(shader, sizeof log - 1, NULL, log);
+            log[sizeof log - 1] = '\0';
+            printf("load_shader compile failed: %s\n", log);
+            glDeleteShader(shader);
+            shader = 0;
+        }
+    }
+    return shader;
+}
+
+GLuint generate_texture()
+{
+    const int width = 256, height = width;
+    typedef struct { GLubyte r, b, g, a; }  Texel;
+    Texel image[height][width];
+
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            const Texel flip[2] = {{0,255,0,255}, {255,0,255,255}};
+            image[y][x] = flip[(x ^ y) & 1];
+        }
+    }
+
+    GLuint tex;
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, image);
+
+    return tex;
+}
+
+int main(int argc, char *argv[])
+{
+    const char vshadersrc[] =
+        "attribute vec2 position;\n"
+        "attribute vec2 texcoord;\n"
+        "varying vec2 v_texcoord;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(position, 0.0, 1.0);\n"
+        "    v_texcoord = texcoord;\n"
+        "}\n";
+
+    const char fshadersrc[] =
+        "precision mediump float;\n"
+        "varying vec2 v_texcoord;\n"
+        "uniform sampler2D texture;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_FragColor = texture2D(texture, v_texcoord);\n"
+        "}\n";
+
+    GLuint vshader, fshader, prog;
+    GLint linked;
+    unsigned int width = 512, height = 512;
+
+    if (!mir_eglapp_init(argc, argv, &width, &height))
+        return 1;
+
+    vshader = load_shader(vshadersrc, GL_VERTEX_SHADER);
+    assert(vshader);
+    fshader = load_shader(fshadersrc, GL_FRAGMENT_SHADER);
+    assert(fshader);
+    prog = glCreateProgram();
+    assert(prog);
+    glAttachShader(prog, vshader);
+    glAttachShader(prog, fshader);
+    glLinkProgram(prog);
+
+    glGetProgramiv(prog, GL_LINK_STATUS, &linked);
+    if (!linked)
+    {
+        GLchar log[1024];
+        glGetProgramInfoLog(prog, sizeof log - 1, NULL, log);
+        log[sizeof log - 1] = '\0';
+        printf("Link failed: %s\n", log);
+        return 2;
+    }
+
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glViewport(0, 0, width, height);
+
+    glUseProgram(prog);
+
+    const GLfloat square[] =
+    { // position      texcoord
+        -0.5f, +0.5f,  0.0f, 1.0f,
+        +0.5f, +0.5f,  1.0f, 1.0f,
+        +0.5f, -0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.0f, 0.0f,
+    };
+    GLint position = glGetAttribLocation(prog, "position");
+    GLint texcoord = glGetAttribLocation(prog, "texcoord");
+    glVertexAttribPointer(position, 2, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat),
+                          square);
+    glVertexAttribPointer(texcoord, 2, GL_FLOAT, GL_FALSE, 4*sizeof(GLfloat),
+                          square+2);
+
+    glEnableVertexAttribArray(position);
+    glEnableVertexAttribArray(texcoord);
+
+    GLuint tex = generate_texture();
+    glBindTexture(GL_TEXTURE_2D, tex);
+
+    while (mir_eglapp_running())
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        mir_eglapp_swap_buffers();
+    }
+
+    mir_eglapp_shutdown();
+
+    return 0;
+}
