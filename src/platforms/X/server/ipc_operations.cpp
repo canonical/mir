233aaa5fe5a075733de045e5538a329a9e0686ab
Cemil Azizoglu <cemil.azizoglu@canonical.com>
2015-04-08 16:20:58 -0500

    Enable pack buffer.

--- src/platforms/X/server/ipc_operations.cpp
+++ src/platforms/X/server/ipc_operations.cpp
@@ -35,10 +35,10 @@ mgx::IpcOperations::IpcOperations()
 }
 
 void mgx::IpcOperations::pack_buffer(
-    mg::BufferIpcMessage& /*packer*/, Buffer const& /*buffer*/, BufferIpcMsgType /*msg_type*/) const
+    mg::BufferIpcMessage& packer, Buffer const& buffer, BufferIpcMsgType msg_type) const
 {
     CALLED
-#if 0
+
     if (msg_type == mg::BufferIpcMsgType::full_msg)
     {
         auto native_handle = buffer.native_buffer_handle();
@@ -55,7 +55,6 @@ void mgx::IpcOperations::pack_buffer(
         packer.pack_flags(native_handle->flags);
         packer.pack_size(buffer.size());
     }
-#endif
 }
 
 void mgx::IpcOperations::unpack_buffer(BufferIpcMessage&, Buffer const&) const
@@ -67,60 +66,8 @@ mg::PlatformOperationMessage mgx::IpcOperations::platform_operation(
     unsigned int const /*op*/, mg::PlatformOperationMessage const& /*request*/)
 {
     CALLED
-#if 0
-    if (op == MirMesaPlatformOperation::auth_magic)
-    {
-        MirMesaAuthMagicRequest auth_magic_request;
-        if (request.data.size() == sizeof(auth_magic_request))
-        {
-            std::memcpy(&auth_magic_request, request.data.data(), request.data.size());
-        }
-        else
-        {
-            BOOST_THROW_EXCEPTION(
-                std::runtime_error("Invalid request message for auth_magic platform operation"));
-        }
 
-        MirMesaAuthMagicResponse auth_magic_response{-1};
-
-        try
-        {
-            drm_auth->auth_magic(auth_magic_request.magic);
-            auth_magic_response.status = 0;
-        }
-        catch (std::exception const& e)
-        {
-            auto errno_ptr = boost::get_error_info<boost::errinfo_errno>(e);
-
-            if (errno_ptr != nullptr)
-                auth_magic_response.status = *errno_ptr;
-            else
-                throw;
-        }
-
-        mg::PlatformOperationMessage response_msg;
-        response_msg.data.resize(sizeof(auth_magic_response));
-        std::memcpy(response_msg.data.data(), &auth_magic_response, sizeof(auth_magic_response));
-        return response_msg;
-    }
-    else if (op == MirMesaPlatformOperation::auth_fd)
-    {
-        if (request.data.size() != 0 || request.fds.size() != 0)
-        {
-            BOOST_THROW_EXCEPTION(
-                std::runtime_error("Invalid request message for auth_fd platform operation"));
-        }
-
-        return mg::PlatformOperationMessage{{},{drm_auth->authenticated_fd()}};
-    }
-    else
-    {
-        BOOST_THROW_EXCEPTION(
-            std::runtime_error("Invalid platform operation"));
-    }
-#else
     BOOST_THROW_EXCEPTION(std::invalid_argument("X platform does not support any platform operations"));
-#endif
 }
 
 std::shared_ptr<mg::PlatformIPCPackage> mgx::IpcOperations::connection_ipc_package()
