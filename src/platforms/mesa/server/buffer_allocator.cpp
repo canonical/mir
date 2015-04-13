83f111dbeafe6e5aa02fcfcdde78b3b38a79dec2
Kevin DuBois <kevin.dubois@canonical.com>
2015-04-13 08:12:38 -0400

    braces and spaces

--- src/platforms/mesa/server/buffer_allocator.cpp
+++ src/platforms/mesa/server/buffer_allocator.cpp
@@ -247,9 +247,12 @@ std::unique_ptr<mg::Buffer> mgm::BufferAllocator::reconstruct_from(
     std::shared_ptr<gbm_bo> bo(
         gbm_bo_import(device, GBM_BO_IMPORT_FD, &data, package->flags),
         [](gbm_bo* bo){ gbm_bo_destroy(bo); });
+
     if (!bo)
+    {
         BOOST_THROW_EXCEPTION(
             std::system_error(errno, std::system_category(), "Failed to import MirBufferPackage"));
+    }
 
     return std::make_unique<mgm::GBMBuffer>(
         bo,
