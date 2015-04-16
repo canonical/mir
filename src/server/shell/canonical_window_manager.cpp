1963b386a7fa3523f6835380f6bd273c1e11b745
Alan Griffiths <alan@octopull.co.uk>
2015-04-16 18:15:02 +0100

    sync active display code from examples to shell

--- src/server/shell/canonical_window_manager.cpp
+++ src/server/shell/canonical_window_manager.cpp
@@ -90,6 +90,8 @@ auto msh::CanonicalWindowManagerPolicy::handle_place_new_surface(
 {
     auto parameters = request_parameters;
 
+    auto const active_display = tools->active_display();
+
     auto width = std::min(display_area.size.width.as_int(), parameters.size.width.as_int());
     auto height = std::min(display_area.size.height.as_int(), parameters.size.height.as_int());
 
@@ -132,7 +134,7 @@ auto msh::CanonicalWindowManagerPolicy::handle_place_new_surface(
 
             // "If there is not room to do that, Mir should place it as if it was the app’s
             // only regular surface."
-            positioned = display_area.contains(parameters.top_left + as_displacement(parameters.size));
+            positioned = active_display.contains(parameters.top_left + as_displacement(parameters.size));
         }
     }
 
@@ -147,12 +149,12 @@ auto msh::CanonicalWindowManagerPolicy::handle_place_new_surface(
 
         if (edge_attachment && mir_edge_attachment_vertical)
         {
-            if (display_area.contains(top_right + Displacement{width, height}))
+            if (active_display.contains(top_right + Displacement{width, height}))
             {
                 parameters.top_left = top_right;
                 positioned = true;
             }
-            else if (display_area.contains(top_left + Displacement{-width, height}))
+            else if (active_display.contains(top_left + Displacement{-width, height}))
             {
                 parameters.top_left = top_left + Displacement{-width, 0};
                 positioned = true;
@@ -166,7 +168,7 @@ auto msh::CanonicalWindowManagerPolicy::handle_place_new_surface(
                 parameters.top_left = bot_left;
                 positioned = true;
             }
-            else if (display_area.contains(top_left + Displacement{width, -height}))
+            else if (active_display.contains(top_left + Displacement{width, -height}))
             {
                 parameters.top_left = top_left + Displacement{0, -height};
                 positioned = true;
@@ -180,8 +182,8 @@ auto msh::CanonicalWindowManagerPolicy::handle_place_new_surface(
         // opening it, Mir should position it horizontally centered, and vertically
         // such that the top margin is half the bottom margin. (Vertical centering
         // would look too low, and would allow little room for cascading.)"
-        auto centred = display_area.top_left + 0.5*(
-            as_displacement(display_area.size) - as_displacement(parameters.size));
+        auto centred = active_display.top_left + 0.5*(
+            as_displacement(active_display.size) - as_displacement(parameters.size));
 
         parameters.top_left = centred - DeltaY{(display_area.size.height.as_int()-height)/6};
     }
