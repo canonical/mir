c3fb407d74ac9f0db566f28586c421407e9c79e6
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2015-05-11 12:41:53 +1000

    Rename map→ store in PersistentSurfaceStore tests

--- tests/unit-tests/shell/test_persistent_surface_store.cpp
+++ tests/unit-tests/shell/test_persistent_surface_store.cpp
@@ -36,12 +36,12 @@ TEST(PersistentSurfaceStore, id_for_surface_is_idempotent)
 {
     using namespace testing;
 
-    msh::DefaultPersistentSurfaceStore map;
+    msh::DefaultPersistentSurfaceStore store;
 
     auto surface = std::make_shared<NiceMock<mtd::MockSurface>>();
 
-    auto& id_one = map.id_for_surface(surface);
-    auto& id_two = map.id_for_surface(surface);
+    auto& id_one = store.id_for_surface(surface);
+    auto& id_two = store.id_for_surface(surface);
 
     EXPECT_THAT(id_one, Eq(std::ref(id_two)));
 }
@@ -50,13 +50,13 @@ TEST(PersistentSurfaceStore, id_is_stable_under_aliasing)
 {
     using namespace testing;
 
-    msh::DefaultPersistentSurfaceStore map;
+    msh::DefaultPersistentSurfaceStore store;
 
     auto surface = std::make_shared<NiceMock<mtd::MockSurface>>();
     auto surface_alias = surface;
 
-    auto& id_one = map.id_for_surface(surface);
-    auto& id_two = map.id_for_surface(surface_alias);
+    auto& id_one = store.id_for_surface(surface);
+    auto& id_two = store.id_for_surface(surface_alias);
 
     EXPECT_THAT(id_one, Eq(std::ref(id_two)));
 }
@@ -65,13 +65,13 @@ TEST(PersistentSurfaceStore, can_lookup_surface_by_id)
 {
     using namespace testing;
 
-    msh::DefaultPersistentSurfaceStore map;
+    msh::DefaultPersistentSurfaceStore store;
 
     auto surface = std::make_shared<NiceMock<mtd::MockSurface>>();
 
-    auto& id = map.id_for_surface(surface);
+    auto& id = store.id_for_surface(surface);
 
-    auto looked_up_surface = map.surface_for_id(id);
+    auto looked_up_surface = store.surface_for_id(id);
 
     EXPECT_THAT(looked_up_surface, Eq(surface));
 }
@@ -80,16 +80,16 @@ TEST(PersistentSurfaceStore, retrieves_correct_surface)
 {
     using namespace testing;
 
-    msh::DefaultPersistentSurfaceStore map;
+    msh::DefaultPersistentSurfaceStore store;
 
     auto surface_one = std::make_shared<NiceMock<mtd::MockSurface>>();
     auto surface_two = std::make_shared<NiceMock<mtd::MockSurface>>();
 
-    auto& id_one = map.id_for_surface(surface_one);
-    auto& id_two = map.id_for_surface(surface_two);
+    auto& id_one = store.id_for_surface(surface_one);
+    auto& id_two = store.id_for_surface(surface_two);
 
-    auto looked_up_surface_one = map.surface_for_id(id_one);
-    auto looked_up_surface_two = map.surface_for_id(id_two);
+    auto looked_up_surface_one = store.surface_for_id(id_one);
+    auto looked_up_surface_two = store.surface_for_id(id_two);
 
     EXPECT_THAT(looked_up_surface_one, Eq(surface_one));
     EXPECT_THAT(looked_up_surface_two, Eq(surface_two));
@@ -99,14 +99,14 @@ TEST(PersistentSurfaceStore, can_roundtrip_ids_to_strings)
 {
     using namespace testing;
 
-    msh::DefaultPersistentSurfaceStore map;
+    msh::DefaultPersistentSurfaceStore store;
 
     auto surface = std::make_shared<NiceMock<mtd::MockSurface>>();
 
-    auto& id_one = map.id_for_surface(surface);
+    auto& id_one = store.id_for_surface(surface);
 
-    auto buf = map.serialise_id(id_one);
-    auto& id_two = map.deserialise_id(buf);
+    auto buf = store.serialise_id(id_one);
+    auto& id_two = store.deserialise_id(buf);
 
     EXPECT_THAT(id_one, Eq(std::ref(id_two)));
 }
@@ -115,21 +115,21 @@ TEST(PersistentSurfaceStore, deserialising_wildly_incorrect_buffer_raises_except
 {
     using namespace testing;
 
-    msh::DefaultPersistentSurfaceStore map;
+    msh::DefaultPersistentSurfaceStore store;
 
     std::vector<uint8_t> buf(5, 'a');
 
-    EXPECT_THROW(map.deserialise_id(buf), std::invalid_argument);
+    EXPECT_THROW(store.deserialise_id(buf), std::invalid_argument);
 }
 
 TEST(PersistentSurfaceStore, deserialising_invalid_buffer_raises_exception)
 {
     using namespace testing;
 
-    msh::DefaultPersistentSurfaceStore map;
+    msh::DefaultPersistentSurfaceStore store;
 
     // This is the right size, but isn't a UUID because it lacks the XX-XX-XX structure
     std::vector<uint8_t> buf(36, 'a');
 
-    EXPECT_THROW(map.deserialise_id(buf), std::invalid_argument);
+    EXPECT_THROW(store.deserialise_id(buf), std::invalid_argument);
 }
