b5b20f8f2bf61d46eaf401d833b02a46bdc778da
Alan Griffiths <alan@octopull.co.uk>
2015-05-15 14:32:33 +0100

    The client also needs the hack

--- src/platforms/mesa/client/client_platform.cpp
+++ src/platforms/mesa/client/client_platform.cpp
@@ -26,6 +26,7 @@
 #include "mir_toolkit/mesa/platform_operation.h"
 
 #include <cstring>
+#include <dlfcn.h>
 
 namespace mcl=mir::client;
 namespace mclm=mir::client::mesa;
@@ -55,6 +56,18 @@ constexpr size_t division_ceiling(size_t a, size_t b)
     return ((a - 1) / b) + 1;
 }
 
+// Hack around the way mesa loads mir: This hack makes the
+// necessary symbols global.
+void ensure_loaded_with_rtld_global()
+{
+    Dl_info info;
+
+    // Cast dladdr itself to work around g++-4.8 warnings (LP: #1366134)
+    typedef int (safe_dladdr_t)(void(*func)(), Dl_info *info);
+    safe_dladdr_t *safe_dladdr = (safe_dladdr_t*)&dladdr;
+    safe_dladdr(&ensure_loaded_with_rtld_global, &info);
+    dlopen(info.dli_fname,  RTLD_NOW | RTLD_NOLOAD | RTLD_GLOBAL);
+}
 }
 
 mclm::ClientPlatform::ClientPlatform(
@@ -66,6 +79,7 @@ mclm::ClientPlatform::ClientPlatform(
       display_container(display_container),
       gbm_dev{nullptr}
 {
+    ensure_loaded_with_rtld_global();
 }
 
 std::shared_ptr<mcl::ClientBufferFactory> mclm::ClientPlatform::create_buffer_factory()
