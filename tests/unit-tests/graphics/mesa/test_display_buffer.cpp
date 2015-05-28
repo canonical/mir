a46e97e3f7faadf406521724d3c20e824f52a772
Daniel van Vugt <daniel.van.vugt@canonical.com>
2015-05-28 18:01:07 +0800

    Add a regression test

--- tests/unit-tests/graphics/mesa/test_display_buffer.cpp
+++ tests/unit-tests/graphics/mesa/test_display_buffer.cpp
@@ -154,6 +154,29 @@ TEST_F(MesaDisplayBufferTest, bypass_buffer_is_held_for_full_frame)
     EXPECT_EQ(original_count, mock_bypassable_buffer.use_count());
 }
 
+TEST_F(MesaDisplayBufferTest, bypass_buffer_only_referenced_once_by_db)
+{
+    graphics::mesa::DisplayBuffer db(
+        create_platform(),
+        null_display_report(),
+        {mock_kms_output},
+        nullptr,
+        display_area,
+        mir_orientation_normal,
+        gl_config,
+        mock_egl.fake_egl_context);
+
+    auto original_count = mock_bypassable_buffer.use_count();
+
+    EXPECT_TRUE(db.post_renderables_if_optimizable(bypassable_list));
+    EXPECT_EQ(original_count+1, mock_bypassable_buffer.use_count());
+
+    db.post();
+
+    // Bypass buffer still held by DB only one ref above the original
+    EXPECT_EQ(original_count+1, mock_bypassable_buffer.use_count());
+}
+
 TEST_F(MesaDisplayBufferTest, normal_orientation_with_bypassable_list_can_bypass)
 {
     graphics::mesa::DisplayBuffer db(
