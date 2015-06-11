87724f2e3ab5c62789a2c1e1ae34df720a8d2222
Cemil Azizoglu <cemil.azizoglu@canonical.com>
2015-06-11 14:40:10 -0500

    Add x11 platform tests.

--- tests/unit-tests/graphics/mesa/x11/test_platform.cpp
+++ tests/unit-tests/graphics/mesa/x11/test_platform.cpp
@@ -20,22 +20,30 @@
 
 #include "mir_test_doubles/platform_factory.h"
 #include "mir_test_doubles/mock_drm.h"
+#include "mir_test_doubles/mock_gbm.h"
+#include "mir_test_doubles/mock_x11.h"
+#include "mir/shared_library.h"
+#include "mir_test_framework/executable_path.h"
+#include "mir_test_framework/udev_environment.h"
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
 namespace mg = mir::graphics;
 namespace mtd = mir::test::doubles;
+namespace mtf = mir_test_framework;
 
 namespace
 {
-class X11GraphicsPlatform : public ::testing::Test
+const char probe_platform[] = "probe_graphics_platform";
+
+class X11GraphicsPlatformTest : public ::testing::Test
 {
 public:
     void SetUp()
     {
-//        ::testing::Mock::VerifyAndClearExpectations(&mock_drm);
-//        ::testing::Mock::VerifyAndClearExpectations(&mock_gbm);
+        ::testing::Mock::VerifyAndClearExpectations(&mock_drm);
+        ::testing::Mock::VerifyAndClearExpectations(&mock_gbm);
 //        fake_devices.add_standard_device("standard-drm-devices");
     }
 
@@ -44,22 +52,23 @@ public:
         return mtd::create_platform_with_null_dependencies();
     }
 
-//    ::testing::NiceMock<mtd::MockDRM> mock_drm;
-//    ::testing::NiceMock<mtd::MockGBM> mock_gbm;
+    ::testing::NiceMock<mtd::MockDRM> mock_drm;
+    ::testing::NiceMock<mtd::MockGBM> mock_gbm;
+    ::testing::NiceMock<mtd::MockX11> mock_x11;
 //    mtf::UdevEnvironment fake_devices;
 };
 }
 
-TEST_F(X11GraphicsPlatform, a_failure_while_creating_a_platform_results_in_an_error)
+TEST_F(X11GraphicsPlatformTest, failure_to_open_x11_display_results_in_an_error)
 {
     using namespace ::testing;
 
-//    EXPECT_CALL(mock_drm, open(_,_,_))
-//        .WillRepeatedly(Return(-1));
+    EXPECT_CALL(mock_x11, XOpenDisplay(_))
+        .WillRepeatedly(Return(nullptr));
 
     try
     {
-//        auto platform = create_platform();
+        auto platform = create_platform();
     } catch(...)
     {
         return;
@@ -68,3 +77,70 @@ TEST_F(X11GraphicsPlatform, a_failure_while_creating_a_platform_results_in_an_er
     FAIL() << "Expected an exception to be thrown.";
 }
 
+TEST_F(X11GraphicsPlatformTest, failure_to_open_drm_results_in_an_error)
+{
+    using namespace ::testing;
+
+    EXPECT_CALL(mock_drm, open(_,_,_))
+            .WillRepeatedly(Return(-1));
+
+    try
+    {
+        auto platform = create_platform();
+    } catch(...)
+    {
+        return;
+    }
+
+    FAIL() << "Expected an exception to be thrown.";
+}
+
+TEST_F(X11GraphicsPlatformTest, failure_to_create_gbm_device_results_in_an_error)
+{
+    using namespace ::testing;
+
+    EXPECT_CALL(mock_gbm, gbm_create_device(mock_drm.fake_drm.fd()))
+        .WillRepeatedly(Return(nullptr));
+
+    try
+    {
+        auto platform = create_platform();
+    } catch(...)
+    {
+        return;
+    }
+
+    FAIL() << "Expected an exception to be thrown.";
+}
+
+TEST_F(X11GraphicsPlatformTest, probe_returns_unsupported_when_no_drm_udev_devices)
+{
+    mtf::UdevEnvironment udev_environment;
+
+    mir::SharedLibrary platform_lib{mtf::server_platform("server-mesa-x11")};
+    auto probe = platform_lib.load_function<mg::PlatformProbe>(probe_platform);
+    EXPECT_EQ(mg::PlatformPriority::unsupported, probe());
+}
+
+TEST_F(X11GraphicsPlatformTest, probe_returns_unsupported_when_x_cannot_open_display)
+{
+    using namespace ::testing;
+
+    EXPECT_CALL(mock_x11, XOpenDisplay(_))
+        .WillRepeatedly(Return(nullptr));
+
+    mir::SharedLibrary platform_lib{mtf::server_platform("server-mesa-x11")};
+    auto probe = platform_lib.load_function<mg::PlatformProbe>(probe_platform);
+    EXPECT_EQ(mg::PlatformPriority::unsupported, probe());
+}
+
+TEST_F(X11GraphicsPlatformTest, probe_returns_best_when_drm_render_nodes_exist)
+{
+    mtf::UdevEnvironment udev_environment;
+
+    udev_environment.add_standard_device("standard-drm-render-nodes");
+
+    mir::SharedLibrary platform_lib{mtf::server_platform("server-mesa-x11")};
+    auto probe = platform_lib.load_function<mg::PlatformProbe>(probe_platform);
+    EXPECT_EQ(mg::PlatformPriority::best, probe());
+}
