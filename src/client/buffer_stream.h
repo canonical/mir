ea32c34e2e14b48f2c1b807fe10a628dfa2efd31
Kevin DuBois <kevin.dubois@canonical.com>
2015-06-19 08:01:07 -0400

    we don\'t need to wait for the rpc callback to complete for the submit_buffer call

--- src/client/buffer_stream.h
+++ src/client/buffer_stream.h
@@ -122,12 +122,9 @@ private:
     void release_cpu_region();
     MirWaitHandle* exchange(std::function<void()> const& done, std::unique_lock<std::mutex> lk);
     MirWaitHandle* submit(std::function<void()> const& done, std::unique_lock<std::mutex> lk);
-    void submit_done();
 
     mutable std::mutex mutex; // Protects all members of *this
 
-    std::condition_variable submit_cv;
-    bool submitting = true;
     bool using_exchange_buffer = true;
     std::function<void()> on_incoming_buffer;
     std::queue<mir::protobuf::Buffer> incoming_buffers;
