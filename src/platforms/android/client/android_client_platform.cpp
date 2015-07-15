cb248ad6e8e7b0584031ada1c49ef85153bc8c3b
Daniel van Vugt <daniel.van.vugt@canonical.com>
2015-07-15 16:27:21 +0800

    Resolve another TODO (inline header location)

--- src/platforms/android/client/android_client_platform.cpp
+++ src/platforms/android/client/android_client_platform.cpp
@@ -17,7 +17,7 @@
  */
 
 #include "mir/graphics/android/mir_native_window.h"
-#include "../server/android_format_conversion-inl.h" // TODO move this
+#include "android_format_conversion-inl.h"
 #include "mir/client_context.h"
 #include "android_client_platform.h"
 #include "gralloc_registrar.h"
