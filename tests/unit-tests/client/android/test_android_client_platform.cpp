d13ecf8f709c54b69d6aa37a71ede26904a2a623
Daniel van Vugt <daniel.van.vugt@canonical.com>
2015-07-16 16:52:54 +0800

    Start an attempt at AndroidClientPlatformTest coverage

--- tests/unit-tests/client/android/test_android_client_platform.cpp
+++ tests/unit-tests/client/android/test_android_client_platform.cpp
@@ -19,31 +19,64 @@
 #include "mir/client_platform.h"
 #include "mir/test/doubles/mock_client_context.h"
 #include "mir/test/doubles/mock_egl_native_surface.h"
+#include "mir/test/doubles/mock_egl.h"
 #include "mir_test_framework/client_platform_factory.h"
 
 #include <EGL/egl.h>
 
 #include <gtest/gtest.h>
 
-namespace mcl = mir::client;
-namespace mt = mir::test;
-namespace mtd = mt::doubles;
-namespace mtf = mir_test_framework;
+using namespace testing;
+using namespace mir::client;
+using namespace mir::test;
+using namespace mir::test::doubles;
+using namespace mir_test_framework;
 
-TEST(AndroidClientPlatformTest, egl_native_display_is_egl_default_display)
+struct AndroidClientPlatformTest : public Test
 {
-    mtd::MockEGLNativeSurface surface;
-    auto platform = mtf::create_android_client_platform();
-    auto mock_egl_native_surface = std::make_shared<mtd::MockEGLNativeSurface>();
+    AndroidClientPlatformTest()
+        : platform{create_android_client_platform()}
+    {
+    }
+
+    void SetUp()
+    {
+    }
+
+    std::shared_ptr<mir::client::ClientPlatform> platform;
+    MockEGL mock_egl;
+};
+
+TEST_F(AndroidClientPlatformTest, egl_native_display_is_egl_default_display)
+{
+    MockEGLNativeSurface surface;
+    auto mock_egl_native_surface = std::make_shared<MockEGLNativeSurface>();
     auto native_display = platform->create_egl_native_display();
     EXPECT_EQ(EGL_DEFAULT_DISPLAY, *native_display);
 }
 
-TEST(AndroidClientPlatformTest, egl_native_window_is_set)
+TEST_F(AndroidClientPlatformTest, egl_native_window_is_set)
 {
-    mtd::MockEGLNativeSurface surface;
-    auto platform = mtf::create_android_client_platform();
-    auto mock_egl_native_surface = std::make_shared<mtd::MockEGLNativeSurface>();
+    MockEGLNativeSurface surface;
+    auto mock_egl_native_surface = std::make_shared<MockEGLNativeSurface>();
     auto egl_native_window = platform->create_egl_native_window(&surface);
     EXPECT_NE(nullptr, egl_native_window);
+}
+
+TEST_F(AndroidClientPlatformTest, egl_pixel_format_asks_the_driver)
+{
+    auto const d = reinterpret_cast<EGLDisplay>(0x1234);
+    auto const c = reinterpret_cast<EGLConfig>(0x5678);
+
+    /*
+     * We can't verify the driver is giving the right answer here, because
+     * firstly there is no driver (it's mocked and/or a could be headless) and
+     * secondly EGLConfig is opaque so we can't construct proper input.
+     * What we can verify is that we're calling the right extension, but
+     * that's arguably not a very strong test...
+     */
+
+    // TODO
+    auto pf = platform->get_egl_pixel_format(d, c);
+    (void)pf;
 }
