a09b0208dd5006a771a8a63d7c7a5f55c08af759
Cemil Azizoglu <cemil.azizoglu@canonical.com>
2015-08-05 15:55:23 -0500

    Fix failing test

--- src/platforms/mesa/server/x11/platform.cpp
+++ src/platforms/mesa/server/x11/platform.cpp
@@ -31,6 +31,9 @@ mgx::Platform::Platform(std::shared_ptr<mx::X11Connection> const& conn)
       udev{std::make_shared<mir::udev::Context>()},
       drm{std::make_shared<mesa::helpers::DRMHelper>(mesa::helpers::DRMNodeToUse::render)}
 {
+    if (!conn)
+        BOOST_THROW_EXCEPTION(std::runtime_error("Need valid x11 display"));
+
     drm->setup(udev);
     gbm.setup(*drm);
 }
