975bef4229af9ba8d1e571cf696ffeaa24f0f3c6
Alan Griffiths <alan@octopull.co.uk>
2015-08-14 12:15:16 +0100

    Delete some __android legacy

--- 3rd_party/android-deps/cutils/logd.h
+++ 3rd_party/android-deps/cutils/logd.h
@@ -34,16 +34,4 @@
 #include <sys/uio.h>
 #include <stdarg.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-int __android_log_bwrite(int32_t tag, const void *payload, size_t len);
-int __android_log_btwrite(int32_t tag, char type, const void *payload,
-    size_t len);
-
-#ifdef __cplusplus
-}
-#endif
-
 #endif /* _LOGD_H */
