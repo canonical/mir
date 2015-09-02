acb2da97d9f8bc2a715e7cf4b55db24209c6734f
Alan Griffiths <alan@octopull.co.uk>
2015-09-02 17:33:14 +0100

    tentative MirBlob API

---
+++ include/client/mir_toolkit/mir_blob.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2015 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MIR_TOOLKIT_MIR_BLOB_H_
+#define MIR_TOOLKIT_MIR_BLOB_H_
+
+#include <mir_toolkit/client_types.h>
+
+#ifdef __cplusplus
+/**
+ * \addtogroup mir_toolkit
+ * @{
+ */
+extern "C" {
+#endif
+
+/**
+ * Create a blob from a display configuration
+ *
+ * \param [in] configuration  The display configuration
+ * \return                 A blob
+ */
+MirBlob* mir_blob_from_display_configuration(MirDisplayConfiguration* configuration);
+
+/**
+ * Create a blob from a display configuration
+ *
+ * \warning will abort() if the blob doesn't represent a meaningful display configuration
+ *
+ * \param [in] blob        The blob
+ * \return                 A display configuration
+ */
+MirDisplayConfiguration* mir_blob_to_display_configuration(MirBlob* blob);
+
+/**
+ * Get the size of a blob
+ * \param [in] blob        The blob
+ * \return                 the size
+ */
+size_t mir_blob_size(MirBlob* blob);
+
+/**
+ * Get the data of a blob
+ * \param [in] blob        The blob
+ * \return                 the data
+ */
+void* mir_blob_data(MirBlob* blob);
+
+/**
+ * Release a blob object
+ * \param [in] blob        The blob
+ */
+void mir_blob_release(MirBlob* blob);
+
+#ifdef __cplusplus
+}
+/**@}*/
+#endif
+
+#endif /* MIR_TOOLKIT_MIR_BLOB_H_ */
