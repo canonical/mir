be8b3a979ba46580f79b0d28cae97f9889d06fa3
Kevin DuBois <kevin.dubois@canonical.com>
2015-09-03 07:36:54 -0400

    add an option to start up using nbs

--- src/server/compositor/buffer_stream_factory.cpp
+++ src/server/compositor/buffer_stream_factory.cpp
@@ -22,6 +22,8 @@
 #include "buffer_stream_surfaces.h"
 #include "mir/graphics/buffer_properties.h"
 #include "buffer_queue.h"
+#include "stream.h"
+#include "buffer_map.h"
 #include "mir/graphics/buffer.h"
 #include "mir/graphics/buffer_id.h"
 #include "mir/graphics/graphic_buffer_allocator.h"
@@ -58,10 +60,19 @@ std::shared_ptr<mc::BufferStream> mc::BufferStreamFactory::create_buffer_stream(
 }
 
 std::shared_ptr<mc::BufferStream> mc::BufferStreamFactory::create_buffer_stream(
-    mf::BufferStreamId, std::shared_ptr<mf::BufferSink> const&,
+    mf::BufferStreamId id, std::shared_ptr<mf::BufferSink> const& sink,
     int nbuffers, mg::BufferProperties const& buffer_properties)
 {
-    auto switching_bundle = std::make_shared<mc::BufferQueue>(
-        nbuffers, gralloc, buffer_properties, *policy_factory);
-    return std::make_shared<mc::BufferStreamSurfaces>(switching_bundle);
+    if (nbuffers == 0)
+    {
+        return std::make_shared<mc::Stream>(
+            std::make_unique<mc::BufferMap>(id, sink, gralloc),
+            buffer_properties.size, buffer_properties.format);
+    }
+    else
+    {
+        auto switching_bundle = std::make_shared<mc::BufferQueue>(
+            nbuffers, gralloc, buffer_properties, *policy_factory);
+        return std::make_shared<mc::BufferStreamSurfaces>(switching_bundle);
+    }
 }
