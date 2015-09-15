36f7179380b1eef84939407c26fc21d0d3422fd9
Alan Griffiths <alan@octopull.co.uk>
2015-09-15 12:24:48 +0100

    Just fix the problem, leave broken code alone

--- tests/integration-tests/graphics/mesa/test_buffer_integration.cpp
+++ tests/integration-tests/graphics/mesa/test_buffer_integration.cpp
@@ -114,7 +114,17 @@ protected:
         if (need_to_clear_env)
             unsetenv(graphics_lib_env);
 
-        platform = std::make_shared<StubGraphicPlatform>();
+        if (options->get<bool>("tests-use-real-graphics"))
+        {
+            platform = create_host_platform(
+                options,
+                std::make_shared<mtd::NullEmergencyCleanup>(),
+                mr::null_display_report());
+        }
+        else
+        {
+            platform = std::make_shared<StubGraphicPlatform>();
+        }
 
         auto conf_policy = std::make_shared<mg::CloneDisplayConfigurationPolicy>();
         display = platform->create_display(
