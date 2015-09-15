6630a85ea14ca19f1de93ae3350bc465456cc985
Andreas Pokorny <andreas.pokorny@canonical.com>
2015-09-15 10:34:39 +0200

    extend update_package_abis script

--- tools/update_package_abis.sh
+++ tools/update_package_abis.sh
@@ -20,7 +20,8 @@ packages="\
     mir-client-platform-mesa:MIR_CLIENT_PLATFORM_ABI \
     mir-platform-graphics-android:MIR_SERVER_GRAPHICS_PLATFORM_ABI \
     mir-platform-graphics-mesa-x:MIR_SERVER_GRAPHICS_PLATFORM_ABI \
-    mir-platform-graphics-mesa-kms:MIR_SERVER_GRAPHICS_PLATFORM_ABI"
+    mir-platform-graphics-mesa-kms:MIR_SERVER_GRAPHICS_PLATFORM_ABI \
+    mir-platform-input-evdev:MIR_SERVER_INPUT_PLATFORM_ABI"
 
 package_name()
 {
