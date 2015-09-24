7b242f43674acdd7c3d77080e7f2571056985cd0
Brandon Schaefer <brandontschaefer@gmail.com>
2015-09-24 12:29:37 -0700

    * TEST COMMIT (need to see what CI thinks the path to the cursor is)

--- tests/unit-tests/input/test_xcursor_loader.cpp
+++ tests/unit-tests/input/test_xcursor_loader.cpp
@@ -91,6 +91,7 @@ void set_xcursor_path()
         old_xcursor_path = strdup(old);
     auto test_cursor_path =mir_test_framework::executable_path() + std::string("/testing-cursor-theme");
     setenv("XCURSOR_PATH", test_cursor_path.c_str(), 1);
+    std::cerr << "PATH: " << test_cursor_path << std::endl;
 }
 
 void restore_xcursor_path()
