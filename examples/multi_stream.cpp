aeb2fa4fcae68a587b424c4ea0a4cd92156b36fe
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2015-10-15 16:04:56 +1100

    Examples: Fix abgr8888 pixel writing.
    We should probably write the red component somewhere rather than just duplicating
    the alpha channel :)

--- examples/multi_stream.cpp
+++ examples/multi_stream.cpp
@@ -46,7 +46,7 @@ public:
                 (uint32_t) a << 24 |
                 (uint32_t) b << 16 |
                 (uint32_t) g << 8 |
-                (uint32_t) a;
+                (uint32_t) r;
             break;
         case mir_pixel_format_xbgr_8888:
             *((uint32_t*) addr) =
