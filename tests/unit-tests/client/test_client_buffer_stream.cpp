54f43f4ddfabef924c18759a716b23e016afb491
Kevin DuBois <kevin.dubois@canonical.com>
2015-10-27 13:58:05 -0400

    add a test that checks the current id

--- tests/unit-tests/client/test_client_buffer_stream.cpp
+++ tests/unit-tests/client/test_client_buffer_stream.cpp
@@ -657,4 +657,23 @@ TEST_P(ClientBufferStream, returns_correct_surface_parameters_with_nondefault_fo
     EXPECT_THAT(params.pixel_format, Eq(format));
 }
 
+TEST_P(ClientBufferStream, keeps_accurate_buffer_id)
+{
+    mcl::BufferStream stream(
+        nullptr, mock_protobuf_server, mode,
+        std::make_shared<StubClientPlatform>(mt::fake_shared(mock_factory)),
+        response, perf_report, "", size, nbuffers);
+    for(auto i = 0u; i < 2; i++)
+    {
+        mp::Buffer buffer;
+        fill_protobuf_buffer_from_package(&buffer, a_buffer_package());
+        buffer.set_buffer_id(i);
+        buffer.set_width(size.width.as_int());
+        buffer.set_height(size.height.as_int());
+        stream.buffer_available(buffer);
+    }
+
+    EXPECT_THAT(stream.get_current_buffer_id(), Eq(1));
+}
+
 INSTANTIATE_TEST_CASE_P(BufferSemanticsMode, ClientBufferStream, Bool());
