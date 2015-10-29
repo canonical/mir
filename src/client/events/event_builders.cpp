2d610b151cc652b4977de74849e0cdebfea34629
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2015-10-29 12:37:02 +1100

    MirSurfaceOutputEvent: Also send the output_id of the output the surface is on.

--- src/client/events/event_builders.cpp
+++ src/client/events/event_builders.cpp
@@ -99,7 +99,8 @@ mir::EventUPtr mev::make_event(
     mf::SurfaceId const& surface_id,
     int dpi,
     float scale,
-    MirFormFactor form_factor)
+    MirFormFactor form_factor,
+    uint32_t output_id)
 {
     auto e = new MirEvent;
     memset(e, 0, sizeof(*e));
@@ -109,6 +110,7 @@ mir::EventUPtr mev::make_event(
     e->surface_output.dpi = dpi;
     e->surface_output.scale = scale;
     e->surface_output.form_factor = form_factor;
+    e->surface_output.output_id = output_id;
 
     return make_event_uptr(e);
 }
