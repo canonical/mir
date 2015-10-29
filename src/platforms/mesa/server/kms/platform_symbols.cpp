c2eb99c9b66f57065f89714b561ab714391eeafa
Andreas Pokorny <andreas.pokorny@canonical.com>
2015-10-29 12:42:17 +0100

    revert the probably incomplete krillin fix
    Having the reload of the kms library done just before guest platform creation seems to be too late. Even though it happens before the first notable call to eglGetDisplay. This will bring back the easily reproducible problems on mediatek devices.

--- src/platforms/mesa/server/kms/platform_symbols.cpp
+++ src/platforms/mesa/server/kms/platform_symbols.cpp
@@ -99,11 +99,17 @@ struct RealPosixProcessOperations : public mgm::PosixProcessOperations
 
 // Hack around the way mesa loads mir: This hack makes the
 // necessary symbols global.
-void ensure_loaded_with_rtld_global()
+extern "C" int __attribute__((constructor))
+ensure_loaded_with_rtld_global()
 {
     Dl_info info;
-    dladdr(reinterpret_cast<void*>(&ensure_loaded_with_rtld_global), &info);
+
+    // Cast dladdr itself to work around g++-4.8 warnings (LP: #1366134)
+    typedef int (safe_dladdr_t)(int(*func)(), Dl_info *info);
+    safe_dladdr_t *safe_dladdr = (safe_dladdr_t*)&dladdr;
+    safe_dladdr(&ensure_loaded_with_rtld_global, &info);
     dlopen(info.dli_fname,  RTLD_NOW | RTLD_NOLOAD | RTLD_GLOBAL);
+    return 0;
 }
 
 }
@@ -189,6 +195,5 @@ std::shared_ptr<mg::Platform> create_guest_platform(
     std::shared_ptr<mg::DisplayReport> const&,
     std::shared_ptr<mg::NestedContext> const& nested_context)
 {
-    ensure_loaded_with_rtld_global();
     return std::make_shared<mgm::GuestPlatform>(nested_context);
 }
