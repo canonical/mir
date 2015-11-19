2bc8c04077e8b4212d11025e069ef65f68fbe3b7
Cemil Azizoglu <cemil.azizoglu@canonical.com>
2015-11-18 18:11:48 -0600

    FTBFS with -DMIR_PLATFORM=android

--- tests/unit-tests/graphics/test_platform_prober.cpp
+++ tests/unit-tests/graphics/test_platform_prober.cpp
@@ -103,8 +103,10 @@ std::shared_ptr<void> ensure_android_probing_succeeds()
 
 class ServerPlatformProbeMockDRM : public ::testing::Test
 {
+#ifndef MIR_BUILD_PLATFORM_ANDROID
 public:
     ::testing::NiceMock<mtd::MockDRM> mock_drm;
+#endif
 };
 
 }
