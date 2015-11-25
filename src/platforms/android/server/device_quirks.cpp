f58c68aac1e33b26d0973e225f9ab036c9d21659
Alexandros Frantzis <alexandros.frantzis@canonical.com>
2015-11-25 11:51:03 +0200

    android: Don't close gralloc if it's not safe to do so on particular devices

--- src/platforms/android/server/device_quirks.cpp
+++ src/platforms/android/server/device_quirks.cpp
@@ -35,7 +35,7 @@ int mga::PropertiesOps::property_get(
 namespace
 {
 char const* const num_framebuffers_opt = "enable-num-framebuffers-quirk";
-char const* const single_gralloc_instance_opt = "enable-single-gralloc-instance-quirk";
+char const* const gralloc_can_be_safely_closed_opt = "enable-gralloc-can-be-safely-closed-quirk";
 char const* const width_alignment_opt = "enable-width-alignment-quirk";
 
 std::string determine_device_name(mga::PropertiesWrapper const& properties)
@@ -55,7 +55,7 @@ unsigned int num_framebuffers_for(std::string const& device_name, bool quirk_ena
         return 2;
 }
 
-unsigned int gralloc_reopenable_after_close_for(std::string const& device_name, bool quirk_enabled)
+unsigned int gralloc_can_be_safely_closed_for(std::string const& device_name, bool quirk_enabled)
 {
     return !(quirk_enabled && device_name == std::string{"krillin"});
 }
@@ -64,7 +64,7 @@ unsigned int gralloc_reopenable_after_close_for(std::string const& device_name, 
 mga::DeviceQuirks::DeviceQuirks(PropertiesWrapper const& properties)
     : device_name(determine_device_name(properties)),
       num_framebuffers_(num_framebuffers_for(device_name, true)),
-      gralloc_reopenable_after_close_(gralloc_reopenable_after_close_for(device_name, true)),
+      gralloc_can_be_safely_closed_(gralloc_can_be_safely_closed_for(device_name, true)),
       enable_width_alignment_quirk{true}
 {
 }
@@ -72,7 +72,7 @@ mga::DeviceQuirks::DeviceQuirks(PropertiesWrapper const& properties)
 mga::DeviceQuirks::DeviceQuirks(PropertiesWrapper const& properties, mo::Option const& options)
     : device_name(determine_device_name(properties)),
       num_framebuffers_(num_framebuffers_for(device_name, options.get(num_framebuffers_opt, true))),
-      gralloc_reopenable_after_close_(gralloc_reopenable_after_close_for(device_name, options.get(single_gralloc_instance_opt, true))),
+      gralloc_can_be_safely_closed_(gralloc_can_be_safely_closed_for(device_name, options.get(gralloc_can_be_safely_closed_opt, true))),
       enable_width_alignment_quirk(options.get(width_alignment_opt, true))
 {
 }
@@ -82,9 +82,9 @@ unsigned int mga::DeviceQuirks::num_framebuffers() const
     return num_framebuffers_;
 }
 
-bool mga::DeviceQuirks::gralloc_reopenable_after_close() const
+bool mga::DeviceQuirks::gralloc_can_be_safely_closed() const
 {
-    return gralloc_reopenable_after_close_;
+    return gralloc_can_be_safely_closed_;
 }
 
 int mga::DeviceQuirks::aligned_width(int width) const
@@ -100,9 +100,9 @@ void mga::DeviceQuirks::add_options(boost::program_options::options_description&
         (num_framebuffers_opt,
          boost::program_options::value<bool>()->default_value(true),
          "[platform-specific] Enable allocating 3 framebuffers (MX3 quirk) [{true,false}]")
-        (single_gralloc_instance_opt,
+        (gralloc_can_be_safely_closed_opt,
          boost::program_options::value<bool>()->default_value(true),
-         "[platform-specific] Allocate a single gralloc instance (krillin quirk)  [{true,false}]")
+         "[platform-specific] Only close gralloc if it is safe to do so (krillin quirk)  [{true,false}]")
          (width_alignment_opt,
           boost::program_options::value<bool>()->default_value(true),
           "[platform-specific] Enable width alignment (vegetahd quirk) [{true,false}]");
--- src/platforms/android/server/device_quirks.h
+++ src/platforms/android/server/device_quirks.h
@@ -61,7 +61,7 @@ public:
     DeviceQuirks(PropertiesWrapper const& properties, mir::options::Option const& options);
 
     unsigned int num_framebuffers() const;
-    bool gralloc_reopenable_after_close() const;
+    bool gralloc_can_be_safely_closed() const;
     int aligned_width(int width) const;
 
     static void add_options(boost::program_options::options_description& config);
@@ -71,7 +71,7 @@ private:
     DeviceQuirks & operator=(DeviceQuirks const&) = delete;
     std::string const device_name;
     unsigned int const num_framebuffers_;
-    bool const gralloc_reopenable_after_close_;
+    bool const gralloc_can_be_safely_closed_;
     bool const enable_width_alignment_quirk;
 };
 }
