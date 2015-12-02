1a60ce2f07ae3bb1f3baeecb58720130b7f7925e
Kevin DuBois <kevin.dubois@canonical.com>
2015-12-02 07:55:53 -0500

    adopt Albertos suggestion to match the sequence of events in the surface

--- tests/acceptance-tests/test_client_focus_notification.cpp
+++ tests/acceptance-tests/test_client_focus_notification.cpp
@@ -35,62 +35,13 @@ using namespace ::testing;
 
 namespace
 {
-struct FocusLogger
-{
-    void log_focus_event(MirSurface* surface, MirSurfaceFocusState state)
-    {
-        std::lock_guard<std::mutex> lk(mutex);
-        focus_events[surface].push_back(state);
-        cv.notify_all();
-    }
-
-    void wait_for_num_focus_events(unsigned int num, MirSurface* surface, std::chrono::seconds until)
-    {
-        std::unique_lock<std::mutex> lk(mutex);
-        if (!cv.wait_for(lk, until, [this, num, surface]
-            {
-                return ((focus_events.find(surface) != focus_events.end()) &&
-                    (focus_events[surface].size() >= num));
-            }))
-        {
-            throw std::logic_error("timeout waiting for events");
-        }
-    }
-
-    std::vector<MirSurfaceFocusState> events_for(MirSurface* surface)
-    {
-        std::lock_guard<std::mutex> lk(mutex);
-        if (focus_events.find(surface) == focus_events.end()) 
-            throw std::logic_error("no events");
-        return focus_events[surface];
-    }
-
-    static void handle_event(MirSurface* surface, MirEvent const* ev, void* context)
-    {
-        if (mir_event_type_surface == mir_event_get_type(ev))
-        {
-            auto surface_ev = mir_event_get_surface_event(ev);
-            if (mir_surface_attrib_focus == mir_surface_event_get_attribute(surface_ev))
-            {
-                auto self = static_cast<FocusLogger*>(context);
-                self->log_focus_event(surface,
-                    static_cast<MirSurfaceFocusState>(mir_surface_event_get_attribute_value(surface_ev)));
-            }
-        }
-    }
-private:
-    std::mutex mutex;
-    std::condition_variable cv;
-    std::map<MirSurface*, std::vector<MirSurfaceFocusState>> focus_events;
-};
-
 struct FocusSurface
 {
-    FocusSurface(MirConnection* connection, std::shared_ptr<FocusLogger> const& logger) :
+    FocusSurface(MirConnection* connection) :
         connection(connection)
     {
         auto spec = mir_connection_create_spec_for_normal_surface(connection, 100, 100, mir_pixel_format_abgr_8888);
-        mir_surface_spec_set_event_handler(spec, FocusLogger::handle_event, logger.get());
+        mir_surface_spec_set_event_handler(spec, FocusSurface::handle_event, this);
 
         surface = mir_surface_create_sync(spec);
         mir_surface_spec_release(spec);
@@ -101,6 +52,27 @@ struct FocusSurface
     ~FocusSurface()
     {
         if (!released) release();
+    }
+
+    static void handle_event(MirSurface* surface, MirEvent const* ev, void* context)
+    {
+        if (mir_event_type_surface == mir_event_get_type(ev))
+        {
+            auto surface_ev = mir_event_get_surface_event(ev);
+            if (mir_surface_attrib_focus == mir_surface_event_get_attribute(surface_ev))
+            {
+                auto self = static_cast<FocusSurface*>(context);
+                self->log_focus_event(surface,
+                    static_cast<MirSurfaceFocusState>(mir_surface_event_get_attribute_value(surface_ev)));
+            }
+        }
+    }
+
+    void log_focus_event(MirSurface*, MirSurfaceFocusState state)
+    {
+        std::lock_guard<std::mutex> lk(mutex);
+        focus_events.push_back(state);
+        cv.notify_all();
     }
 
     MirSurface* native_handle() const
@@ -115,46 +87,63 @@ struct FocusSurface
         released = true;
     }
 
-    MirConnection* const connection;
-    MirSurface* surface;
-    bool released{false};
+    void expect_focus_event_sequence(std::vector<MirSurfaceFocusState> const& seq)
+    {
+        std::unique_lock<std::mutex> lk(mutex);
+        if (!cv.wait_for(lk, timeout, [this, &seq]
+            {
+                return focus_events.size() >= seq.size();
+            }))
+        {
+            throw std::logic_error("timeout waiting for events");
+        }
+        EXPECT_THAT(focus_events, ContainerEq(seq));
+    }
+
+private:
+    std::mutex mutex;
+    std::condition_variable cv;
+    std::vector<MirSurfaceFocusState> focus_events;
+    bool released {false};
+    MirConnection* connection = nullptr;
+    MirSurface* surface = nullptr;
+    std::chrono::seconds timeout{5};
 };
 
 struct ClientFocusNotification : mtf::ConnectedClientHeadlessServer
 {
-    std::shared_ptr<FocusLogger> logger{std::make_shared<FocusLogger>()};
+    std::unique_ptr<FocusSurface> make_surface()
+    {
+        auto connection = mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__);
+        EXPECT_THAT(connection, NotNull());
+        return std::make_unique<FocusSurface>(connection);
+    }
 };
 }
 
 TEST_F(ClientFocusNotification, a_surface_is_notified_of_receiving_focus)
 {
-    FocusSurface surface(mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__), logger);
-    surface.release();
-
-    logger->wait_for_num_focus_events(2, surface.native_handle(), std::chrono::seconds(5));
-
-    auto log = logger->events_for(surface.native_handle());
-    EXPECT_THAT(log[0], Eq(mir_surface_focused));
-    EXPECT_THAT(log[1], Eq(mir_surface_unfocused));
+    std::vector<MirSurfaceFocusState> const focus_sequence = {mir_surface_focused, mir_surface_unfocused};
+    auto surface = make_surface();
+    surface->release();
+    surface->expect_focus_event_sequence(focus_sequence);
 }
 
 TEST_F(ClientFocusNotification, two_surfaces_are_notified_of_gaining_and_losing_focus)
 {
-    FocusSurface surface1(mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__), logger);
-    logger->wait_for_num_focus_events(1, surface1.native_handle(), std::chrono::seconds(5));
-    FocusSurface surface2(mir_connect_sync(new_connection().c_str(), __PRETTY_FUNCTION__), logger);
-    surface2.release();
-    surface1.release();
-    logger->wait_for_num_focus_events(4, surface1.native_handle(), std::chrono::seconds(5));
-    logger->wait_for_num_focus_events(2, surface2.native_handle(), std::chrono::seconds(5));
+    std::vector<MirSurfaceFocusState> const initial_focus = {mir_surface_focused};
+    std::vector<MirSurfaceFocusState> const focus_sequence1 =
+        {mir_surface_focused, mir_surface_unfocused, mir_surface_focused, mir_surface_unfocused};
+    std::vector<MirSurfaceFocusState> const focus_sequence2 = {mir_surface_focused, mir_surface_unfocused};
 
-    auto log = logger->events_for(surface1.native_handle());
-    EXPECT_THAT(log[0], Eq(mir_surface_focused));
-    EXPECT_THAT(log[1], Eq(mir_surface_unfocused));
-    EXPECT_THAT(log[2], Eq(mir_surface_focused));
-    EXPECT_THAT(log[3], Eq(mir_surface_unfocused));
+    auto surface1 = make_surface();
+    surface1->expect_focus_event_sequence(initial_focus);
 
-    log = logger->events_for(surface2.native_handle());
-    EXPECT_THAT(log[0], Eq(mir_surface_focused));
-    EXPECT_THAT(log[1], Eq(mir_surface_unfocused));
+    auto surface2 = make_surface();
+
+    surface2->release();
+    surface1->release();
+
+    surface1->expect_focus_event_sequence(focus_sequence1);
+    surface2->expect_focus_event_sequence(focus_sequence2);
 }
