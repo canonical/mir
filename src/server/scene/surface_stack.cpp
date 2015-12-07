3471c3dd4f600e8c02282e7576740d668db426ac
Daniel van Vugt <daniel.van.vugt@canonical.com>
2015-12-07 17:54:18 +0800

    Revert spurious merge r3171. Everything in it is incorrect.
    The good news is that since the merge is still in the bzr history, the
    aforementioned conflicts are still resolved as far as bzr is concerned.

--- src/server/scene/surface_stack.cpp
+++ src/server/scene/surface_stack.cpp
@@ -168,16 +168,12 @@ int ms::SurfaceStack::frames_pending(mc::CompositorID id) const
             auto const tracker = rendering_trackers.find(surface.get());
             if (tracker != rendering_trackers.end() && tracker->second->is_exposed_in(id))
             {
-                auto const tracker = rendering_trackers.find(surface.get());
-                if (tracker != rendering_trackers.end() && tracker->second->is_exposed_in(id))
-                {
-                    // Note that we ask the surface and not a Renderable.
-                    // This is because we don't want to waste time and resources
-                    // on a snapshot till we're sure we need it...
-                    int ready = surface->buffers_ready_for_compositor(id);
-                    if (ready > result)
-                        result = ready;
-                }
+                // Note that we ask the surface and not a Renderable.
+                // This is because we don't want to waste time and resources
+                // on a snapshot till we're sure we need it...
+                int ready = surface->buffers_ready_for_compositor(id);
+                if (ready > result)
+                    result = ready;
             }
         }
     }
