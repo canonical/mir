12dbee7acda00c6f57bc95a414a57d68807854ae
Kevin DuBois <kevin.dubois@canonical.com>
2015-12-21 09:33:32 -0500

    find fd leak, and plug

--- src/client/mir_connection.cpp
+++ src/client/mir_connection.cpp
@@ -310,6 +310,8 @@ void MirConnection::surface_created(SurfaceCreationRequest* request)
         if (!surface_proto->has_error())
             surface_proto->set_error(error.what());
         // failed to create buffer_stream, so clean up FDs it doesn't own
+        for (auto i = 0; i < surface_proto->fd_size(); i++)
+            ::close(surface_proto->fd(i));
         if (surface_proto->has_buffer_stream() && surface_proto->buffer_stream().has_buffer())
             for (int i = 0; i < surface_proto->buffer_stream().buffer().fd_size(); i++)
                 ::close(surface_proto->buffer_stream().buffer().fd(i));
