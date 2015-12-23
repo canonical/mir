f28b092e0e544388771dc96888fb23c832ad3bdd
Andreas Pokorny <andreas.pokorny@canonical.com>
2015-12-23 08:42:48 +0100

    add another override

--- src/server/scene/global_event_sender.h
+++ src/server/scene/global_event_sender.h
@@ -38,7 +38,7 @@ public:
     void handle_display_config_change(graphics::DisplayConfiguration const& config) override;
     void handle_input_device_change(std::vector<std::shared_ptr<mir::input::Device>> const& devices) override;
     void send_ping(int32_t serial) override;
-    void send_buffer(frontend::BufferStreamId id, graphics::Buffer& buffer, graphics::BufferIpcMsgType);
+    void send_buffer(frontend::BufferStreamId id, graphics::Buffer& buffer, graphics::BufferIpcMsgType) override;
 
 private:
     std::shared_ptr<SessionContainer> const sessions;
