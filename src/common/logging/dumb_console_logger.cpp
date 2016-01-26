4e66f48dd2c66053a82b7184fe5cd14dffebfbd1
Alan Griffiths <alan@octopull.co.uk>
2016-01-26 17:43:51 +0000

    More meaningful timestamps in DumbConsoleLogger

--- src/common/logging/dumb_console_logger.cpp
+++ src/common/logging/dumb_console_logger.cpp
@@ -42,9 +42,10 @@ void ml::DumbConsoleLogger::log(ml::Severity severity,
 
     struct timespec ts;
     clock_gettime(CLOCK_REALTIME, &ts);
+    char datetime[32];
     char now[32];
-    snprintf(now, sizeof(now), "%ld.%06ld",
-             (long)ts.tv_sec, ts.tv_nsec / 1000);
+    strftime(datetime, sizeof(datetime), "%F %T", localtime(&ts.tv_sec));
+    snprintf(now, sizeof(now), "%s.%06ld", datetime, ts.tv_nsec / 1000);
 
     out << "["
         << now
