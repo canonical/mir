6d846728916bbe7846ab1da4805aa53d0133efd2
Daniel van Vugt <daniel.van.vugt@canonical.com>
2016-04-07 14:26:36 +0800

    And don't rename the test case either, because recent landings on trunk makes the old name correct now.

--- tests/acceptance-tests/test_latency.cpp
+++ tests/acceptance-tests/test_latency.cpp
@@ -272,7 +272,7 @@ struct ClientLatency : mtf::ConnectedClientHeadlessServer
 };
 }
 
-TEST_F(ClientLatency, triple_buffered_client_has_less_than_three_frames_latency)
+TEST_F(ClientLatency, triple_buffered_client_has_less_than_two_frames_latency)
 {
     using namespace testing;
 
