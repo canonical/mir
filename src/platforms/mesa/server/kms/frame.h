db98a137fc24695a7178c2cc306de8d49cf8314c
Daniel van Vugt <daniel.van.vugt@canonical.com>
2016-04-07 16:51:13 +0800

    Improve docs and prototype FrameClock

--- src/platforms/mesa/server/kms/frame.h
+++ src/platforms/mesa/server/kms/frame.h
@@ -20,21 +20,44 @@
 #define MIR_GRAPHICS_FRAME_H_
 
 #include <cstdint>
+#include <functional>
 
 namespace mir
 {
 namespace graphics
 {
 
-/*
+/**
+ * Unique identifier of a frame displayed to the user.
+ *
  * This weird terminology is actually pretty standard in graphics texts,
  * OpenGL and Xorg. For more information see:
  *   https://www.opengl.org/registry/specs/OML/glx_sync_control.txt
  */
 struct Frame
 {
-    uint64_t ust = 0;  // Unadjusted System Time
-    uint64_t msc = 0;  // Media Stream Counter
+    uint64_t ust = 0;  /**< Unadjusted System Time in nanoseconds: This must
+                            stay in its raw integer form so that (GLX) clients
+                            can match their CLOCK_MONOTONIC to it. */
+    uint64_t msc = 0;  /**< Media Stream Counter: The physical frame count
+                            from the display hardware (or as close to it as
+                            can be calculated).
+                            You should not make the assumption that this
+                            starts low, or that successive queries will
+                            increase by one. Because it's entirely possible
+                            that you missed a frame or frames so the delta
+                            between msc values may be greater than one. */
+};
+
+class FrameClock
+{
+public:
+    typedef std::function<void(Frame const&)> FrameCallback;
+
+    virtual ~FrameClock() = default;
+    virtual Frame last_frame() const = 0;
+    /// For simplicity you should assume only one callback is supported
+    virtual void on_next_frame(FrameCallback const&) = 0;
 };
 
 }
