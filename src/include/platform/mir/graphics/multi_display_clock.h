59c48d6aad50ec26bba0fa6551946afdae7c59a4
Daniel van Vugt <daniel.van.vugt@canonical.com>
2016-04-11 17:40:36 +0800

    Improve docs

--- src/include/platform/mir/graphics/multi_display_clock.h
+++ src/include/platform/mir/graphics/multi_display_clock.h
@@ -31,11 +31,9 @@ namespace graphics
 {
 
 /**
- * MultiDisplayClock is a re-imagining of the timerless multi-monitor
- * frame sync algorithm, implemented within the DisplayClock interface.
- * This allows clients to sync to the virtual display(s) without needing
- * to know about physical multiple displays. They all appear as a single
- * DisplayClock.
+ * MultiDisplayClock is a virtual display clock that can represent any
+ * number of child clocks. It ticks at the rate of the fastest child,
+ * providing the user (and hence client app) a single clock to sync to.
  */
 class MultiDisplayClock : public DisplayClock
 {
