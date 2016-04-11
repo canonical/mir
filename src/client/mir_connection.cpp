79d36b61bd17fa279bbfcaabc183597ad70b6418
Kevin DuBois <kevin.dubois@canonical.com>
2016-04-11 08:40:12 -0400

    port over some patches that will reduce the LOC in notify-buffers-directyl

--- src/client/mir_connection.cpp
+++ src/client/mir_connection.cpp
@@ -365,7 +365,7 @@ void MirConnection::surface_created(SurfaceCreationRequest* request)
                          spec.surface_name.value() : ""};
 
         stream = std::make_shared<mcl::BufferStream>(
-            this, request->wh, server, platform,
+            this, request->wh, server, platform, surface_map, buffer_factory,
             surface_proto->buffer_stream(), make_perf_report(logger), name,
             mir::geometry::Size{surface_proto->width(), surface_proto->height()}, nbuffers);
     }
@@ -810,7 +810,7 @@ void MirConnection::stream_created(StreamCreationRequest* request_raw)
     try
     {
         auto stream = std::make_shared<mcl::BufferStream>(
-            this, request->wh, server, platform,
+            this, request->wh, server, platform, surface_map, buffer_factory,
             *protobuf_bs, make_perf_report(logger), std::string{},
             mir::geometry::Size{request->parameters.width(), request->parameters.height()}, nbuffers);
         surface_map->insert(mf::BufferStreamId(protobuf_bs->id().value()), stream);
@@ -1197,8 +1197,6 @@ void MirConnection::allocate_buffer(
 
 void MirConnection::release_buffer(int buffer_id)
 {
-    surface_map->erase(buffer_id);
-
     mp::BufferRelease request;
     auto released_buffer = request.add_buffers();
     released_buffer->set_buffer_id(buffer_id);
