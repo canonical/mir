d6ed36a09e947b56b422af61880d18935bfe505b
Daniel van Vugt <daniel.van.vugt@canonical.com>
2016-05-06 15:57:26 +0800

    Now runs a client

--- tests/performance-tests/test_compositor.cpp
+++ tests/performance-tests/test_compositor.cpp
@@ -19,7 +19,8 @@
 #include "mir_test_framework/async_server_runner.h"
 #include <gtest/gtest.h>
 #include <cstdio>
-#include <string>
+#include <cstdlib>
+#include <cstring>
 #include <unistd.h>
 #include <string>
 
@@ -33,9 +34,8 @@ std::string mir_bin_dir()
     if (len < 0)
         len = 0;
     path[len] = '\0';
-    // Unlike dirname(), we wish to return a path with trailing '/'
     if (auto slash = strrchr(path, '/'))
-        slash[+1] = '\0';
+        *slash = '\0';
     return path;
 }
 
@@ -51,6 +51,29 @@ void kill_nicely(pid_t pid)
         ++count;
     }
     kill(pid, SIGKILL);
+}
+
+int exec_cmd(char const* cmd)
+{
+    auto buf = strdup(cmd);
+    size_t argc = 1;
+    char* argv[256] = {buf};
+    char *c = buf;
+    while (*c)
+    {
+        if (*c == ' ')
+        {
+            *c = '\0';
+            if (argc < (sizeof(argv)/sizeof(argv[0]) - 1))
+            {
+                argv[argc] = c + 1;
+                ++argc;
+            }
+        }
+        ++c;
+    }
+    argv[argc] = NULL;
+    return execv(argv[0], argv);
 }
 
 FILE* popen_with_pid(char const* cmd, pid_t& pid)
@@ -74,31 +97,8 @@ FILE* popen_with_pid(char const* cmd, pid_t& pid)
         close(pipe_out);
         dup2(pipe_in, 1);  // Child stdout goes into pipe_in
         close(pipe_in);
-
-        auto buf = strdup(cmd);
-        size_t argc = 1;
-        char* argv[256] = {buf};
-        char *c = buf;
-        while (*c)
-        {
-            if (*c == ' ')
-            {
-                *c = '\0';
-                if (argc < (sizeof(argv)/sizeof(argv[0]) - 1))
-                {
-                    argv[argc] = c + 1;
-                    ++argc;
-                }
-            }
-            ++c;
-        }
-        argv[argc] = NULL;
-
-        for (size_t i = 0; i < argc; ++i)
-            printf("argv[%d] = {%s}\n", (int)i, argv[i]);
-
-        if (execv(argv[0], argv) < 0)
-            exit(errno);
+        exec_cmd(cmd);
+        exit(errno);
     }
     else
     {
@@ -124,24 +124,61 @@ FILE* popen_with_timeout(char const* cmd, std::chrono::seconds timeout)
    return out;
 }
 
+bool spawn_and_forget(char const* cmd)
+{
+    int pid = fork();
+    if (pid == 0)
+    {
+        // Silence stdout/stderr
+        close(1);
+        close(2);
+        exec_cmd(cmd);
+        exit(errno);
+    }
+    return (pid > 0);
+}
+
+bool spawn_and_forget(std::string const& cmd)
+{
+    return spawn_and_forget(cmd.c_str());
+}
+
+bool wait_for_file(char const* path, std::chrono::seconds timeout)
+{
+    struct stat s;
+    int count = 0, max = timeout.count();
+    int ret;
+    while ((ret = stat(path, &s)) < 0 && errno == ENOENT && count < max)
+    {
+        ++count;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    return ret == 0;
+}
+
 } // anonymous namespace
 
 TEST(CompositorPerformance, stuff)
 {
     auto const bin_dir = mir_bin_dir();
     printf("Mir bin dir {%s}\n", bin_dir.c_str());
-    auto const server_cmd =
-        bin_dir+"mir_demo_server --compositor-report=log";
+    auto const mir_sock = "/tmp/mir_socket_"+std::to_string(getpid());
+    auto const server_cmd = bin_dir+"/mir_demo_server "
+                          + "--compositor-report=log "
+                          + "-f " + mir_sock;
 
-    if (auto server_output = popen_with_timeout(server_cmd.c_str(),
-                                                std::chrono::seconds(5)))
+    auto server_output = popen_with_timeout(server_cmd.c_str(),
+                                            std::chrono::seconds(5));
+    ASSERT_TRUE(server_output);
+    ASSERT_TRUE(wait_for_file(mir_sock.c_str(), std::chrono::seconds(5)));
+
+    setenv("MIR_SOCKET", mir_sock.c_str(), 1);
+    spawn_and_forget(bin_dir+"/mir_demo_client_egltriangle");
+
+    char line[256];
+    while (fgets(line, sizeof(line), server_output))
     {
-        char line[256];
-        while (fgets(line, sizeof(line), server_output))
-        {
-            printf("read {%s}\n", line);
-        }
-        printf("Server output ended.\n");
-        fclose(server_output);
     }
+    printf("Server output ended.\n");
+    fclose(server_output);
 }
