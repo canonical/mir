da4d8ac81a49fdc6b3cde0dc07d3aced439239fd
Daniel van Vugt <daniel.van.vugt@canonical.com>
2016-05-06 15:34:57 +0800

    Working server spawning prototype

---
+++ tests/performance-tests/test_compositor.cpp
@@ -0,0 +1,147 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Daniel van Vugt <daniel.van.vugt@canonical.com>
+ */
+
+#include "mir_test_framework/async_server_runner.h"
+#include <gtest/gtest.h>
+#include <cstdio>
+#include <string>
+#include <unistd.h>
+#include <string>
+
+namespace
+{
+
+std::string mir_bin_dir()
+{
+    char path[256];
+    auto len = readlink("/proc/self/exe", path, sizeof(path)-1);
+    if (len < 0)
+        len = 0;
+    path[len] = '\0';
+    // Unlike dirname(), we wish to return a path with trailing '/'
+    if (auto slash = strrchr(path, '/'))
+        slash[+1] = '\0';
+    return path;
+}
+
+void kill_nicely(pid_t pid)
+{
+    if (kill(pid, SIGTERM) < 0)
+        return;
+    int const timeout = 5;
+    int status, count = 0;
+    while (0 == waitpid(pid, &status, WNOHANG) && count < timeout)
+    {
+        sleep(1);
+        ++count;
+    }
+    kill(pid, SIGKILL);
+}
+
+FILE* popen_with_pid(char const* cmd, pid_t& pid)
+{
+    int pipefd[2];
+    if (pipe(pipefd))
+        return NULL;
+
+    int const& pipe_out = pipefd[0];
+    int const& pipe_in = pipefd[1];
+
+    pid = fork();
+    if (pid < 0)
+    {
+        close(pipe_in);
+        close(pipe_out);
+        return NULL;
+    }
+    else if (pid == 0)
+    {
+        close(pipe_out);
+        dup2(pipe_in, 1);  // Child stdout goes into pipe_in
+        close(pipe_in);
+
+        auto buf = strdup(cmd);
+        size_t argc = 1;
+        char* argv[256] = {buf};
+        char *c = buf;
+        while (*c)
+        {
+            if (*c == ' ')
+            {
+                *c = '\0';
+                if (argc < (sizeof(argv)/sizeof(argv[0]) - 1))
+                {
+                    argv[argc] = c + 1;
+                    ++argc;
+                }
+            }
+            ++c;
+        }
+        argv[argc] = NULL;
+
+        for (size_t i = 0; i < argc; ++i)
+            printf("argv[%d] = {%s}\n", (int)i, argv[i]);
+
+        if (execv(argv[0], argv) < 0)
+            exit(errno);
+    }
+    else
+    {
+        close(pipe_in);
+    }
+
+    return fdopen(pipe_out, "r");
+}
+
+FILE* popen_with_timeout(char const* cmd, std::chrono::seconds timeout)
+{
+   pid_t pid;
+   FILE* out = popen_with_pid(cmd, pid);
+   if (out)
+   {
+       std::thread killer([timeout,pid]()
+       {
+           std::this_thread::sleep_for(timeout);
+           kill_nicely(pid);
+       });
+       killer.detach();
+   }
+   return out;
+}
+
+} // anonymous namespace
+
+TEST(CompositorPerformance, stuff)
+{
+    auto const bin_dir = mir_bin_dir();
+    printf("Mir bin dir {%s}\n", bin_dir.c_str());
+    auto const server_cmd =
+        bin_dir+"mir_demo_server --compositor-report=log";
+
+    if (auto server_output = popen_with_timeout(server_cmd.c_str(),
+                                                std::chrono::seconds(5)))
+    {
+        char line[256];
+        while (fgets(line, sizeof(line), server_output))
+        {
+            printf("read {%s}\n", line);
+        }
+        printf("Server output ended.\n");
+        fclose(server_output);
+    }
+}
