96b1fe197a41fd554abe428e82253e1f2fc7a4df
Cemil Azizoglu <cemil.azizoglu@canonical.com>
2016-06-09 20:36:22 -0400

    Cleanup properly after server window is X-ed out

--- src/platforms/mesa/server/x11/graphics/display.cpp
+++ src/platforms/mesa/server/x11/graphics/display.cpp
@@ -30,6 +30,8 @@
 #include <fcntl.h>
 #include <mutex>
 
+#include <X11/Xatom.h>
+
 #define MIR_LOG_COMPONENT "x11-display"
 #include "mir/log.h"
 
@@ -167,6 +169,9 @@ mgx::X11Window::X11Window(::Display* x_dpy, EGLDisplay egl_dpy, geom::Size const
         };
 
         XSetWMHints(x_dpy, win, &wm_hints);
+
+        Atom wmDeleteMessage = XInternAtom(x_dpy, "WM_DELETE_WINDOW", False);
+        XSetWMProtocols(x_dpy, win, &wmDeleteMessage, 1);
     }
 
     XMapWindow(x_dpy, win);
