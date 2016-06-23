61e50a5f255ad324009fde36f8e52a1bc92b80f4
Andreas Pokorny <andreas.pokorny@canonical.com>
2016-06-23 17:09:02 +0200

    fix review findings

--- src/client/mir_surface.cpp
+++ src/client/mir_surface.cpp
@@ -446,7 +446,7 @@ void MirSurface::handle_event(MirEvent const& e)
         size_t length = 0;
         auto keymap_event = mir_event_get_keymap_event(&e);
         mir_keymap_event_get_keymap_buffer(keymap_event, &buffer, &length);
-        keymapper->set_keymap(buffer, length);
+        keymapper->set_keymap_for_all_devices(buffer, length);
         break;
     }
     case mir_event_type_resize:
