35fd3f0261e82872b91654df4ae0cd7a6f8562c4
Brandon Schaefer <brandon.schaefer@canonical.com>
2016-07-26 16:51:17 -0700

    * Clean some things up

--- examples/pointer_confinement.c
+++ examples/pointer_confinement.c
@@ -17,16 +17,17 @@
  */
 
 #include "eglapp.h"
-#include <cassert>
-#include <GLES2/gl2.h>
 #include "mir_toolkit/mir_client_library.h"
+
+#include <GLES2/gl2.h>
+#include <assert.h>
 #include <stdio.h>
 #include <signal.h>
-#include <atomic>
+#include <math.h>
 
-std::atomic<int> mouse_x{0};
-std::atomic<int> mouse_y{0};
-std::atomic<bool> done{false};
+float mouse_x = 0.0f;
+float mouse_y = 0.0f;
+sig_atomic_t done = false;
 
 static void shutdown(int signum)
 {
@@ -37,7 +38,7 @@ static void shutdown(int signum)
     }
 }
 
-constexpr float normalize(float xy, float wh, float size = 0.0f) { return (xy - wh/2.0f + size) / (wh/2.0f); }
+static float normalize(float xy, float wh, float size) { return (xy - wh/2.0f + size) / (wh/2.0f); }
 
 static GLuint load_shader(const char* src, GLenum type)
 {
@@ -45,13 +46,13 @@ static GLuint load_shader(const char* src, GLenum type)
     if (shader)
     {
         GLint compiled;
-        glShaderSource(shader, 1, &src, nullptr);
+        glShaderSource(shader, 1, &src, NULL);
         glCompileShader(shader);
         glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
         if (!compiled)
         {
             GLchar log[1024];
-            glGetShaderInfoLog(shader, sizeof log - 1, nullptr, log);
+            glGetShaderInfoLog(shader, sizeof log - 1, NULL, log);
             log[sizeof log - 1] = '\0';
             printf("load_shader compile failed: %s\n", log);
             glDeleteShader(shader);
@@ -65,18 +66,28 @@ static void handle_input_event(MirInputEvent const* event, MirSurface* surface)
 {
     if (mir_input_event_get_type(event) == mir_input_event_type_pointer)
     {
-        auto const* pev = mir_input_event_get_pointer_event(event);
-        auto dx = mir_pointer_event_axis_value(pev, mir_pointer_axis_relative_x) / 2;
-        auto dy = mir_pointer_event_axis_value(pev, mir_pointer_axis_relative_y) / 2;
+        MirPointerEvent const* pev = mir_input_event_get_pointer_event(event);
+        float dx = mir_pointer_event_axis_value(pev, mir_pointer_axis_relative_x);
+        float dy = mir_pointer_event_axis_value(pev, mir_pointer_axis_relative_y);
 
-        mouse_x.fetch_add(dx);
+        mouse_x += dx;
         // - because opengl coords
-        mouse_y.fetch_add(-dy);
+        mouse_y += -dy;
     }
-    else if(mir_input_event_get_type(event) == mir_input_event_type_key)
+    else if (mir_input_event_get_type(event) == mir_input_event_type_touch)
     {
-        auto const* kev = mir_input_event_get_keyboard_event(event);
-        auto key_code   = mir_keyboard_event_key_code(kev);
+        MirTouchEvent const* tev = mir_input_event_get_touch_event(event);
+        float dx = mir_touch_event_axis_value(tev, 0, mir_pointer_axis_relative_x);
+        float dy = mir_touch_event_axis_value(tev, 0, mir_pointer_axis_relative_y);
+
+        mouse_x += dx;
+        // - because opengl coords
+        mouse_y += -dy;
+    }
+    else if (mir_input_event_get_type(event) == mir_input_event_type_key)
+    {
+        MirKeyboardEvent const* kev = mir_input_event_get_keyboard_event(event);
+        int key_code   = mir_keyboard_event_key_code(kev);
 
         if (mir_keyboard_event_action(kev) != mir_keyboard_action_up)
             return;
@@ -90,9 +101,9 @@ static void handle_input_event(MirInputEvent const* event, MirSurface* surface)
             // We start out grabbed
             static bool grabbed = true;
 
-            auto* spec = mir_connection_create_spec_for_changes(mir_eglapp_native_connection());
-            auto state = mir_pointer_unconfined;
-            MirCursorConfiguration* conf = nullptr;
+            MirSurfaceSpec* spec = mir_connection_create_spec_for_changes(mir_eglapp_native_connection());
+            MirPointerConfinementState state = mir_pointer_unconfined;
+            MirCursorConfiguration* conf = NULL;
 
             if (!grabbed)
             {
@@ -117,8 +128,8 @@ static void handle_input_event(MirInputEvent const* event, MirSurface* surface)
         else if (key_code == XKB_KEY_f)
         {
             static bool fullscreen = false;
-            auto* spec = mir_connection_create_spec_for_changes(mir_eglapp_native_connection());
-            auto state = mir_surface_state_restored;
+            MirSurfaceSpec* spec = mir_connection_create_spec_for_changes(mir_eglapp_native_connection());
+            MirSurfaceState state = mir_surface_state_restored;
 
             if (!fullscreen)
             {
@@ -151,12 +162,9 @@ int main(int argc, char* argv[])
 {
     char const v_shader_src[] =
     "attribute vec4 v_position;  \n"
-    "attribute vec4 a_color;     \n"
-    "varying vec4 v_color;       \n"
     "                            \n"
     "void main()                 \n"
     "{                           \n"
-    "  v_color     = a_color;    \n"
     "  gl_Position = v_position; \n"
     "}                           \n";
 
@@ -165,11 +173,9 @@ int main(int argc, char* argv[])
     "precision mediump float;  \n"
     "#endif                    \n"
     "                          \n"
-    "varying vec4 v_color;     \n"
-    "                          \n"
     "void main()               \n"
     "{                         \n"
-    "  gl_FragColor = v_color; \n"
+    "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); \n"
     "}                         \n";
 
     int sqaure_size = 10;
@@ -180,15 +186,15 @@ int main(int argc, char* argv[])
     width  /= 2;
     height /= 2;
 
-    auto spec = mir_connection_create_spec_for_changes(mir_eglapp_native_connection());
+    MirSurfaceSpec* spec = mir_connection_create_spec_for_changes(mir_eglapp_native_connection());
     mir_surface_spec_set_width (spec, width);
     mir_surface_spec_set_height(spec, height);
 
     mir_surface_apply_spec(mir_eglapp_native_surface(), spec);
     mir_surface_spec_release(spec);
 
-    mouse_x = width  / 2;
-    mouse_y = height / 2;
+    mouse_x = width  / 2.0;
+    mouse_y = height / 2.0;
 
     GLuint vshader = load_shader(v_shader_src, GL_VERTEX_SHADER);
     assert(vshader);
@@ -205,7 +211,7 @@ int main(int argc, char* argv[])
     if (!linked)
     {
         GLchar log[1024];
-        glGetProgramInfoLog(prog, sizeof log - 1, nullptr, log);
+        glGetProgramInfoLog(prog, sizeof log - 1, NULL, log);
         log[sizeof log - 1] = '\0';
         printf("Link failed: %s\n", log);
         return 2;
@@ -214,7 +220,7 @@ int main(int argc, char* argv[])
     glUseProgram(prog);
 
     MirSurface* surface = mir_eglapp_native_surface();
-    mir_surface_set_event_handler(surface, handle_event, nullptr);
+    mir_surface_set_event_handler(surface, handle_event, NULL);
 
     spec = mir_connection_create_spec_for_changes(mir_eglapp_native_connection());
     mir_surface_spec_set_pointer_confinement(spec, mir_pointer_confined_to_surface);
@@ -226,7 +232,6 @@ int main(int argc, char* argv[])
     mir_surface_configure_cursor(surface, NULL);
 
     glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-    GLfloat square_color[] = {1.0f, 0.0f, 0.0f, 1.0f};
 
     signal(SIGINT, shutdown);
     signal(SIGTERM, shutdown);
@@ -244,17 +249,17 @@ int main(int argc, char* argv[])
          *| 5-1    2  |
          *|-----------|
         **/
-        float four_zero_x = normalize(mouse_x % width, width,  sqaure_size);
-        float four_zero_y = normalize(mouse_y % height, height, sqaure_size);
+        float four_zero_x = normalize(fmod(mouse_x, width), width,  sqaure_size);
+        float four_zero_y = normalize(fmod(mouse_y, height), height, sqaure_size);
 
-        float five_one_x  = normalize(mouse_x % width, width);
-        float five_one_y  = normalize(mouse_y % height, height);
+        float five_one_x  = normalize(fmod(mouse_x, width), width, 0.0f);
+        float five_one_y  = normalize(fmod(mouse_y, height), height, 0.0f);
 
-        float two_x       = normalize(mouse_x % width, width, sqaure_size);
-        float two_y       = normalize(mouse_y % height, height);
+        float two_x       = normalize(fmod(mouse_x, width), width, sqaure_size);
+        float two_y       = normalize(fmod(mouse_y, height), height, 0.0f);
 
-        float three_x     = normalize(mouse_x % width, width);
-        float three_y     = normalize(mouse_y % height, height, sqaure_size);
+        float three_x     = normalize(fmod(mouse_x, width), width, 0.0f);
+        float three_y     = normalize(fmod(mouse_y, height), height, sqaure_size);
 
         GLfloat square_vert[] = { five_one_x , five_one_y , 0.0f,
                                   two_x      , two_y      , 0.0f,
@@ -263,7 +268,6 @@ int main(int argc, char* argv[])
 
         glEnableVertexAttribArray(0);
 
-        glVertexAttrib4fv(1, square_color);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, square_vert);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
