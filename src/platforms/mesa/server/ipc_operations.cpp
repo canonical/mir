f3f14bd31443de1f815e552b9035a77f17f40c1d
Kevin DuBois <kevin.dubois@canonical.com>
2016-08-22 10:04:23 -0400

    make sure to pack stride for shmbuffers too

--- src/platforms/mesa/server/ipc_operations.cpp
+++ src/platforms/mesa/server/ipc_operations.cpp
@@ -22,6 +22,7 @@
 #include "mir/graphics/platform.h"
 #include "mir/graphics/nested_context.h"
 #include "mir/graphics/platform_operation_message.h"
+#include "mir/renderer/sw/pixel_source.h"
 #include "mir/libname.h"
 #include "display_helpers.h"
 #include "drm_authentication.h"
@@ -84,7 +85,10 @@ void mgm::IpcOperations::pack_buffer(
             packer.pack_fd(mir::Fd(IntOwnedFd{native_handle->fd[i]}));
         }
 
-        //packer.pack_stride(buffer.stride());
+
+        auto native_buffer = const_cast<mg::Buffer*>(&buffer)->native_buffer_base();
+        if (auto pixel_source = dynamic_cast<mir::renderer::software::PixelSource*>(native_buffer))
+            packer.pack_stride(pixel_source->stride());
         packer.pack_flags(native_handle->flags);
         packer.pack_size(buffer.size());
     }
