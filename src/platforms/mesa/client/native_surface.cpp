e7d82604eee56768ffbeae503c482607405e1fc1
Cemil Azizoglu <cemil.azizoglu@canonical.com>
2016-08-31 10:20:35 -0500

    use_egl_native_window works and all existing tests pass.

--- src/platforms/mesa/client/native_surface.cpp
+++ src/platforms/mesa/client/native_surface.cpp
@@ -20,7 +20,6 @@
 
 #include <cstring>
 
-#include "native_buffer.h"
 #include "mir/client_buffer.h"
 #include "mir/uncaught.h"
 
@@ -81,11 +80,8 @@ try
 
     auto buffer = surface->get_current_buffer();
 
-    auto buffer_to_driver = std::dynamic_pointer_cast<mir::graphics::mesa::NativeBuffer>(
-        buffer->native_buffer_handle());
-    if (!buffer_to_driver)
-        return MIR_MESA_FALSE;
-    *buffer_package = *buffer_to_driver;
+    auto buffer_to_driver = buffer->native_buffer_handle();
+    memcpy(buffer_package, buffer_to_driver.get(), sizeof(MirBufferPackage));
     return MIR_MESA_TRUE;
 }
 catch (std::exception const& e)
