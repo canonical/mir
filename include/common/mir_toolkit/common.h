2e1783697b80a60e06d32761b5d4789340169c9a
Alan Griffiths <alan@octopull.co.uk>
2016-09-02 10:11:45 +0100

    Paint bikeshed and fix doc comments

--- include/common/mir_toolkit/common.h
+++ include/common/mir_toolkit/common.h
@@ -201,19 +201,37 @@ typedef enum MirEdgeAttachment
 
 // Inspired by GdkGravity
 /**
- * Reference point for aligning a window relative to a rectangle.
+ * Reference point for aligning a surface relative to a rectangle.
+ * Each element (surface and rectangle) has a MirPlacementGravity assigned.
  */
 typedef enum MirPlacementGravity
 {
-    mir_placement_gravity_northwest = 1,   // the reference point is at the top left corner.
-    mir_placement_gravity_north,           // the reference point is in the middle of the top edge.
-    mir_placement_gravity_northeast,       // the reference point is at the top right corner.
-    mir_placement_gravity_west,            // the reference point is at the middle of the left edge.
-    mir_placement_gravity_centre,          // the reference point is at the center of the window.
-    mir_placement_gravity_east,            // the reference point is at the middle of the right edge.
-    mir_placement_gravity_southwest,       // the reference point is at the lower left corner.
-    mir_placement_gravity_south,           // the reference point is at the middle of the lower edge.
-    mir_placement_gravity_southeast,       // the reference point is at the lower right corner.
+    /// the reference point is at the center.
+    mir_placement_gravity_center    = 0,
+
+    /// the reference point is at the middle of the left edge.
+    mir_placement_gravity_west      = 1 << 0,
+
+    /// the reference point is at the middle of the right edge.
+    mir_placement_gravity_east      = 1 << 1,
+
+    /// the reference point is in the middle of the top edge.
+    mir_placement_gravity_north     = 1 << 2,
+
+    /// the reference point is at the middle of the lower edge.
+    mir_placement_gravity_south     = 1 << 3,
+
+    /// the reference point is at the top left corner.
+    mir_placement_gravity_northwest = mir_placement_gravity_north | mir_placement_gravity_west,
+
+    /// the reference point is at the top right corner.
+    mir_placement_gravity_northeast = mir_placement_gravity_north | mir_placement_gravity_east,
+
+    /// the reference point is at the lower left corner.
+    mir_placement_gravity_southwest = mir_placement_gravity_south | mir_placement_gravity_west,
+
+    /// the reference point is at the lower right corner.
+    mir_placement_gravity_southeast = mir_placement_gravity_south | mir_placement_gravity_east
 } MirPlacementGravity;
 
 // Inspired by GdkAnchorHints
@@ -223,41 +241,54 @@ typedef enum MirPlacementGravity
  * These hints determine how the window should be positioned in the case that
  * the surface would fall off-screen if placed in its ideal position.
  *
- * For example, mir_placement_hints_flip_x will invert the x component of
- * aux_rect_placement_offset and replace mir_placement_gravity_northwest with
- * mir_placement_gravity_northeast and vice versa if the window extends
+ * For example, \p mir_placement_hints_flip_x will invert the x component of
+ * \p aux_rect_placement_offset and replace \p mir_placement_gravity_northwest
+ * with \p mir_placement_gravity_northeast and vice versa if the window extends
  * beyond the left or right edges of the monitor.
  *
- * If mir_placement_hints_slide_x is set, the window can be shifted
+ * If \p mir_placement_hints_slide_x is set, the window can be shifted
  * horizontally to fit on-screen.
  *
- * If mir_placement_hints_resize_x is set, the window can be shrunken
+ * If \p mir_placement_hints_resize_x is set, the window can be shrunken
  * horizontally to fit.
  *
- * If mir_placement_hints_antipodes is set then the rect gravity may be
- * substituted with the opposite corner (e.g. mir_placement_gravity_northeast
- * to mir_placement_gravity_southwest) in combination with other options.
+ * If \p mir_placement_hints_antipodes is set then the rect gravity may be
+ * substituted with the opposite corner (e.g. \p mir_placement_gravity_northeast
+ * to \p mir_placement_gravity_southwest) in combination with other options.
  *
  * When multiple flags are set, flipping should take precedence over sliding,
  * which should take precedence over resizing.
  */
 typedef enum MirPlacementHints
 {
-    mir_placement_hints_flip_x   = 1 << 0,  // allow flipping anchors horizontally
-    mir_placement_hints_flip_y   = 1 << 1,  // allow flipping anchors vertically
-    mir_placement_hints_slide_x  = 1 << 2,  // allow sliding window horizontally
-    mir_placement_hints_slide_y  = 1 << 3,  // allow sliding window vertically
-    mir_placement_hints_resize_x = 1 << 4,  // allow resizing window horizontally
-    mir_placement_hints_resize_y = 1 << 5,  // allow resizing window vertically
-    mir_placement_hints_antipodes= 1 << 6,  // allow flipping aux_anchor to opposite corner
+    /// allow flipping anchors horizontally
+    mir_placement_hints_flip_x   = 1 << 0,
 
-    // allow flipping anchors on both axes
+    /// allow flipping anchors vertically
+    mir_placement_hints_flip_y   = 1 << 1,
+
+    /// allow sliding window horizontally
+    mir_placement_hints_slide_x  = 1 << 2,
+
+    /// allow sliding window vertically
+    mir_placement_hints_slide_y  = 1 << 3,
+
+    /// allow resizing window horizontally
+    mir_placement_hints_resize_x = 1 << 4,
+
+    /// allow resizing window vertically
+    mir_placement_hints_resize_y = 1 << 5,
+
+    /// allow flipping aux_anchor to opposite corner
+    mir_placement_hints_antipodes= 1 << 6,
+
+    /// allow flipping anchors on both axes
     mir_placement_hints_flip_any = mir_placement_hints_flip_x|mir_placement_hints_flip_y,
 
-    // allow sliding window on both axes
+    /// allow sliding window on both axes
     mir_placement_hints_slide_any  = mir_placement_hints_slide_x|mir_placement_hints_slide_y,
 
-    // allow resizing window on both axes
+    /// allow resizing window on both axes
     mir_placement_hints_resize_any = mir_placement_hints_resize_x|mir_placement_hints_resize_y,
 } MirPlacementHints;
 
