a17238279f0af3831da1d01eec8aa2e6e947b262
Daniel van Vugt <daniel.van.vugt@canonical.com>
2016-09-02 15:08:10 +0800

    Shrink public headers again and clean up.

--- include/platform/mir/graphics/frame.h
+++ include/platform/mir/graphics/frame.h
@@ -25,26 +25,24 @@
 namespace mir { namespace graphics {
 
 /*
- * Using bare types because it's critical we maintain these values accurately
- * from the driver code and then passed all the way to clients, and clients of
- * clients. Existing drivers and APIs seem to use int64_t for this, and it
- * has to be signed because sometimes time is negative (in the future).
- */
-typedef int64_t Microseconds;
-
-/*
- * Also note that we use Timestamp instead of std::chrono::time_point because
+ * Using bare int types because we must maintain these values accurately from
+ * the driver code and all the way to clients, and clients of clients ad
+ * infinitum. int64_t is used because that's what drivers tend to provide
+ * and we also need to support negative durations (sometimes things are in
+ * the future).
+ *   Also note that we use Timestamp instead of std::chrono::time_point because
  * we need to be able to switch clocks dynamically at runtime, depending on
- * which one any given driver claims to use.
+ * which one any given graphics driver claims to use. They will always use
+ * one of the kernel clocks...
  */
 struct Timestamp
 {
     clockid_t clock_id;
-    Microseconds microseconds;
+    int64_t microseconds;
 
     Timestamp() : clock_id{CLOCK_MONOTONIC}, microseconds{0} {}
-    // Note sure why gcc-4.9 (vivid) demands this over {c,u}
-    Timestamp(clockid_t c, Microseconds u) : clock_id{c}, microseconds{u} {}
+    // Note sure why gcc-4.9 (vivid) demands this TODO
+    Timestamp(clockid_t clk, int64_t usec) : clock_id{clk}, microseconds{usec} {}
 
     static Timestamp now(clockid_t clock_id)
     {
