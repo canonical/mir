09c67dc927f48fb81b8f890c60c0cd7c4a53c68e
Kevin DuBois <kevin.dubois@canonical.com>
2016-09-13 07:43:41 -0400

    stup ipc operations test

---
+++ tests/unit-tests/platforms/nested/test_ipc_operations.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2016 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#include <gtest/gtest.h>
+using namespace testing;
+
+namespace
+{
+struct NestedIPCOperations : testing::Test
+{
+    mtd::StubBuffer;
+};
+}
+
+TEST_F(NestedIPCOperations, uses_guest_platform_on_guest_buffers)
+{
+}
