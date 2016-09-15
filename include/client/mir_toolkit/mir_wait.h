544ff11407da453a0e1ddf3d2a54fc0a7c3487e0
Daniel van Vugt <daniel.van.vugt@canonical.com>
2016-09-15 13:50:08 +0800

    Document a feature of mir_wait_for*() that we seemingly rely on in the NBS code but had never explicitly documented.

--- include/client/mir_toolkit/mir_wait.h
+++ include/client/mir_toolkit/mir_wait.h
@@ -32,7 +32,7 @@ struct MirWaitHandle;
 
 /**
  * Wait on the supplied handle until all instances of the associated request
- * have completed.
+ * have completed. Returns immediately if zero are pending.
  *   \param [in] wait_handle  Handle returned by an asynchronous request
  */
 void mir_wait_for(MirWaitHandle *wait_handle);
@@ -41,7 +41,8 @@ void mir_wait_for(MirWaitHandle *wait_handle);
  * Wait on the supplied handle until one instance of the associated request
  * has completed. Use this instead of mir_wait_for in a threaded environment
  * to ensure that the act of waiting does not clear all results associated
- * with the wait handle; only one.
+ * with the wait handle; only one. Unlike mir_wait_for, this function does
+ * not return if zero are pending and instead waits for one.
  *   \param [in] wait_handle  Handle returned by an asynchronous request
  */
 void mir_wait_for_one(MirWaitHandle *wait_handle);
