5e64aea773de44be24ace8d1fc9fcd46771e9321
Brandon Schaefer <brandon.schaefer@canonical.com>
2016-09-21 16:25:21 -0700

    * Disable test that doesnt test what its currently attempting to test

--- tests/acceptance-tests/test_client_input.cpp
+++ tests/acceptance-tests/test_client_input.cpp
@@ -587,7 +587,8 @@ TEST_F(TestClientInput, usb_direct_input_devices_work)
     first_client.all_events_received.wait_for(2s);
 }
 
-TEST_F(TestClientInput, receives_one_touch_event_per_frame)
+// Enable once lp:1570698 is fixed
+TEST_F(TestClientInput, DISABLED_receives_one_touch_event_per_frame)
 {
     positions[first] = screen_geometry;
     Client first_client(new_connection(), first);
