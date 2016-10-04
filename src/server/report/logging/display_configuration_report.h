3c0d8f2942ffb9f6b523663504b75dc4f905633f
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2016-10-04 17:48:51 +1100

    DisplayConfigurationReport: Log configuration with appropriate severity

--- src/server/report/logging/display_configuration_report.h
+++ src/server/report/logging/display_configuration_report.h
@@ -25,7 +25,7 @@
 
 namespace mir
 {
-namespace logging { class Logger; }
+namespace logging { class Logger; enum class Severity; }
 
 namespace report
 {
@@ -48,7 +48,9 @@ public:
         std::exception const& error) override;
 
 private:
-    void log_configuration(graphics::DisplayConfiguration const& configuration) const;
+    void log_configuration(
+        mir::logging::Severity severity,
+        graphics::DisplayConfiguration const& configuration) const;
     std::shared_ptr<mir::logging::Logger> const logger;
 };
 }
