05f42209865f9731d9a1d115d245e6081a85dca2
Kevin DuBois <kevin.dubois@canonical.com>
2016-11-14 09:20:14 -0500

    fix up stubs

--- tests/include/mir/test/doubles/stub_host_connection.h
+++ tests/include/mir/test/doubles/stub_host_connection.h
@@ -164,6 +164,7 @@ public:
     {
         return true;
     }
+    void* request_interface(char const*, int) { return nullptr; }
 };
 
 struct MockHostConnection : StubHostConnection
@@ -175,6 +176,7 @@ struct MockHostConnection : StubHostConnection
     MOCK_METHOD5(create_surface, std::shared_ptr<graphics::nested::HostSurface>
         (std::shared_ptr<graphics::nested::HostStream> const&, geometry::Displacement,
          graphics::BufferProperties, char const*, uint32_t));
+    MOCK_METHOD2(request_interface, void*(char const*, int));
 
     void emit_input_event(MirEvent const& event, mir::geometry::Rectangle const& source_frame)
     {
