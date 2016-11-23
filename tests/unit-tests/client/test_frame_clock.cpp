5f9a14e118b7b5a0c9e85da8397b7ca3d81ab630
Daniel van Vugt <daniel.van.vugt@canonical.com>
2016-11-23 18:02:59 +0800

    Some actual useful test cases

--- tests/unit-tests/client/test_frame_clock.cpp
+++ tests/unit-tests/client/test_frame_clock.cpp
@@ -34,7 +34,7 @@ public:
 
     void SetUp()
     {
-        fake_time = PosixTimestamp();
+        fake_time = PosixTimestamp(CLOCK_MONOTONIC, 12345678ns);
         int const hz = 60;
         one_frame = std::chrono::nanoseconds(1000000000L/hz);
     }
@@ -48,6 +48,7 @@ protected:
 TEST_F(FrameClockTest, unthrottled_without_a_period)
 {
     FrameClock clock(with_fake_time);
+
     PosixTimestamp a;
     auto b = clock.next_frame_after(a);
     EXPECT_EQ(a, b);
@@ -57,12 +58,58 @@ TEST_F(FrameClockTest, unthrottled_without_a_period)
 
 TEST_F(FrameClockTest, first_frame_is_within_one_frame)
 {
-    fake_time.nanoseconds = 12345678ns;
     auto a = fake_time;
     FrameClock clock(with_fake_time);
     clock.set_period(one_frame);
+
     PosixTimestamp b;
     auto c = clock.next_frame_after(b);
     EXPECT_GE(c, a);
     EXPECT_LE(c-a, one_frame);
+}
+
+TEST_F(FrameClockTest, interval_is_perfectly_smooth)
+{
+    FrameClock clock(with_fake_time);
+    clock.set_period(one_frame);
+
+    PosixTimestamp a;
+    auto b = clock.next_frame_after(a);
+
+    // Render time varies but our interval should not...
+    fake_time.nanoseconds += one_frame/13;
+
+    auto c = clock.next_frame_after(b);
+    EXPECT_EQ(one_frame, c - b);
+
+    fake_time.nanoseconds += one_frame/7;
+
+    auto d = clock.next_frame_after(c);
+    EXPECT_EQ(one_frame, d - c);
+
+    fake_time.nanoseconds += one_frame/5;
+
+    auto e = clock.next_frame_after(d);
+    EXPECT_EQ(one_frame, e - d);
+}
+
+TEST_F(FrameClockTest, long_render_time_is_recoverable_without_decimation)
+{
+    FrameClock clock(with_fake_time);
+    clock.set_period(one_frame);
+
+    PosixTimestamp a = fake_time;
+    auto b = clock.next_frame_after(a);
+
+    fake_time.nanoseconds += one_frame * 5 / 4;
+
+    auto c = clock.next_frame_after(b);
+    EXPECT_EQ(one_frame, c - b);
+
+    fake_time.nanoseconds += one_frame * 7 / 6;
+
+    auto d = clock.next_frame_after(c);
+    EXPECT_EQ(one_frame, d - c);
+
+    EXPECT_LT(d, fake_time);
 }
