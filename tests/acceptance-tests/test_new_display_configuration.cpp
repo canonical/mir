7d89ba3e6642e9d675159598147e0338765fb87e
Daniel van Vugt <daniel.van.vugt@canonical.com>
2016-12-01 17:09:42 +0800

    Test case too

--- tests/acceptance-tests/test_new_display_configuration.cpp
+++ tests/acceptance-tests/test_new_display_configuration.cpp
@@ -1085,6 +1085,42 @@ TEST_F(DisplayConfigurationTest, client_receives_edid)
     client.disconnect();
 }
 
+TEST_F(DisplayConfigurationTest, client_receives_model_string_from_edid)
+{
+    static char const edid[129] =
+        "\x00\xff\xff\xff\xff\xff\xff\x00\x10\xac\x46\xf0\x4c\x4a\x31\x41"
+        "\x05\x19\x01\x04\xb5\x34\x20\x78\x3a\x1d\xf5\xae\x4f\x35\xb3\x25"
+        "\x0d\x50\x54\xa5\x4b\x00\x81\x80\xa9\x40\xd1\x00\x71\x4f\x01\x01"
+        "\x01\x01\x01\x01\x01\x01\x28\x3c\x80\xa0\x70\xb0\x23\x40\x30\x20"
+        "\x36\x00\x06\x44\x21\x00\x00\x1a\x00\x00\x00\xff\x00\x59\x43\x4d"
+        "\x30\x46\x35\x31\x52\x41\x31\x4a\x4c\x0a\x00\x00\x00\xfc\x00\x44"
+        "\x45\x4c\x4c\x20\x55\x32\x34\x31\x33\x0a\x20\x20\x00\x00\x00\xfd"
+        "\x00\x38\x4c\x1e\x51\x11\x00\x0a\x20\x20\x20\x20\x20\x20\x01\x42";
+
+    mtd::StubDisplayConfigurationOutput monitor{
+        mg::DisplayConfigurationOutputId{48},
+        {{{1920, 1200}, 60.0}},
+        {mir_pixel_format_abgr_8888}};
+
+    auto begin = (uint8_t const*)edid;
+    monitor.edid.assign(begin, begin+128);
+
+    auto config = std::make_shared<mtd::StubDisplayConfig>(
+                      std::vector<mg::DisplayConfigurationOutput>{monitor});
+
+    apply_config_change_and_wait_for_propagation(config);
+
+    DisplayClient client{new_connection()};
+    client.connect();
+
+    auto base_config = client.get_base_config();
+    auto output = mir_display_config_get_output(base_config.get(), 0);
+
+    EXPECT_STREQ("DELL U2413", mir_output_get_model(output));
+
+    client.disconnect();
+}
+
 namespace
 {
 MATCHER_P(IsSameModeAs, mode, "")
