65d9fde4080c8a8f46c07c9336b954a94c6e3210
Kevin DuBois <kevin.dubois@canonical.com>
2017-01-04 12:31:19 -0500

    allocate buffers

--- src/server/graphics/nested/platform.cpp
+++ src/server/graphics/nested/platform.cpp
@@ -73,22 +73,29 @@ public:
     {
     }
 
-    std::shared_ptr<mg::Buffer> alloc_buffer(mg::BufferProperties const& buffer_properties) override
+    std::shared_ptr<mg::Buffer> alloc_buffer(mg::BufferProperties const& properties) override
     {
-        if ((buffer_properties.size.width >= mir::geometry::Width{480}) &&
-            (buffer_properties.size.height >= mir::geometry::Height{480}))
+        if (passthrough_candidate(properties.size))
         {
-            return std::make_shared<mgn::Buffer>(connection, buffer_properties.size, buffer_properties.format);
+            return std::make_shared<mgn::Buffer>(connection, properties.size, properties.format);
         }
         else
         {
-            return guest_allocator->alloc_buffer(buffer_properties);
+            return guest_allocator->alloc_buffer(properties);
         }
     }
 
-    std::shared_ptr<mg::Buffer> alloc_buffer(mg::BufferRequestMessage const&)
+    std::shared_ptr<mg::Buffer> alloc_buffer(mg::BufferRequestMessage const& properties)
     {
-        return nullptr;
+        if (passthrough_candidate(properties.size))
+        {
+            return std::make_shared<mgn::Buffer>(
+                connection, properties.size, properties.native_format, properties.native_flags);
+        }
+        else
+        {
+            return guest_allocator->alloc_buffer(properties);
+        }
     }
 
     std::vector<MirPixelFormat> supported_pixel_formats() override
@@ -97,6 +104,10 @@ public:
     }
 
 private:
+    bool passthrough_candidate(mir::geometry::Size size)
+    {
+        return (size.width >= mir::geometry::Width{480}) && (size.height >= mir::geometry::Height{480});
+    }
     std::shared_ptr<mgn::HostConnection> const connection;
     std::shared_ptr<mg::GraphicBufferAllocator> const guest_allocator;
 };
