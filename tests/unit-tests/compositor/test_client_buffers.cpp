7c13025fda7c1e3e07de819a9f05c845b03fc991
Kevin DuBois <kevin.dubois@canonical.com>
2017-01-05 12:48:28 -0500

    port over some tests to drive the allocator out of the map

--- tests/unit-tests/compositor/test_client_buffers.cpp
+++ tests/unit-tests/compositor/test_client_buffers.cpp
@@ -61,40 +61,34 @@ struct ClientBuffers : public Test
 {
     std::shared_ptr<mtd::MockEventSink> mock_sink = std::make_shared<testing::NiceMock<mtd::MockEventSink>>();
     mg::BufferProperties properties{geom::Size{42,43}, mir_pixel_format_abgr_8888, mg::BufferUsage::hardware};
-    MockBufferAllocator mock_allocator;
-    StubBufferAllocator stub_allocator;
-    mc::BufferMap map{mock_sink, mt::fake_shared(stub_allocator)};
+    mtd::StubBuffer stub_buffer(properties);
+    mc::BufferMap map{mock_sink};
 };
 
 TEST_F(ClientBuffers, sends_full_buffer_on_allocation)
 {
-    auto stub_buffer = std::make_shared<mtd::StubBuffer>();
-    EXPECT_CALL(mock_allocator, alloc_buffer(Ref(properties)))
-        .WillOnce(Return(stub_buffer));
     EXPECT_CALL(*mock_sink, add_buffer(Ref(*stub_buffer)));
-    mc::BufferMap map{mock_sink, mt::fake_shared(mock_allocator)};
-    EXPECT_THAT(map.add_buffer(properties), Eq(stub_buffer->id()));
+    mc::BufferMap map{mock_sink};
+    EXPECT_THAT(map.add_buffer(mt::fake_shared(stub_buffer)), Eq(stub_buffer->id()));
 }
 
 TEST_F(ClientBuffers, access_of_nonexistent_buffer_throws)
 {
-    auto stub_buffer = std::make_unique<mtd::StubBuffer>();
     EXPECT_THROW({
-        auto buffer = map[stub_buffer->id()];
+        auto buffer = map[stub_buffer.id()];
     }, std::logic_error);
 }
 
 TEST_F(ClientBuffers, removal_of_nonexistent_buffer_throws)
 {
-    auto stub_buffer = std::make_unique<mtd::StubBuffer>();
     EXPECT_THROW({
-        map.remove_buffer(stub_buffer->id());
+        map.remove_buffer(stub_buffer.id());
     }, std::logic_error);
 }
 
 TEST_F(ClientBuffers, can_access_once_added)
 {
-    map.add_buffer(properties);
+    map.add_buffer(mt::fake_shared(stub_buffer));
     ASSERT_THAT(stub_allocator.map, SizeIs(1));
     ASSERT_THAT(stub_allocator.ids, SizeIs(1));
     auto buffer = map[stub_allocator.ids[0]];
@@ -103,7 +97,7 @@ TEST_F(ClientBuffers, can_access_once_added)
 
 TEST_F(ClientBuffers, sends_update_msg_to_send_buffer)
 {
-    map.add_buffer(properties);
+    map.add_buffer(mt::fake_shared(stub_buffer));
     ASSERT_THAT(stub_allocator.map, SizeIs(1));
     ASSERT_THAT(stub_allocator.ids, SizeIs(1));
     auto buffer = map[stub_allocator.ids[0]];
@@ -113,7 +107,7 @@ TEST_F(ClientBuffers, sends_update_msg_to_send_buffer)
 
 TEST_F(ClientBuffers, sends_no_update_msg_if_buffer_is_not_around)
 {
-    map.add_buffer(properties);
+    map.add_buffer(mt::fake_shared(stub_buffer));
     ASSERT_THAT(stub_allocator.map, SizeIs(1));
     ASSERT_THAT(stub_allocator.ids, SizeIs(1));
     auto buffer = map[stub_allocator.ids[0]];
@@ -122,7 +116,7 @@ TEST_F(ClientBuffers, sends_no_update_msg_if_buffer_is_not_around)
     map.remove_buffer(stub_allocator.ids[0]);
     map.send_buffer(stub_allocator.ids[0]);
 }
-
+#if 0
 TEST_F(ClientBuffers, can_remove_buffer_from_send_callback)
 {
     map.add_buffer(properties);
@@ -156,3 +150,4 @@ TEST_F(ClientBuffers, sends_error_buffer_when_alloc_fails)
         map.add_buffer(properties);
     }, std::runtime_error);
 }
+#endif
