e936e756478d45167f9d7371de65ed6750d84a83
Brandon Schaefer <brandon.schaefer@canonical.com>
2017-01-09 09:36:02 -0800

    * Replace the use of mir_connection_create_screencast with the new way

--- tests/acceptance-tests/throwback/test_client_library_errors.cpp
+++ tests/acceptance-tests/throwback/test_client_library_errors.cpp
@@ -441,9 +441,5 @@ TEST_F(ClientLibraryErrorsDeathTest, surface_spec_attaching_invalid_rectangle)
 
 TEST_F(ClientLibraryErrorsDeathTest, creating_screencast_with_invalid_connection)
 {
-    MirScreencastParameters params{{0, 0, 1, 1}, 1, 1, mir_pixel_format_abgr_8888};
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-    MIR_EXPECT_DEATH(mir_connection_create_screencast_sync(nullptr, &params), "");
-#pragma GCC diagnostic pop
+    MIR_EXPECT_DEATH(mir_create_screencast_spec(nullptr), "");
 }
