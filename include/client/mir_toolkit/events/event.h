b14bc251d391457cf353412be3087c30baa362e3
Andreas Pokorny <andreas.pokorny@canonical.com>
2017-01-24 11:05:12 +0100

    deprecate enum value too

--- include/client/mir_toolkit/events/event.h
+++ include/client/mir_toolkit/events/event.h
@@ -48,7 +48,7 @@ typedef enum
        when old style event type was mir_event_type_key or mir_event_type_motion */
     mir_event_type_input,
     mir_event_type_keymap,
-    mir_event_type_input_configuration, /*  __attribute__ ((deprecated("use the input config change callback and mir_event_type_input_device_state instead"))), */
+    MIR_DEPRECATED_ENUM(mir_event_type_input_configuration, "mir_connection_set_input_config_change_callback and mir_event_type_input_device_state instead"),
     MIR_DEPRECATED_ENUM(mir_event_type_surface_output, "mir_event_type_window_output"),
     mir_event_type_window_output = mir_event_type_surface_output,
     mir_event_type_input_device_state,
