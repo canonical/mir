b7f22927e55021d8b5dfbd879b885951bb0a88ce
Kevin DuBois <kevin.dubois@canonical.com>
2017-02-08 12:58:41 -0500

    change screencasting to use a TextureTarget instead of a TextureSource

---
+++ include/renderers/gl/mir/renderer/gl/texture_target.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright © 2017 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Kevin DuBois <kevin.dubois@canonical.com>
+ */
+
+#ifndef MIR_RENDERER_GL_TEXTURE_TARGET_H_
+#define MIR_RENDERER_GL_TEXTURE_TARGET_H_
+
+namespace mir
+{
+namespace renderer
+{
+namespace gl
+{
+
+class TextureTarget
+{
+public:
+    virtual ~TextureTarget() = default;
+
+    /* Bind the target as a texture suitable for rendering to via glFramebufferTexture2D */
+    virtual void bind_for_write() = 0;
+
+protected:
+    TextureTarget() = default;
+    TextureTarget(TextureTarget const&) = delete;
+    TextureTarget& operator=(TextureTarget const&) = delete;
+};
+
+}
+}
+}
+
+#endif /* MIR_RENDERER_GL_TEXTURE_TARGET_H_ */
