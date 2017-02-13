93c023b2f93c7e3e83bc5a0d63fee67a7fe36d1f
Kevin DuBois <kevin.dubois@canonical.com>
2017-02-13 09:52:39 -0500

    remember the nested buffer

--- src/server/graphics/nested/buffer.cpp
+++ src/server/graphics/nested/buffer.cpp
@@ -18,6 +18,7 @@
 
 #include "mir/renderer/sw/pixel_source.h"
 #include "mir/renderer/gl/texture_source.h"
+#include "mir/renderer/gl/texture_target.h"
 #include "mir/graphics/egl_extensions.h"
 #include "mir/graphics/buffer_ipc_message.h"
 #include "mir_toolkit/mir_buffer.h"
