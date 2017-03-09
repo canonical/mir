92a9303ab1568ba96b0b2336de641d1cf5118866
Kevin DuBois <kevin.dubois@canonical.com>
2017-03-09 07:52:48 -0500

    undo 3587

--- src/platforms/mesa/server/kms/guest_platform.cpp
+++ src/platforms/mesa/server/kms/guest_platform.cpp
@@ -82,3 +82,8 @@ mir::UniqueModulePtr<mg::Display> mgm::GuestPlatform::create_display(
 {
     BOOST_THROW_EXCEPTION(std::runtime_error("mgm::GuestPlatform cannot create display\n"));
 }
+
+EGLNativeDisplayType mgm::GuestPlatform::egl_native_display() const
+{
+    return gbm.device;
+}
