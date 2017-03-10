1df192ed26ee6d62d1a67c749f564aca3818f2f6
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2017-03-10 15:17:22 +1100

    MesaDisplayConfigurationTest: Don't test unnecessary properties.
    We don't care what order the DisplayConfigurationOutputs are visited in the config,
    nor do we care what their DisplayConfigurationOutputIds are - just that they're
    unique.

--- tests/unit-tests/platforms/mesa/kms/test_display_configuration.cpp
+++ tests/unit-tests/platforms/mesa/kms/test_display_configuration.cpp
@@ -162,6 +162,41 @@ public:
     mtf::UdevEnvironment fake_devices;
 };
 
+MATCHER_P(Unique, scratch_vector, "")
+{
+    if (std::any_of(
+        scratch_vector.begin(),
+        scratch_vector.end(),
+        [&arg](auto const& candidate) { return arg == candidate; }))
+    {
+        return false;
+    }
+    const_cast<typename std::remove_const<scratch_vector_type>::type&>(scratch_vector).push_back(arg);
+    return true;
+}
+
+MATCHER_P(DisplayConfigsAreEquivalent, expected_configs, "")
+{
+    using namespace testing;
+
+    std::vector<mg::DisplayConfigurationOutput> outputs;
+    std::vector<mg::DisplayConfigurationOutputId> output_ids;
+    arg->for_each_output([&](mg::DisplayConfigurationOutput const& output)
+                          {
+                              outputs.push_back(output);
+                              output_ids.push_back(output.id);
+                              outputs.back().id = mg::DisplayConfigurationOutputId{0};
+                          });
+
+
+    Matcher<decltype(outputs)> config_matcher = UnorderedElementsAreArray(expected_configs);
+    std::vector<mg::DisplayConfigurationOutputId> scratch_space;
+    Matcher<decltype(output_ids)> ids_are_unique = Each(Unique(scratch_space));
+
+    return
+        config_matcher.MatchAndExplain(outputs, result_listener) &&
+        ids_are_unique.MatchAndExplain(output_ids, result_listener);
+}
 }
 
 TEST_F(MesaDisplayConfigurationTest, configuration_is_read_correctly)
@@ -209,7 +244,7 @@ TEST_F(MesaDisplayConfigurationTest, configuration_is_read_correctly)
     std::vector<mg::DisplayConfigurationOutput> const expected_outputs =
     {
         {
-            mg::DisplayConfigurationOutputId{connector0_id},
+            mg::DisplayConfigurationOutputId{0},
             mg::DisplayConfigurationCardId{0},
             mg::DisplayConfigurationOutputType::hdmia,
             {},
@@ -231,7 +266,7 @@ TEST_F(MesaDisplayConfigurationTest, configuration_is_read_correctly)
             {}
         },
         {
-            mg::DisplayConfigurationOutputId{connector1_id},
+            mg::DisplayConfigurationOutputId{0},
             mg::DisplayConfigurationCardId{0},
             mg::DisplayConfigurationOutputType::unknown,
             {},
@@ -253,7 +288,7 @@ TEST_F(MesaDisplayConfigurationTest, configuration_is_read_correctly)
             {}
         },
         {
-            mg::DisplayConfigurationOutputId{connector2_id},
+            mg::DisplayConfigurationOutputId{0},
             mg::DisplayConfigurationCardId{0},
             mg::DisplayConfigurationOutputType::edp,
             {},
@@ -281,16 +316,7 @@ TEST_F(MesaDisplayConfigurationTest, configuration_is_read_correctly)
 
     auto conf = display->configuration();
 
-    size_t output_count{0};
-
-    conf->for_each_output([&](mg::DisplayConfigurationOutput const& output)
-    {
-        ASSERT_LT(output_count, expected_outputs.size());
-        EXPECT_EQ(expected_outputs[output_count], output) << "output_count: " << output_count;
-        ++output_count;
-    });
-
-    EXPECT_EQ(expected_outputs.size(), output_count);
+    EXPECT_THAT(conf, DisplayConfigsAreEquivalent(expected_outputs));
 }
 
 TEST_F(MesaDisplayConfigurationTest, reads_subpixel_information_correctly)
@@ -442,88 +468,6 @@ TEST_F(MesaDisplayConfigurationTest, reads_updated_subpixel_information)
     }
 }
 
-TEST_F(MesaDisplayConfigurationTest, get_kms_connector_id_returns_correct_id)
-{
-    uint32_t const crtc0_id{10};
-    uint32_t const encoder0_id{20};
-    uint32_t const possible_crtcs_mask_empty{0};
-    std::vector<uint32_t> const connector_ids{30, 31};
-    std::vector<uint32_t> encoder_ids{20};
-
-    /* Set up DRM resources */
-    mtd::FakeDRMResources& resources(mock_drm.fake_drm);
-
-    resources.reset();
-
-    resources.add_crtc(crtc0_id, modes0[1]);
-    resources.add_encoder(encoder0_id, crtc0_id, possible_crtcs_mask_empty);
-    for (auto id : connector_ids)
-    {
-        resources.add_connector(id, DRM_MODE_CONNECTOR_DVID,
-                                DRM_MODE_CONNECTED, encoder0_id,
-                                modes0, encoder_ids,
-                                geom::Size());
-    }
-
-    resources.prepare();
-
-    /* Test body */
-    auto display = create_display(create_platform());
-
-    std::shared_ptr<mg::DisplayConfiguration> conf = display->configuration();
-    auto const& kms_conf = std::static_pointer_cast<mgm::KMSDisplayConfiguration>(conf);
-
-    size_t output_count{0};
-
-    conf->for_each_output([&](mg::DisplayConfigurationOutput const& output)
-    {
-        ASSERT_LT(output_count, connector_ids.size());
-
-        EXPECT_EQ(connector_ids[output_count],
-                  kms_conf->get_kms_connector_id(output.id));
-        ++output_count;
-    });
-}
-
-TEST_F(MesaDisplayConfigurationTest, get_kms_connector_id_throws_on_invalid_id)
-{
-    uint32_t const crtc0_id{10};
-    uint32_t const encoder0_id{20};
-    uint32_t const possible_crtcs_mask_empty{0};
-    std::vector<uint32_t> const connector_ids{30, 31};
-    std::vector<uint32_t> encoder_ids{20};
-
-    /* Set up DRM resources */
-    mtd::FakeDRMResources& resources(mock_drm.fake_drm);
-
-    resources.reset();
-
-    resources.add_crtc(crtc0_id, modes0[1]);
-    resources.add_encoder(encoder0_id, crtc0_id, possible_crtcs_mask_empty);
-    for (auto id : connector_ids)
-    {
-        resources.add_connector(id, DRM_MODE_CONNECTOR_VGA,
-                                DRM_MODE_CONNECTED, encoder0_id,
-                                modes0, encoder_ids,
-                                geom::Size());
-    }
-
-    resources.prepare();
-
-    /* Test body */
-    auto display = create_display(create_platform());
-
-    std::shared_ptr<mg::DisplayConfiguration> conf = display->configuration();
-    auto const& kms_conf = std::static_pointer_cast<mgm::KMSDisplayConfiguration>(conf);
-
-    EXPECT_THROW({
-        kms_conf->get_kms_connector_id(mg::DisplayConfigurationOutputId{29});
-    }, std::runtime_error);
-    EXPECT_THROW({
-        kms_conf->get_kms_connector_id(mg::DisplayConfigurationOutputId{32});
-    }, std::runtime_error);
-}
-
 TEST_F(MesaDisplayConfigurationTest, returns_updated_configuration)
 {
     using namespace ::testing;
@@ -545,7 +489,7 @@ TEST_F(MesaDisplayConfigurationTest, returns_updated_configuration)
     std::vector<mg::DisplayConfigurationOutput> const expected_outputs_before =
     {
         {
-            mg::DisplayConfigurationOutputId(connector_ids[0]),
+            mg::DisplayConfigurationOutputId{0},
             mg::DisplayConfigurationCardId{0},
             mg::DisplayConfigurationOutputType::composite,
             {},
@@ -567,7 +511,7 @@ TEST_F(MesaDisplayConfigurationTest, returns_updated_configuration)
             {}
         },
         {
-            mg::DisplayConfigurationOutputId(connector_ids[1]),
+            mg::DisplayConfigurationOutputId{0},
             mg::DisplayConfigurationCardId{0},
             mg::DisplayConfigurationOutputType::vga,
             {},
@@ -593,7 +537,7 @@ TEST_F(MesaDisplayConfigurationTest, returns_updated_configuration)
     std::vector<mg::DisplayConfigurationOutput> const expected_outputs_after =
     {
         {
-            mg::DisplayConfigurationOutputId(connector_ids[0]),
+            mg::DisplayConfigurationOutputId{0},
             mg::DisplayConfigurationCardId{0},
             mg::DisplayConfigurationOutputType::composite,
             {},
@@ -615,7 +559,7 @@ TEST_F(MesaDisplayConfigurationTest, returns_updated_configuration)
             {}
         },
         {
-            mg::DisplayConfigurationOutputId(connector_ids[1]),
+            mg::DisplayConfigurationOutputId{0},
             mg::DisplayConfigurationCardId{0},
             mg::DisplayConfigurationOutputType::vga,
             {},
@@ -664,16 +608,7 @@ TEST_F(MesaDisplayConfigurationTest, returns_updated_configuration)
 
     auto conf = display->configuration();
 
-    size_t output_count{0};
-
-    conf->for_each_output([&](mg::DisplayConfigurationOutput const& output)
-    {
-        ASSERT_LT(output_count, expected_outputs_before.size());
-        EXPECT_EQ(expected_outputs_before[output_count], output) << "output_count: " << output_count;
-        ++output_count;
-    });
-
-    EXPECT_EQ(expected_outputs_before.size(), output_count);
+    EXPECT_THAT(conf, DisplayConfigsAreEquivalent(expected_outputs_before));
 
     /* Reset DRM resources and check again */
     resources.reset();
@@ -703,19 +638,10 @@ TEST_F(MesaDisplayConfigurationTest, returns_updated_configuration)
 
     conf = display->configuration();
 
-    output_count = 0;
-
-    conf->for_each_output([&](mg::DisplayConfigurationOutput const& output)
-    {
-        ASSERT_LT(output_count, expected_outputs_after.size());
-        EXPECT_EQ(expected_outputs_after[output_count], output) << "output_count: " << output_count;
-        ++output_count;
-    });
-
-    EXPECT_EQ(expected_outputs_after.size(), output_count);
+    EXPECT_THAT(conf, DisplayConfigsAreEquivalent(expected_outputs_after));
 }
 
-TEST_F(MesaDisplayConfigurationTest, new_monitor_defaults_to_preferred_mode)
+TEST_F(MesaDisplayConfigurationTest, new_monitor_defaults_to_whatever_mode_drm_set_it_up_with)
 {
     using namespace ::testing;
     using namespace std::chrono_literals;
@@ -727,12 +653,11 @@ TEST_F(MesaDisplayConfigurationTest, new_monitor_defaults_to_preferred_mode)
     geom::Size const connector_physical_sizes_mm_after{512, 642};
     std::vector<uint32_t> possible_encoder_ids_empty;
     uint32_t const possible_crtcs_mask_empty{0};
-    int const noutputs = 1;
 
-    mg::DisplayConfigurationOutput const expected_outputs_before[noutputs] =
+    std::vector<mg::DisplayConfigurationOutput> const expected_outputs_before =
     {
         {
-            mg::DisplayConfigurationOutputId(connector_ids[0]),
+            mg::DisplayConfigurationOutputId{0},
             mg::DisplayConfigurationCardId{0},
             mg::DisplayConfigurationOutputType::composite,
             {},
@@ -755,10 +680,10 @@ TEST_F(MesaDisplayConfigurationTest, new_monitor_defaults_to_preferred_mode)
         },
     };
 
-    mg::DisplayConfigurationOutput const expected_outputs_after[noutputs] =
+    std::vector<mg::DisplayConfigurationOutput> const expected_outputs_after =
     {
         {
-            mg::DisplayConfigurationOutputId(connector_ids[0]),
+            mg::DisplayConfigurationOutputId{0},
             mg::DisplayConfigurationCardId{0},
             mg::DisplayConfigurationOutputType::composite,
             {},
@@ -796,14 +721,7 @@ TEST_F(MesaDisplayConfigurationTest, new_monitor_defaults_to_preferred_mode)
 
     auto display = create_display(create_platform());
     auto conf = display->configuration();
-    int output_count = 0;
-    conf->for_each_output([&](mg::DisplayConfigurationOutput const& output)
-    {
-        ASSERT_LT(output_count, noutputs);
-        EXPECT_EQ(expected_outputs_before[output_count], output) << "output_count: " << output_count;
-        ++output_count;
-    });
-    EXPECT_EQ(noutputs, output_count);
+    EXPECT_THAT(conf, DisplayConfigsAreEquivalent(expected_outputs_before));
 
     // Now simulate a change of monitor with different capabilities where the
     // old current_mode does not exist. Mir should choose the preferred mode.
@@ -824,14 +742,7 @@ TEST_F(MesaDisplayConfigurationTest, new_monitor_defaults_to_preferred_mode)
     ASSERT_TRUE(handler_signal.wait_for(10s));
 
     conf = display->configuration();
-    output_count = 0;
-    conf->for_each_output([&](mg::DisplayConfigurationOutput const& output)
-    {
-        ASSERT_LT(output_count, noutputs);
-        EXPECT_EQ(expected_outputs_after[output_count], output) << "output_count: " << output_count;
-        ++output_count;
-    });
-    EXPECT_EQ(noutputs, output_count);
+    EXPECT_THAT(conf, DisplayConfigsAreEquivalent(expected_outputs_after));
 }
 
 TEST_F(MesaDisplayConfigurationTest, does_not_query_drm_unnecesarily)
