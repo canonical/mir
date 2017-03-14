4871e344cee23334a62b361c317ea169de8dbaee
Daniel van Vugt <daniel.van.vugt@canonical.com>
2017-03-14 16:37:57 +0800

    Make the first regression test more likely to fail on regressions.

--- tests/unit-tests/renderers/gl/test_gl_renderer.cpp
+++ tests/unit-tests/renderers/gl/test_gl_renderer.cpp
@@ -318,8 +318,16 @@ TEST_F(GLRenderer, swaps_buffers_after_rendering)
 
 TEST_F(GLRenderer, unchanged_viewport_avoids_gl_calls)
 {
+    int const screen_width = 1920;
+    int const screen_height = 1080;
     mir::geometry::Rectangle const view_area{{0,0}, {1920,1080}};
 
+    ON_CALL(mock_egl, eglQuerySurface(_,_,EGL_WIDTH,_))
+        .WillByDefault(DoAll(SetArgPointee<3>(screen_width),
+                             Return(EGL_TRUE)));
+    ON_CALL(mock_egl, eglQuerySurface(_,_,EGL_HEIGHT,_))
+        .WillByDefault(DoAll(SetArgPointee<3>(screen_height),
+                             Return(EGL_TRUE)));
     ON_CALL(mock_display_buffer, view_area())
         .WillByDefault(Return(view_area));
 
