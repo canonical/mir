50a748bac86fdcd15d2e9c00d3ade264e7ac6f5f
Alan Griffiths <alan@octopull.co.uk>
2017-03-23 17:39:16 +0000

    Correct API documentation

--- include/client/mir_toolkit/extensions/drag_and_drop.h
+++ include/client/mir_toolkit/extensions/drag_and_drop.h
@@ -31,8 +31,8 @@ typedef struct MirDragAndDropEvent MirDragAndDropEvent;
 typedef struct MirDragAndDropV1
 {
     /**
-     * Request drag and drop. If the request succeeds a window event with a
-     * "start drag" handle will be received.
+     * Request drag and drop. If the request succeeds a "start drag" handle will
+     * be received via the callback set by set_start_drag_and_drop_callback().
      *
      * \warning An invalid cookie will terminate the client connection.
      *
@@ -42,7 +42,8 @@ typedef struct MirDragAndDropV1
     void (*request_drag_and_drop)(MirWindow* window, MirCookie const* cookie);
 
     /**
-     * Set the drag and drop callback. This receives
+     * Set the drag and drop callback.
+     * This receives a MirDragAndDropEvent if and when drag and drop begins.
      *
      * \param [in] window   The window
      * \param [in] callback The callback function.
@@ -53,7 +54,9 @@ typedef struct MirDragAndDropV1
                void* context);
 
     /**
-     * Retrieve any "drag & drop" handle associated with the event.
+     * Retrieve any "start drag & drop" handle associated with the event.
+     * The handle is used to provide drag and drop metadata and content to the
+     * service supporting drag and drop (e.g. on Ubuntu the content hub).
      *
      * \param [in] event The event
      * \return           The associated drag handle or NULL
@@ -62,6 +65,8 @@ typedef struct MirDragAndDropV1
 
     /**
      * Retrieve any "drag & drop" handle associated with the event.
+     * The handle is used to retrieve drag and drop metadata and content from
+     * the service supporting drag and drop (e.g. on Ubuntu the content hub).
      *
      * \param [in] event The event
      * \return           The associated drag handle or NULL
