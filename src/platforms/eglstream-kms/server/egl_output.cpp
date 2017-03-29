e6205aaad5e39811dff6d9eecfe38d841e0dddb4
Daniel van Vugt <daniel.van.vugt@canonical.com>
2017-03-29 15:21:43 +0800

    And restore the bit we do want.

--- src/platforms/eglstream-kms/server/egl_output.cpp
+++ src/platforms/eglstream-kms/server/egl_output.cpp
@@ -186,6 +186,12 @@ void mgek::EGLOutput::reset()
     }
 }
 
+geom::Size mgek::EGLOutput::size() const
+{
+    drmModeModeInfo const& mode = connector->modes[mode_index];
+    return {mode.hdisplay, mode.vdisplay};
+}
+
 int mgek::EGLOutput::max_refresh_rate() const
 {
     drmModeModeInfo const& current_mode = connector->modes[mode_index];
