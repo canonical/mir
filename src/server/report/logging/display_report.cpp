0d86d2c3e6c3598c9a1fa1e7e7a522a97a94a4ea
Kevin DuBois <kevin.dubois@canonical.com>
2017-04-05 08:09:39 -0400

    tests to pass

--- src/server/report/logging/display_report.cpp
+++ src/server/report/logging/display_report.cpp
@@ -90,13 +90,22 @@ void mrl::DisplayReport::report_vt_switch_back_failure()
 
 void mrl::DisplayReport::report_egl_configuration(EGLDisplay disp, EGLConfig config)
 {
-    std::string extensions { eglQueryString(disp, EGL_EXTENSIONS) };
+    auto ext = eglQueryString(disp, EGL_EXTENSIONS);
+    std::string extensions { ext ? ext : "" };
     logger->log(ml::Severity::informational, "Display EGL Extensions: " + extensions, component());
 
-    std::string client_extensions(eglQueryString(EGL_NO_DISPLAY, EGL_EXTENSIONS));
-    logger->log(ml::Severity::informational, "EGL_EXT_client_extensions: " + client_extensions, component());
-    if (client_extensions.empty())
+    auto client_ext = eglQueryString(EGL_NO_DISPLAY, EGL_EXTENSIONS);
+    if (client_ext)
+    {
+        logger->log(ml::Severity::informational,
+            "EGL_EXT_client_extensions: " + std::string{client_ext}, component());
+    }
+    else
+    {
+        logger->log(ml::Severity::informational, "EGL_EXT_client_extensions not supported", component());
+        //clear out error
         eglGetError();
+    }
 
     #define STRMACRO(X) {#X, X}
     struct {std::string name; EGLint val;} egl_string_mapping [] =
