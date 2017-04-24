8dba8830056279de90f12c3e41fc41dc248fd60e
Alan Griffiths <alan@octopull.co.uk>
2017-04-24 15:01:11 +0100

    Sensible defaults for deprecation logic

--- include/client/mir_toolkit/deprecations.h
+++ include/client/mir_toolkit/deprecations.h
@@ -2,28 +2,35 @@
  * Copyright © 2017 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 3,
+ * under the terms of the GNU Lesser General Public License version 3,
  * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Authored by: Alan Griffiths <alan@octopull.co.uk>
  */
 
 #ifndef MIR_DEPRECATIONS_H_
 #define MIR_DEPRECATIONS_H_
 
-#ifdef MIR_ENABLE_DEPRECATIONS
+#ifndef MIR_ENABLE_DEPRECATIONS
+    // use __GNUC__ < 6 as a proxy for building on Ubunutu 16.04LTS ("Xenial")
+    #if !defined(__GNUC__) || __GNUC__ >= 6
+        #define MIR_ENABLE_DEPRECATIONS 1
+    #else
+        #define MIR_ENABLE_DEPRECATIONS 0
+    #endif
+#endif
+
+#if MIR_ENABLE_DEPRECATIONS
     #define MIR_FOR_REMOVAL_IN_VERSION_1(message)\
-    MIR_FOR_REMOVAL_IN_VERSION_1("Use MirWindow instead")
+    MIR_FOR_REMOVAL_IN_VERSION_1(message)
 #else
-#define MIR_FOR_REMOVAL_IN_VERSION_1(message)
+    #define MIR_FOR_REMOVAL_IN_VERSION_1(message)
 #endif
 
 #endif //MIR_DEPRECATIONS_H_
