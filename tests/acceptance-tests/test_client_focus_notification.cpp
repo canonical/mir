444afb93764075a7daaa9801fd010d85da6a75b7
Alan Griffiths <alan@octopull.co.uk>
2017-05-31 17:00:56 +0100

    Remove unused headers

--- tests/acceptance-tests/test_client_focus_notification.cpp
+++ tests/acceptance-tests/test_client_focus_notification.cpp
@@ -18,12 +18,7 @@
 
 #include "mir_toolkit/mir_client_library.h"
 
-#include "mir/test/signal.h"
-#include "mir/test/event_matchers.h"
-
 #include "mir_test_framework/connected_client_headless_server.h"
-#include "mir_test_framework/process.h"
-#include "mir/test/cross_process_sync.h"
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
