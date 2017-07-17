3adbd0e1d9f28f2ac0344bada0ba18466c079b42
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2017-07-17 11:20:12 +1000

    client: Don't wait for a BufferOperation::released event before freeing the client buffer.
    The only reason to keep a track of buffers that the client has asked to be released but that
    the server hasn't acknowledged the release of is so that we can handle ::update operations
    on those buffers.
    
    But there's nothing the client library can usefully do on that ::update operation, so instead
    we can just ignore updates to buffers the client doesn't know about.
    
    This *does* mean that we don't get debug messages when the client receives an update to a
    buffer that it doesn't know about, but that's an acceptable loss.

--- src/client/buffer_vault.cpp
+++ src/client/buffer_vault.cpp
@@ -90,7 +90,9 @@ mcl::BufferVault::~BufferVault()
             buffer->set_callback(ignore_buffer, nullptr);
         } 
         if (!disconnected_)
+        {
             free_buffer(it.first);
+        }
     }
     catch (...)
     {
@@ -110,6 +112,10 @@ void mcl::BufferVault::alloc_buffer(geom::Size size, MirPixelFormat format, int 
 void mcl::BufferVault::free_buffer(int free_id)
 {
     server_requests->free_buffer(free_id);
+    if (auto map = surface_map.lock())
+    {
+        map->erase(free_id);
+    }
 }
 
 void mcl::BufferVault::realloc_buffer(int free_id, geom::Size size, MirPixelFormat format, int usage)
