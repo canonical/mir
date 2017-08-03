f5cc2e325d8ad08fe414bc9c6f920babf52fab96
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2017-08-03 12:24:49 +1000

    ThreadExectuor: Oh my god, the bees!
    Static storage duration of something containing std::mutex and std::condition_variable plays badly
    with fork(), such as our esteemed testsuite uses.
    
    It's also possible that Mir shells would fork() under certain circumstances, and it'd be nice if
    we didn't make that break horribly.
    
    So: register a pthread_atfork handler to quiesce the ThreadExecutor into a known-safe idle state,
    and then restart it on the other side of the fork.

--- src/server/frontend/default_ipc_factory.cpp
+++ src/server/frontend/default_ipc_factory.cpp
@@ -44,23 +44,17 @@ namespace
 class ThreadExecutor : public mir::Executor
 {
 public:
+    ThreadExecutor() = default;
+
+    ThreadExecutor(ThreadExecutor const&) = delete;
+    ThreadExecutor& operator=(ThreadExecutor const&) = delete;
+
     void do_work() noexcept
     {
+        std::unique_lock<std::mutex> lock{queue_mutex};
+        state = State::Running;
         for(;;)
         {
-            std::unique_lock<std::mutex> lock{queue_mutex};
-            queue_notifier.wait(
-                lock,
-                [this]()
-                {
-                    return shutdown || !tasks.empty();
-                });
-
-            if (shutdown)
-            {
-                return;
-            }
-
             while (!tasks.empty())
             {
                 std::function<void()> task;
@@ -78,19 +72,24 @@ public:
                 task = nullptr;
                 lock.lock();
             }
+
+            if (state != State::Running)
+            {
+                return;
+            }
+
+            queue_notifier.wait(
+                lock,
+                [this]()
+                {
+                    return (state != State::Running) || !tasks.empty();
+                });
         }
     }
 
     ~ThreadExecutor()
     {
-        {
-            std::lock_guard<std::mutex> lock{queue_mutex};
-            shutdown = true;
-        }
-        queue_notifier.notify_all();
-
-        if (dispatch_thread.joinable())
-            dispatch_thread.join();
+        quiesce();
     }
 
     void spawn(std::function<void()>&& work) override
@@ -99,7 +98,7 @@ public:
             std::lock_guard<std::mutex> lock{queue_mutex};
             tasks.emplace_back(std::move(work));
 
-            if (!dispatch_thread.joinable())
+            if (state == State::NotYetStarted)
             {
                 /*
                  * Block all signals on the dispatch thread.
@@ -115,16 +114,108 @@ public:
         queue_notifier.notify_all();
     }
 
+    void quiesce()
+    {
+        {
+            std::lock_guard<std::mutex> lock{queue_mutex};
+            state = State::Quiesced;
+        }
+        queue_notifier.notify_all();
+
+        if (dispatch_thread.joinable())
+            dispatch_thread.join();
+    }
+
+    void resume()
+    {
+        std::lock_guard<std::mutex> lock{queue_mutex};
+        state = State::NotYetStarted;
+        if (!tasks.empty())
+        {
+            /*
+             * Block all signals on the dispatch thread.
+             *
+             * Threads inherit their parent's signal mask, so use a SignalBlocker to block
+             * all signals *before* spawning the thread (and then restore the signal mask
+             * when this constructor completes).
+             */
+            mir::SignalBlocker blocker;
+            dispatch_thread = std::thread{std::bind(&ThreadExecutor::do_work, this)};
+        }
+    }
+
+    void discard()
+    {
+        std::lock_guard<std::mutex> lock{queue_mutex};
+        tasks.clear();
+        state = State::NotYetStarted;
+    }
 private:
     std::thread dispatch_thread;
 
     std::mutex queue_mutex;
     std::condition_variable queue_notifier;
-    bool shutdown{false};
+    enum class State
+    {
+        NotYetStarted,
+        Running,
+        Quiesced
+    } state{State::NotYetStarted};
     std::deque<std::function<void()>> tasks;
 };
 
-ThreadExecutor executor;
+mir::Executor& system_executor()
+{
+    static std::once_flag setup;
+    static ThreadExecutor executor;
+
+    std::call_once(
+        setup,
+        []()
+        {
+            /*
+             * fork() interacts with threads by screaming about being
+             * smothered by moths and then gibbering in a corner.
+             *
+             * Conveniently, our test-suite makes extensive use of fork(),
+             * and runs all the tests from a single main process, meaning
+             * that once a single test has called executor.spawn() every
+             * subsequent call to fork() is a call from a multithreaded
+             * program.
+             *
+             * Enter the moths.
+             *
+             * We can get around this by quiescing the executor; temporarily
+             * halting its execution thread and then resuming it post-fork.
+             */
+            pthread_atfork(
+                []()
+                {
+                    // Pre-fork
+                    executor.quiesce();
+                },
+                []()
+                {
+                    /*
+                     * Post-fork, in the parent:
+                     * Resume execution, executing any functors queued
+                     * since quiescence.
+                     */
+                    executor.resume();
+                },
+                []()
+                {
+                    /*
+                     * Post-fork, in the child:
+                     * Discard any tasks that snuck in after pre-fork but before fork();
+                     * they'll be handled in the parent.
+                     */
+                    executor.discard();
+                });
+        });
+
+    return executor;
+}
 }
 
 mf::DefaultIpcFactory::DefaultIpcFactory(
@@ -245,5 +336,5 @@ std::shared_ptr<mf::detail::DisplayServer> mf::DefaultIpcFactory::make_mediator(
         input_changer,
         extensions,
         buffer_allocator,
-        executor);
+        system_executor());
 }
