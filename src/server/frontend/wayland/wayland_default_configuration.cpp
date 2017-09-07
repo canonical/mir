fc62b59f7398d891e4380cc8b082d4ccf565e5ce
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2017-09-07 10:33:58 +1000

    Wayland: Fix up copyright header

--- src/server/frontend/wayland/wayland_default_configuration.cpp
+++ src/server/frontend/wayland/wayland_default_configuration.cpp
@@ -1,8 +1,8 @@
 /*
- * Copyright © 2015 Canonical Ltd.
+ * Copyright © 2015, 2017 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 3,
+ * under the terms of the GNU General Public License version 2 or 3,
  * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
