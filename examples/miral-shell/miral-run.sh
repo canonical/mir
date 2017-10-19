a73cc27b915432e5d92ffa90db90d4eac55a13c9
Alan Griffiths <alan@octopull.co.uk>
2017-10-19 10:34:34 +0100

    Don't assume that qtubuntu-desktop is available

--- examples/miral-shell/miral-run.sh
+++ examples/miral-shell/miral-run.sh
@@ -14,4 +14,15 @@ if [ "$1" = "gnome-terminal" ]
 then extras='--app-id com.canonical.miral.Terminal'
 fi
 unset QT_QPA_PLATFORMTHEME
-MIR_SOCKET=${socket} XDG_SESSION_TYPE=mir GDK_BACKEND=mir QT_QPA_PLATFORM=ubuntumirclient SDL_VIDEODRIVER=wayland "$@" ${extras}&
+
+qt_qpa_platform=ubuntumirclient
+qtubuntu_desktop_installed=$(apt list qtubuntu-desktop
+ 2>/dev/null | grep installed | wc -l)
+if [ "${qtubuntu_desktop_installed}" == "0" ]
+then
+    echo "** Warning ** defaulting to Wayland backend for Qt"
+    echo "For the best experience install qtubuntu-desktop - run \"sudo apt install qtubuntu-desktop\""
+    qt_qpa_platform=wayland
+fi
+
+MIR_SOCKET=${socket} XDG_SESSION_TYPE=mir GDK_BACKEND=mir QT_QPA_PLATFORM=${qt_qpa_platform} SDL_VIDEODRIVER=wayland "$@" ${extras}&
