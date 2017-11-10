aa2ea25022c317ed28562cbb2ce32f5a96096ec9
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2017-11-10 16:47:27 +1100

    BasicSurface: mir_window_{types,states} is not a valid {type,state}.

--- src/server/scene/basic_surface.cpp
+++ src/server/scene/basic_surface.cpp
@@ -450,7 +450,7 @@ MirWindowType ms::BasicSurface::set_type(MirWindowType t)
      * happens to be always-false because clang has chosen an unsigned type as the
      * underlying storage.
      */
-    if (t < mir_window_type_normal || t > mir_window_types)
+    if (t < mir_window_type_normal || t >= mir_window_types)
     {
         BOOST_THROW_EXCEPTION(std::logic_error("Invalid surface "
             "type."));
@@ -475,7 +475,7 @@ MirWindowState ms::BasicSurface::state() const
 
 MirWindowState ms::BasicSurface::set_state(MirWindowState s)
 {
-    if (s < mir_window_state_unknown || s > mir_window_states)
+    if (s < mir_window_state_unknown || s >= mir_window_states)
         BOOST_THROW_EXCEPTION(std::logic_error("Invalid surface state."));
 
     std::unique_lock<std::mutex> lg(guard);
