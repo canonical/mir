00ea90ab3c76aeafd364c75e3e04012293380ac1
Alan Griffiths <alan.griffiths@canonical.com>
2017-12-26 15:25:33 +0000

    Scale and center the image instead of tiling it

--- examples/miral-shell/decoration_provider.cpp
+++ examples/miral-shell/decoration_provider.cpp
@@ -269,33 +269,42 @@ void render_background(MirBufferStream* buffer_stream, MirGraphicsRegion& graphi
     {
         static uint8_t const pattern[4] = {0x00, 0x00, 0x00, 0x00 };
 
-        char* row = (&graphics_region)->vaddr;
+        char* row = graphics_region.vaddr;
 
-        for (int j = 0; j < (&graphics_region)->height; j++)
+        for (int j = 0; j < graphics_region.height; j++)
         {
             uint32_t* pixel = (uint32_t*)row;
 
-            for (int i = 0; i < (&graphics_region)->width; i++)
+            for (int i = 0; i < graphics_region.width; i++)
                 memcpy(pixel + i, pattern, sizeof pixel[i]);
 
-            row += (&graphics_region)->stride;
+            row += graphics_region.stride;
         }
     }
 
     {
         char unsigned const* src = mir_peace_wall.pixel_data;
 
-        char* row = (&graphics_region)->vaddr;
+        char* row = graphics_region.vaddr;
 
-        for (int j = 0; j < (&graphics_region)->height; j++)
+        // scale the image to be bigger than the output
+        int const scale = std::max(
+            (graphics_region.height + (mir_peace_wall.height-1))/mir_peace_wall.height,
+            (graphics_region.width  + (mir_peace_wall.width-1))/mir_peace_wall.width);
+
+        // Center the output on the image
+        int const vertical_offset = (mir_peace_wall.height*scale - graphics_region.height)/2;
+        int const horizontal_offset = (mir_peace_wall.width*scale - graphics_region.width)/2;
+
+        for (int j = 0; j < graphics_region.height; j++)
         {
-            auto const src_row = src + mir_peace_wall.bytes_per_pixel * ((j%mir_peace_wall.height)*mir_peace_wall.width);
+            auto const src_row = src + mir_peace_wall.bytes_per_pixel*mir_peace_wall.width*((j+vertical_offset)/scale);
 
-            for (int i = 0; i < (&graphics_region)->width; i++)
+            for (int i = 0; i < graphics_region.width; i++)
             {
                 // src:  RGBA
                 // dest:XRGB
-                auto const src_pixel = src_row + mir_peace_wall.bytes_per_pixel * (i%mir_peace_wall.width);
+                auto const src_pixel = src_row + mir_peace_wall.bytes_per_pixel * ((i+horizontal_offset)/scale);
                 auto const dst_pixel = row + 4*i;
 
                 dst_pixel[0] = src_pixel[2]; // blue
@@ -304,7 +313,7 @@ void render_background(MirBufferStream* buffer_stream, MirGraphicsRegion& graphi
                 dst_pixel[3] = 0;
             }
 
-            row += (&graphics_region)->stride;
+            row += graphics_region.stride;
         }
     }
 
