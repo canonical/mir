192c65a96c3cf7ba98ebc469646627a6960a2857
Alan Griffiths <alan@octopull.co.uk>
2018-04-04 17:05:47 +0100

    Stub implementations of wl_data_offer

--- src/server/frontend_wayland/data_device_manager.cpp
+++ src/server/frontend_wayland/data_device_manager.cpp
@@ -20,27 +20,80 @@
 
 namespace mf = mir::frontend;
 
+//Source Client -> DataDeviceManager::create_data_source()
+//Source Client -> *DataSource::offer(type)
+//...
+//
+//Dest Client   : DataDevice::selection(DataOffer)
+//Dest Client   : DataOffer::offer(type)
+//Dest Client   -> DataOffer::receive(type, fd)
+//
+//...
+//Source Client : DataSource::send(type, fd)
+//Source Client -> fd::write(), fd::close()
+
 namespace
 {
+struct DataOffer : mf::wayland::DataOffer
+{
+protected:
+    DataOffer(struct wl_client* client, struct wl_resource* parent, uint32_t id) :
+        mf::wayland::DataOffer(client, parent, id)
+    {
+    }
+
+    void accept(uint32_t serial, std::experimental::optional<std::string> const& mime_type) override
+    {
+        (void)serial, (void)mime_type;
+        puts(__PRETTY_FUNCTION__);
+    }
+
+    void receive(std::string const& mime_type, mir::Fd fd) override
+    {
+        (void)mime_type, (void)fd;
+        puts(__PRETTY_FUNCTION__);
+    }
+
+    void destroy() override
+    {
+        puts(__PRETTY_FUNCTION__);
+    }
+
+    void finish() override
+    {
+        puts(__PRETTY_FUNCTION__);
+    }
+
+    void set_actions(uint32_t dnd_actions, uint32_t preferred_action) override
+    {
+        puts(__PRETTY_FUNCTION__);
+        (void)dnd_actions, (void)preferred_action;
+    }
+};
+
 struct DataSource : mf::wayland::DataSource
 {
     DataSource(struct wl_client* client, struct wl_resource* parent, uint32_t id) :
         mf::wayland::DataSource{client, parent, id}
     {
+        puts(__PRETTY_FUNCTION__);
     }
 
     void offer(std::string const& mime_type) override
     {
+        puts(__PRETTY_FUNCTION__);
         (void)mime_type;
     }
 
     void destroy() override
     {
+        puts(__PRETTY_FUNCTION__);
         wl_resource_destroy(resource);
     }
 
     void set_actions(uint32_t dnd_actions) override
     {
+        puts(__PRETTY_FUNCTION__);
         (void)dnd_actions;
     }
 };
@@ -50,22 +103,26 @@ struct DataDevice : mf::wayland::DataDevice
     DataDevice(struct wl_client* client, struct wl_resource* parent, uint32_t id) :
         mf::wayland::DataDevice(client, parent, id)
     {
+        puts(__PRETTY_FUNCTION__);
     }
 
     void start_drag(
         std::experimental::optional<struct wl_resource*> const& source, struct wl_resource* origin,
         std::experimental::optional<struct wl_resource*> const& icon, uint32_t serial) override
     {
+        puts(__PRETTY_FUNCTION__);
         (void)source, (void)origin, (void)icon, (void)serial;
     }
 
     void set_selection(std::experimental::optional<struct wl_resource*> const& source, uint32_t serial) override
     {
+        puts(__PRETTY_FUNCTION__);
         (void)source, (void)serial;
     }
 
     void release() override
     {
+        puts(__PRETTY_FUNCTION__);
         wl_resource_destroy(resource);
     }
 };
@@ -74,10 +131,12 @@ struct DataDevice : mf::wayland::DataDevice
 mf::DataDeviceManager::DataDeviceManager(struct wl_display* display) :
     wayland::DataDeviceManager(display, 3)
 {
+    puts(__PRETTY_FUNCTION__);
 }
 
 void mf::DataDeviceManager::create_data_source(struct wl_client* client, struct wl_resource* resource, uint32_t id)
 {
+    puts(__PRETTY_FUNCTION__);
     (void)client, (void)resource, (void)id;
     new DataSource{client, resource, id};
 }
@@ -85,6 +144,7 @@ void mf::DataDeviceManager::create_data_source(struct wl_client* client, struct 
 void mf::DataDeviceManager::get_data_device(
     struct wl_client* client, struct wl_resource* resource, uint32_t id, struct wl_resource* seat)
 {
+    puts(__PRETTY_FUNCTION__);
     (void)seat;
     new DataDevice{client, resource, id};
 }
