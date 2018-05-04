5ec72e1ed1ab91a90e482ad3a39fc84baddcfd71
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2018-05-04 12:38:12 +1000

    UdevWrapper: Wrap more udev_device calls

--- src/include/platform/mir/udev/wrapper.h
+++ src/include/platform/mir/udev/wrapper.h
@@ -62,6 +62,9 @@ public:
     virtual char const* devnode() const = 0;
     virtual char const* property(char const *name) const = 0;
     virtual dev_t devnum() const = 0;
+    virtual char const* sysname() const = 0;
+    virtual bool initialised() const = 0;
+    virtual char const* syspath() const = 0;
 protected:
     Device() = default;
 };
