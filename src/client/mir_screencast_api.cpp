add404a988ecb8891749e894f0660ee46d080616
Michał Sawicz <michal@sawicz.net>
2018-07-26 10:57:58 +0200

    [copyright] fix copyright declaration years

--- src/client/mir_screencast_api.cpp
+++ src/client/mir_screencast_api.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014,2016 Canonical Ltd.
+ * Copyright © 2014, 2016 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2 or 3,
