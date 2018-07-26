2e64665c7b136ae855b671ee8ded2c8acf0ec281
Michał Sawicz <michal@sawicz.net>
2018-07-26 10:57:24 +0200

    [copyright] fix copyright declaration

--- examples/mirclient/animated_cursor_demo_client.c
+++ examples/mirclient/animated_cursor_demo_client.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 Canonical LTD
+ * Copyright © 2015 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 or 3 as
