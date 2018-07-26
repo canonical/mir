0308745a17cbe7c587c45aa3359360a92411d8e4
Michał Sawicz <michal@sawicz.net>
2018-07-26 10:50:41 +0200

    [copyright] fix typos in license in various source and header files

--- examples/example-server-lib/floating_window_manager.h
+++ examples/example-server-lib/floating_window_manager.h
@@ -6,7 +6,7 @@
  * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty ofb
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
