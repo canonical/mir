d1341ce753f69ba603cfb9adbd248b6a9343306a
Alan Griffiths <alan@octopull.co.uk>
2018-08-03 12:53:19 +0100

    Launchpad builders might need a longer sleep (otherwise we need a better solution).

--- tests/miral/test_server.cpp
+++ tests/miral/test_server.cpp
@@ -229,7 +229,7 @@ void miral::TestServer::TearDown()
     // There's a race between closing a client and closing the server.
     // AutoSendBuffer is trying to send *after* SessionMediator is destroyed.
     // This sleep() is not a good fix, but a good fix would be deep in legacy code.
-    std::this_thread::sleep_for(10ms);
+    std::this_thread::sleep_for(100ms);
 
     TestDisplayServer::stop_server();
     testing::Test::TearDown();
