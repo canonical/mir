c6b8c58c6b25338e49d827fdcd10e6888c645077
William Wold <wm@wmww.sh>
2018-09-04 21:24:23 -0700

    internal emitter improvements

--- src/server/frontend_wayland/generator/emitter.cpp
+++ src/server/frontend_wayland/generator/emitter.cpp
@@ -19,6 +19,7 @@
 #include "emitter.h"
 
 std::string const Emitter::single_indent = "    ";
+Emitter const Emitter::null{std::shared_ptr<Emitter::Impl>(nullptr)};
 
 class Emitter::Impl
 {
@@ -79,8 +80,8 @@ private:
 class SeqEmitter : public Emitter::Impl
 {
 public:
-    SeqEmitter(std::vector<Emitter> const && children, Emitter const && delimiter = nullptr, bool at_start = false, bool at_end = false)
-        : children(move(children)),
+    SeqEmitter(std::vector<Emitter> const& children, Emitter const& delimiter = nullptr, bool at_start = false, bool at_end = false)
+        : children(children),
           delimiter{delimiter},
           at_start{at_start},
           at_end{at_end}
@@ -96,7 +97,7 @@ public:
         bool is_start = true;
         for (auto& i: children)
         {
-            if (!is_start && delimiter.is_valid())
+            if (delimiter.is_valid() && !is_start)
                 delimiter.emit(state);
             is_start = false;
             i.emit(state);
@@ -115,7 +116,7 @@ private:
 class LayoutEmitter : public Emitter::Impl
 {
 public:
-    LayoutEmitter(Emitter && child, bool clear_line_before = true, bool clear_line_after = true, std::string indent="")
+    LayoutEmitter(Emitter const& child, bool clear_line_before = true, bool clear_line_after = true, std::string indent="")
         : child{std::move(child)},
           clear_line_before{clear_line_before},
           clear_line_after{clear_line_after},
@@ -148,78 +149,48 @@ private:
     std::string const indent;
 };
 
-Emitter::Emitter(std::string const& text)
-    : impl{text.empty() ? nullptr : std::make_shared<StringEmitter>(text)}
+std::shared_ptr<Emitter::Impl const> Emitter::string(std::string text)
 {
+    if (text.empty())
+        return {};
+    else
+        return std::make_shared<StringEmitter>(text);
 }
 
-Emitter::Emitter(const char* text)
-    : impl{text != nullptr && *text!='\0' ?
-               std::make_shared<StringEmitter>(std::string{text}) :
-               nullptr}
+std::shared_ptr<Emitter::Impl const> Emitter::seq(std::vector<Emitter> const& children,
+                                            Emitter const& delimiter,
+                                            bool at_start,
+                                            bool at_end)
 {
-}
-
-Emitter::Emitter(std::initializer_list<Emitter> const& emitters)
-    : impl{Impl::contains_valid(emitters) ?
-               std::make_shared<SeqEmitter>(emitters) :
-               nullptr}
-{
-}
-
-Emitter::Emitter(std::vector<Emitter> emitters)
-    : impl{Impl::contains_valid(emitters) ?
-               std::make_shared<SeqEmitter>(move(emitters)) :
-               nullptr}
-{
-}
-
-Emitter::Emitter(Line && line)
-{
-    Emitter e{line.emitters};
-    if (e.is_valid())
-    {
-        impl = std::make_shared<LayoutEmitter>(std::move(e), line.break_before, line.break_after, line.indent);
-    }
-}
-
-Emitter::Emitter(Lines && lines)
-{
-    std::vector<Emitter> l;
-    for (auto& i : lines.emitters)
+    std::vector<Emitter> valid;
+    for (auto const& i : children)
     {
         if (i.is_valid())
-            l.push_back(Emitter{std::make_shared<LayoutEmitter>(std::move(i))});
+            valid.push_back(i);
     }
-    if (!l.empty())
-    {
-        impl = std::make_shared<SeqEmitter>(move(l));
-    }
+    if (valid.empty())
+        return {};
+    else
+        return std::make_shared<SeqEmitter>(move(valid), delimiter, at_start, at_end);
 }
 
-Emitter::Emitter(Block && block)
+std::shared_ptr<Emitter::Impl const> Emitter::layout(Emitter const& child,
+                                               bool clear_line_before,
+                                               bool clear_line_after,
+                                               std::string indent)
 {
-    impl = std::make_shared<LayoutEmitter>(Emitter{"{",
-                                                   Emitter{std::make_shared<LayoutEmitter>(
-                                                       Lines{block.emitters}, true, true, single_indent)},
-                                                   "}"}, true, false);
+    if (clear_line_before == false && clear_line_after == false && indent == "")
+        return child.impl;
+    else if (child.is_valid())
+        return std::make_shared<LayoutEmitter>(child, clear_line_before, clear_line_after, indent);
+    else
+        return {};
 }
 
-Emitter::Emitter(List && list)
-{
-    std::vector<Emitter> l;
-    for (auto& i : list.items)
-    {
-        if (i.is_valid())
-            l.push_back(std::move(i));
-    }
-    if (!l.empty())
-    {
-        impl = std::make_shared<SeqEmitter>(move(l), std::move(list.delimiter));
-        if (!list.indent.empty())
-            impl = std::make_shared<LayoutEmitter>(Emitter{impl}, false, false, list.indent);
-    }
-}
+Emitter::Emitter(std::string const& text) : impl{string(text)} {}
+Emitter::Emitter(const char* text) : impl{text == nullptr ? nullptr : string(text)} {}
+Emitter::Emitter(std::initializer_list<Emitter> emitters) : impl{seq(emitters)} {}
+Emitter::Emitter(std::vector<Emitter> const& emitters) : impl{seq(emitters)} {}
 
 void Emitter::emit(State state) const
 {
