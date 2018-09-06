7c55a5fe152400130b2007b868211b871f68aa84
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2018-09-06 10:54:12 +1000

    LogindConsoleServices: Work around more puzzling logind behaviour.
    See comment for details.

--- src/server/console/logind_console_services.cpp
+++ src/server/console/logind_console_services.cpp
@@ -666,10 +666,32 @@ void mir::LogindConsoleServices::on_pause_device(
         }
         else if ("gone"s == suspend_type)
         {
+            if (major == 226)
+            {
+                /* This is a DRM device.
+                 *
+                 * During startup, we TakeDevice during probe() to check that we can
+                 * actually use the DRM device; we ReleaseDevice it before returning from probe().
+                 *
+                 * Then, during Platform construction we TakeDevice again.
+                 * For some reason, logind feels the need to send a “gone” signal after the
+                 * second TakeDevice call, which would result in us dropping the device and
+                 * everything breaking.
+                 *
+                 * A DRM device is quite unlikely to *actually* be gone, so just ignore
+                 * PauseDevice("gone") signals for DRM devices.
+                 */
+                mir::log_debug(
+                    "Ignoring logind PauseDevice(\"gone\") event for DRM device %i:%i",
+                    major, minor);
+                return;
+            }
             it->second->emit_removed();
             // The device is gone; logind promises not to send further events for it
             me->acquired_devices.erase(it);
             // …unfortunately, logind is a FILTHY LIAR.
+            // We're safe to call this asynchronously; there must be a running main loop,
+            // because we've been dispatched from it.
             logind_session_call_release_device(
                 me->session_proxy.get(),
                 major, minor,
