e60ef83efb84673055f810bf685ebe44d267d653
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2018-10-22 14:13:50 +1100

    tests: Fix license of newly-added test to GPL

--- tests/unit-tests/platforms/eglstream-kms/server/test_threaded_drm_event_handler.cpp
+++ tests/unit-tests/platforms/eglstream-kms/server/test_threaded_drm_event_handler.cpp
@@ -2,15 +2,15 @@
  * Copyright © 2018 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License version 2 or 3,
+ * under the terms of the GNU General Public License version 2 or 3,
  * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by: Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
