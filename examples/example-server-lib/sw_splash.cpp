c15fe224d19597781f06562c114072927cdef8d9
Alan Griffiths <alan@octopull.co.uk>
2018-12-11 15:04:48 +0000

    Tidy up code

--- examples/example-server-lib/sw_splash.cpp
+++ examples/example-server-lib/sw_splash.cpp
@@ -19,7 +19,6 @@
 #include "sw_splash.h"
 
 #include <wayland-client.h>
-#include <wayland-client-core.h>
 
 #include <sys/mman.h>
 #include <sys/types.h>
@@ -35,85 +34,37 @@
 
 namespace
 {
-struct DrawContext
+template<typename Type>
+auto make_scoped(Type* owned, void(*deleter)(Type*)) -> std::unique_ptr<Type, void(*)(Type*)>
 {
-    int width = 400;
-    int height = 400;
-
-    void* content_area;
-    struct wl_display* display;
-    struct wl_surface* surface;
-    struct wl_callback* new_frame_signal;
-    struct Buffers
-    {
-        struct wl_buffer* buffer;
-        bool available;
-    } buffers[4];
-    bool waiting_for_buffer;
-
-    uint8_t pattern[4] = { 0x14, 0x48, 0xDD, 0xFF };
-};
+    return {owned, deleter};
 }
 
-struct SwSplash::Self : SplashSession
+struct Globals
 {
-    struct wl_compositor* compositor = nullptr;
-    struct wl_shm* shm = nullptr;
-    struct wl_seat* seat = nullptr;
-    struct wl_output* output = nullptr;
-    struct wl_shell* shell = nullptr;
+    wl_compositor* compositor = nullptr;
+    wl_shm* shm = nullptr;
+    wl_seat* seat = nullptr;
+    wl_output* output = nullptr;
+    wl_shell* shell = nullptr;
 
-    struct OutputInfo
-    {
-        struct wl_output* wl_output;
-        int32_t x;
-        int32_t y;
-        int32_t width;
-        int32_t height;
-    };
+    void init(struct wl_display* display);
 
-    std::vector<OutputInfo> output_info;
+private:
+    static void new_global(
+        void* data,
+        struct wl_registry* registry,
+        uint32_t id,
+        char const* interface,
+        uint32_t version);
 
-    DrawContext ctx;
-
-    std::mutex mutable mutex;
-    std::weak_ptr<mir::scene::Session> session_;
-
-    std::shared_ptr<mir::scene::Session> session() const override
-    {
-        std::lock_guard<decltype(mutex)> lock{mutex};
-        return session_.lock();
-    }
+    static void global_remove(
+        void* data,
+        struct wl_registry* registry,
+        uint32_t name);
 };
 
-namespace
-{
-struct wl_shm_pool* make_shm_pool(struct wl_shm* shm, int size, void **data)
-{
-    struct wl_shm_pool *pool;
-    int fd;
-
-    fd = open("/dev/shm", O_TMPFILE | O_RDWR | O_EXCL, S_IRWXU);
-    if (fd < 0) {
-        return NULL;
-    }
-
-    posix_fallocate(fd, 0, size);
-
-    *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (*data == MAP_FAILED) {
-        close(fd);
-        return NULL;
-    }
-
-    pool = wl_shm_create_pool(shm, fd, size);
-
-    close(fd);
-
-    return pool;
-}
-
-void new_global(
+void Globals::new_global(
     void* data,
     struct wl_registry* registry,
     uint32_t id,
@@ -121,7 +72,7 @@ void new_global(
     uint32_t version)
 {
     (void)version;
-    SwSplash::Self* self = static_cast<decltype(self)>(data);
+    Globals* self = static_cast<decltype(self)>(data);
 
     if (strcmp(interface, "wl_compositor") == 0)
     {
@@ -148,7 +99,7 @@ void new_global(
     }
 }
 
-void global_remove(
+void Globals::global_remove(
     void* data,
     struct wl_registry* registry,
     uint32_t name)
@@ -158,9 +109,116 @@ void global_remove(
     (void)name;
 }
 
-wl_registry_listener const registry_listener = {
-    new_global,
-    global_remove
+void Globals::init(struct wl_display* display)
+{
+    wl_registry_listener const registry_listener = {
+        new_global,
+        global_remove
+    };
+
+    auto const registry = make_scoped(wl_display_get_registry(display), &wl_registry_destroy);
+
+    wl_registry_add_listener(registry.get(), &registry_listener, this);
+    wl_display_roundtrip(display);
+}
+
+struct OutputInfo
+{
+    struct wl_output* wl_output;
+    int32_t x;
+    int32_t y;
+    int32_t width;
+    int32_t height;
+};
+
+using Outputs = std::vector<OutputInfo>;
+
+void output_geometry(void *data,
+    struct wl_output *wl_output,
+    int32_t x,
+    int32_t y,
+    int32_t /*physical_width*/,
+    int32_t /*physical_height*/,
+    int32_t /*subpixel*/,
+    const char */*make*/,
+    const char */*model*/,
+    int32_t /*transform*/)
+{
+    Outputs* outputs = static_cast<decltype(outputs)>(data);
+
+    for (auto& oi : *outputs)
+    {
+        if (wl_output == oi.wl_output)
+        {
+            oi.x = x;
+            oi.y = y;
+            return;
+        }
+    }
+    outputs->push_back({wl_output, x, y, 0, 0});
+}
+
+void output_mode(void *data,
+    struct wl_output *wl_output,
+    uint32_t flags,
+    int32_t width,
+    int32_t height,
+    int32_t /*refresh*/)
+{
+    if (!(WL_OUTPUT_MODE_CURRENT & flags))
+        return;
+
+    Outputs* outputs = static_cast<decltype(outputs)>(data);
+
+    for (auto& oi : *outputs)
+    {
+        if (wl_output == oi.wl_output)
+        {
+            oi.width = width;
+            oi.height = height;
+            return;
+        }
+    }
+    outputs->push_back({wl_output, 0, 0, width, height});
+}
+
+void output_done(void* data, struct wl_output* wl_output)
+{
+    (void)data;
+    (void)wl_output;
+}
+
+void output_scale(void* data, struct wl_output* wl_output, int32_t factor)
+{
+    (void)data;
+    (void)wl_output;
+    (void)factor;
+}
+
+wl_output_listener const output_listener = {
+    &output_geometry,
+    &output_mode,
+    &output_done,
+    &output_scale,
+};
+
+struct DrawContext
+{
+    int width = 400;
+    int height = 400;
+
+    void* content_area;
+    struct wl_display* display;
+    struct wl_surface* surface;
+    struct wl_callback* new_frame_signal;
+    struct Buffer
+    {
+        wl_buffer* buffer = nullptr;
+        bool available = false;
+    } buffers[4];
+    bool waiting_for_buffer;
+
+    uint8_t pattern[4] = { 0x14, 0x48, 0xDD, 0xFF };
 };
 
 void draw_new_stuff(void* data, struct wl_callback* callback, uint32_t time);
@@ -219,11 +277,11 @@ void draw_new_stuff(
 void update_free_buffers(void* data, struct wl_buffer* buffer)
 {
     DrawContext* ctx = static_cast<decltype(ctx)>(data);
-    for (int i = 0; i < 4 ; ++i)
+    for (auto& b: ctx->buffers)
     {
-        if (ctx->buffers[i].buffer == buffer)
+        if (b.buffer == buffer)
         {
-            ctx->buffers[i].available = true;
+            b.available = true;
         }
     }
 
@@ -240,75 +298,51 @@ wl_buffer_listener const buffer_listener = {
     update_free_buffers
 };
 
-void output_geometry(void *data,
-    struct wl_output *wl_output,
-    int32_t x,
-    int32_t y,
-    int32_t /*physical_width*/,
-    int32_t /*physical_height*/,
-    int32_t /*subpixel*/,
-    const char */*make*/,
-    const char */*model*/,
-    int32_t /*transform*/)
+struct wl_shm_pool* make_shm_pool(struct wl_shm* shm, int size, void **data)
 {
-    SwSplash::Self* self = static_cast<decltype(self)>(data);
+    struct wl_shm_pool *pool;
+    int fd;
 
-    for (auto& oi : self->output_info)
-    {
-        if (wl_output == oi.wl_output)
-        {
-            oi.x = x;
-            oi.y = y;
-            return;
-        }
+    fd = open("/dev/shm", O_TMPFILE | O_RDWR | O_EXCL, S_IRWXU);
+    if (fd < 0) {
+        return NULL;
     }
-    self->output_info.push_back({wl_output, x, y, 0, 0});
-}
 
-void output_mode(void *data,
-    struct wl_output *wl_output,
-    uint32_t flags,
-    int32_t width,
-    int32_t height,
-    int32_t /*refresh*/)
-{
-    if (!(WL_OUTPUT_MODE_CURRENT & flags))
-        return;
+    posix_fallocate(fd, 0, size);
 
-    SwSplash::Self* self = static_cast<decltype(self)>(data);
-
-    for (auto& oi : self->output_info)
-    {
-        if (wl_output == oi.wl_output)
-        {
-            oi.width = width;
-            oi.height = height;
-            return;
-        }
+    *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (*data == MAP_FAILED) {
+        close(fd);
+        return NULL;
     }
-    self->output_info.push_back({wl_output, 0, 0, width, height});
+
+    pool = wl_shm_create_pool(shm, fd, size);
+
+    close(fd);
+
+    return pool;
+}
 }
 
-void output_done(void* data, struct wl_output* wl_output)
+struct SwSplash::Self : SplashSession
 {
-    (void)data;
-    (void)wl_output;
-}
+    Globals globals;
 
-void output_scale(void* data, struct wl_output* wl_output, int32_t factor)
-{
-    (void)data;
-    (void)wl_output;
-    (void)factor;
-}
+    Outputs outputs;
 
-wl_output_listener const output_listener = {
-    &output_geometry,
-    &output_mode,
-    &output_done,
-    &output_scale,
+    DrawContext ctx;
+
+    std::mutex mutable mutex;
+    std::weak_ptr<mir::scene::Session> session_;
+
+    std::shared_ptr<mir::scene::Session> session() const override
+    {
+        std::lock_guard<decltype(mutex)> lock{mutex};
+        return session_.lock();
+    }
+
+    void operator()(struct wl_display* display);
 };
-}
 
 SwSplash::SwSplash() : self{std::make_shared<Self>()} {}
 
@@ -325,42 +359,39 @@ SwSplash::operator std::shared_ptr<SplashSession>() const
     return self;
 }
 
-void SwSplash::operator()(struct wl_display* display)
+void SwSplash::Self::operator()(struct wl_display* display)
 {
-    struct wl_registry* registry = wl_display_get_registry(display);
+    globals.init(display);
 
-    wl_registry_add_listener(registry, &registry_listener, self.get());
+    wl_output_add_listener(globals.output, &output_listener, &outputs);
     wl_display_roundtrip(display);
 
-    wl_output_add_listener(self->output, &output_listener, self.get());
-    wl_display_roundtrip(display);
-
-    DrawContext* ctx = &self->ctx;
-
-    for (auto const& oi : self->output_info)
+    for (auto const& oi : outputs)
     {
-        ctx->width = std::max(ctx->width, oi.width);
-        ctx->height = std::max(ctx->height, oi.height);
+        ctx.width = std::max(ctx.width, oi.width);
+        ctx.height = std::max(ctx.height, oi.height);
     }
 
-    struct wl_shm_pool* shm_pool = make_shm_pool(self->shm, ctx->width * ctx->height * 4, &ctx->content_area);
+    struct wl_shm_pool* shm_pool = make_shm_pool(globals.shm, ctx.width * ctx.height * 4, &ctx.content_area);
 
-    for (int i = 0; i < 4; ++i)
+    for (auto& b: ctx.buffers)
     {
-        ctx->buffers[i].buffer =
-            wl_shm_pool_create_buffer(shm_pool, 0, ctx->width, ctx->height, ctx->width*4, WL_SHM_FORMAT_ARGB8888);
-        ctx->buffers[i].available = true;
-        wl_buffer_add_listener(ctx->buffers[i].buffer, &buffer_listener, ctx);
+        b.buffer = wl_shm_pool_create_buffer(shm_pool, 0, ctx.width, ctx.height, ctx.width*4, WL_SHM_FORMAT_ARGB8888);
+        b.available = true;
+        wl_buffer_add_listener(b.buffer, &buffer_listener, &ctx);
     }
 
-    ctx->display = display;
-    ctx->surface = wl_compositor_create_surface(self->compositor);
+    wl_shm_pool_destroy(shm_pool);
 
-    struct wl_shell_surface* window = wl_shell_get_shell_surface(self->shell, ctx->surface);
-    wl_shell_surface_set_toplevel(window);
+    ctx.display = display;
+    ctx.surface = wl_compositor_create_surface(globals.compositor);
+
+    auto const window = make_scoped(wl_shell_get_shell_surface(globals.shell, ctx.surface), &wl_shell_surface_destroy);
+    wl_shell_surface_set_fullscreen(window.get(), WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT, 0, nullptr);
+    wl_shell_surface_set_toplevel(window.get());
 
     struct wl_callback* first_frame = wl_display_sync(display);
-    wl_callback_add_listener(first_frame, &frame_listener, ctx);
+    wl_callback_add_listener(first_frame, &frame_listener, &ctx);
 
     auto const time_limit = std::chrono::steady_clock::now() + std::chrono::seconds(2);
 
@@ -368,10 +399,25 @@ void SwSplash::operator()(struct wl_display* display)
     {
         wl_display_dispatch(display);
 
-        for (auto& x : ctx->pattern)
+        for (auto& x : ctx.pattern)
+        {
             x =  3*x/4;
+        }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
     while (std::chrono::steady_clock::now() < time_limit);
+
+    for (auto const& b: ctx.buffers)
+    {
+        if (b.buffer)
+            wl_buffer_destroy(b.buffer);
+    }
+
+    wl_surface_destroy(ctx.surface);
+}
+
+void SwSplash::operator()(struct wl_display* display)
+{
+    (*self)(display);
 }
