502023f210a054e3490379212104df11a6ead978
Alan Griffiths <alan@octopull.co.uk>
2018-12-14 09:31:37 +0000

    Restore background text

--- examples/example-server-lib/decoration_provider.cpp
+++ examples/example-server-lib/decoration_provider.cpp
@@ -37,7 +37,7 @@
 
 namespace
 {
-//char const* const wallpaper_name = "wallpaper";
+struct OutputInfo;
 
 struct preferred_codecvt : std::codecvt_byname<wchar_t, char, std::mbstate_t>
 {
@@ -52,7 +52,7 @@ struct Printer
     Printer(Printer const&) = delete;
     Printer& operator=(Printer const&) = delete;
 
-//    void printhelp(MirGraphicsRegion const& region);
+    void printhelp(OutputInfo const& region);
 
 private:
     std::wstring_convert<preferred_codecvt> converter;
@@ -87,137 +87,6 @@ Printer::~Printer()
     }
 }
 
-//void Printer::printhelp(MirGraphicsRegion const& region)
-//{
-//    if (!working)
-//        return;
-//
-//    static char const* const helptext[] =
-//        {
-//            "Welcome to miral-shell",
-//            "",
-//            "Keyboard shortcuts:",
-//            "",
-//            "  o Switch apps: Alt-Tab, tap or click on the corresponding window",
-//            "  o Next (previous) app window: Alt-` (Alt-Shift-`)",
-//            "",
-//            "  o Move window: Alt-leftmousebutton drag (three finger drag)",
-//            "  o Resize window: Alt-middle_button drag (three finger pinch)",
-//            "",
-//            "  o Maximize/restore current window (to display size). : Alt-F11",
-//            "  o Maximize/restore current window (to display height): Shift-F11",
-//            "  o Maximize/restore current window (to display width) : Ctrl-F11",
-//            "",
-//            "  o Switch workspace: Meta-Alt-[F1|F2|F3|F4]",
-//            "  o Switch workspace taking active window: Meta-Ctrl-[F1|F2|F3|F4]",
-//            "",
-//            "  o To exit: Ctrl-Alt-BkSp",
-//        };
-//
-//    int help_width = 0;
-//    unsigned int help_height = 0;
-//    unsigned int line_height = 0;
-//
-//    for (auto const* rawline : helptext)
-//    {
-//        int line_width = 0;
-//
-//        auto const line = converter.from_bytes(rawline);
-//
-//        auto const fwidth = std::min(region.width / 60, 20);
-//
-//        FT_Set_Pixel_Sizes(face, fwidth, 0);
-//
-//        for (auto const& ch : line)
-//        {
-//            FT_Load_Glyph(face, FT_Get_Char_Index(face, ch), FT_LOAD_DEFAULT);
-//            auto const glyph = face->glyph;
-//            FT_Render_Glyph(glyph, FT_RENDER_MODE_NORMAL);
-//
-//            line_width += glyph->advance.x >> 6;
-//            line_height = std::max(line_height, glyph->bitmap.rows + glyph->bitmap.rows/2);
-//        }
-//
-//        if (help_width < line_width) help_width = line_width;
-//        help_height += line_height;
-//    }
-//
-//    int base_y = (region.height - help_height)/2;
-//    auto* const region_address = reinterpret_cast<char unsigned*>(region.vaddr);
-//
-//    for (auto const* rawline : helptext)
-//    {
-//        int base_x = (region.width - help_width)/2;
-//
-//        auto const line = converter.from_bytes(rawline);
-//
-//        for (auto const& ch : line)
-//        {
-//            FT_Load_Glyph(face, FT_Get_Char_Index(face, ch), FT_LOAD_DEFAULT);
-//            auto const glyph = face->glyph;
-//            FT_Render_Glyph(glyph, FT_RENDER_MODE_NORMAL);
-//
-//            auto const& bitmap = glyph->bitmap;
-//            auto const x = base_x + glyph->bitmap_left;
-//
-//            if (static_cast<int>(x + bitmap.width) <= region.width)
-//            {
-//                unsigned char* src = bitmap.buffer;
-//
-//                auto const y = base_y - glyph->bitmap_top;
-//                auto* dest = region_address + y * region.stride + 4 * x;
-//
-//                for (auto row = 0u; row != bitmap.rows; ++row)
-//                {
-//                    for (auto col = 0u; col != 4 * bitmap.width; ++col)
-//                    {
-//                        dest[col] = (0xff*src[col / 4] + (dest[col] * (0xff - src[col / 4])))/0xff;
-//                    }
-//
-//                    src += bitmap.pitch;
-//                    dest += region.stride;
-//
-//                    if (dest > region_address + region.height * region.stride)
-//                        break;
-//                }
-//            }
-//
-//            base_x += glyph->advance.x >> 6;
-//        }
-//        base_y += line_height;
-//    }
-//}
-//
-//void render_background(MirBufferStream* buffer_stream, MirGraphicsRegion& graphics_region)
-//{
-//    {
-//        uint8_t const bottom_colour[] = { 0x20, 0x54, 0xe9 };   // Ubuntu orange
-//        uint8_t const top_colour[] =    { 0x33, 0x33, 0x33 };   // Cool grey
-//
-//        char* row = graphics_region.vaddr;
-//
-//        for (int j = 0; j < graphics_region.height; j++)
-//        {
-//            uint8_t pattern[4];
-//
-//            for (auto i = 0; i != 3; ++i)
-//                pattern[i] = (j*bottom_colour[i] + (graphics_region.height-j)*top_colour[i])/graphics_region.height;
-//            pattern[3] = 0xff;
-//
-//            uint32_t* pixel = (uint32_t*)row;
-//            for (int i = 0; i < graphics_region.width; i++)
-//                memcpy(pixel + i, pattern, sizeof pixel[i]);
-//
-//            row += graphics_region.stride;
-//        }
-//    }
-//
-    static Printer printer;
-//    printer.printhelp(*&graphics_region);
-//
-//    mir_buffer_stream_swap_buffers_sync(buffer_stream);
-//}
-
 struct OutputInfo
 {
     OutputInfo(Output const& output) :
@@ -235,6 +104,107 @@ struct OutputInfo
     wl_buffer* buffer = nullptr;
     bool waiting_for_buffer;
 };
+
+void Printer::printhelp(OutputInfo const& region)
+{
+    if (!working)
+        return;
+
+    static char const* const helptext[] =
+        {
+            "Welcome to miral-shell",
+            "",
+            "Keyboard shortcuts:",
+            "",
+            "  o Switch apps: Alt-Tab, tap or click on the corresponding window",
+            "  o Next (previous) app window: Alt-` (Alt-Shift-`)",
+            "",
+            "  o Move window: Alt-leftmousebutton drag (three finger drag)",
+            "  o Resize window: Alt-middle_button drag (three finger pinch)",
+            "",
+            "  o Maximize/restore current window (to display size). : Alt-F11",
+            "  o Maximize/restore current window (to display height): Shift-F11",
+            "  o Maximize/restore current window (to display width) : Ctrl-F11",
+            "",
+            "  o Switch workspace: Meta-Alt-[F1|F2|F3|F4]",
+            "  o Switch workspace taking active window: Meta-Ctrl-[F1|F2|F3|F4]",
+            "",
+            "  o To exit: Ctrl-Alt-BkSp",
+        };
+
+    int help_width = 0;
+    unsigned int help_height = 0;
+    unsigned int line_height = 0;
+
+    for (auto const* rawline : helptext)
+    {
+        int line_width = 0;
+
+        auto const line = converter.from_bytes(rawline);
+
+        auto const fwidth = std::min(region.output.width / 60, 20);
+
+        FT_Set_Pixel_Sizes(face, fwidth, 0);
+
+        for (auto const& ch : line)
+        {
+            FT_Load_Glyph(face, FT_Get_Char_Index(face, ch), FT_LOAD_DEFAULT);
+            auto const glyph = face->glyph;
+            FT_Render_Glyph(glyph, FT_RENDER_MODE_NORMAL);
+
+            line_width += glyph->advance.x >> 6;
+            line_height = std::max(line_height, glyph->bitmap.rows + glyph->bitmap.rows/2);
+        }
+
+        if (help_width < line_width) help_width = line_width;
+        help_height += line_height;
+    }
+
+    int base_y = (region.output.height - help_height)/2;
+    auto* const region_address = reinterpret_cast<char unsigned*>(region.content_area);
+
+    for (auto const* rawline : helptext)
+    {
+        int base_x = (region.output.width - help_width)/2;
+
+        auto const line = converter.from_bytes(rawline);
+
+        for (auto const& ch : line)
+        {
+            FT_Load_Glyph(face, FT_Get_Char_Index(face, ch), FT_LOAD_DEFAULT);
+            auto const glyph = face->glyph;
+            FT_Render_Glyph(glyph, FT_RENDER_MODE_NORMAL);
+
+            auto const& bitmap = glyph->bitmap;
+            auto const x = base_x + glyph->bitmap_left;
+
+            if (static_cast<int>(x + bitmap.width) <= region.output.width)
+            {
+                unsigned char* src = bitmap.buffer;
+
+                auto const y = base_y - glyph->bitmap_top;
+                auto* dest = region_address + y * 4*region.output.width + 4 * x;
+
+                for (auto row = 0u; row != bitmap.rows; ++row)
+                {
+                    for (auto col = 0u; col != 4 * bitmap.width; ++col)
+                    {
+                        dest[col] = (0xff*src[col / 4] + (dest[col] * (0xff - src[col / 4])))/0xff;
+                    }
+
+                    src += bitmap.pitch;
+                    dest += 4*region.output.width;
+
+                    if (dest > region_address + region.output.height * 4*region.output.width)
+                        break;
+                }
+            }
+
+            base_x += glyph->advance.x >> 6;
+        }
+        base_y += line_height;
+    }
+}
 
 using Outputs = std::map<Output const*, OutputInfo>;
 
@@ -254,13 +224,6 @@ private:
     miral::WindowManagerTools tools;
     Globals globals;
     Outputs outputs;
-
-//    mir::client::Connection connection;
-//    struct Wallpaper { mir::client::Surface surface; mir::client::Window window; MirBufferStream* stream; };
-//    std::vector<Wallpaper> wallpaper;
-//
-//    static void handle_event_for_background(MirWindow* window, MirEvent const* event, void* context_);
-//    void handle_event_for_background(MirWindow* window, MirEvent const* ev);
 };
 
 DecorationProvider::Self::Self(miral::WindowManagerTools const& tools) :
@@ -321,6 +284,10 @@ void DecorationProvider::Self::init(struct wl_display* display)
 
             row += 4*ctx.output.width;
         }
+
+        static Printer printer;
+
+        printer.printhelp(ctx);
 
         wl_surface_attach(ctx.surface, ctx.buffer, 0, 0);
         wl_surface_commit(ctx.surface);
