79357e3f6944454cd5841c6400883217004c33f3
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2019-04-16 12:58:13 +1000

    Add bare-minimum documentation

--- benchmarks/commit_to_present.py
+++ benchmarks/commit_to_present.py
@@ -4,10 +4,43 @@ import babeltrace
 import sys
 from text_histogram import histogram
 
-trace_path = sys.argv[1]
+howto = """
+Process a LTTNG trace for per-client commit-to-present latencies.
 
-trace_collection = babeltrace.TraceCollection()
-trace_collection.add_trace(trace_path, 'ctf')
+This works on an lttng trace with the mir_server_wayland:sw_buffer_committed
+and mir_server_compositor:buffers_in_frame tracepoints enabled. To generate
+such a trace you need to run a Mir server with the --compositor-report=lttng
+option. For example:
+
+> miral-app --compositor-report=lttng
+
+Once you have your compositor active, you can get an LTTNG trace like so:
+> lttng create mir-trace
+> lttng enable-event -u "mir_server_wayland:sw_buffer_committed"
+> lttng enable-event -u "mir_server_compositor:buffers_in_frame"
+> lttng start
+
+At this point you can interact with applications in your Mir session;
+LTTNG will be recording the necessary events. When you're done…
+
+> lttng stop
+> lttng destroy
+
+This will (by default) leave you with a trace in
+~/lttng-traces/mir-trace-DIGITS-MORE_DIGITS/ust/uid/$YOUR_UID/64-bit
+
+This trace can then be processed with
+> python3 commit_to_present.py ~/lttng-traces/mir-trace-$DIGITS-$MORE_DIGITS/ust/uid/$YOUR_UID/64-bit
+"""
+
+try:
+    trace_path = sys.argv[1]
+
+    trace_collection = babeltrace.TraceCollection()
+    trace_collection.add_trace(trace_path, 'ctf')
+except:
+    print(howto)
+    raise
 
 class ClientStats:
     def __init__(self):
