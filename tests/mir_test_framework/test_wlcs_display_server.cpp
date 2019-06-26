2c02380914dbb6ddac3aa505fdbb0c9bc4d77de6
Alan Griffiths <alan@octopull.co.uk>
2019-06-26 11:41:06 +0100

    Move miral::TestWlcsDisplayServer to the mirtest-dev package

--- tests/mir_test_framework/test_wlcs_display_server.cpp
+++ tests/mir_test_framework/test_wlcs_display_server.cpp
@@ -17,7 +17,7 @@
  *              Alan Griffiths <alan@octopull.co.uk>
  */
 
-#include "test_wlcs_display_server.h"
+#include <miral/test_wlcs_display_server.h>
 
 #include <wlcs/pointer.h>
 #include <wlcs/touch.h>
