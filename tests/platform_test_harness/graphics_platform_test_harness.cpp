6640a799a0993d300b27bedb082b09d587940e3d
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2019-10-01 16:59:57 +1000

    platform-test-harness: Add copyright

--- tests/platform_test_harness/graphics_platform_test_harness.cpp
+++ tests/platform_test_harness/graphics_platform_test_harness.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright © 2019 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
+ */
+
 #include "mir/graphics/platform.h"
 #include "mir/graphics/display.h"
 #include "mir/graphics/display_buffer.h"
