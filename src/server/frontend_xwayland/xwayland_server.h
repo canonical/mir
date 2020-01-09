26f36f675c2aaa0b109dc52bc5a69f2a8326d6a1
Alan Griffiths <alan@octopull.co.uk>
2020-01-09 12:12:38 +0000

    Fix threading for XWaylandServer::spawn_thread

--- src/server/frontend_xwayland/xwayland_server.h
+++ src/server/frontend_xwayland/xwayland_server.h
@@ -20,6 +20,7 @@
 #define MIR_FRONTEND_XWAYLAND_SERVER_H
 
 #include <memory>
+#include <mutex>
 #include <thread>
 #include <string>
 
@@ -47,7 +48,8 @@ private:
     /// Called after fork() if we should turn into XWayland
     void execl_xwayland(int wl_client_client_fd, int wm_client_fd);
     /// Called after fork() if we should continue on as Mir
-    void connect_wm_to_xwayland(int wl_client_server_fd, int wm_server_fd);
+    void connect_wm_to_xwayland(
+        int wl_client_server_fd, int wm_server_fd, std::unique_lock<std::mutex>& spawn_thread_lock);
     void new_spawn_thread();
 
     struct SocketFd
@@ -67,7 +69,7 @@ private:
         FAILED = -2
     };
 
-    std::shared_ptr<WaylandConnector> wc;
+    std::shared_ptr<WaylandConnector> const wc;
     std::shared_ptr<WaylandConnector> const wlc;
     std::shared_ptr<dispatch::MultiplexingDispatchable> const dispatcher;
     std::unique_ptr<dispatch::ThreadedDispatcher> const xserver_thread;
@@ -76,10 +78,11 @@ private:
     std::shared_ptr<dispatch::ReadableFd> const fd_dispatcher;
     std::string const xwayland_path;
 
-    Status xserver_status = STOPPED;
+    std::mutex mutable spawn_thread_mutex;
     std::thread spawn_thread;
-    pid_t pid;
-    bool terminate = false;
+    pid_t spawn_thread_pid;
+    Status spawn_thread_xserver_status{Status::STOPPED};
+    bool spawn_thread_terminate{false};
 };
 } /* frontend */
 } /* mir */
