dbedb80b1019dc321cf5488054a69a1230610672
Alan Griffiths <alan@octopull.co.uk>
2020-01-14 12:59:45 +0000

    Drop some obsolete scripts and add -x11-display option to miral-app

--- examples/miral-shell/miral-app.sh
+++ examples/miral-shell/miral-app.sh
@@ -41,6 +41,14 @@ while [ -e "${XDG_RUNTIME_DIR}/wayland-${port}" ]; do
 done
 wayland_display=wayland-${port}
 
+port=0
+
+while [ -e "/tmp/.X11-unix/X${port}" ]; do
+    let port+=1
+done
+
+x11_display=${port}
+
 while [ $# -gt 0 ]
 do
   if [ "$1" == "--help" -o "$1" == "-h" ]
@@ -52,6 +60,7 @@ do
     echo "    -terminal <terminal>        use <terminal> instead of '${terminal}'"
     echo "    -socket <socket>            set the legacy mir socket [${socket}]"
     echo "    -wayland-display <socket>   set the wayland socket [${wayland_display}]"
+    echo "    -x11-display <socket>       set the X11 socket [${x11_display}]"
     echo "    -bindir <bindir>            path to the miral executable [${bindir}]"
     # omit    -demo-server as mir_demo_server is in the mir-test-tools package
     exit 0
@@ -59,6 +68,7 @@ do
   elif [ "$1" == "-terminal" ];         then shift; terminal=$1
   elif [ "$1" == "-socket" ];           then shift; socket=$1
   elif [ "$1" == "-wayland-display" ];  then shift; wayland_display=$1
+  elif [ "$1" == "-x11-display" ];      then shift; x11_display=$1
   elif [ "$1" == "-bindir" ];           then shift; bindir=$1
   elif [ "$1" == "-demo-server" ];      then miral_server=mir_demo_server
   elif [ "${1:0:2}" == "--" ];          then break
@@ -76,10 +86,10 @@ unset QT_QPA_PLATFORMTHEME
 if [ "${miral_server}" == "miral-shell" ]
 then
   # miral-shell can launch it's own terminal with Ctrl-Alt-T
-  MIR_SERVER_FILE=${socket} WAYLAND_DISPLAY=${wayland_display} MIR_SERVER_SHELL_TERMINAL_EMULATOR=${terminal} NO_AT_BRIDGE=1 ${hostsocket} exec dbus-run-session -- ${bindir}${miral_server} ${enable_mirclient} $*
+  MIR_SERVER_FILE=${socket} WAYLAND_DISPLAY=${wayland_display} MIR_SERVER_X11_DISPLAY=${x11_display} MIR_SERVER_SHELL_TERMINAL_EMULATOR=${terminal} NO_AT_BRIDGE=1 ${hostsocket} exec dbus-run-session -- ${bindir}${miral_server} ${enable_mirclient} $*
 else
   # miral-kiosk (and mir_demo_server) need a terminal launched
-  sh -c "MIR_SERVER_FILE=${socket} WAYLAND_DISPLAY=${wayland_display} ${hostsocket} ${bindir}${miral_server} ${enable_mirclient} $*"&
+  sh -c "MIR_SERVER_FILE=${socket} MIR_SERVER_SHELL_X11_DISPLAY=${x11_display} WAYLAND_DISPLAY=${wayland_display} ${hostsocket} ${bindir}${miral_server} ${enable_mirclient} $*"&
 
   # Fixup for weird gnome-terminal script on Ubuntu
   if [ "${terminal}" == "gnome-terminal" ] && [ -e "/usr/bin/gnome-terminal.real" ]
@@ -89,6 +99,6 @@ else
 
   while [ ! -e "${XDG_RUNTIME_DIR}/${wayland_display}" ]; do echo "waiting for ${wayland_display}"; sleep 1 ;done
 
-  MIR_SOCKET=${socket} XDG_SESSION_TYPE=mir GDK_BACKEND=${gdk_backend} QT_QPA_PLATFORM=${qt_qpa} SDL_VIDEODRIVER=${sdl_videodriver} WAYLAND_DISPLAY=${wayland_display} NO_AT_BRIDGE=1 dbus-run-session -- ${terminal}
+  MIR_SOCKET=${socket} XDG_SESSION_TYPE=mir GDK_BACKEND=${gdk_backend} QT_QPA_PLATFORM=${qt_qpa} SDL_VIDEODRIVER=${sdl_videodriver} WAYLAND_DISPLAY=${wayland_display} NO_AT_BRIDGE=1 DISPLAY=:${x11_display} dbus-run-session -- ${terminal}
   killall ${bindir}${miral_server} || killall ${bindir}${miral_server}.bin
 fi
