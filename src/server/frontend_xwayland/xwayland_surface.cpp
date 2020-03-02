d7f6ff8ac6eb061e9ffd6520593f3b0cc3c4714d
William Wold <wm@wmww.sh>
2020-03-02 07:52:12 -0500

    XWaylandSurface: lower surface on minimize

--- src/server/frontend_xwayland/xwayland_surface.cpp
+++ src/server/frontend_xwayland/xwayland_surface.cpp
@@ -714,6 +714,15 @@ void mf::XWaylandSurface::scene_surface_state_set(MirWindowState new_state)
         state = cached.state.updated_from(new_state);
     }
     inform_client_of_window_state(state);
+    if (new_state == mir_window_state_minimized || new_state == mir_window_state_minimized)
+    {
+        connection->configure_window(
+            window,
+            std::experimental::nullopt,
+            std::experimental::nullopt,
+            std::experimental::nullopt,
+            XCB_STACK_MODE_BELOW);
+    }
 }
 
 void mf::XWaylandSurface::scene_surface_resized(geometry::Size const& new_size)
