311bfecfea0fc27ce40b74428c305fcc20e05d56
William Wold <wm@wmww.sh>
2020-03-12 09:24:13 -0400

    mgx::DisplayBuffer: construct with full view area

--- src/platforms/mesa/server/x11/graphics/display.cpp
+++ src/platforms/mesa/server/x11/graphics/display.cpp
@@ -271,12 +271,11 @@ mgx::Display::Display(::Display* x_dpy,
             x_dpy,
             configuration->id,
             *window,
-            actual_size,
+            configuration->extents(),
             shared_egl.context(),
             last_frame,
             report,
             *gl_config);
-        display_buffer->set_view_area(configuration->extents());
         outputs.push_back(std::make_unique<OutputInfo>(move(window), move(display_buffer), configuration));
         top_left.x = geom::X{top_left.x.as_int() + actual_size.width.as_int()};
     }
