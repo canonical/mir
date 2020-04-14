a1b1a07ffe6ea43c2930cabe3a54155fa2f6e31b
Alan Griffiths <alan@octopull.co.uk>
2020-04-14 10:37:08 +0100

    Fix FTBFS in PPA

--- tests/miral/external_client.cpp
+++ tests/miral/external_client.cpp
@@ -93,8 +93,8 @@ TEST_F(ExternalClient, default_app_env_is_as_expected)
 
 TEST_F(ExternalClient, default_app_env_x11_is_as_expected)
 {
-    if (access("/tmp/.X11-unix/", W_OK) == 0)
-        return; // Starting an X server on LP builder doesn't work - skip the test
+    if (getenv("XDG_RUNTIME_DIR") == nullptr || access("/tmp/.X11-unix/", W_OK) == 0)
+        return; // Starting an X server on LP builder, or Fedora CI, doesn't work - skip the test
 
     add_server_init(x11);
     add_to_environment("MIR_SERVER_ENABLE_X11", "");
@@ -120,8 +120,8 @@ TEST_F(ExternalClient, override_app_env_can_set_gdk_backend)
 
 TEST_F(ExternalClient, override_app_env_x11_can_unset)
 {
-    if (access("/tmp/.X11-unix/", W_OK) == 0)
-        return; // Starting an X server on LP builder doesn't work - skip the test
+    if (getenv("XDG_RUNTIME_DIR") == nullptr || access("/tmp/.X11-unix/", W_OK) == 0)
+        return; // Starting an X server on LP builder, or Fedora CI, doesn't work - skip the test
 
     add_to_environment(app_x11_env, "-GDK_BACKEND");
     add_server_init(x11);
@@ -133,8 +133,8 @@ TEST_F(ExternalClient, override_app_env_x11_can_unset)
 
 TEST_F(ExternalClient, override_app_env_x11_can_unset_and_set)
 {
-    if (access("/tmp/.X11-unix/", W_OK) == 0)
-        return; // Starting an X server on LP builder doesn't work - skip the test
+    if (getenv("XDG_RUNTIME_DIR") == nullptr || access("/tmp/.X11-unix/", W_OK) == 0)
+        return; // Starting an X server on LP builder, or Fedora CI, doesn't work - skip the test
 
     add_to_environment(app_x11_env, "-GDK_BACKEND:QT_QPA_PLATFORM=xcb");
     add_server_init(x11);
@@ -147,8 +147,8 @@ TEST_F(ExternalClient, override_app_env_x11_can_unset_and_set)
 
 TEST_F(ExternalClient, override_app_env_x11_can_set_and_unset)
 {
-    if (access("/tmp/.X11-unix/", W_OK) == 0)
-        return; // Starting an X server on LP builder doesn't work - skip the test
+    if (getenv("XDG_RUNTIME_DIR") == nullptr || access("/tmp/.X11-unix/", W_OK) == 0)
+        return; // Starting an X server on LP builder, or Fedora CI, doesn't work - skip the test
 
     add_to_environment(app_x11_env, "QT_QPA_PLATFORM=xcb:-GDK_BACKEND");
     add_server_init(x11);
@@ -161,8 +161,8 @@ TEST_F(ExternalClient, override_app_env_x11_can_set_and_unset)
 
 TEST_F(ExternalClient, stray_separators_are_ignored)
 {
-    if (access("/tmp/.X11-unix/", W_OK) == 0)
-        return; // Starting an X server on LP builder doesn't work - skip the test
+    if (getenv("XDG_RUNTIME_DIR") == nullptr || access("/tmp/.X11-unix/", W_OK) == 0)
+        return; // Starting an X server on LP builder, or Fedora CI, doesn't work - skip the test
 
     add_to_environment(app_x11_env, "::QT_QPA_PLATFORM=xcb::-GDK_BACKEND::");
     add_server_init(x11);
@@ -175,8 +175,8 @@ TEST_F(ExternalClient, stray_separators_are_ignored)
 
 TEST_F(ExternalClient, empty_override_does_nothing)
 {
-    if (access("/tmp/.X11-unix/", W_OK) == 0)
-        return; // Starting an X server on LP builder doesn't work - skip the test
+    if (getenv("XDG_RUNTIME_DIR") == nullptr || access("/tmp/.X11-unix/", W_OK) == 0)
+        return; // Starting an X server on LP builder, or Fedora CI, doesn't work - skip the test
 
     add_to_environment(app_x11_env, "");
     add_server_init(x11);
@@ -192,8 +192,8 @@ TEST_F(ExternalClient, empty_override_does_nothing)
 
 TEST_F(ExternalClient, strange_override_does_nothing)
 {
-    if (access("/tmp/.X11-unix/", W_OK) == 0)
-        return; // Starting an X server on LP builder doesn't work - skip the test
+    if (getenv("XDG_RUNTIME_DIR") == nullptr || access("/tmp/.X11-unix/", W_OK) == 0)
+        return; // Starting an X server on LP builder, or Fedora CI, doesn't work - skip the test
 
     add_to_environment(app_x11_env, "=====");
     add_server_init(x11);
@@ -209,8 +209,8 @@ TEST_F(ExternalClient, strange_override_does_nothing)
 
 TEST_F(ExternalClient, another_strange_override_does_nothing)
 {
-    if (access("/tmp/.X11-unix/", W_OK) == 0)
-        return; // Starting an X server on LP builder doesn't work - skip the test
+    if (getenv("XDG_RUNTIME_DIR") == nullptr || access("/tmp/.X11-unix/", W_OK) == 0)
+        return; // Starting an X server on LP builder, or Fedora CI, doesn't work - skip the test
 
     add_to_environment(app_x11_env, ":::");
     add_server_init(x11);
