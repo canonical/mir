f2ccfe35e6e27a052302fa5f7afc8180f1f9bd24
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2020-04-22 16:52:08 +1000

    rpi-dispmanx: Whitespace

--- src/platforms/rpi-dispmanx/buffer_allocator.cpp
+++ src/platforms/rpi-dispmanx/buffer_allocator.cpp
@@ -352,8 +352,6 @@ public:
                 bounce_buffer.get(),
                 stride);
 
-
-
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
