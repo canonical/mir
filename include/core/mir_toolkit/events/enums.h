1d91882f12b48bd8b2447a192d40095f45c1cd09
Alan Griffiths <alan@octopull.co.uk>
2020-05-14 10:07:09 +0100

    More event enums

--- include/core/mir_toolkit/events/enums.h
+++ include/core/mir_toolkit/events/enums.h
@@ -102,6 +102,110 @@ typedef enum {
     mir_keyboard_actions
 } MirKeyboardAction;
 
+/**
+ * Possible per touch actions for state changing
+ */
+typedef enum {
+    /* This touch point is going up */
+    mir_touch_action_up = 0,
+    /* This touch point is going down */
+    mir_touch_action_down = 1,
+    /* Axis values have changed on this touch point */
+    mir_touch_action_change = 2,
+
+    mir_touch_actions
+} MirTouchAction;
+
+/**
+ * Identifiers for touch axis
+ */
+typedef enum {
+/* Axis representing the x coordinate for the touch */
+    mir_touch_axis_x = 0,
+/* Axis representing the y coordinate for the touch */
+    mir_touch_axis_y = 1,
+/* Axis representing pressure of the touch */
+    mir_touch_axis_pressure = 2,
+/* Axis representing the length of the major axis of an ellipse
+   centered at the touch point */
+    mir_touch_axis_touch_major = 3,
+/* Axis representing the length of the minor axis of an ellipse
+   centered at the touch point */
+    mir_touch_axis_touch_minor = 4,
+/* Axis representing the diameter of a circle centered on the touch
+   point */
+    mir_touch_axis_size = 5,
+
+    mir_touch_axes
+} MirTouchAxis;
+
+/**
+ * Identifiers for per-touch tool types
+ */
+typedef enum {
+// Tool type could not be determined
+    mir_touch_tooltype_unknown = 0,
+// Touch is made with a finger
+    mir_touch_tooltype_finger = 1,
+// Touch is made with a stylus
+    mir_touch_tooltype_stylus = 2,
+
+    mir_touch_tooltypes
+} MirTouchTooltype;
+
+
+/**
+ * Possible pointer actions
+ */
+typedef enum {
+    /* A pointer button has come up */
+    mir_pointer_action_button_up = 0,
+    /* A pointer button has gone down */
+    mir_pointer_action_button_down = 1,
+    /* The pointer has entered the surface to which this event was delivered */
+    mir_pointer_action_enter = 2,
+    /* The pointer has left the surface to which this event was delivered */
+    mir_pointer_action_leave = 3,
+    /* Axis values have changed for the pointer */
+    mir_pointer_action_motion = 4,
+
+    mir_pointer_actions
+} MirPointerAction;
+
+/**
+ * Identifiers for pointer axis
+ */
+typedef enum {
+/* Absolute axis containing the x coordinate of the pointer */
+    mir_pointer_axis_x = 0,
+/* Absolute axis containing the y coordinate of the pointer */
+    mir_pointer_axis_y = 1,
+/* Relative axis containing ticks reported by the vertical scroll wheel */
+    mir_pointer_axis_vscroll = 2,
+/* Relative axis containing ticks reported by the horizontal scroll wheel */
+    mir_pointer_axis_hscroll = 3,
+/* Relative axis containing the last reported x differential from the pointer */
+    mir_pointer_axis_relative_x = 4,
+/* Relative axis containing the last reported y differential from the pointer */
+    mir_pointer_axis_relative_y = 5,
+
+    mir_pointer_axes
+} MirPointerAxis;
+
+/*
+ * Identifiers for pointer buttons
+ */
+typedef enum {
+    mir_pointer_button_primary   = 1 << 0,
+    mir_pointer_button_secondary = 1 << 1,
+    mir_pointer_button_tertiary  = 1 << 2,
+    mir_pointer_button_back      = 1 << 3,
+    mir_pointer_button_forward   = 1 << 4,
+    mir_pointer_button_side      = 1 << 5,
+    mir_pointer_button_extra     = 1 << 6,
+    mir_pointer_button_task      = 1 << 7
+} MirPointerButton;
+
 #ifdef __cplusplus
 }
 /**@}*/
