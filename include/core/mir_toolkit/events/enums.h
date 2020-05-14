88e0d1d84915e1a13527f4ecd2ae6e914e687979
Alan Griffiths <alan@octopull.co.uk>
2020-05-14 09:57:14 +0100

    Move another enum.

--- include/core/mir_toolkit/events/enums.h
+++ include/core/mir_toolkit/events/enums.h
@@ -87,6 +87,21 @@ typedef enum {
 } MirInputEventModifier;
 typedef unsigned int MirInputEventModifiers;
 
+/**
+ * Possible actions for changing key state
+ */
+typedef enum {
+    /* A key has come up (released) */
+    mir_keyboard_action_up,
+    /* A key has gone down (pressed) */
+    mir_keyboard_action_down,
+    /* System policy has triggered a key repeat on a key
+       which was already down */
+    mir_keyboard_action_repeat,
+
+    mir_keyboard_actions
+} MirKeyboardAction;
+
 #ifdef __cplusplus
 }
 /**@}*/
