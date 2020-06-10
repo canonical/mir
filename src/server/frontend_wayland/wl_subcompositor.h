d4623d3e289c4d4d3385e67a84d91d1a672076a2
William Wold <wm@wmww.sh>
2020-06-10 10:42:20 -0400

    Make subsurface parent a raw pointer

--- src/server/frontend_wayland/wl_subcompositor.h
+++ src/server/frontend_wayland/wl_subcompositor.h
@@ -80,7 +80,7 @@ private:
 
     WlSurface* const surface;
     /// This class is responsible for removing itself from the parent's children list when needed
-    std::unique_ptr<WlSurface, std::function<void(WlSurface*)>> const parent;
+    WlSurface* const parent;
     std::shared_ptr<bool> const parent_destroyed;
     bool synchronized_;
     std::experimental::optional<WlSurfaceState> cached_state;
