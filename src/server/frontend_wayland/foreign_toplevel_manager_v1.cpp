3298f66567fda15eb9a5c1f1b5042d9948d1502d
William Wold <wm@wmww.sh>
2020-08-25 15:06:34 -0400

    Address RAOF's review

--- src/server/frontend_wayland/foreign_toplevel_manager_v1.cpp
+++ src/server/frontend_wayland/foreign_toplevel_manager_v1.cpp
@@ -161,7 +161,7 @@ public:
     /// Sends the required .state event
     void send_state(MirWindowFocusState focused, MirWindowState state);
 
-    /// Sends the .closed event and makes this surface innert
+    /// Sends the .closed event and makes this surface inert
     void should_close();
 
 private:
@@ -249,7 +249,7 @@ void mf::ForeignSceneObserver::surface_removed(std::shared_ptr<scene::Surface> c
     auto const iter = surface_observers.find(surface);
     if (iter == surface_observers.end())
     {
-        log_error("Can not remove ForeignSurfaceObserver: surface %p not in observers map");
+        log_error("Can not remove ForeignSurfaceObserver: surface %p not in observers map", surface.get());
     }
     else
     {
