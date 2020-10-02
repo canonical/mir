e94c4bd10d7346739d5dc4a46b5ab0918e12fff0
William Wold <wm@wmww.sh>
2020-10-01 20:58:08 -0700

    Add miral::Output::logical_group_id property

--- src/miral/output.cpp
+++ src/miral/output.cpp
@@ -100,6 +100,11 @@ auto miral::Output::is_same_output(Output const& other) const -> bool
     return self->card_id == other.self->card_id && self->id == other.self->id;
 }
 
+auto miral::Output::logical_group_id() const -> int
+{
+    return self->logical_group_id.as_value();
+}
+
 bool miral::operator==(Output::PhysicalSizeMM const& lhs, Output::PhysicalSizeMM const& rhs)
 {
     return lhs.width == rhs.width && lhs.height == rhs.height;
