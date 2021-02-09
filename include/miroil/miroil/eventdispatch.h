c4d6d486d655efa2732ccb16bd2cb27805673b96
Erlend <me@erlend.io>
2021-02-09 19:29:35 +0100

    moving eventdispatch to miroil

---
+++ include/miroil/miroil/eventdispatch.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2017 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MIROIL_EVENTDISPATCH_H
+#define MIROIL_EVENTDISPATCH_H
+
+namespace miral { class Window; }
+
+struct MirInputEvent;
+
+namespace miroil
+{
+void dispatchInputEvent(const miral::Window& window, const MirInputEvent* event);
+}
+
+#endif //MIROIL_EVENTDISPATCH_H
