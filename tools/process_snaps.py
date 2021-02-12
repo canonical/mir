0e7407395e129510992fd0861931fd5cf92febba
Michał Sawicz <michal@sawicz.net>
2021-02-12 12:10:13 +0100

    [snaps] drop mir-kiosk-apps and chromium-mir-kiosk

--- tools/process_snaps.py
+++ tools/process_snaps.py
@@ -33,12 +33,6 @@ SNAPS = {
         "edge": {"ppa": "dev", "recipe": "mir-kiosk-edge"},
         "beta": {"ppa": "rc", "recipe": "mir-kiosk-beta"},
     },
-    "mir-kiosk-apps": {
-        "edge": {"recipe": "mir-kiosk-apps"},
-    },
-    "chromium-mir-kiosk": {
-        "edge": {"recipe": "chromium-mir-kiosk"},
-    },
     "mir-test-tools": {
         "edge": {"ppa": "dev", "recipe": "mir-test-tools-edge"},
         "beta": {"ppa": "rc", "recipe": "mir-test-tools"},
