7281f8371d56b0e916203a84ecbd00cc9a72dd00
William Wold <wm@wmww.sh>
2021-04-29 22:47:13 -0700

    Add SurfaceStateStack

---
+++ src/include/server/mir/scene/surface_state_stack.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright © 2021 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 or 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: William Wold <william.wold@canonical.com>
+ */
+
+#ifndef MIR_SCENE_SURFACE_STATE_STACK_
+#define MIR_SCENE_SURFACE_STATE_STACK_
+
+#include "mir_toolkit/common.h"
+
+namespace mir
+{
+namespace scene
+{
+/// Represents a surface's current state, as well as the states it will fall back to as states are removed. For example
+/// A minimized surface may also be fullscreen (which will become relevant when it's unminimized) but not maximized
+/// (which will become relevant when it's unfullscreened). This class keeps track of all the active and inactive states.
+/// States have this order of precedence (earlier ones will be the active state if present):
+/// - hidden, minimized, fullscreen, attached, maximized/vertmaximized/horizmaximized, restored
+/// This struct can NOT be safely accessed by multiple threads at once.
+struct SurfaceStateStack
+{
+public:
+    explicit SurfaceStateStack(MirWindowState initial)
+        : hidden{false},
+          minimized{false},
+          fullscreen{false},
+          attached{false},
+          horiz_maximized{false},
+          vert_maximized{false}
+    {
+        set_active_state(initial);
+    }
+    /// This class can be freely copied and assigned
+    SurfaceStateStack(const SurfaceStateStack&) = default;
+    SurfaceStateStack& operator=(const SurfaceStateStack&) = default;
+
+    /// Returns the currently active state
+    auto active_state() const -> MirWindowState;
+
+    /// Sets the currently active state, pushes to and pops from the stack as needed
+    void set_active_state(MirWindowState state);
+
+    /// Returns if the given state is present
+    auto has(MirWindowState state) const -> bool;
+
+    /// Adds the given state (which may or may not effect the active state) and returns this object. Sending
+    /// mir_window_state_restored is NOT allowed, as restored is the baseline state all surfaces always have.
+    auto with(MirWindowState state) -> SurfaceStateStack&;
+
+    /// Removes the given state (which may or may not effect the active state) and returns this object. Sending
+    /// mir_window_state_restored is NOT allowed, as restored is the baseline state all surfaces always have.
+    auto without(MirWindowState state) -> SurfaceStateStack&;
+
+private:
+    bool hidden : 1;
+    bool minimized : 1;
+    bool fullscreen : 1;
+    bool attached : 1;
+    bool horiz_maximized : 1;
+    bool vert_maximized : 1;
+
+    void set_enabled(MirWindowState state, bool enabled);
+};
+}
+}
+
+#endif // MIR_SCENE_SURFACE_STATE_STACK_
---
+++ src/server/scene/surface_state_stack.cpp
@@ -0,0 +1,198 @@
+/*
+ * Copyright © 2021 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 or 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: William Wold <william.wold@canonical.com>
+ */
+
+#include "mir/scene/surface_state_stack.h"
+#include "mir/fatal.h"
+
+namespace ms = mir::scene;
+
+auto ms::SurfaceStateStack::active_state() const -> MirWindowState
+{
+    if (hidden)
+    {
+        return mir_window_state_hidden;
+    }
+    else if (minimized)
+    {
+        return mir_window_state_minimized;
+    }
+    else if (fullscreen)
+    {
+        return mir_window_state_fullscreen;
+    }
+    else if (attached)
+    {
+        return mir_window_state_attached;
+    }
+    else if (vert_maximized && horiz_maximized)
+    {
+        return mir_window_state_maximized;
+    }
+    else if (vert_maximized)
+    {
+        return mir_window_state_vertmaximized;
+    }
+    else if (horiz_maximized)
+    {
+        return mir_window_state_horizmaximized;
+    }
+    else
+    {
+        return mir_window_state_restored;
+    }
+}
+
+void ms::SurfaceStateStack::set_active_state(MirWindowState state)
+{
+    switch (state)
+    {
+    case mir_window_state_hidden:
+        hidden          = true;
+        break;
+
+    case mir_window_state_minimized:
+        hidden          = false;
+        minimized       = true;
+        break;
+
+    case mir_window_state_fullscreen:
+        hidden          = false;
+        minimized       = false;
+        fullscreen      = true;
+        break;
+
+    case mir_window_state_attached:
+        hidden          = false;
+        minimized       = false;
+        fullscreen      = false;
+        attached        = true;
+        break;
+
+    case mir_window_state_maximized:
+        hidden          = false;
+        minimized       = false;
+        fullscreen      = false;
+        attached        = false;
+        horiz_maximized = true;
+        vert_maximized  = true;
+        break;
+
+    case mir_window_state_horizmaximized:
+        hidden          = false;
+        minimized       = false;
+        fullscreen      = false;
+        attached        = false;
+        horiz_maximized = true;
+        vert_maximized  = false;
+        break;
+
+    case mir_window_state_vertmaximized:
+        hidden          = false;
+        minimized       = false;
+        fullscreen      = false;
+        attached        = false;
+        horiz_maximized = false;
+        vert_maximized  = true;
+        break;
+
+    case mir_window_state_restored:
+        hidden          = false;
+        minimized       = false;
+        fullscreen      = false;
+        attached        = false;
+        horiz_maximized = false;
+        vert_maximized  = false;
+        break;
+
+    case mir_window_state_unknown:
+        break;
+
+    default:
+        fatal_error("Invalid window state %d sent to SurfaceStateStack::set_active_state()", state);
+    }
+}
+
+auto ms::SurfaceStateStack::has(MirWindowState state) const -> bool
+{
+    switch (state)
+    {
+    case mir_window_state_hidden: return hidden;
+    case mir_window_state_minimized: return minimized;
+    case mir_window_state_fullscreen: return fullscreen;
+    case mir_window_state_attached: return attached;
+    case mir_window_state_maximized: return horiz_maximized && vert_maximized;
+    case mir_window_state_horizmaximized: return horiz_maximized;
+    case mir_window_state_vertmaximized: return vert_maximized;
+    case mir_window_state_restored: return (!horiz_maximized && !vert_maximized);
+    default: return false;
+    }
+}
+
+auto ms::SurfaceStateStack::with(MirWindowState state) -> SurfaceStateStack&
+{
+    set_enabled(state, true);
+    return *this;
+}
+
+auto ms::SurfaceStateStack::without(MirWindowState state) -> SurfaceStateStack&
+{
+    set_enabled(state, false);
+    return *this;
+}
+
+void ms::SurfaceStateStack::set_enabled(MirWindowState state, bool enabled)
+{
+    switch (state)
+    {
+    case mir_window_state_hidden:
+        hidden = enabled;
+        break;
+
+    case mir_window_state_minimized:
+        minimized = enabled;
+        break;
+
+    case mir_window_state_fullscreen:
+        fullscreen = enabled;
+        break;
+
+    case mir_window_state_attached:
+        attached = enabled;
+        break;
+
+    case mir_window_state_maximized:
+        horiz_maximized = enabled;
+        vert_maximized = enabled;
+        break;
+
+    case mir_window_state_horizmaximized:
+        horiz_maximized = enabled;
+        break;
+
+    case mir_window_state_vertmaximized:
+        vert_maximized = enabled;
+        break;
+
+    case mir_window_state_restored:
+        fatal_error("Sending mir_window_state_restored to SurfaceStateStack::with()/without() is not allowed");
+        break;
+
+    default:
+        fatal_error("Invalid window state %d", state);
+    }
+}
