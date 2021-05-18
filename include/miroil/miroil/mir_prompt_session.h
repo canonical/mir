e413983610e0de63e7a2e2fd191be6ec90f2ce8a
Erlend <me@erlend.io>
2021-05-18 10:16:57 +0200

    adding mir connection and mir_prompt_Session

--- include/miroil/miroil/mir_prompt_session.h
+++ include/miroil/miroil/mir_prompt_session.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 Canonical Ltd.
+ * Copyright © 2021 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2 or 3,
@@ -14,70 +14,35 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef MIR_TOOLKIT_MIR_PROMPT_SESSION_H_
-#define MIR_TOOLKIT_MIR_PROMPT_SESSION_H_
+#ifndef MIROIL_MIR_PROMPT_SESSION_H_
+#define MIROIL_MIR_PROMPT_SESSION_H_
+#include <memory>
+#include <functional>
 
-#include "client_types.h"
+typedef struct MirPromptSession MirPromptSession;
+typedef void (*MirClientFdCallback)(MirPromptSession *prompt_session, size_t count, int const* fds, void* context);
 
+namespace miroil 
+{
+    
+class MirPromptSession
+{
+public:    
+    MirPromptSession(::MirPromptSession * promptSession);
+    MirPromptSession(MirPromptSession const& src);
+    MirPromptSession(MirPromptSession && src);
+    ~MirPromptSession();
 
+    auto operator=(MirPromptSession const& src) -> MirPromptSession&;
+    auto operator=(MirPromptSession&& src) -> MirPromptSession&;
 
+    bool operator==(MirPromptSession const& other);
+    
+    bool new_fds_for_prompt_providers(unsigned int no_of_fds, MirClientFdCallback callback, void * context);
+    
+    ::MirPromptSession * prompt_session;    
+};
 
-#include <sys/types.h>
-#include <stdbool.h>
-
-#ifdef __cplusplus
-/**
- * \addtogroup mir_toolkit
- * @{
- */
-extern "C" {
-#endif
-
-/**
- * Create and start a new prompt session
- *   \param [in] connection             The connection
- *   \param [in] application_pid        The process id of the initiating application
- *   \param [in] state_change_callback  The function to be called when a prompt session state change occurs
- *   \param [in,out] context            User data passed to the callback functions
- *   \return                            A handle that can be passed to mir_wait_for
- */
-MirPromptSession *mir_connection_create_prompt_session_sync(
-    MirConnection* connection,
-    pid_t application_pid,
-    MirPromptSessionStateChangeCallback state_change_callback,
-    void *context);
-
-/**
- * Allocate some FDs for prompt providers to connect on
- *
- * Prompt helpers need to allocate connection FDs it will pass to
- * prompt providers to use when connecting to the server. The server can
- * then associate them with the prompt session.
- *
- *   \warning This API is tentative until the implementation of prompt sessions is complete
- *   \param [in] prompt_session  The prompt session
- *   \param [in] no_of_fds       The number of fds to allocate
- *   \param [in] callback        Callback invoked when request completes
- *   \param [in,out] context     User data passed to the callback function
- *   \return                     A handle that can be passed to mir_wait_for
- */
-MirWaitHandle* mir_prompt_session_new_fds_for_prompt_providers(
-    MirPromptSession *prompt_session,
-    unsigned int no_of_fds,
-    MirClientFdCallback callback,
-    void * context);
-
-/**
- * Stop and release the specified prompt session
- *   \param [in] prompt_session  The prompt session
- */
-void mir_prompt_session_release_sync(MirPromptSession *prompt_session);
-
-#ifdef __cplusplus
 }
-/**@}*/
-#endif
 
-#endif /* MIR_TOOLKIT_MIR_PROMPT_SESSION_H_ */
-
-
+#endif /* MIROIL_MIR_PROMPT_SESSION_H_ */
