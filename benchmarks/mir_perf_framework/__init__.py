8b93b549a3383ddac7673444b70661b0405b49ec
Alan Griffiths <alan@octopull.co.uk>
2021-06-14 12:37:56 +0100

    Drop performance framework as it expects mirclient

--- benchmarks/mir_perf_framework/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This modules provides a framework to facilitate writing Mir performance tests
-"""
-
-from .server import Server
-from .client import Client
-from .performance_test import PerformanceTest
