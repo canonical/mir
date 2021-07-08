0b1b2672f5ec4704db3301e8058362fce4e24cbc
William Wold <wm@wmww.sh>
2021-07-08 16:06:02 -0700

    VirtualKeyboardV1: support keymaps

--- src/server/frontend_wayland/virtual_keyboard_v1.cpp
+++ src/server/frontend_wayland/virtual_keyboard_v1.cpp
@@ -19,7 +19,7 @@
 #include "virtual_keyboard_v1.h"
 #include "virtual-keyboard-unstable-v1_wrapper.h"
 #include "wayland_wrapper.h"
-#include "wl_seat.h"
+#include "wayland_wrapper.h"
 
 #include "mir/input/event_builder.h"
 #include "mir/input/input_sink.h"
@@ -29,10 +29,14 @@
 #include "mir/input/touchscreen_settings.h"
 #include "mir/input/input_device_registry.h"
 #include "mir/input/input_device.h"
+#include "mir/input/device.h"
+#include "mir/input/mir_keyboard_config.h"
+#include "mir/input/buffer_keymap.h"
 #include "mir/fatal.h"
 #include "mir/log.h"
 
 #include <mutex>
+#include <cstring>
 
 namespace mf = mir::frontend;
 namespace mw = mir::wayland;
@@ -64,6 +68,49 @@ auto mir_keyboard_action(uint32_t wayland_state) -> MirKeyboardAction
         BOOST_THROW_EXCEPTION(std::runtime_error(
             "Invalid virtual keyboard key state " + std::to_string(wayland_state)));
     }
+}
+
+auto load_keymap(uint32_t format, mir::Fd fd, size_t size) -> std::shared_ptr<mi::Keymap>
+{
+    if (format != mw::Keyboard::KeymapFormat::xkb_v1)
+    {
+        BOOST_THROW_EXCEPTION(std::runtime_error("invalid keymap format " + std::to_string(format)));
+    }
+
+    std::vector<char> buffer(size);
+    char* current = buffer.data();
+    size_t remaining = size;
+    while (remaining > 0)
+    {
+        auto const result = read(fd, current, remaining);
+        if (result < 0)
+        {
+            BOOST_THROW_EXCEPTION(std::runtime_error(
+                "failed to read from keymap fd: " +
+                std::string{strerror(errno)}));
+        }
+        else if (result == 0)
+        {
+            BOOST_THROW_EXCEPTION(std::runtime_error(
+                "keymap fd hit EOF " +
+                std::to_string(remaining) +
+                " bytes before specified size"));
+        }
+        else
+        {
+            current += result;
+            remaining -= result;
+        }
+    }
+
+    // Keymaps seem to be null-terminated. It's unclear if they're supposed to be or not. Either way, BufferKeymap does
+    // not expect a null-terminated keymap
+    while (buffer.back() == '\0')
+    {
+        buffer.pop_back();
+    }
+
+    return std::make_shared<mi::BufferKeymap>("virtual-keyboard-keymap", std::move(buffer), XKB_KEYMAP_FORMAT_TEXT_V1);
 }
 }
 
@@ -155,7 +202,7 @@ class VirtualKeyboardV1
     : public wayland::VirtualKeyboardV1
 {
 public:
-    VirtualKeyboardV1(wl_resource* resource, WlSeat& seat, std::shared_ptr<VirtualKeyboardV1Ctx> const& ctx);
+    VirtualKeyboardV1(wl_resource* resource, std::shared_ptr<VirtualKeyboardV1Ctx> const& ctx);
     ~VirtualKeyboardV1();
 
 private:
@@ -164,7 +211,8 @@ private:
     void modifiers(uint32_t mods_depressed, uint32_t mods_latched, uint32_t mods_locked, uint32_t group) override;
 
     std::shared_ptr<VirtualKeyboardV1Ctx> const ctx;
-    std::shared_ptr<VirtualKeyboardDevice> const device;
+    std::shared_ptr<VirtualKeyboardDevice> const keyboard_device;
+    std::weak_ptr<input::Device> const device_handle;
 };
 }
 }
@@ -173,9 +221,7 @@ auto mf::create_virtual_keyboard_manager_v1(
     wl_display* display,
     std::shared_ptr<mi::InputDeviceRegistry> const& device_registry) -> std::shared_ptr<VirtualKeyboardManagerV1Global>
 {
-    auto ctx = std::shared_ptr<VirtualKeyboardV1Ctx>{new VirtualKeyboardV1Ctx{
-        device_registry,
-    }};
+    auto ctx = std::shared_ptr<VirtualKeyboardV1Ctx>{new VirtualKeyboardV1Ctx{device_registry}};
     return std::make_shared<VirtualKeyboardManagerV1Global>(display, std::move(ctx));
 }
 
@@ -202,44 +248,39 @@ mf::VirtualKeyboardManagerV1::VirtualKeyboardManagerV1(
 
 void mf::VirtualKeyboardManagerV1::create_virtual_keyboard(struct wl_resource* seat, struct wl_resource* id)
 {
-    auto const wl_seat = WlSeat::from(seat);
-    if (!wl_seat)
-    {
-        fatal_error("create_virtual_keyboard() received invalid wl_seat");
-    }
-    new VirtualKeyboardV1{id, *wl_seat, ctx};
+    (void)seat;
+    new VirtualKeyboardV1{id, ctx};
 }
 
 mf::VirtualKeyboardV1::VirtualKeyboardV1(
     wl_resource* resource,
-    WlSeat& seat,
     std::shared_ptr<VirtualKeyboardV1Ctx> const& ctx)
     : wayland::VirtualKeyboardV1{resource, Version<1>()},
       ctx{ctx},
-      device{std::make_shared<VirtualKeyboardDevice>()}
+      keyboard_device{std::make_shared<VirtualKeyboardDevice>()},
+      device_handle{ctx->device_registry->add_device(keyboard_device)}
 {
-    (void)seat;
-    ctx->device_registry->add_device(device);
 }
 
 mf::VirtualKeyboardV1::~VirtualKeyboardV1()
 {
-    ctx->device_registry->remove_device(device);
+    ctx->device_registry->remove_device(keyboard_device);
 }
 
 void mf::VirtualKeyboardV1::keymap(uint32_t format, mir::Fd fd, uint32_t size)
 {
-    (void)format;
-    (void)fd;
-    (void)size;
-    // TODO
-    log_info("Ignoring zwp_virtual_keyboard_v1.keymap()");
+    if (auto const device = device_handle.lock())
+    {
+        auto keymap = load_keymap(format, std::move(fd), size);
+        MirKeyboardConfig const config{std::move(keymap)};
+        device->apply_keyboard_configuration(config);
+    }
 }
 
 void mf::VirtualKeyboardV1::key(uint32_t time, uint32_t key, uint32_t state)
 {
     (void)time;
-    device->use([&](input::InputSink* sink, input::EventBuilder* builder)
+    keyboard_device->use([&](input::InputSink* sink, input::EventBuilder* builder)
         {
             sink->handle_input(builder->key_event(std::chrono::nanoseconds{}, mir_keyboard_action(state), 0, key));
         });
@@ -255,6 +296,6 @@ void mf::VirtualKeyboardV1::modifiers(
     (void)mods_latched;
     (void)mods_locked;
     (void)group;
-    // TODO
     log_info("Ignoring zwp_virtual_keyboard_v1.modifiers()");
+    // Currently we keep track of and send modifiers in the Wayland frontend, so handling this is not needed
 }
