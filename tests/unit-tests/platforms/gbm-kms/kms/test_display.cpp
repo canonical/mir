52e291dd76716a148b7287e7d839063a71383efb
Alan Griffiths <alan@octopull.co.uk>
2021-07-28 12:55:29 +0100

    Re-enable MesaDisplayTest.constructor_throws_if_egl_khr_image_pixmap_not_supported

--- tests/unit-tests/platforms/gbm-kms/kms/test_display.cpp
+++ tests/unit-tests/platforms/gbm-kms/kms/test_display.cpp
@@ -707,8 +707,7 @@ TEST_F(MesaDisplayTest, outputs_correct_string_for_successful_drm_mode_set_crtc_
     reporter->report_successful_drm_mode_set_crtc_on_construction();
 }
 
-// Disabled until gbm-kms drm platform and mir platform properly shows support for those extensions
-TEST_F(MesaDisplayTest, DISABLED_constructor_throws_if_egl_khr_image_pixmap_not_supported)
+TEST_F(MesaDisplayTest, constructor_throws_if_egl_khr_image_pixmap_not_supported)
 {
     using namespace ::testing;
 
