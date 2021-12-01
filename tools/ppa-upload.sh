1de3b54ae358a51cbbca554c94284362845b53b2
Michał Sawicz <michal.sawicz@canonical.com>
2021-12-01 11:38:47 +0100

    [ci] import GitHub CI

--- tools/ppa-upload.sh
+++ tools/ppa-upload.sh
@@ -8,10 +8,10 @@ if [ -z "${RELEASE}" ]; then
   exit 1
 fi
 
-GIT_BRANCH=${TRAVIS_BRANCH-$( git rev-parse --abbrev-ref HEAD )}
+GIT_BRANCH=${GITHUB_REF-$( git rev-parse --abbrev-ref HEAD )}
 # determine the patch release
-if ! [[ "${GIT_BRANCH}" =~ ^(master|(release/|v)([0-9\.]+))$ ]]; then
-  echo "ERROR: This script should only run on master or release tags" >&2
+if ! [[ "${GIT_BRANCH}" =~ ^(refs/(heads|tags)/)?(main|(release/|v)([0-9\.]+))$ ]]; then
+  echo "ERROR: This script should only run on main or release tags" >&2
   echo "  or branches." >&2
   exit 3
 fi
@@ -39,13 +39,13 @@ fi
 
 GIT_REVISION=$( git rev-parse --short HEAD )
 
-if [[ "${GIT_BRANCH}" =~ ^(release/|v)([0-9\.]+)$ ]]; then
+if [[ "${GIT_BRANCH}" =~ ^(refs/(heads|tags)/)?(release/|v)([0-9\.]+)$ ]]; then
   # we're on a release branch
   TARGET_PPA=ppa:mir-team/rc
-  MIR_SERIES=${BASH_REMATCH[2]}
-  if [[ "$( git describe --exact-match )" =~ ^v[0-9\.]+$ ]] 2> /dev/null; then
+  MIR_SERIES=${BASH_REMATCH[4]}
+  if [[ "$( git describe --tags --exact-match )" =~ ^v([0-9\.]+)$ ]]; then
     # this is a final release, use the tag version
-    MIR_VERSION=$( git describe | sed 's/^v//' )
+    MIR_VERSION=${BASH_REMATCH[1]}
   else
     # find the last tagged patch version
     PATCH_VERSION=$( git describe --abbrev=0 --match "v${MIR_SERIES}*" \
@@ -58,8 +58,8 @@ if [[ "${GIT_BRANCH}" =~ ^(release/|v)([0-9\.]+)$ ]]; then
       MIR_VERSION=$( echo ${PATCH_VERSION} | perl -pe 's/^((\d+\.)*)(\d+)$/$1.($3+1)/e' )
     fi
 
-    # use the number of commits since master
-    GIT_COMMITS=$( git rev-list --count origin/master..HEAD )
+    # use the number of commits since main
+    GIT_COMMITS=$( git rev-list --count origin/main..HEAD )
     MIR_VERSION=${MIR_VERSION}~rc${GIT_COMMITS}-g${GIT_REVISION}
   fi
 else
