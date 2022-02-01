94fa9b4a31c96ce4a5316c88cdefa389412529cb
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2022-02-01 14:22:51 +1100

    mc::MultiThreadedCompositor: Migrate to mir::system_executor.
    Use the system threadpool rather than having a dedicated one for the compositor.

--- src/server/compositor/multi_threaded_compositor.h
+++ src/server/compositor/multi_threaded_compositor.h
@@ -85,7 +85,6 @@ private:
     std::shared_ptr<CompositorReport> const report;
 
     std::vector<std::unique_ptr<CompositingFunctor>> thread_functors;
-    std::vector<std::future<void>> futures;
 
     std::atomic<CompositorState> state;
     std::chrono::milliseconds fixed_composite_delay;
@@ -95,7 +94,6 @@ private:
     void schedule_compositing(int number_composites, geometry::Rectangle const& damage) const;
 
     std::shared_ptr<mir::scene::Observer> observer;
-    mir::thread::BasicThreadPool thread_pool;
 };
 
 }
