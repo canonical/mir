d7f4d0f29e327f5f9c1b39c0a43ca165c11c1803
William Wold <wm@wmww.sh>
2022-04-08 16:56:13 -0400

    Add zwlr_screencopy_manager_v1 protocol name to MirAL

--- src/miral/wayland_extensions.cpp
+++ src/miral/wayland_extensions.cpp
@@ -44,6 +44,7 @@ char const* const miral::WaylandExtensions::zwp_virtual_keyboard_manager_v1{"zwp
 /// Not in the header, but keeping around for ABI compat
 char const* const miral::WaylandExtensions::zwp_input_method_v2{"zwp_input_method_manager_v2"};
 char const* const miral::WaylandExtensions::zwp_input_method_manager_v2{"zwp_input_method_manager_v2"};
+char const* const miral::WaylandExtensions::zwlr_screencopy_manager_v1{"zwlr_screencopy_manager_v1"};
 
 namespace
 {
