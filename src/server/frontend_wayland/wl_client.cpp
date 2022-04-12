0f43c06693bdb3164cc5d76f1bc037a1d7dde81a
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2022-04-12 16:37:17 +1000

    frontend_wayland: Add StdLayoutUPtr
    We need to store some unique pointers in structs that libwayland
    uses `offset_of` on, and so must be StandardLayoutType.
    
    `std::unique_ptr` is not *guaranteed* to be a StandardLayoutType,
    and, indeed, with the libstdc++ from GCC 11 Clang does *not* think
    that `std::unique_ptr` is a StandardLayoutType.
    
    (See [the relevant LLVM bug](https://github.com/llvm/llvm-project/issues/53021))

--- src/server/frontend_wayland/wl_client.cpp
+++ src/server/frontend_wayland/wl_client.cpp
@@ -17,6 +17,7 @@
  */
 
 #include "wl_client.h"
+#include "std_layout_uptr.h"
 
 #include "null_event_sink.h"
 #include "mir/frontend/session_authorizer.h"
@@ -50,7 +51,7 @@ struct ConstructionCtx
     std::shared_ptr<msh::Shell> const shell;
     std::shared_ptr<mf::SessionAuthorizer> const session_authorizer;
     /// Needs to be a pointer so std::is_standard_layout passes
-    std::unique_ptr<std::function<void(mf::WlClient&)>> const client_created_callback;
+    mir::StdLayoutUPtr<std::function<void(mf::WlClient&)>> const client_created_callback;
 };
 
 static_assert(
@@ -72,7 +73,7 @@ struct ClientCtx
         return ctx;
     }
 
-    std::unique_ptr<mf::WlClient> const client;
+    mir::StdLayoutUPtr<mf::WlClient> const client;
     wl_listener destroy_listener;
 };
 
