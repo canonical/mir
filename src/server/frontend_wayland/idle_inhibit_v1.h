460ae6ddf87b15a5a43b01a9b9d83ffda2c03cb0
Grayson Guarino <graysonguarino@gmail.com>
2022-05-12 11:21:56 -0500

    Remove unused include

--- src/server/frontend_wayland/idle_inhibit_v1.h
+++ src/server/frontend_wayland/idle_inhibit_v1.h
@@ -18,7 +18,6 @@
 #define MIR_FRONTEND_IDLE_INHIBIT_V1_H_
 
 #include "idle-inhibit-unstable-v1_wrapper.h"
-#include "mir/scene/surface_observer.h"
 #include "mir/scene/idle_hub.h"
 
 #include <memory>
