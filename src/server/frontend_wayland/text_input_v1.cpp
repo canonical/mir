612c2f2b5ef538b7101eb6db32b448e75fe8c663
Grayson Guarino <graysonguarino@gmail.com>
2022-05-16 11:12:48 -0500

    Begin implementing frontend

--- src/server/frontend_wayland/text_input_v1.cpp
+++ src/server/frontend_wayland/text_input_v1.cpp
@@ -15,3 +15,301 @@
  */
 
 #include "text_input_v1.h"
+
+//#include "wl_seat.h" - TODO - Remove??
+#include "wl_surface.h"
+#include "mir/executor.h"
+#include "mir/scene/text_input_hub.h"
+
+#include <memory>
+#include <boost/throw_exception.hpp>
+#include <deque>
+#include <utility>
+
+namespace mf = mir::frontend;
+namespace mw = mir::wayland;
+namespace ms = mir::scene;
+
+auto wayland_to_mir_content_hint(uint32_t hint) -> ms::TextInputContentHint
+{
+#define WAYLAND_TO_MIR_HINT(wl_name, mir_name) \
+    (hint & mw::TextInputV1::ContentHint::wl_name ? \
+        ms::TextInputContentHint::mir_name : \
+        ms::TextInputContentHint::none)
+
+    return
+        WAYLAND_TO_MIR_HINT(auto_completion, completion) |
+        WAYLAND_TO_MIR_HINT(auto_correction, spellcheck) |
+        WAYLAND_TO_MIR_HINT(auto_capitalization, auto_capitalization) |
+        WAYLAND_TO_MIR_HINT(lowercase, lowercase) |
+        WAYLAND_TO_MIR_HINT(uppercase, uppercase) |
+        WAYLAND_TO_MIR_HINT(titlecase, titlecase) |
+        WAYLAND_TO_MIR_HINT(hidden_text, hidden_text) |
+        WAYLAND_TO_MIR_HINT(sensitive_data, sensitive_data) |
+        WAYLAND_TO_MIR_HINT(latin, latin) |
+        WAYLAND_TO_MIR_HINT(multiline, multiline);
+
+#undef WAYLAND_TO_MIR_HINT
+}
+
+auto wayland_to_mir_content_purpose(uint32_t purpose) -> ms::TextInputContentPurpose
+{
+    switch (purpose)
+    {
+        case mw::TextInputV1::ContentPurpose::normal:
+            return ms::TextInputContentPurpose::normal;
+
+        case mw::TextInputV1::ContentPurpose::alpha:
+            return ms::TextInputContentPurpose::alpha;
+
+        case mw::TextInputV1::ContentPurpose::digits:
+            return ms::TextInputContentPurpose::digits;
+
+        case mw::TextInputV1::ContentPurpose::number:
+            return ms::TextInputContentPurpose::number;
+
+        case mw::TextInputV1::ContentPurpose::phone:
+            return ms::TextInputContentPurpose::phone;
+
+        case mw::TextInputV1::ContentPurpose::url:
+            return ms::TextInputContentPurpose::url;
+
+        case mw::TextInputV1::ContentPurpose::email:
+            return ms::TextInputContentPurpose::email;
+
+        case mw::TextInputV1::ContentPurpose::name:
+            return ms::TextInputContentPurpose::name;
+
+        case mw::TextInputV1::ContentPurpose::password:
+            return ms::TextInputContentPurpose::password;
+
+        case mw::TextInputV1::ContentPurpose::date:
+            return ms::TextInputContentPurpose::date;
+
+        case mw::TextInputV1::ContentPurpose::time:
+            return ms::TextInputContentPurpose::time;
+
+        case mw::TextInputV1::ContentPurpose::datetime:
+            return ms::TextInputContentPurpose::datetime;
+
+        case mw::TextInputV1::ContentPurpose::terminal:
+            return ms::TextInputContentPurpose::terminal;
+
+        default:
+            BOOST_THROW_EXCEPTION(std::invalid_argument{"Invalid text content purpose " + std::to_string(purpose)});
+    }
+}
+
+struct TextInputV1Ctx
+{
+    std::shared_ptr<mir::Executor> const wayland_executor;
+    std::shared_ptr<ms::TextInputHub> const text_input_hub;
+};
+
+class TextInputManagerV1Global
+    : public mw::TextInputManagerV1::Global
+{
+public:
+    TextInputManagerV1Global(wl_display* display, std::shared_ptr<TextInputV1Ctx> ctx);
+
+private:
+    void bind(wl_resource* new_resource) override;
+
+    std::shared_ptr<TextInputV1Ctx> const ctx;
+};
+
+class TextInputManagerV1
+    : public mw::TextInputManagerV1
+{
+public:
+    TextInputManagerV1(wl_resource* resource, std::shared_ptr<TextInputV1Ctx> ctx);
+
+private:
+    void create_text_input(wl_resource* id) override;
+
+    std::shared_ptr<TextInputV1Ctx> const ctx;
+};
+
+class TextInputV1
+    : public mw::TextInputV1
+{
+public:
+    TextInputV1(wl_resource* resource, std::shared_ptr<TextInputV1Ctx>  ctx);
+    ~TextInputV1();
+
+private:
+    struct Handler : ms::TextInputChangeHandler
+    {
+        Handler(TextInputV1* const text_input, std::shared_ptr<mir::Executor> const wayland_executor)
+            : text_input{text_input},
+              wayland_executor{wayland_executor}
+        {
+        }
+
+        void text_changed(ms::TextInputChange const& change) override
+        {
+            wayland_executor->spawn([text_input=text_input, change]()
+                {
+                    if (text_input)
+                    {
+                        text_input.value().send_text_change(change);
+                    }
+                });
+        }
+
+        mw::Weak<TextInputV1> const text_input;
+        std::shared_ptr<mir::Executor> const wayland_executor;
+    };
+
+    static size_t constexpr max_remembered_serials{10};
+
+    std::shared_ptr<TextInputV1Ctx> const ctx;
+//    mf::WlSeat& seat;
+    std::shared_ptr<Handler> const handler;
+    mw::Weak<mf::WlSurface> current_surface;
+    /// Set to true if and only if the text input has been enabled since the last commit
+    bool on_new_input_field{false};
+    /// nullopt if the state is inactive, otherwise holds the pending and/or committed state
+    std::optional<ms::TextInputState> pending_state;
+    /// The first value is the number of commits we had received when a state was submitted to the text input hub. The
+    /// second value is the serial the hub gave us for that state. When we get a change from the input method we match
+    /// it's state serial to the corresponding commit count, which is the serial we send to the client. We only remember
+    /// a small number of serials.
+    std::deque<std::pair<uint32_t, ms::TextInputStateSerial>> state_serials;
+    /// The number of commits we've received
+    uint32_t commit_count{0};
+
+    /// Sends the text change to the client if possible
+    void send_text_change(ms::TextInputChange const& change);
+
+    /// Returns the client serial (aka the commit count) that corresponds to the given state serial
+    auto find_client_serial(ms::TextInputStateSerial state_serial) const -> std::optional<uint32_t>;
+
+//    /// From WlSeat::FocusListener -- TODO - remove?
+//    void focus_on(mf::WlSurface* surface) override;
+
+    /// From wayland::TextInputV1
+    /// @{
+    void activate(wl_resource* seat, wl_resource* surface) override;
+    void deactivate(wl_resource* seat) override;
+    void show_input_panel() override;
+    void hide_input_panel() override;
+    void reset() override;
+    void set_surrounding_text(std::string const& text, uint32_t cursor, uint32_t anchor) override;
+    void set_content_type(uint32_t hint, uint32_t purpose) override;
+    void set_cursor_rectangle(int32_t x, int32_t y, int32_t width, int32_t height) override;
+    void set_preferred_language(std::string const& language) override;
+    void commit_state(uint32_t serial) override;
+    void invoke_action(uint32_t button, uint32_t index) override;
+    /// @}
+};
+
+auto mf::create_text_input_manager_v1(
+    wl_display *display,
+    std::shared_ptr<Executor> const& wayland_executor,
+    std::shared_ptr<scene::TextInputHub> const& text_input_hub)
+-> std::shared_ptr<wayland::TextInputManagerV1::Global>
+{
+    auto ctx = std::make_shared<TextInputV1Ctx>(TextInputV1Ctx{wayland_executor, text_input_hub});
+    return std::make_shared<TextInputManagerV1Global>(display, std::move(ctx));
+}
+
+TextInputManagerV1Global::TextInputManagerV1Global(
+    wl_display* display,
+    std::shared_ptr<TextInputV1Ctx> ctx)
+    : Global{display, Version<1>()},
+      ctx{std::move(ctx)}
+{
+}
+
+void TextInputManagerV1Global::bind(wl_resource* new_resource)
+{
+    new TextInputManagerV1{new_resource, ctx};
+}
+
+TextInputManagerV1::TextInputManagerV1(wl_resource *resource, std::shared_ptr<TextInputV1Ctx> ctx)
+    : mw::TextInputManagerV1{resource, Version<1>()},
+      ctx{std::move(ctx)}
+{
+
+}
+
+void TextInputManagerV1::create_text_input(wl_resource *id)
+{
+    new TextInputV1{id, ctx};
+}
+
+TextInputV1::TextInputV1(
+    wl_resource *resource,
+    std::shared_ptr<TextInputV1Ctx> ctx)
+    : mw::TextInputV1{resource, Version<1>()},
+      ctx{std::move(ctx)}
+{
+
+}
+
+void TextInputV1::send_text_change(const ms::TextInputChange &change)
+{
+
+}
+
+auto TextInputV1::find_client_serial(ms::TextInputStateSerial state_serial) const -> std::optional<uint32_t>
+{
+    // TODO
+    return std::optional<uint32_t>();
+}
+
+void TextInputV1::activate(wl_resource *seat, wl_resource *surface)
+{
+    // TODO
+}
+
+void TextInputV1::deactivate(wl_resource *seat)
+{
+    // TODO
+}
+
+void TextInputV1::show_input_panel()
+{
+    // TODO
+}
+
+void TextInputV1::hide_input_panel()
+{
+    // TODO
+}
+
+void TextInputV1::reset()
+{
+    // TODO
+}
+
+void TextInputV1::set_surrounding_text(const std::string &text, uint32_t cursor, uint32_t anchor)
+{
+    // TODO
+}
+
+void TextInputV1::set_content_type(uint32_t hint, uint32_t purpose)
+{
+    // TODO
+}
+
+void TextInputV1::set_cursor_rectangle(int32_t x, int32_t y, int32_t width, int32_t height)
+{
+    // TODO
+}
+
+void TextInputV1::set_preferred_language(const std::string &language)
+{
+    // TODO
+}
+
+void TextInputV1::commit_state(uint32_t serial)
+{
+    // TODO
+}
+
+void TextInputV1::invoke_action(uint32_t button, uint32_t index)
+{
+    // TODO
+}
--- src/server/frontend_wayland/text_input_v1.h
+++ src/server/frontend_wayland/text_input_v1.h
@@ -14,7 +14,28 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef MIR_TEXT_INPUT_V1_H
-#define MIR_TEXT_INPUT_V1_H
+#ifndef MIR_FRONTEND_TEXT_INPUT_V1_H
+#define MIR_FRONTEND_TEXT_INPUT_V1_H
 
-#endif //MIR_TEXT_INPUT_V1_H
+#include "text-input-unstable-v1_wrapper.h"
+
+#include <memory>
+
+namespace mir
+{
+class Executor;
+namespace scene
+{
+class TextInputHub;
+}
+namespace frontend
+{
+auto create_text_input_manager_v1(
+    wl_display* display,
+    std::shared_ptr<Executor> const& wayland_executor,
+    std::shared_ptr<scene::TextInputHub> const& text_input_hub)
+-> std::shared_ptr<wayland::TextInputManagerV1::Global>;
+}
+}
+
+#endif //MIR_FRONTEND_TEXT_INPUT_V1_H
