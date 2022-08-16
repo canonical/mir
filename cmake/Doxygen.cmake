048470f6a90bd7198d1b534a4693a935a03dd81e
Alan Griffiths <alan@octopull.co.uk>
2022-08-16 12:59:27 +0100

    Don't rely on shell expansion (that doesn't happen)

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -27,7 +27,7 @@ if(DOXYGEN_FOUND)
     set(ALL "ALL")
   endif()
   add_custom_target(doc ${ALL}
-                    COMMAND rm -rf ${CMAKE_BINARY_DIR}/doc/{html,xml}
+                    COMMAND rm -rf ${CMAKE_BINARY_DIR}/doc/html ${CMAKE_BINARY_DIR}/doc/xml
                     COMMAND ${DOXYGEN_EXECUTABLE} ${PROJECT_BINARY_DIR}/Doxyfile
                     SOURCES ${PROJECT_BINARY_DIR}/Doxyfile
                     DEPENDS guides)
