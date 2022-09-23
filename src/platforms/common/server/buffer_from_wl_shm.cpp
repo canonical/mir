e771f2e9cbe6c79b95dcf43c3db3396d900340ea
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2022-09-23 16:34:29 +1000

    platform/buffer_from_wl_shm: Relax SIGBUS handling
    Rather than require that protected access to a memory region occur on
    a specific thread, and only one region at a time, support protected
    access across arbitrary threads and an arbitrary number of regions.

--- src/platforms/common/server/buffer_from_wl_shm.cpp
+++ src/platforms/common/server/buffer_from_wl_shm.cpp
@@ -20,6 +20,7 @@
 #include "mir/graphics/egl_context_executor.h"
 #include "mir/renderer/sw/pixel_source.h"
 #include "mir/executor.h"
+#include "mir/synchronised.h"
 #include <sys/mman.h>
 
 #define MIR_LOG_COMPONENT "wayland-gfx-helpers"
@@ -79,32 +80,15 @@ MirPixelFormat wl_format_to_mir_format(uint32_t format)
 class ShmBufferSIGBUSHandler
 {
 public:
-    ShmBufferSIGBUSHandler()
-    {
-        struct sigaction sig_handler_desc;
-        sigfillset(&sig_handler_desc.sa_mask);
-        sig_handler_desc.sa_flags = SA_SIGINFO;
-        sig_handler_desc.sa_sigaction = &sigbus_handler;
-        auto old_handler = new struct sigaction;
-
-        if (sigaction(SIGBUS, &sig_handler_desc, old_handler))
-        {
-            using namespace std::string_literals;
-            delete old_handler;
-            BOOST_THROW_EXCEPTION((
-                std::system_error{
-                    errno,
-                    std::system_category(),
-                    "Failed to install SIGBUS handler for Wayland SHM"
-                }));
-        }
-        previous_handler = old_handler;
-    }
+    ShmBufferSIGBUSHandler() = default;
  
     ~ShmBufferSIGBUSHandler()
     {
-        sigaction(SIGBUS, previous_handler.load(), nullptr);
-        delete previous_handler;
+        if (previous_handler.load())
+        {
+            sigaction(SIGBUS, previous_handler.load(), nullptr);
+            delete previous_handler;
+        }
     }
  
     class AccessProtector
@@ -173,33 +157,100 @@ public:
      *
      * \returns A handle representing this memory access guard. As long as the guard is
      *          live, accesses within the protected range are safe.
-     * \note    The returned value is THREAD LOCAL. It must not be passed across threads.
      */
     auto static protect_access_to(void* addr, size_t len) -> std::shared_ptr<AccessProtector>
     {
         auto protector = std::shared_ptr<AccessProtector>{new AccessProtector{addr, len}};
-        current_access = protector;
+        auto locked_list = current_access.lock();
+        install_sigbus_handler();
+        locked_list->push_back(protector);
         return protector;
     }
 
 private:
     friend class AccessProtector;
+
+    static void install_sigbus_handler()
+    {
+        struct sigaction sig_handler_desc;
+        sigfillset(&sig_handler_desc.sa_mask);
+        sig_handler_desc.sa_flags = SA_SIGINFO;
+        sig_handler_desc.sa_sigaction = &sigbus_handler;
+        auto old_handler = new struct sigaction;
+
+        /* Note: while the documentation for sigaction doesn't explicitly
+         * describe it as threadsafe, it's not marked as thread*un*safe,
+         * as many things are, and it's explicitly called out that
+         * concurrent sigaction/sigwait invokes undefined behaviour.
+         *
+         * Assume it's threadsafe.
+         */
+        if (sigaction(SIGBUS, &sig_handler_desc, old_handler))
+        {
+            using namespace std::string_literals;
+            delete old_handler;
+            BOOST_THROW_EXCEPTION((
+                std::system_error{
+                    errno,
+                    std::system_category(),
+                    "Failed to install SIGBUS handler for Wayland SHM"
+                }));
+        }
+        if (old_handler->sa_sigaction != &sigbus_handler)
+        {
+            // Because we install the handler every time, we only want
+            // to save the old handler when it's not ours!
+            auto to_delete = previous_handler.exchange(old_handler);
+            delete to_delete;
+        }
+    }
+
     static void sigbus_handler(int sig, siginfo_t* info, void* ucontext)
     {
-        if (auto protector = current_access.load().lock())
+        if (info->si_code == BUS_ADRERR)
         {
-            if (protector->within_protected_region(info->si_addr) &&
-                protector->provide_fallback_mapping())
+            /* The kernel guarantees that si_code >= 0 can only be sent from
+             * the kernel, and BUS_ADRERR is > 0.
+             *
+             * We've been invoked by the kernel, and we've been invoked for an
+             * invalid address access - this means that we're in *synchronous*
+             * signal context: we're interrupting the thread that is performing
+             * the read or write to memory *during* that read or write.
+             *
+             * That means we *can't* be interrupting an async-signal-unsafe
+             * function. (Under the fairly reasonable assumption that we're
+             * not doing something absolutely bonkers, like trying to store
+             * pthread mutexes in a file-backed mmap()ed region).
+             *
+             * *That* means that we have access to sensible concurrency primitives!
+             * *And* the memory allocator!
+             * What bliss!
+             *
+             * So, even though this is a signal handler, we can use normal
+             * code.
+             */
+            auto locked_list = current_access.lock();
+            for (auto& weak_protector : *locked_list)
             {
-                // We've replaced the client-provided mapping with one that will
-                // not fault; it is now safe to continue.
-                return;
+                if (auto protector = weak_protector.lock())
+                {
+                    if (protector->within_protected_region(info->si_addr) &&
+                        protector->provide_fallback_mapping())
+                    {
+                        // We've replaced the client-provided mapping with one that will
+                        // not fault; it is now safe to continue.
+                        return;
+                    }
+                }
             }
         }
 
-        // We're not protecting access on this thread, the access is not in our
-        // protected range, or the fallback mapping failed. We cannot save you now.
-        // Call the previous SIGBUS handler.
+        /* The SIGBUS is not a physical address error, the access is not in our
+         * protected range, or the fallback mapping failed.
+         *
+         * We cannot save you now.
+         * Call the previous SIGBUS handler.
+         */
         sigaction(SIGBUS, previous_handler.load(), nullptr);
         if (previous_handler.load()->sa_flags & SA_SIGINFO)
         {
@@ -210,12 +261,12 @@ private:
             (previous_handler.load()->sa_handler)(sig);
         }
     }
-    static thread_local std::atomic<std::weak_ptr<AccessProtector>> current_access;
+    static mir::Synchronised<std::vector<std::weak_ptr<AccessProtector>>> current_access;
     static std::atomic<struct sigaction*> previous_handler;
 };
 std::weak_ptr<ShmBufferSIGBUSHandler> sigbus_handler;
 std::atomic<struct sigaction*> ShmBufferSIGBUSHandler::previous_handler;
-thread_local std::atomic<std::weak_ptr<ShmBufferSIGBUSHandler::AccessProtector>>
+mir::Synchronised<std::vector<std::weak_ptr<ShmBufferSIGBUSHandler::AccessProtector>>>
     ShmBufferSIGBUSHandler::current_access;
 
 /**
