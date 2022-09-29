5dcf24eba39acdf93a232238fa1b99c035e7a674
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2022-09-29 16:58:58 +1000

    wayland: Add start of ShmBacking implementation.
    This will let us implement `wl_shm` and `wl_shm_pool` ourselves, which will
    dramatically simplify our lifetime management of SHM buffers.

---
+++ src/server/frontend_wayland/shm_backing.h
@@ -0,0 +1,57 @@
+#include "mir/fd.h"
+#include "mir/renderer/sw/pixel_source.h"
+
+#include <span>
+#include <sys/mman.h>
+
+namespace mir
+{
+template<typename T>
+class Mapping
+{
+public:
+    virtual ~Mapping() = default;
+
+    virtual T* data() = 0;
+    virtual size_t len() const = 0;
+};
+
+class ReadMappableRange
+{
+public:
+    virtual ~ReadMappableRange() = default;
+
+    virtual auto map_ro() -> std::unique_ptr<Mapping<unsigned char const>> = 0;
+};
+
+class WriteMappableRange
+{
+public:
+    virtual ~WriteMappableRange() = default;
+
+    virtual auto map_wo() -> std::unique_ptr<Mapping<unsigned char>> = 0;
+};
+
+class RWMappableRange : public ReadMappableRange, public WriteMappableRange
+{
+public:
+    virtual ~RWMappableRange() = default;
+
+    virtual auto map_rw() -> std::unique_ptr<Mapping<unsigned char>> = 0;
+};
+
+class RWShmBacking
+{
+public:
+    RWShmBacking(mir::Fd const& backing_store, size_t claimed_size);
+
+    static auto get_rw_range(std::shared_ptr<RWShmBacking> pool, size_t start, size_t len)
+        -> std::unique_ptr<RWMappableRange>;
+
+private:
+
+    void* mapped_address;
+    [[maybe_unused]]size_t size;
+    bool size_is_trustworthy{false};
+};
+}
