687f0d5241639316f6c48e7b48e7c25a7c44610f
Alan Griffiths <alan@octopull.co.uk>
2023-01-12 14:03:00 +0000

    Fix MediatingDisplayChanger::set_power_mode()

--- src/server/scene/mediating_display_changer.cpp
+++ src/server/scene/mediating_display_changer.cpp
@@ -318,6 +318,16 @@ void ms::MediatingDisplayChanger::configure_for_hardware_change(
             auto existing_configuration = base_configuration_;
 
             display_configuration_policy->apply_to(*conf);
+            {
+                std::lock_guard lock{power_mode_mutex};
+                conf->for_each_output([this](mg::UserDisplayConfigurationOutput &output)
+                    {
+                        if (output.used)
+                        {
+                            output.power_mode = power_mode;
+                        }
+                    });
+            }
             base_configuration_ = conf;
             if (base_configuration_applied)
             {
@@ -516,20 +526,27 @@ void ms::MediatingDisplayChanger::set_base_configuration(std::shared_ptr<mg::Dis
 
 void ms::MediatingDisplayChanger::set_power_mode(MirPowerMode new_power_mode)
 {
-    std::lock_guard lock{power_mode_mutex};
-    if (new_power_mode == power_mode)
     {
-        return;
-    }
-    power_mode = new_power_mode;
-    std::shared_ptr<graphics::DisplayConfiguration> const config = display->configuration();
-    config->for_each_output([&](mg::UserDisplayConfigurationOutput& output)
+        std::lock_guard lock{power_mode_mutex};
+        if (new_power_mode == power_mode)
         {
-            if (output.used)
-            {
-                output.power_mode = new_power_mode;
-            }
-        });
-    apply_config(config);
+            return;
+        }
+        power_mode = new_power_mode;
+    }
+    decltype(base_configuration_) const config = [this]
+        {
+            std::lock_guard lg{configuration_mutex};
+            return base_configuration_->clone();
+        }();
+
+    config->for_each_output([&](mg::UserDisplayConfigurationOutput &output)
+    {
+        if (output.used)
+        {
+            output.power_mode = new_power_mode;
+        }
+    });
+    configure_for_hardware_change(config);
 }
 
