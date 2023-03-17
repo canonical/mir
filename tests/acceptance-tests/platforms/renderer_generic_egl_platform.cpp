240bc3d68e1273c501283977c924011f3793b22a
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2023-03-17 17:27:25 +1100

    tests: Add renderer-generic-egl RenderingPlatformTest.
    This is not *super* useful, but it turns out that some versions of GTest will helpfully produce a
    test failure if you have a parametrised test that has no instantiation, and on non-Ubuntu builds
    we don't build eglstream-kms (the only non-dispmanx platform that has a RenderingPlatformTest).

---
+++ tests/acceptance-tests/platforms/renderer_generic_egl_platform.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright © Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 or 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "test_rendering_platform.h"
+
+#include "mir_test_framework/executable_path.h"
+
+namespace mtf = mir_test_framework;
+
+namespace
+{
+class RenderingGenericEGLEnv : public mir::test::PlatformTestHarness
+{
+public:
+    void setup() override
+    {
+    }
+
+    void teardown() override
+    {
+    }
+
+    auto platform_module() -> std::shared_ptr<mir::SharedLibrary> override
+    {
+        return std::make_shared<mir::SharedLibrary>(mtf::server_platform("renderer-egl-generic"));
+    }
+};
+
+RenderingGenericEGLEnv platform_harness;
+}
+
+INSTANTIATE_TEST_SUITE_P(RenderingGenericEGL, RenderingPlatformTest, testing::Values(&platform_harness));
+
