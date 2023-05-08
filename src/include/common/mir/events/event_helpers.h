56dcd0f9e69b3b20ec604b3942345e136a05a38c
Sophie Winter <git@phie.me>
2023-05-08 06:15:31 -0700

    Update

--- src/include/common/mir/events/event_helpers.h
+++ src/include/common/mir/events/event_helpers.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 Canonical Ltd.
+ * Copyright © 2023 Canonical Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version 2 or 3,
