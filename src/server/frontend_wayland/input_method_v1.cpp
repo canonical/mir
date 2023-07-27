b62c5314a849a7b4f641f09b35d330331a785800
Matthew Kosarek <matthew@matthewkosarek.xyz>
2023-07-27 18:19:19 -0400

    Proper shell upon activation, but running into a weird snag with linking

--- src/server/frontend_wayland/input_method_v1.cpp
+++ src/server/frontend_wayland/input_method_v1.cpp
@@ -20,6 +20,7 @@
 namespace mf = mir::frontend;
 namespace ms = mir::scene;
 
+/// Handles activation and deactivation of the InputMethodContextV1
 class mf::InputMethodV1::Instance : wayland::InputMethodV1
 {
 public:
@@ -41,12 +42,18 @@ public:
         bool /*new_input_field*/,
         scene::TextInputState const& /*state*/)
     {
-        //input_method->send_activate_event(input_method->resource);
+        context = std::make_unique<InputMethodContextV1>(*static_cast<wayland::InputMethodV1*>(this));
+        send_activate_event(context->resource);
     }
 
     void deactivated()
     {
-        //input_method->send_deactivate_event(input_method->resource);
+        if (context)
+        {
+            auto resource = context->resource;
+            context = nullptr;
+            send_deactivate_event(resource);
+        }
     }
 
 private:
@@ -73,8 +80,85 @@ private:
         Instance* const input_method;
     };
 
+    class InputMethodContextV1 : public wayland::InputMethodContextV1
+    {
+    public:
+        InputMethodContextV1(const wayland::InputMethodV1& method)
+            : wayland::InputMethodContextV1(method)
+        {
+        }
+
+    private:
+        void commit_string(uint32_t /*serial*/, const std::string &/*text*/) override
+        {
+
+        }
+
+        void preedit_string(uint32_t /*serial*/, const std::string &/*text*/, const std::string &/*commit*/) override
+        {
+
+        }
+
+        void preedit_styling(uint32_t /*index*/, uint32_t /*length*/, uint32_t /*style*/) override
+        {
+
+        }
+
+        void preedit_cursor(int32_t /*index*/) override
+        {
+
+        }
+
+        void delete_surrounding_text(int32_t /*index*/, uint32_t /*length*/) override
+        {
+
+        }
+
+        void cursor_position(int32_t /*index*/, int32_t /*anchor*/) override
+        {
+
+        }
+
+        void modifiers_map(struct wl_array */*map*/) override
+        {
+
+        }
+
+        void keysym(uint32_t /*serial*/, uint32_t /*time*/, uint32_t /*sym*/, uint32_t /*state*/, uint32_t /*modifiers*/) override
+        {
+
+        }
+
+        void grab_keyboard(struct wl_resource */*keyboard*/) override
+        {
+
+        }
+
+        void key(uint32_t /*serial*/, uint32_t /*time*/, uint32_t /*key*/, uint32_t /*state*/) override
+        {
+
+        }
+
+        void modifiers(uint32_t /*serial*/, uint32_t /*mods_depressed*/, uint32_t /*mods_latched*/, uint32_t /*mods_locked*/,
+            uint32_t /*group*/) override
+        {
+
+        }
+
+        void language(uint32_t /*serial*/, const std::string &/*language*/) override
+        {
+
+        }
+
+        void text_direction(uint32_t /*serial*/, uint32_t /*direction*/) override
+        {
+
+        }
+    };
+
     mf::InputMethodV1* method;
     std::shared_ptr<StateObserver> const state_observer;
+    std::unique_ptr<InputMethodContextV1> context = nullptr;
 };
 
 mf::InputMethodV1::InputMethodV1(
