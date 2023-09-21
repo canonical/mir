49dc39d528ec9b83f54e5e059e6fa8fc48bc3912
Matthew Kosarek <matthew@matthewkosarek.xyz>
2023-09-21 16:58:25 -0400

    (#2509) Handling integer overflows for dimensions for XWaylandSurface

--- src/server/frontend_xwayland/xwayland_surface.cpp
+++ src/server/frontend_xwayland/xwayland_surface.cpp
@@ -1223,8 +1223,12 @@ void mf::XWaylandSurface::prep_surface_spec(ProofOfMutexLock const&, msh::Surfac
 
 #define SCALE_SIZE(type, prop) \
     if (mods.prop) \
-    { \
-        mods.prop = std::max(geom::type{1}, mods.prop.value() * inv_scale); \
+    {                          \
+        auto scaled_value = mods.prop.value() * inv_scale; \
+        if (scaled_value.as_int() >= 0)            \
+        {                      \
+            mods.prop = scaled_value; \
+        } \
     }
 
     SCALE_SIZE(Width, width);
