743c278425d51eff692df7790e7e7edf38a9637d
Alan Griffiths <alan@octopull.co.uk>
2023-10-17 09:52:14 +0100

    Missing header comment and header guards

--- src/platforms/wayland/wl_egl_display_provider.h
+++ src/platforms/wayland/wl_egl_display_provider.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright © Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 2 or 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MIR_PLATFORM_WAYLAND_DISPLAY_PROVIDER_H_
+#define MIR_PLATFORM_WAYLAND_DISPLAY_PROVIDER_H_
 
 #include "mir/graphics/platform.h"
 
@@ -53,3 +71,5 @@ private:
     std::shared_ptr<EGLDisplayProvider> const egl_provider;
 };
 }
+
+#endif // MIR_PLATFORM_WAYLAND_DISPLAY_PROVIDER_H_
