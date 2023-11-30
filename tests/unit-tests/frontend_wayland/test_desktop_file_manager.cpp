74df7fe47f20cf0b5ee638365e907d6ef59a8099
Matthew Kosarek <matthew@matthewkosarek.xyz>
2023-11-30 14:45:07 -0500

    fix: wrong licensing

--- tests/unit-tests/frontend_wayland/test_desktop_file_manager.cpp
+++ tests/unit-tests/frontend_wayland/test_desktop_file_manager.cpp
@@ -1,25 +1,23 @@
 /*
  * Copyright © Canonical Ltd.
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License version 2 or 3,
- * as published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 or 3 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "src/server/frontend_wayland/desktop_file_manager.h"
-#include "mir/scene/observer.h"
 #include "mir/test/doubles/mock_surface.h"
 #include "mir/test/doubles/mock_scene_session.h"
 #include "mir/scene/surface_observer.h"
-#include "mir/test/doubles/mock_frontend_surface_stack.h"
 #include "mir/test/doubles/explicit_executor.h"
 #include "mir/test/fake_shared.h"
 #include <gmock/gmock.h>
