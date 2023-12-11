50ee3ae655b9eb9d3cf7d0299fd484d6bee324b8
Matthew Kosarek <matthew@matthewkosarek.xyz>
2023-12-11 14:33:57 -0500

    feedback: comments that agree with code and naming

--- src/miral/basic_window_manager.cpp
+++ src/miral/basic_window_manager.cpp
@@ -2848,11 +2848,15 @@ void miral::BasicWindowManager::update_application_zones_and_attached_windows()
     {
         Rectangle zone_rect = area->area;
 
-        /// The first pass will modify the application zone as it goes
-        /// The second pass will use the final application zone
+        // The placement algorithm operates on windows spanning the width of the screen first,
+        // followed by any other "attached" window (e.g. a window attached to the side of the screen
+        // vertically), and finally maximized windows. This ensures that windows attached to the
+        // top and bottom of the screen (in the "full_width_attached_windows" list) push windows
+        // attached to the sides of the screen or elsewhere (in the "other_attached_windows") out
+        // of the way. Maximized windows are then guaranteed to be on top, as they appear last.
         std::vector<WindowInfo*> full_width_attached_windows;
         std::vector<WindowInfo*> other_attached_windows;
-        std::vector<WindowInfo*> second_pass;
+        std::vector<WindowInfo*> maximized_windows;
 
         for (auto const& window : area->attached_windows)
         {
@@ -2869,8 +2873,8 @@ void miral::BasicWindowManager::update_application_zones_and_attached_windows()
                         // force everyone else out of the way when they're placed. By "east-to-west", we are
                         // referring to "top" and "bottom" panels, as opposed to "left" and "right" panels.
                         MirPlacementGravity  edges = info.attached_edges();
-                        bool isFullWidth = (edges & mir_placement_gravity_east) && (edges & mir_placement_gravity_west);
-                        if (isFullWidth)
+                        bool is_full_width = (edges & mir_placement_gravity_east) && (edges & mir_placement_gravity_west);
+                        if (is_full_width)
                             full_width_attached_windows.push_back(&info);
                         else
                             other_attached_windows.push_back(&info);
@@ -2880,7 +2884,7 @@ void miral::BasicWindowManager::update_application_zones_and_attached_windows()
                 case mir_window_state_maximized:
                 case mir_window_state_horizmaximized:
                 case mir_window_state_vertmaximized:
-                    second_pass.push_back(&info);
+                    maximized_windows.push_back(&info);
                     break;
 
                 default:
@@ -2914,7 +2918,7 @@ void miral::BasicWindowManager::update_application_zones_and_attached_windows()
             zone_rect = apply_exclusive_rect_to_application_zone(zone_rect, exclusive_rect, info.attached_edges());
         }
 
-        for (auto info_ptr : second_pass)
+        for (auto info_ptr : maximized_windows)
         {
             place_attached_to_zone(*info_ptr, zone_rect);
         }
