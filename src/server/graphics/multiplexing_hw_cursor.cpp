b8f9d016d59abd52d4a0fe5c82c000f039628b0c
Alan Griffiths <alan@octopull.co.uk>
2024-02-16 10:49:12 +0000

    Merge pull request #3206 from canonical/fix-hw-cursor
    graphics/MultiplexingDisplay: Add HW cursor support.

---
+++ src/server/graphics/multiplexing_hw_cursor.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright © Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 or 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "multiplexing_hw_cursor.h"
+
+#include "mir/graphics/display.h"
+#include <boost/throw_exception.hpp>
+
+namespace mg = mir::graphics;
+
+namespace
+{
+auto construct_platform_cursors(std::span<mg::Display*> platform_displays) -> std::vector<std::shared_ptr<mg::Cursor>>
+{
+    std::vector<std::shared_ptr<mg::Cursor>> cursors;
+    for (auto display : platform_displays)
+    {
+        cursors.push_back(display->create_hardware_cursor());
+        if (cursors.back() == nullptr)
+        {
+            BOOST_THROW_EXCEPTION((std::runtime_error{"Platform failed to create hardware cursor"}));
+        }
+    }
+    return cursors;
+}
+}
+
+mg::MultiplexingCursor::MultiplexingCursor(std::span<Display*> platform_displays)
+    : platform_cursors{construct_platform_cursors(platform_displays)}
+{
+}
+
+void mg::MultiplexingCursor::show(CursorImage const& image)
+{
+    for (auto& cursor : platform_cursors)
+    {
+        cursor->show(image);
+    }
+}
+
+void mg::MultiplexingCursor::hide()
+{
+    for (auto& cursor: platform_cursors)
+    {
+        cursor->hide();
+    }
+}
+
+void mg::MultiplexingCursor::move_to(geometry::Point position)
+{
+    for (auto& cursor : platform_cursors)
+    {
+        cursor->move_to(position);
+    }
+}
