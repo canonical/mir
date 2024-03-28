c4d1917eec1a74a93827ae815d75338fa2dc209c
Matthew Kosarek <matthew@matthewkosarek.xyz>
2024-03-28 09:17:57 -0400

    WIP

--- tools/symbols_map_generator.py
+++ tools/symbols_map_generator.py
@@ -23,6 +23,7 @@ import glob
 import subprocess
 from pathlib import Path
 from typing import Literal, TypedDict
+import clang.cindex
 
 LibraryName = Literal[
     "miral", "common", "core", "miroil", "platform",
@@ -53,8 +54,43 @@ library_to_header_map: dict[LibraryName, HeaderDirectories] = {
     }
 }
 
+def to_definition(namespace_stack: list[str], value: str):
+    if (len(namespace_stack) == 0):
+        return value
+    return "::".join(namespace_stack) + "::" + value
+
+def traverse(node: clang.cindex.Cursor, namespace_stack: list[str]):
+    next_namespace_stack = namespace_stack.copy()
+    if node.kind == clang.cindex.CursorKind.CLASS_DECL:
+        next_namespace_stack.append(node.displayname)
+    elif node.kind == clang.cindex.CursorKind.STRUCT_DECL:
+        next_namespace_stack.append(node.displayname)
+    elif node.kind == clang.cindex.CursorKind.NAMESPACE:
+        next_namespace_stack.append(node.displayname)
+    elif (node.kind == clang.cindex.CursorKind.FUNCTION_DECL 
+          or node.kind == clang.cindex.CursorKind.CXX_METHOD):
+        print(node.lexical_parent.displayname)
+        print(to_definition(namespace_stack, node.displayname))
+
+    for child in node.get_children():
+        traverse(child, next_namespace_stack)
+
 def main():
     logging.debug("Symbols map generation is beginning")
+    s = '''
+    #include "x.h" 
+    namespace mir{}
+    struct X{ void get() {} };
+    int fac(int n) {
+        return (n>1) ? n*fac(n-1) : 1;
+    }
+    '''
+
+    # idx = clang.cindex.Index.create()
+    # tu = idx.parse('tmp.cpp', args=['-std=c++20'],  
+    #                 unsaved_files=[('tmp.cpp', s)],  options=0)
+    # root = tu.cursor        
+    # traverse(root, [])
     for library in library_to_header_map:
         directories: HeaderDirectories = library_to_header_map[library]
         include_flags: list[str] = []
@@ -67,16 +103,23 @@ def main():
             path = Path.cwd().parent.joinpath(external_directory)
             files = glob.glob(path.absolute().as_posix() + '/**/*.h', recursive=True)
             for file in files:
-                cmd = [
-                    "clang-18",
-                    "-extract-api",
-                    "-x",
-                    "c++-header",
-                    file,
-                    "-std=c++2b"
-                ] + include_flags
-                print(cmd)
-                subprocess.run(cmd) 
+                s = open(file).read()
+                idx = clang.cindex.Index.create()
+                tu = idx.parse('tmp.cpp', args=['-std=c++20'],  
+                                unsaved_files=[('tmp.cpp', s)],  options=0)
+                root = tu.cursor        
+                traverse(root, [])
+                return
+                # cmd = [
+                #     "clang++-19",
+                #     "-extract-api",
+                #     "-x",
+                #     "c++-header",
+                #     file,
+                #     "-std=c++2b"
+                # ] + include_flags
+                # print(cmd)
+                # subprocess.run(cmd) 
 
     
 
