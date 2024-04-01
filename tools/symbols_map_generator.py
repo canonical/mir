78529ac8656f207e03fbd2dee0fcf5c1e71cb710
Matthew Kosarek <matthew@matthewkosarek.xyz>
2024-04-01 14:37:20 -0400

    Can resolve virtual thunks

--- tools/symbols_map_generator.py
+++ tools/symbols_map_generator.py
@@ -19,7 +19,7 @@ import glob
 import os
 import argparse
 from pathlib import Path
-from typing import Literal, TypedDict, get_args
+from typing import Literal, TypedDict, get_args, Optional
 import clang.cindex
 
 _logger = logging.getLogger(__name__)
@@ -31,9 +31,10 @@ LibraryName = Literal[
 
 class LibraryInfo(TypedDict):
     external_headers: list[str]
-    internal_headers: list[str]
+    internal_headers: Optional[list[str]]
     external_output_file: str
-    internal_output_file: str
+    internal_output_file: Optional[str]
+    search_headers: Optional[list[str]]
 
 
 # Directory paths are relative to the root of the Mir project
@@ -42,11 +43,17 @@ library_to_data_map: dict[LibraryName, LibraryInfo] = {
         "external_headers": [
             "include/miral"
         ],
-        "internal_headers": [
-            "src/miral"
+        "external_output_file": "src/miral/symbols.map",
+    },
+    "mir_server": {
+        "external_headers": [
+            "include/server/mir",
+            "src/include/server/mir"
         ],
-        "external_output_file": "src/miral/symbols_external.map",
-        "internal_output_file": "src/miral/symbols_internal.map"
+        "external_output_file": "src/server/symbols1.map",
+        "search_headers": [
+            "src/include/server"
+        ]
     }
 }
 
@@ -151,7 +158,7 @@ def traverse_ast(node: clang.cindex.Cursor, filename: str, result: set[str]) -> 
           or should_generate_as_class_or_struct(node))
           and node.location.file.name == filename
           and not node.is_pure_virtual_method()):
-        parent = node.lexical_parent
+        parent = node.semantic_parent
         namespace_str = create_symbol_name(node)
 
         # Walk up the tree to build the namespace
@@ -159,8 +166,8 @@ def traverse_ast(node: clang.cindex.Cursor, filename: str, result: set[str]) -> 
             if parent.kind == clang.cindex.CursorKind.TRANSLATION_UNIT:
                 break
 
-            namespace_str = f"{parent.displayname}::{namespace_str}"
-            parent = parent.lexical_parent
+            namespace_str = f"{parent.spelling}::{namespace_str}"
+            parent = parent.semantic_parent
 
         # Classes and structs have a specific outpu
         if (node.kind == clang.cindex.CursorKind.CLASS_DECL
@@ -171,18 +178,61 @@ def traverse_ast(node: clang.cindex.Cursor, filename: str, result: set[str]) -> 
             result.add(f"{namespace_str};")
             
             # Check if we're marked virtual
+            is_virtual = False
             if ((node.kind == clang.cindex.CursorKind.CXX_METHOD
                 or node.kind == clang.cindex.CursorKind.DESTRUCTOR
                 or node.kind == clang.cindex.CursorKind.CONSTRUCTOR
                 or node.kind == clang.cindex.CursorKind.CONVERSION_FUNCTION)
                 and node.is_virtual_method()):
                 result.add(f"non-virtual?thunk?to?{namespace_str};")
+                is_virtual = True
             else:
                 # Check if we're marked override
                 for  child in node.get_children():
                     if child.kind == clang.cindex.CursorKind.CXX_OVERRIDE_ATTR:
                         result.add(f"non-virtual?thunk?to?{namespace_str};")
+                        is_virtual = True
                         break
+
+            # If this is a virtual/override, then we iterate the virtual base
+            # classes.. If we find find the provided method in those classes
+            # we need to generate a a virtual?thunk?to? for the method/constructor/deconstructor.
+            if is_virtual:
+                def search_class_hierarchy_for_virtual_thunk(derived: clang.cindex.Cursor):
+                    assert (derived.kind == clang.cindex.CursorKind.CLASS_DECL
+                            or derived.kind == clang.cindex.CursorKind.STRUCT_DECL
+                            or derived.kind == clang.cindex.CursorKind.CLASS_TEMPLATE)
+                    
+                    # Find the base classes for the derived class
+                    base_classes: list[clang.cindex.Cursor] = []
+                    for child in derived.get_children():
+                        if child.kind != clang.cindex.CursorKind.CXX_BASE_SPECIFIER:
+                            continue
+
+                        class_or_struct_node = clang.cindex.conf.lib.clang_getCursorDefinition(child)
+                        if class_or_struct_node is None:
+                            continue
+
+                        base_classes.append(class_or_struct_node)
+                        if not clang.cindex.conf.lib.clang_isVirtualBase(child):
+                            continue
+
+                        # Search the immediate base classes for the function name
+                        for other_child in class_or_struct_node.get_children():
+                            if (other_child.displayname == node.displayname):
+                                result.add(f"virtual?thunk?to?{namespace_str};")
+                                return True
+
+                    # Looks like it wasn't in any of our direct ancestors, so let's 
+                    # try their ancestors too
+                    for base in base_classes:
+                        if search_class_hierarchy_for_virtual_thunk(base):
+                            return True
+                        
+                    return False
+                    
+                search_class_hierarchy_for_virtual_thunk(node.semantic_parent)
+                            
 
     # Traverse down the tree if we can
     if (node.kind == clang.cindex.CursorKind.TRANSLATION_UNIT
@@ -195,17 +245,21 @@ def traverse_ast(node: clang.cindex.Cursor, filename: str, result: set[str]) -> 
     return result
 
 
-def process_directory(directory: str) -> list[str]:
+def process_directory(directory: str, search_dirs: Optional[list[str]]) -> list[str]:
     current_set = set()
 
     files_dir = get_absolute_path_form_project_path(directory)
     _logger.debug(f"Processing external directory: {files_dir}")
     files = glob.glob(files_dir + '/**/*.h', recursive=True)
+    search_variables = []
+    for dir in search_dirs:
+        search_variables.append(f"-I{get_absolute_path_form_project_path(dir)}")
+    args = ['-std=c++20', '-x', 'c++-header', '-nostdlibinc'] + search_variables
     for file in files:
         idx = clang.cindex.Index.create()
         tu = idx.parse(
             file, 
-            args=['-std=c++20', '-x', 'c++-header', '-nostdlibinc'],  
+            args=args,  
             options=0)
         root = tu.cursor
         list(traverse_ast(root, file, current_set))
@@ -250,32 +304,36 @@ def main():
 
     library = args.library
     try:
-        directories: LibraryInfo = library_to_data_map[library]
+        library_data: LibraryInfo = library_to_data_map[library]
     except KeyError:
         _logger.error(f"The provided library has yet to be implmented: {library}")
         exit(1)
 
     _logger.debug(f"Generating symbols for {library}")
 
+    search_dirs = library_data["search_headers"]
     # First, we process the external symbols
     external_symbols = []
-    for directory in directories["external_headers"]:
-        external_symbols += process_directory(directory)
+    for header_dir in library_data["external_headers"]:
+        external_symbols += process_directory(header_dir, search_dirs)
     output_symbols_to_file(
         external_symbols,
         library,
         args.symbol_version,
-        directories["external_output_file"])
+        library_data["external_output_file"])
     
     # Then, we process the internal symbols
     internal_symbols = []
-    for directory in directories["internal_headers"]:
-        internal_symbols += process_directory(directory)
-    output_symbols_to_file(
-        internal_symbols,
-        library,
-        args.symbol_version,
-        directories["internal_output_file"])
+    if "internal_headers" in library_data:
+        for header_dir in library_data["internal_headers"]:
+            internal_symbols += process_directory(header_dir, search_dirs)
+        
+        if "internal_output_file" in library_data:
+            output_symbols_to_file(
+                internal_symbols,
+                library,
+                args.symbol_version,
+                library_data["internal_output_file"])
     exit(0)
 
     
