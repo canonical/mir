472b1776d764c8fc6929880da81d5c11bd1ae404
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2024-04-15 15:50:24 +1000

    common/Signal: New worker-thread synchronisaton primitive.
    This is *very* much like `std::binary_semaphore`, but doesn't have the preconditon that
    the signal must not be raised before attempting to raise it.
    
    Use it to fix the UB (on shutdown) in `mir::ThreadPoolExecutor`

---
+++ src/common/signal.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright © Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 or 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "mir/signal.h"
+
+mir::Signal::Signal()
+  : flag{false}
+{
+}
+
+void mir::Signal::raise()
+{
+    flag = true;
+    flag.notify_all();
+}
+
+void mir::Signal::wait()
+{
+    flag.wait(false);
+    flag = false;
+}
