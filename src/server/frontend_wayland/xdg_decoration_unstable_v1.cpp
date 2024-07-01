66326012f0649389527dbca49de84fa8e3ee7496
Tarek Yasser <tarekspeed7@gamil.com>
2024-07-01 12:41:10 +0300

    Wrap `toplevels_with_decorations` in a class.

--- src/server/frontend_wayland/xdg_decoration_unstable_v1.cpp
+++ src/server/frontend_wayland/xdg_decoration_unstable_v1.cpp
@@ -16,10 +16,10 @@
 
 #include "xdg_decoration_unstable_v1.h"
 
+#include "mir/log.h"
 #include "mir/shell/surface_specification.h"
 #include "mir/wayland/client.h"
 #include "mir/wayland/protocol_error.h"
-#include "mir/log.h"
 
 #include "xdg-decoration-unstable-v1_wrapper.h"
 #include "xdg_output_v1.h"
@@ -32,6 +32,31 @@ namespace mir
 {
 namespace frontend
 {
+class ToplevelsWithDecorations
+{
+public:
+    ToplevelsWithDecorations() :
+        toplevels_with_decorations{std::make_shared<std::unordered_set<wl_resource*>>()}
+    {
+    }
+
+    /// \return true if no duplicates existed before insertion, false otherwise.
+    bool registerToplevel(wl_resource* toplevel)
+    {
+        auto [_, inserted] = toplevels_with_decorations->insert(toplevel);
+        return inserted;
+    }
+
+    /// \return true if only one element was erase, false otherwise.
+    bool unregisterToplevel(wl_resource* toplevel)
+    {
+        return toplevels_with_decorations->erase(toplevel) == 1;
+    }
+
+private:
+    std::shared_ptr<std::unordered_set<wl_resource*>> toplevels_with_decorations;
+};
+
 class XdgDecorationManagerV1 : public wayland::XdgDecorationManagerV1
 {
 public:
@@ -48,7 +73,7 @@ public:
 
 private:
     void get_toplevel_decoration(wl_resource* id, wl_resource* toplevel) override;
-	std::shared_ptr<std::unordered_set<wl_resource*>> toplevels_with_decorations;
+    ToplevelsWithDecorations toplevels_with_decorations;
 };
 
 class XdgToplevelDecorationV1 : public wayland::XdgToplevelDecorationV1
@@ -88,19 +113,13 @@ void mir::frontend::XdgDecorationManagerV1::Global::bind(wl_resource* new_zxdg_d
 
 mir::frontend::XdgDecorationManagerV1::XdgDecorationManagerV1(wl_resource* resource) :
     mir::wayland::XdgDecorationManagerV1{resource, Version<1>{}},
-	toplevels_with_decorations(std::make_shared<std::unordered_set<wl_resource*>>())
+    toplevels_with_decorations{}
 {
 }
 
 void mir::frontend::XdgDecorationManagerV1::get_toplevel_decoration(wl_resource* id, wl_resource* toplevel)
 {
     using Error = mir::frontend::XdgToplevelDecorationV1::Error;
-
-    if (toplevels_with_decorations->contains(toplevel))
-    {
-        BOOST_THROW_EXCEPTION(mir::wayland::ProtocolError(
-            resource, Error::already_constructed, "Decoration already constructed for this toplevel"));
-    }
 
     auto* tl = mir::frontend::XdgToplevelStable::from(toplevel);
     if (!tl)
@@ -109,15 +128,22 @@ void mir::frontend::XdgDecorationManagerV1::get_toplevel_decoration(wl_resource*
     }
 
     auto decoration = new XdgToplevelDecorationV1{id, tl};
-    toplevels_with_decorations->insert(toplevel);
+    if (!toplevels_with_decorations.registerToplevel(toplevel))
+    {
+        BOOST_THROW_EXCEPTION(mir::wayland::ProtocolError(
+            resource, Error::already_constructed, "Decoration already constructed for this toplevel"));
+    }
 
-    decoration->add_destroy_listener([toplevels_with_decorations = this->toplevels_with_decorations, toplevel]()
-                                     { toplevels_with_decorations->erase(toplevel); });
+    decoration->add_destroy_listener(
+        [&toplevels_with_decorations = this->toplevels_with_decorations, toplevel]()
+        {
+            toplevels_with_decorations.unregisterToplevel(toplevel);
+        });
 
     tl->add_destroy_listener(
-        [toplevels_with_decorations = this->toplevels_with_decorations, client = this->client, toplevel]()
+        [&toplevels_with_decorations = this->toplevels_with_decorations, client = this->client, toplevel]()
         {
-            if (!client->is_being_destroyed() && toplevels_with_decorations->erase(toplevel) > 0)
+            if (!client->is_being_destroyed() && !toplevels_with_decorations.unregisterToplevel(toplevel))
             {
                 mir::log_warning("Toplevel destroyed before attached decoration!");
                 // https://github.com/canonical/mir/issues/3452
@@ -153,6 +179,12 @@ void mir::frontend::XdgToplevelDecorationV1::update_mode(uint32_t new_mode)
     send_configure_event(mode);
 }
 
-void mir::frontend::XdgToplevelDecorationV1::set_mode(uint32_t mode) { update_mode(mode); }
+void mir::frontend::XdgToplevelDecorationV1::set_mode(uint32_t mode)
+{
+    update_mode(mode);
+}
 
-void mir::frontend::XdgToplevelDecorationV1::unset_mode() { update_mode(default_mode); }
+void mir::frontend::XdgToplevelDecorationV1::unset_mode()
+{
+    update_mode(default_mode);
+}
