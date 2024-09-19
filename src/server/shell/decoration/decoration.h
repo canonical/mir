c130db51008b1eb9d1820ce56ec8ae300564690f
Tarek Ismail <tarek.ismail@canonical.com>
2024-09-19 18:09:12 +0300

    Move input observer to `Decoration` and event handling to `InputManager`

--- src/server/shell/decoration/decoration.h
+++ src/server/shell/decoration/decoration.h
@@ -17,7 +17,13 @@
 #ifndef MIR_SHELL_DECORATION_DECORATION_H_
 #define MIR_SHELL_DECORATION_DECORATION_H_
 
-#include "mir_toolkit/event.h"
+
+#include "mir/scene/null_surface_observer.h"
+#include "mir/scene/surface.h"
+#include <mir/geometry/size.h>
+#include <mir/geometry/forward.h>
+#include <mir/events/event.h>
+
 #include <string>
 
 namespace mir
@@ -30,29 +36,68 @@ namespace decoration
 class Decoration
 {
 public:
-    Decoration() = default;
-    virtual ~Decoration() = default;
+    struct InputObserver : public mir::scene::NullSurfaceObserver
+    {
+        InputObserver(Decoration* decoration) :
+            decoration{decoration}
+        {
+        }
+
+        /// Overrides from NullSurfaceObserver
+        /// @{
+        void input_consumed(mir::scene::Surface const*, std::shared_ptr<MirEvent const> const& event) override
+        {
+            if (mir_event_get_type(event.get()) != mir_event_type_input)
+                return;
+
+            decoration->handle_input_event(event);
+        }
+        /// @}
+
+        Decoration* const decoration;
+    };
+
+    Decoration() = default; // For tests
+
+    Decoration(std::shared_ptr<mir::scene::Surface> decoration_surface) :
+        decoration_surface{decoration_surface},
+        observer{std::make_shared<InputObserver>(this)}
+    {
+        decoration_surface->register_interest(observer);
+    }
+
+    virtual ~Decoration()
+    {
+        decoration_surface->unregister_interest(*observer);
+    }
 
     // Called whenever the visible aspect of the window changes.
     // Examples: Scale, title, focus, visibility, size, etc...
     virtual void redraw() = 0;
 
-    // Called whenever decorations receive input events
+    // Called whenever the input state changes
     // Mouse up, down, dragging, enter, leave...
-    virtual void handle_input() = 0;
+    virtual void input_state_changed() = 0;
 
+    // Called whenever the decoration surface receives an input
+    virtual void handle_input_event(std::shared_ptr<MirEvent const> const& /*event*/) { /* Empty for tests */ }
+
+    // Basic functionality that all decorations _should_ have
     virtual void request_toggle_maximize() = 0;
     virtual void request_close() = 0;
     virtual void request_minimize() = 0;
     virtual void request_move(MirInputEvent const*) = 0;
     virtual void request_resize(MirInputEvent const*, MirResizeEdge) = 0;
-
     virtual void set_cursor(std::string const& cursor_name) = 0;
     virtual void set_scale(float new_scale) = 0;
 
 private:
     Decoration(Decoration const&) = delete;
     Decoration& operator=(Decoration const&) = delete;
+
+protected:
+    std::shared_ptr<mir::scene::Surface> decoration_surface;
+    std::shared_ptr<InputObserver> observer;
 };
 }
 }
