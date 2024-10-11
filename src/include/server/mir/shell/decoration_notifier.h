1a407840709583c01f2cd10591d2423461d8c53a
Tarek Ismail <tarek.ismail@canonical.com>
2024-10-11 17:34:13 +0300

    Move `msd::DecorationNotifier` implementation to its own file

--- src/include/server/mir/shell/decoration_notifier.h
+++ src/include/server/mir/shell/decoration_notifier.h
@@ -16,16 +16,13 @@
 
 #include <mir/shell/decoration.h>
 
-#include "mir/scene/null_surface_observer.h"
 #include "mir/scene/surface.h"
 #include "mir/shell/shell.h"
-#include "mir_toolkit/common.h"
 
 #include <mir/geometry/size.h>
 #include <mir/geometry/forward.h>
 #include <mir/events/event.h>
 
-#include <string>
 #include <memory>
 
 namespace mir::shell::decoration
@@ -36,76 +33,13 @@ public:
     DecorationNotifier(
         std::shared_ptr<mir::scene::Surface> decoration_surface,
         std::shared_ptr<mir::scene::Surface> window_surface,
-        Decoration* const decoration) :
-        decoration_surface{decoration_surface},
-        window_surface{window_surface},
-        input_observer{std::make_shared<InputObserver>(decoration)},
-        window_observer{std::make_shared<WindowSurfaceObserver>(decoration)}
-    {
-        decoration_surface->register_interest(input_observer);
-        window_surface->register_interest(window_observer);
-    }
+        Decoration* const decoration);
 
-    ~DecorationNotifier()
-    {
-        if (decoration_surface)
-            decoration_surface->unregister_interest(*input_observer);
-
-        if (window_surface)
-            window_surface->unregister_interest(*window_observer);
-    }
+    ~DecorationNotifier();
 
 private:
-    struct InputObserver : public mir::scene::NullSurfaceObserver
-    {
-        InputObserver(Decoration* decoration) :
-            decoration{decoration}
-        {
-        }
-
-        /// Overrides from NullSurfaceObserver
-        /// @{
-        void input_consumed(mir::scene::Surface const*, std::shared_ptr<MirEvent const> const& event) override
-        {
-            if (mir_event_get_type(event.get()) != mir_event_type_input)
-                return;
-
-            decoration->handle_input_event(event);
-        }
-        /// @}
-
-        Decoration* const decoration;
-    };
-
-    struct WindowSurfaceObserver : public mir::scene::NullSurfaceObserver
-    {
-    public:
-        WindowSurfaceObserver(Decoration* const& decoration) :
-            decoration{decoration}
-        {
-        }
-
-        /// Overrides from NullSurfaceObserver
-        /// @{
-        void attrib_changed(mir::scene::Surface const* window_surface, MirWindowAttrib attrib, int value) override
-        {
-            decoration->attrib_changed(window_surface, attrib, value);
-        }
-
-        void window_resized_to(mir::scene::Surface const* window_surface, geometry::Size const& window_size) override
-        {
-            decoration->window_resized_to(window_surface, window_size);
-        }
-
-        void renamed(mir::scene::Surface const* window_surface, std::string const& name) override
-        {
-            decoration->window_renamed(window_surface, name);
-        }
-        /// @}
-
-        Decoration* const decoration;
-
-    };
+    struct InputObserver;
+    struct WindowSurfaceObserver;
 
     std::shared_ptr<mir::scene::Surface> decoration_surface;
     std::shared_ptr<mir::scene::Surface> window_surface;
