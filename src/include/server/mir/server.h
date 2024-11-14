ddba2e4e6235afb9e4be98ed073c0378e0c5fed6
Tarek Ismail <tarek.ismail@canonical.com>
2024-11-14 17:07:51 +0200

    Expose `the_buffer_allocator` in `mir::Server`

--- src/include/server/mir/server.h
+++ src/include/server/mir/server.h
@@ -34,7 +34,7 @@ template<class Observer>
 class ObserverRegistrar;
 
 namespace compositor { class Compositor; class DisplayBufferCompositorFactory; class CompositorReport; }
-namespace graphics { class Cursor; class DisplayPlatform; class RenderingPlatform; class Display; class GLConfig; class DisplayConfigurationPolicy; class DisplayConfigurationObserver; }
+namespace graphics { class Cursor; class DisplayPlatform; class RenderingPlatform; class Display; class GLConfig; class DisplayConfigurationPolicy; class DisplayConfigurationObserver; class GraphicBufferAllocator; }
 namespace input { class CompositeEventFilter; class InputDispatcher; class CursorListener; class CursorImages; class TouchVisualizer; class InputDeviceHub; class InputDeviceRegistry;}
 namespace logging { class Logger; }
 namespace options { class Option; }
@@ -437,6 +437,9 @@ public:
 
     auto the_idle_handler() const ->
         std::shared_ptr<shell::IdleHandler>;
+
+    auto the_buffer_allocator() const
+        -> std::shared_ptr<graphics::GraphicBufferAllocator>;
 
 /** @} */
 
