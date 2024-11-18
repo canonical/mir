9b1935a46b16c95931f501e20821365ff478bf16
Christopher James Halse Rogers <christopher.halse.rogers@canonical.com>
2024-11-18 17:43:04 +1100

    Document `MultiMonitorArbiter` a bit

--- src/server/compositor/multi_monitor_arbiter.h
+++ src/server/compositor/multi_monitor_arbiter.h
@@ -32,6 +32,42 @@ namespace graphics { class Buffer; }
 namespace compositor
 {
 
+/**
+ * Coordinate surface buffer consumption across different outputs
+ *
+ * When multiple compositors have a view on the same surface, they will
+ * request buffers from that surface at their own pace, gated by either
+ * the display VSync (for compositors driving physical outputs), by
+ * client consumption (for compositors for screencopy), or some other
+ * mechanism.
+ *
+ * These timings will (almost always) not align, so if each compositor's
+ * request advanced the surface buffer we would request frames at some
+ * multiple of the frequencies involved.
+ *
+ * As an additional constraint, it must always be possible for a compositor
+ * to acquire a buffer.
+ *
+ * To avoid this, the MultiMonitorArbiter stores up to two buffers -
+ * a current buffer and a next buffer - and tracks which compositor has
+ * seen the current buffer. The next buffer is only advanced when a
+ * compositor requests a buffer *and* that compositor has seen the current buffer.
+ *
+ * This results in the buffer queue being advanced at only the rate of the fastest
+ * compositor.
+ *
+ * Unfortunately, because this doesn't have any feedback into any other
+ * component, we can get into a state where there *is* a new buffer available
+ * for a surface, but the *Compositor* isn't going to request a buffer until
+ * something else changes. This is most easy to trigger around mode switches,
+ * where we destroy all Compositors and then create new ones which then,
+ * definitionally, haven't seen the current buffer.
+ *
+ * This system will need to be significantly overhauled in future. A variety
+ * of Wayland extensions we wish to support - notably wp_presentation and
+ * wp_fifo - expect that a surface's buffer queue is synchronised to a single
+ * output.
+ */
 class MultiMonitorArbiter : public std::enable_shared_from_this<MultiMonitorArbiter>
 {
 public:
