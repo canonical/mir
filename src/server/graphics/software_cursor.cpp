7a544abe957808bbe21298337a580d24505e61fe
Tarek Ismail <tarek.ismail@canonical.com>
2025-02-04 18:38:13 +0200

    Use pixman to scale cursor images

--- src/server/graphics/software_cursor.cpp
+++ src/server/graphics/software_cursor.cpp
@@ -15,6 +15,8 @@
  */
 
 #include "software_cursor.h"
+#include "mir/geometry/displacement.h"
+#include "mir/geometry/forward.h"
 #include "mir/graphics/cursor_image.h"
 #include "mir/graphics/graphic_buffer_allocator.h"
 #include "mir/graphics/pixel_format_utils.h"
@@ -23,7 +25,11 @@
 #include "mir/input/scene.h"
 #include "mir/renderer/sw/pixel_source.h"
 #include "mir/executor.h"
+#include "mir/log.h"
 
+#include <cstdint>
+#include <memory>
+#include <pixman-1/pixman.h>
 #include <boost/throw_exception.hpp>
 #include <stdexcept>
 #include <mutex>
@@ -127,8 +133,7 @@ mg::SoftwareCursor::SoftwareCursor(
     std::shared_ptr<mg::GraphicBufferAllocator> const& allocator,
     std::shared_ptr<Executor> const& scene_executor,
     std::shared_ptr<mi::Scene> const& scene)
-    : allocator{allocator},
-      scene{scene},
+    : allocator{allocator}, scene{scene},
       format{get_8888_format(allocator->supported_pixel_formats())},
       scene_executor{scene_executor},
       visible(false),
@@ -145,26 +150,13 @@ void mg::SoftwareCursor::show(CursorImage const& cursor_image)
 {
     std::lock_guard lg{guard};
 
-    auto const to_remove = visible ? renderable : nullptr;
+    // Store the cursor image for later use with `set_scale`
+    this->current_cursor_image = &cursor_image;
 
-    geom::Point position{0,0};
-    if (renderable)
-        position = renderable->screen_position().top_left;
+    // TODO Make this user configurable
+    set_scale(1.75);
 
-    renderable = create_renderable_for(cursor_image, position);
-    hotspot = cursor_image.hotspot();
     visible = true;
-
-    scene_executor->spawn([scene = scene, to_remove = to_remove, to_add = renderable]()
-        {
-            // Add the new renderable first, then remove the old one to avoid visual glitches
-            scene->add_input_visualization(to_add);
-
-            if (to_remove)
-            {
-                scene->remove_input_visualization(to_remove);
-            }
-        });
 }
 
 std::shared_ptr<mg::detail::CursorRenderable>
@@ -216,4 +208,68 @@ void mg::SoftwareCursor::move_to(geometry::Point position)
 
     // This doesn't need to be called in a specific order with other potential calls, so it doesn't go on the executor
     scene->emit_scene_changed();
+}
+
+void mir::graphics::SoftwareCursor::set_scale(float new_scale)
+{
+    auto const [width, height] = current_cursor_image->size();
+
+    auto* original_image = pixman_image_create_bits(
+        PIXMAN_a8r8g8b8,
+        width.as_int(),
+        height.as_int(),
+        (uint32_t*)(current_cursor_image->as_argb_8888()),
+        width.as_int() * MIR_BYTES_PER_PIXEL(mir_pixel_format_argb_8888));
+
+    pixman_transform_t transform;
+    pixman_transform_init_identity(&transform);
+    // Counter-intuitively, you use the INVERSE transform, not the "normal" one
+    pixman_transform_scale(NULL, &transform, pixman_double_to_fixed(new_scale), pixman_double_to_fixed(new_scale));
+    pixman_image_set_transform(original_image, &transform);
+
+    // TODO filtering?
+
+    auto const scaled_width = width.as_value() * new_scale;
+    auto const scaled_height = height.as_value() * new_scale;
+    auto buf = std::make_unique<uint32_t[]>(scaled_width * scaled_height);
+    auto* scaled_image = pixman_image_create_bits(
+        PIXMAN_a8r8g8b8,
+        scaled_width,
+        scaled_height,
+        buf.get(),
+        scaled_width * MIR_BYTES_PER_PIXEL(mir_pixel_format_argb_8888));
+
+    // Copy over the pixels into the larger image
+    pixman_image_composite(
+        PIXMAN_OP_SRC, original_image, NULL, scaled_image, 0, 0, 0, 0, 0, 0, scaled_width, scaled_height);
+
+    auto buffer = mrs::alloc_buffer_with_content(
+        *allocator,
+        reinterpret_cast<unsigned char*>(pixman_image_get_data(scaled_image)),
+        {scaled_width, scaled_height},
+        geom::Stride{scaled_width * MIR_BYTES_PER_PIXEL(mir_pixel_format_argb_8888)},
+        mir_pixel_format_argb_8888);
+
+    auto const to_remove = visible ? renderable : nullptr;
+    auto const position = renderable ? renderable->screen_position().top_left : geom::Point{0, 0};
+
+    renderable = std::make_shared<detail::CursorRenderable>(
+        std::move(buffer), position + hotspot - current_cursor_image->hotspot());
+
+    hotspot = current_cursor_image->hotspot() * new_scale;
+
+    scene_executor->spawn(
+        [scene = scene, to_remove = to_remove, to_add = renderable]()
+        {
+            // Add the new renderable first, then remove the old one to avoid visual glitches
+            scene->add_input_visualization(to_add);
+
+            if (to_remove)
+            {
+                scene->remove_input_visualization(to_remove);
+            }
+        });
+
+    pixman_image_unref(original_image);
+    pixman_image_unref(scaled_image);
 }
