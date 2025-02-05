39e6a03a8d12d4006778acaf5f698f1aa25c036e
Tarek Ismail <tarek.ismail@canonical.com>
2025-02-05 17:57:45 +0200

    Properly merge cli/env/.config cursor/keyboard/touch  options with shell cursor options

--- examples/mir_demo_server/server_example.cpp
+++ examples/mir_demo_server/server_example.cpp
@@ -128,6 +128,17 @@ public:
         runner.add_start_callback([this]
             {
                 std::lock_guard lock{config_mutex};
+
+                // Merge the input options collected from the command line,
+                // environment, and default `.config` file with the options we
+                // read from the `.input` file.
+                //
+                // In this case, the `.input` file takes precedence. You can
+                // reverse the arguments to de-prioritize it.
+                mouse.merge_settings_from(input_configuration.mouse());
+                keyboard.merge_settings_from(input_configuration.keyboard());
+                touchpad.merge_settings_from(input_configuration.touchpad());
+
                 apply_config();
             });
     }
