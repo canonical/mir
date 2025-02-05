fc7ebaa743acaae387fd14077230eecb79031b15
Tarek Ismail <tarek.ismail@canonical.com>
2025-02-05 17:57:45 +0200

    Properly merge cli/env/.config cursor/keyboard/touch  options with shell cursor options

--- src/miral/input_device_config.h
+++ src/miral/input_device_config.h
@@ -31,6 +31,7 @@ namespace miral
 struct MouseInputConfiguration
 {
     void apply_to(mir::input::Device& device) const;
+    void merge_settings_from(MouseInputConfiguration const&);
     std::optional<MirPointerHandedness> handedness;
     std::optional<MirPointerAcceleration> acceleration;
     std::optional<double> acceleration_bias;
@@ -43,6 +44,7 @@ class TouchpadInputConfiguration
 {
 public:
     void apply_to(mir::input::Device& device) const;
+    void merge_settings_from(TouchpadInputConfiguration const&);
     std::optional<bool> disable_while_typing;
     std::optional<bool> disable_with_external_mouse;
     std::optional<MirPointerAcceleration> acceleration;
@@ -58,6 +60,7 @@ public:
 class KeyboardInputConfiguration
 {
 public:
+    void merge_settings_from(KeyboardInputConfiguration const&);
     std::optional<int> repeat_rate;
     std::optional<int> repeat_delay;
 };
