1090c32fecd53b4aabbc049edd0c151b5e95d735
Tarek Ismail <tarek.ismail@canonical.com>
2025-02-05 17:57:45 +0200

    Properly merge cli/env/.config cursor/keyboard/touch options with shell options

--- include/miral/miral/input_configuration.h
+++ include/miral/miral/input_configuration.h
@@ -79,6 +79,10 @@ public:
     void vscroll_speed(std::optional<double>const& val);
     void hscroll_speed(std::optional<double>const& val);
 
+    /// Copies the corresponding value from `other` for all unset fields.
+    /// \remark Since MirAL 5.2
+    void merge_settings_from(InputConfiguration::Mouse const& other);
+
 private:
     friend class InputConfiguration::Self;
     class Self;
@@ -118,6 +122,10 @@ public:
     void scroll_mode(std::optional<MirTouchpadScrollMode>const& val);
     void tap_to_click(std::optional<bool>const& val);
 
+    /// Copies the corresponding value from `other` for all unset fields.
+    /// \remark Since MirAL 5.2
+    void merge_settings_from(InputConfiguration::Touchpad const& other);
+
 private:
     friend class InputConfiguration::Self;
     class Self;
@@ -138,6 +146,10 @@ public:
 
     void set_repeat_rate(int new_rate);
     void set_repeat_delay(int new_delay);
+
+    /// Copies the corresponding value from `other` for all unset fields.
+    /// \remark Since MirAL 5.2
+    void merge_settings_from(InputConfiguration::Keyboard const& other);
 
 private:
     friend class InputConfiguration::Self;
