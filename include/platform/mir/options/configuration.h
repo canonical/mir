1f9daba905b8739e464055e4e69ca1fbfe763671
Tarek Ismail <tarek.ismail@canonical.com>
2025-02-11 18:25:53 +0200

    Demo input event transformations with `AccessibilityManager`

--- include/platform/mir/options/configuration.h
+++ include/platform/mir/options/configuration.h
@@ -42,6 +42,7 @@ extern char const* const fatal_except_opt;
 extern char const* const debug_opt;
 extern char const* const composite_delay_opt;
 extern char const* const enable_key_repeat_opt;
+extern char const* const enable_mouse_keys_opt;
 extern char const* const x11_display_opt;
 extern char const* const x11_scale_opt;
 extern char const* const wayland_extensions_opt;
