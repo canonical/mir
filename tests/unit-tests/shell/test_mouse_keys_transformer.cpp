1d76afe5fb69581fdca0d8d5b9595013c2218a3e
Tarek Ismail <tarek.ismail@canonical.com>
2025-03-25 16:38:16 +0200

    Convert some mousekeys tests to parameterized test suites

--- tests/unit-tests/shell/test_mouse_keys_transformer.cpp
+++ tests/unit-tests/shell/test_mouse_keys_transformer.cpp
@@ -120,60 +120,124 @@ struct TestMouseKeysTransformer : testing::Test
     }
 };
 
-TEST_F(TestMouseKeysTransformer, single_keyboard_event_to_single_pointer_motion_event)
+struct TestOneAxisMovement : public TestMouseKeysTransformer, public WithParamInterface<mir::input::XkbSymkey>
 {
-    for (auto const button : {XKB_KEY_KP_2, XKB_KEY_KP_4, XKB_KEY_KP_6, XKB_KEY_KP_8})
-    {
-        auto const timeout = std::chrono::milliseconds(20);
-        auto const repeat_delay = std::chrono::milliseconds(2);
-        auto const num_invocations = timeout / repeat_delay;
+};
 
-        EXPECT_CALL(mock_seat, dispatch_event(_))
-            .Times(AtLeast(num_invocations / 2)) // Account for various timing inconsistencies
-            .WillRepeatedly(
-                [](std::shared_ptr<MirEvent> const& event)
-                {
-                    auto* const pointer_event = event->to_input()->to_pointer();
+TEST_P(TestOneAxisMovement, single_keyboard_event_to_single_pointer_motion_event)
+{
+    auto const timeout = std::chrono::milliseconds(20);
+    auto const repeat_delay = std::chrono::milliseconds(2);
+    auto const num_invocations = timeout / repeat_delay;
 
-                    auto const pointer_action = pointer_event->action();
-                    auto const pointer_relative_motion = pointer_event->motion();
-
-                    ASSERT_EQ(pointer_action, mir_pointer_action_motion);
-
-                    // Make sure the movement is on one axis
-                    if (pointer_relative_motion.dx.as_value() != 0.0f)
-                    {
-                        ASSERT_EQ(pointer_relative_motion.dy.as_value(), 0.0f);
-                    }
-
-                    if (pointer_relative_motion.dy.as_value() != 0.0f)
-                    {
-                        ASSERT_EQ(pointer_relative_motion.dx.as_value(), 0.0f);
-                    }
-                });
-
-        auto down = down_event(button);
-        input_event_transformer.handle(*down);
-
-        // Should generate a bunch of motion events until we handle the up
-        // event
-        auto const step = std::chrono::milliseconds(1);
-        mt::spin_wait_for_condition_or_timeout(
-            [&]
+    EXPECT_CALL(mock_seat, dispatch_event(_))
+        .Times(AtLeast(num_invocations / 2))
+        .WillRepeatedly(
+            [](std::shared_ptr<MirEvent> const& event)
             {
-                clock.advance_by(step);
-                return false;
-            },
-            timeout,
-            step);
+                auto* const pointer_event = event->to_input()->to_pointer();
 
-        input_event_transformer.handle(*up_event(button));
-    }
+                auto const pointer_action = pointer_event->action();
+                auto const pointer_relative_motion = pointer_event->motion();
+
+                ASSERT_EQ(pointer_action, mir_pointer_action_motion);
+
+                // Make sure the movement is on one axis
+                if (pointer_relative_motion.dx.as_value() != 0.0f)
+                {
+                    ASSERT_EQ(pointer_relative_motion.dy.as_value(), 0.0f);
+                }
+
+                if (pointer_relative_motion.dy.as_value() != 0.0f)
+                {
+                    ASSERT_EQ(pointer_relative_motion.dx.as_value(), 0.0f);
+                }
+            });
+
+    auto button = GetParam();
+    input_event_transformer.handle(*down_event(button));
+
+    // Should generate a bunch of motion events until we handle the up
+    // event
+    auto const step = std::chrono::milliseconds(1);
+    mt::spin_wait_for_condition_or_timeout(
+        [&]
+        {
+            clock.advance_by(step);
+            return false;
+        },
+        timeout,
+        step);
+
+    input_event_transformer.handle(*up_event(button));
 }
 
-TEST_F(TestMouseKeysTransformer, multiple_keys_result_in_diagonal_movement)
+INSTANTIATE_TEST_SUITE_P(
+    TestMouseKeysTransformer, TestOneAxisMovement, ::Values(XKB_KEY_KP_2, XKB_KEY_KP_4, XKB_KEY_KP_6, XKB_KEY_KP_8));
+
+struct TestDiagonalMovement :
+    public TestMouseKeysTransformer,
+    public WithParamInterface<std::pair<mir::input::XkbSymkey, mir::input::XkbSymkey>>
 {
-    auto const combinations = {
+};
+
+TEST_P(TestDiagonalMovement, multiple_keys_result_in_diagonal_movement)
+{
+    auto const [key1, key2] = GetParam();
+    auto const timeout = std::chrono::milliseconds(20);
+    auto const repeat_delay = std::chrono::milliseconds(2);
+    auto const num_invocations = timeout / repeat_delay;
+
+    auto count_diagonal = 0;
+    EXPECT_CALL(mock_seat, dispatch_event(_))
+        .Times(AtLeast(num_invocations / 2)) // Account for various timing inconsistencies
+        .WillRepeatedly(
+            [&](std::shared_ptr<MirEvent> const& event)
+            {
+                auto* const pointer_event = event->to_input()->to_pointer();
+
+                auto const pointer_action = pointer_event->action();
+                auto const pointer_relative_motion = pointer_event->motion();
+
+                ASSERT_EQ(pointer_action, mir_pointer_action_motion);
+
+                // Make sure there's movement on both axes
+                if (std::abs(pointer_relative_motion.dx.as_value()) > 0.0f &&
+                    std::abs(pointer_relative_motion.dy.as_value()) > 0.0f)
+                    count_diagonal += 1;
+            });
+
+    auto const step = std::chrono::milliseconds(1);
+
+    auto down1 = down_event(key1);
+    input_event_transformer.handle(*down1);
+    clock.advance_by(step);
+
+    auto down2 = down_event(key2);
+    input_event_transformer.handle(*down2);
+    clock.advance_by(step);
+
+    // Should generate a bunch of motion events until we handle the up
+    // event
+    mt::spin_wait_for_condition_or_timeout(
+        [&]
+        {
+            clock.advance_by(step);
+            return false;
+        },
+        timeout,
+        step);
+
+    input_event_transformer.handle(*up_event(key1));
+    input_event_transformer.handle(*up_event(key2));
+
+    ASSERT_GT(count_diagonal, 0);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    TestMouseKeysTransformer,
+    TestDiagonalMovement,
+    ::Values(
         std::pair{XKB_KEY_KP_2, XKB_KEY_KP_4},
         std::pair{XKB_KEY_KP_2, XKB_KEY_KP_6},
         std::pair{XKB_KEY_KP_4, XKB_KEY_KP_8},
@@ -181,62 +245,7 @@ TEST_F(TestMouseKeysTransformer, multiple_keys_result_in_diagonal_movement)
         std::pair{XKB_KEY_KP_6, XKB_KEY_KP_8},
         std::pair{XKB_KEY_KP_6, XKB_KEY_KP_2},
         std::pair{XKB_KEY_KP_8, XKB_KEY_KP_4},
-        std::pair{XKB_KEY_KP_8, XKB_KEY_KP_6},
-    };
-
-    for (auto const& [key1, key2] : combinations)
-    {
-        auto const timeout = std::chrono::milliseconds(20);
-        auto const repeat_delay = std::chrono::milliseconds(2);
-        auto const num_invocations = timeout / repeat_delay;
-
-        auto count_diagonal = 0;
-        EXPECT_CALL(mock_seat, dispatch_event(_))
-            .Times(AtLeast(num_invocations / 2)) // Account for various timing inconsistencies
-            .WillRepeatedly(
-                [&](std::shared_ptr<MirEvent> const& event)
-                {
-                    auto* const pointer_event = event->to_input()->to_pointer();
-
-                    auto const pointer_action = pointer_event->action();
-                    auto const pointer_relative_motion = pointer_event->motion();
-
-                    ASSERT_EQ(pointer_action, mir_pointer_action_motion);
-
-                    // Make sure there's movement on both axes
-                    if (std::abs(pointer_relative_motion.dx.as_value()) > 0.0f &&
-                        std::abs(pointer_relative_motion.dy.as_value()) > 0.0f)
-                        count_diagonal += 1;
-                });
-
-
-        auto const step = std::chrono::milliseconds(1);
-
-        auto down1 = down_event(key1);
-        input_event_transformer.handle(*down1);
-        clock.advance_by(step);
-
-        auto down2 = down_event(key2);
-        input_event_transformer.handle(*down2);
-        clock.advance_by(step);
-
-        // Should generate a bunch of motion events until we handle the up
-        // event
-        mt::spin_wait_for_condition_or_timeout(
-            [&]
-            {
-                clock.advance_by(step);
-                return false;
-            },
-            timeout,
-            step);
-
-        input_event_transformer.handle(*up_event(key1));
-        input_event_transformer.handle(*up_event(key2));
-
-        ASSERT_GT(count_diagonal, 0);
-    }
-}
+        std::pair{XKB_KEY_KP_8, XKB_KEY_KP_6}));
 
 TEST_F(TestMouseKeysTransformer, clicks_dispatch_pointer_down_and_up_events)
 {
