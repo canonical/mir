fd019b2608be8ebe5022f3e8c1a9c9310d8304f7
Tarek Ismail <tarek.ismail@canonical.com>
2025-04-04 18:03:23 +0200

    Vsync test

--- src/server/graphics/animation_driver.cpp
+++ src/server/graphics/animation_driver.cpp
@@ -17,14 +17,25 @@
 #include "mir/graphics/animation_driver.h"
 #include "mir/time/clock.h"
 #include <chrono>
+#include <iostream>
 
+struct StubListener : public mir::graphics::AnimationObserver
+{
+    virtual void on_vsync(std::chrono::milliseconds) override
+    {
+        std::cerr << "Bleep bloop\n";
+    }
+};
 mir::graphics::AnimationDriver::AnimationDriver(
     std::shared_ptr<DisplayReport> const& to_wrap, std::shared_ptr<time::Clock> const& clock) :
     ObserverMultiplexer<AnimationObserver>{mir::linearising_executor},
     wrapped_report{to_wrap},
     clock{clock},
-    last_vsync{clock->now()}
+    last_vsync{clock->now()},
+    test_observer{std::make_shared<StubListener>()}
 {
+
+    this->register_interest(test_observer);
 }
 
 void mir::graphics::AnimationDriver::on_vsync(std::chrono::milliseconds dt)
