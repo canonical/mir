f4431976ff673c8d001fa13f1f39ff4660c15f74
Alan Griffiths <alan@octopull.co.uk>
2025-04-23 10:57:49 +0100

    Tidy up

--- src/platform/graphics/drm_syncobj.cpp
+++ src/platform/graphics/drm_syncobj.cpp
@@ -1,6 +1,20 @@
-#include "mir/graphics/drm_syncobj.h"
+/*
+* Copyright © Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 2 or 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include "mir/log.h"
+#include "mir/graphics/drm_syncobj.h"
 
 #include <boost/throw_exception.hpp>
 #include <drm.h>
