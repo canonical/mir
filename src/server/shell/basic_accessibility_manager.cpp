c31c4ade9113c76e57d5bf10ed9894e233d3d069
Tarek Ismail <tarek.ismail@canonical.com>
2025-04-23 13:39:56 +0200

    Attempt to appease CI compilers

--- src/server/shell/basic_accessibility_manager.cpp
+++ src/server/shell/basic_accessibility_manager.cpp
@@ -162,13 +162,13 @@ auto mir::shell::BasicAccessibilityManager::simulated_secondary_click()
 
 void mir::shell::BasicAccessibilityManager::ensure_mousekey_pointer_added()
 {
-    if (std::ranges::any_of(std::initializer_list{mousekeys_on, ssc_on}, std::identity{}))
+    if (std::ranges::any_of(std::initializer_list<bool>{mousekeys_on, ssc_on}, std::identity{}))
         mousekey_pointer->add_to_registry(input_device_registry);
 }
 
 void mir::shell::BasicAccessibilityManager::ensure_mousekey_pointer_removed()
 {
-    if (std::ranges::none_of(std::initializer_list{mousekeys_on, ssc_on}, std::identity{}))
+    if (std::ranges::none_of(std::initializer_list<bool>{mousekeys_on, ssc_on}, std::identity{}))
         mousekey_pointer->remove_from_registry(input_device_registry);
 }
 
