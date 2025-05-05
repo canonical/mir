2d1c9c007604596b6b338b539189b967500da6b9
Tarek Ismail <tarek.ismail@canonical.com>
2025-05-05 18:09:04 +0300

    Read locate pointer delay from config options

--- src/miral/locate_pointer.cpp
+++ src/miral/locate_pointer.cpp
@@ -159,7 +159,11 @@ void miral::LocatePointer::operator()(mir::Server& server)
             self->main_loop = server.the_main_loop();
             self->composite_event_filter = server.the_composite_event_filter();
 
-            if (server.get_options()->get<bool>(enable_locate_pointer_opt))
+            auto const options = server.get_options();
+
+            delay(std::chrono::milliseconds{options->get<int>(locate_pointer_delay_opt)});
+
+            if (options->get<bool>(enable_locate_pointer_opt))
                 enable();
             else
                 disable();
