12fdd3df3df75c4dcdf2040b2eebe20bb162fa80
Tarek Ismail <tarek.ismail@canonical.com>
2025-05-05 19:15:06 +0300

    Rework locate pointer tests

--- tests/miral/test_locate_pointer.cpp
+++ tests/miral/test_locate_pointer.cpp
@@ -14,16 +14,20 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "mir/input/composite_event_filter.h"
+#include "mir/input/device.h"
 #include "mir/input/event_builder.h"
+#include "mir/input/input_device_hub.h"
+#include "mir/input/input_device_observer.h"
 #include "mir/input/input_device_registry.h"
 #include "mir/input/input_sink.h"
 #include "mir/input/virtual_input_device.h"
 #include "mir/server.h"
 #include "mir/test/signal.h"
 
+#include "miral/append_event_filter.h"
 #include "miral/locate_pointer.h"
 #include "miral/test_server.h"
-#include "miral/toolkit_event.h"
 
 #include <gmock/gmock.h>
 
@@ -35,9 +39,56 @@ namespace
 static constexpr auto locate_pointer_delay = 100ms;
 static constexpr auto enable_or_disable_delay = 50ms; // Should be plenty for the main loop to execute even on CI
 
-mir::EventUPtr left_ctrl_down_event(mir::input::EventBuilder* builder)
+// Mostly to account for CI slowness.
+// Adds a test pointer and waits till it's added before proceeding
+std::shared_ptr<mir::input::VirtualInputDevice> add_test_pointer(
+    std::weak_ptr<mir::input::InputDeviceRegistry> const& input_device_registry,
+    std::weak_ptr<mir::input::InputDeviceHub> const& input_device_hub)
 {
-    return builder->key_event(std::nullopt, mir_keyboard_action_down, XKB_KEY_Control_L, 29);
+    std::shared_ptr<mir::input::VirtualInputDevice> test_device =
+        std::make_shared<mir::input::VirtualInputDevice>("test-device", mir::input::DeviceCapability::pointer);
+
+    struct SignalingObserver : public mir::input::InputDeviceObserver
+    {
+        using Device = mir::input::Device;
+        std::shared_ptr<Device> const device_to_wait_for;
+        std::weak_ptr<mir::input::InputDeviceHub> const device_hub;
+        mir::test::Signal device_added_;
+
+        SignalingObserver(std::shared_ptr<Device> const& to_wait_for) :
+            device_to_wait_for{to_wait_for}
+        {
+        }
+
+        virtual void device_added(std::shared_ptr<Device> const& device) override
+        {
+            if (device->unique_id() == device_to_wait_for->unique_id())
+                device_added_.raise();
+        }
+
+        virtual void device_changed(std::shared_ptr<Device> const&)
+        {
+        }
+        virtual void device_removed(std::shared_ptr<Device> const&)
+        {
+        }
+        virtual void changes_complete()
+        {
+        }
+
+        void wait()
+        {
+            device_added_.wait();
+        }
+    };
+
+    auto input_device_observer =
+        std::make_shared<SignalingObserver>(input_device_registry.lock()->add_device(test_device).lock());
+    input_device_hub.lock()->add_observer(input_device_observer);
+    input_device_observer->wait();
+    input_device_observer.reset();
+
+    return test_device;
 }
 }
 
@@ -48,9 +99,15 @@ struct TestLocatePointer : miral::TestServer
         add_server_init(
             [this](mir::Server& server)
             {
-                server.add_init_callback([this, &server]
-                                         { server.the_input_device_registry()->add_device(test_device); });
+                server.add_init_callback(
+                    [&server, this]
+                    {
+                        composite_event_filter = server.the_composite_event_filter();
+                        input_device_registry = server.the_input_device_registry();
+                        input_device_hub = server.the_input_device_hub();
+                    });
             });
+
         add_server_init(locate_pointer);
 
         locate_pointer
@@ -66,9 +123,11 @@ struct TestLocatePointer : miral::TestServer
     }
 
     miral::LocatePointer locate_pointer{true};
-    std::shared_ptr<mir::input::VirtualInputDevice> test_device = std::make_shared<mir::input::VirtualInputDevice>(
-        "test-device", mir::input::DeviceCapability::keyboard | mir::input::DeviceCapability::pointer);
     mir::test::Signal locate_pointer_invoked, locate_pointer_enabled;
+
+    std::weak_ptr<mir::input::CompositeEventFilter> composite_event_filter;
+    std::weak_ptr<mir::input::InputDeviceRegistry> input_device_registry;
+    std::weak_ptr<mir::input::InputDeviceHub> input_device_hub;
 };
 
 struct TestDifferentDelays : public TestLocatePointer, public WithParamInterface<std::pair<std::chrono::milliseconds, bool>>{};
@@ -77,24 +136,9 @@ TEST_P(TestDifferentDelays, responding_to_ctrl_down_with_different_delays)
 {
     auto [delay, on_pointer_locate_called] = GetParam();
 
-    std::chrono::nanoseconds up_event_time;
-    test_device->if_started_then(
-        [&up_event_time, delay](auto* sink, auto* builder)
-        {
-            auto down_event = left_ctrl_down_event(builder);
-            up_event_time = std::chrono::nanoseconds{mir_input_event_get_event_time(
-                                           reinterpret_cast<MirInputEvent*>(down_event.get()))} +
-                                       delay;
-
-            sink->handle_input(std::move(down_event));
-        });
+    locate_pointer.schedule_request();
     locate_pointer_invoked.wait_for(delay);
-    test_device->if_started_then(
-        [up_event_time](auto* sink, auto* builder)
-        {
-            auto up_event = builder->key_event(up_event_time, mir_keyboard_action_up, XKB_KEY_Control_L, 29);
-            sink->handle_input(std::move(up_event));
-        });
+    locate_pointer.cancel_request();
 
     EXPECT_THAT(locate_pointer_invoked.raised(), Eq(on_pointer_locate_called));
 }
@@ -123,8 +167,9 @@ TEST_P(TestPointerMovementTracking, cursor_location_is_tracked_on_movement)
             locate_pointer_invoked.raise();
         });
 
+    auto test_device = add_test_pointer(input_device_registry, input_device_hub);
     test_device->if_started_then(
-        [final_pointer_position](auto* sink, auto* builder)
+        [this, final_pointer_position](auto* sink, auto* builder)
         {
             sink->handle_input(builder->pointer_event(
                 std::nullopt,
@@ -135,7 +180,7 @@ TEST_P(TestPointerMovementTracking, cursor_location_is_tracked_on_movement)
                 mir_pointer_axis_source_none,
                 {},
                 {}));
-            sink->handle_input(left_ctrl_down_event(builder));
+            locate_pointer.schedule_request();
         });
 
     locate_pointer_invoked.wait_for(locate_pointer_delay + 10ms);
