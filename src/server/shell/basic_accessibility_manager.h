9604b1772230647845d04d62ef4033593134bdfb
Tarek Ismail <tarek.ismail@canonical.com>
2025-05-07 13:42:58 +0300

    Revert "Remove redundant forward declarations in `basic_accessibility_manager.h`"
    This reverts commit 2e762225614193845f2df7b5c70a1db8d92a7192.

--- src/server/shell/basic_accessibility_manager.h
+++ src/server/shell/basic_accessibility_manager.h
@@ -36,6 +36,11 @@ namespace input
 class CompositeEventFilter;
 class InputEventTransformer;
 }
+namespace shell
+{
+class MouseKeysTransformer;
+class CompositeEventFilter;
+}
 namespace options
 {
 class Option;
