a91bb948ef43f58bc436ac1a315276c5794f6b5a
Alan Griffiths <alan@octopull.co.uk>
2025-05-08 15:30:07 +0200

    Remove warts

--- tests/window_management_tests/test_minimal_window_manager.cpp
+++ tests/window_management_tests/test_minimal_window_manager.cpp
@@ -1390,13 +1390,13 @@ TEST_F(MinimalWindowManagerTest, maximized_window_respects_output_removal)
 TEST_F(MinimalWindowManagerTest, maximized_window_respects_output_unused)
 {
     tools().move_cursor_to(geom::PointF(0, 0));
-    auto const app = this->open_application("test");
+    auto const app = open_application("test");
     miral::WindowSpecification spec;
     spec.state() = mir_window_state_maximized;
     spec.size() = geom::Size(100, 100);
-    auto const window = this->create_window(app, spec);
+    auto const window = create_window(app, spec);
 
-    auto new_output_configs = this->get_initial_output_configs();
+    auto new_output_configs = get_initial_output_configs();
     new_output_configs[0].used = false;
     update_outputs(new_output_configs);
     geom::Rectangle const window_rect(window.top_left(), window.size());
@@ -1406,13 +1406,13 @@ TEST_F(MinimalWindowManagerTest, maximized_window_respects_output_unused)
 TEST_F(MinimalWindowManagerTest, maximized_window_respects_output_disconnected)
 {
     tools().move_cursor_to(geom::PointF(0, 0));
-    auto const app = this->open_application("test");
+    auto const app = open_application("test");
     miral::WindowSpecification spec;
     spec.state() = mir_window_state_maximized;
     spec.size() = geom::Size(100, 100);
-    auto const window = this->create_window(app, spec);
+    auto const window = create_window(app, spec);
 
-    auto new_output_configs = this->get_initial_output_configs();
+    auto new_output_configs = get_initial_output_configs();
     new_output_configs[0].connected = false;
     update_outputs(new_output_configs);
 
@@ -1424,12 +1424,12 @@ TEST_F(MinimalWindowManagerTest, windows_on_removed_output_are_placed_on_next_av
 {
     tools().move_cursor_to(geom::PointF(900, 100));
 
-    auto const app = this->open_application("test");
+    auto const app = open_application("test");
     miral::WindowSpecification spec;
     spec.size() = geom::Size(100, 100);
-    auto const window = this->create_window(app, spec);
+    auto const window = create_window(app, spec);
 
-    auto const new_output_configs = this->output_configs_from_output_rectangles({
+    auto const new_output_configs = output_configs_from_output_rectangles({
         mir::geometry::Rectangle{{0, 0}, {1000, 1000}}
     });
     update_outputs(new_output_configs);
