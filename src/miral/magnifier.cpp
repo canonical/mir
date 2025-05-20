04535aafc196459ef7cd478ed14dee028ff2588a
Matthew Kosarek <matt.kosarek@canonical.com>
2025-05-20 10:25:15 -0400

    Rendering a part of the screen zoomed in

--- src/miral/magnifier.cpp
+++ src/miral/magnifier.cpp
@@ -16,11 +16,14 @@
 
 #include "miral/magnifier.h"
 
-#include <utility>
 #include "mir/server.h"
+#include "mir/synchronised.h"
 #include "mir/compositor/scene.h"
+#include "mir/compositor/scene_element.h"
 #include "mir/scene/basic_surface.h"
 #include "mir/shell/surface_stack.h"
+
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace mc = mir::compositor;
 namespace mi = mir::input;
@@ -31,6 +34,76 @@ namespace geom = mir::geometry;
 
 namespace
 {
+class MagnifierRenderable : public mg::Renderable
+{
+public:
+    MagnifierRenderable(
+        std::shared_ptr<Renderable> const& wrapped,
+        float magnification,
+        geom::Rectangle const& rect)
+        : wrapped(wrapped),
+          magnification(magnification),
+          rect(rect)
+    {
+    }
+
+    ID id() const override
+    {
+        return wrapped->id();
+    }
+
+    std::shared_ptr<mg::Buffer> buffer() const override
+    {
+        return wrapped->buffer();
+    }
+
+    geom::Rectangle screen_position() const override
+    {
+        return wrapped->screen_position();
+    }
+
+    geom::RectangleD src_bounds() const override
+    {
+        return wrapped->src_bounds();
+    }
+
+    std::optional<geom::Rectangle> clip_area() const override
+    {
+        if (auto const clip = wrapped->clip_area())
+        {
+            if (rect.contains(clip.value()))
+                return clip.value();
+        }
+
+        return rect;
+    }
+
+    float alpha() const override
+    {
+        return wrapped->alpha();
+    }
+
+    glm::mat4 transformation() const override
+    {
+        return glm::scale(wrapped->transformation(), glm::vec3(magnification, magnification, 1));
+    }
+
+    bool shaped() const override
+    {
+        return wrapped->shaped();
+    }
+
+    auto surface_if_any() const -> std::optional<mir::scene::Surface const*> override
+    {
+        return wrapped->surface_if_any();
+    }
+
+private:
+    std::shared_ptr<Renderable> wrapped;
+    float magnification;
+    geom::Rectangle rect;
+};
+
 class MagnifierSurface : public ms::BasicSurface
 {
 public:
@@ -51,22 +124,52 @@ public:
             scene(scene)
     {
         set_depth_layer(mir_depth_layer_always_on_top);
-        // scene->register_compositor(this);
+        scene->register_compositor(this);
+        state.lock()->rect = rect;
+    }
+
+    ~MagnifierSurface() override
+    {
+        scene->unregister_compositor(this);
     }
 
     bool visible() const override
     {
-        return true;
+        return state.lock()->visible;
     }
 
-    mg::RenderableList generate_renderables(mc::CompositorID) const override
+    mg::RenderableList generate_renderables(mc::CompositorID id) const override
     {
-        // auto const elements = scene->scene_elements_for(this);
-        return {};
+        if (id == this)
+            return {};
+
+        mg::RenderableList renderables;
+        auto const elements = scene->scene_elements_for(this);
+        auto const rect = state.lock()->rect;
+        auto const scale = state.lock()->scale;
+        for (auto const& element : elements)
+        {
+            if (element->renderable()->screen_position().overlaps(rect))
+            {
+                renderables.push_back(std::make_shared<MagnifierRenderable>(
+                    element->renderable(),
+                    scale,
+                    rect));
+            }
+        }
+        return renderables;
     }
 
 private:
+    struct State
+    {
+        bool visible = true;
+        geom::Rectangle rect = geom::Rectangle({0, 0}, {0, 0});
+        float scale = 2.f;
+    };
+
     std::shared_ptr<mc::Scene> const scene;
+    mir::Synchronised<State> state;
 };
 }
 
@@ -80,7 +183,7 @@ public:
         std::shared_ptr<mir::ObserverRegistrar<mg::DisplayConfigurationObserver>> const& scene_display_configuration_observer_registrar)
         : surface_stack(surface_stack),
           magnifier_surface(std::make_shared<MagnifierSurface>(
-              geom::Rectangle({0, 0}, {300, 300}),
+              geom::Rectangle({0, 0}, {400, 300}),
               scene,
               cursor_image,
               scene_report,
