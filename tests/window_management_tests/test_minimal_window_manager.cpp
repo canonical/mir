14d20d376071fcbb3bb329e6d3550a1ae7457961
Alan Griffiths <alan@octopull.co.uk>
2025-06-30 15:04:42 +0100

    Add a test that uses `raise_tree()`

--- tests/window_management_tests/test_minimal_window_manager.cpp
+++ tests/window_management_tests/test_minimal_window_manager.cpp
@@ -714,25 +714,25 @@ TEST_F(MinimalWindowManagerWithOneWindow, restoring_a_minimized_window_with_no_a
     EXPECT_THAT(focused(windows[0]), Eq(true));
 }
 
-TEST_F(MinimalWindowManagerWithThreeWindows, minimizing_an_inactive_window_maintains_its_place_in_the_z_order)
+TEST_F(MinimalWindowManagerWithThreeWindows, minimizing_an_inactive_window_moves_it_to_the_back_of_the_z_order)
 {
     miral::WindowSpecification spec;
     spec.state() = mir_window_state_minimized;
     request_modify(windows[1], spec);
 
     EXPECT_THAT(is_above(windows[2], windows[1]), Eq(true));
-    EXPECT_THAT(is_above(windows[1], windows[0]), Eq(true));
+    EXPECT_THAT(is_above(windows[0], windows[1]), Eq(true));
 }
 
 TEST_F(MinimalWindowManagerWithThreeWindows,
-    minimizing_the_active_window_selects_a_new_window_and_moves_the_minimized_down_in_the_z_order)
+    minimizing_the_active_window_moves_it_to_the_back_of_the_z_order_and_selects_a_new_window)
 {
     miral::WindowSpecification spec;
     spec.state() = mir_window_state_minimized;
     request_modify(windows[2], spec);
 
     EXPECT_THAT(is_above(windows[1], windows[2]), Eq(true));
-    EXPECT_THAT(is_above(windows[2], windows[0]), Eq(true));
+    EXPECT_THAT(is_above(windows[0], windows[2]), Eq(true));
 }
 
 TEST_F(MinimalWindowManagerWithThreeWindows,
@@ -1435,4 +1435,20 @@ TEST_F(MinimalWindowManagerTest, DISABLED_windows_on_removed_output_are_placed_o
     update_outputs(new_output_configs);
     geom::Rectangle const window_rect(window.top_left(), window.size());
     EXPECT_TRUE(window_rect.overlaps(new_output_configs[0].extents()));
+}
+
+TEST_F(MinimalWindowManagerTest, raising_tree_changes_z_order)
+{
+    auto const app1 = open_application("app1");
+    auto const app2 = open_application("app2");
+
+    miral::WindowSpecification spec;
+    spec.size() = geom::Size(100, 100);
+    auto const window1 = create_window(app1, spec);
+    auto const window2 = create_window(app2, spec);
+
+    ASSERT_TRUE(is_above(window2, window1));
+
+    tools().raise_tree(window1);
+    EXPECT_TRUE(is_above(window1, window2));
 }
