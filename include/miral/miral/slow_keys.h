7d8392f836028870ba9b8c8045f181685b914dd5
Tarek Ismail <tarek.ismail@canonical.com>
2025-07-03 16:48:48 +0300

    Slow keys miral side code

---
+++ include/miral/miral/slow_keys.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright © Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 or 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MIRAL_SLOW_KEYS_H
+#define MIRAL_SLOW_KEYS_H
+
+#include <chrono>
+#include <functional>
+
+namespace mir
+{
+class Server;
+}
+
+namespace miral
+{
+class SlowKeys
+{
+public:
+    explicit SlowKeys(bool enabled_by_default);
+
+    void operator()(mir::Server& server);
+
+    SlowKeys& enable();
+    SlowKeys& disable();
+    SlowKeys& hold_delay(std::chrono::milliseconds hold_delay);
+    SlowKeys& on_key_down(std::function<void(unsigned int)>&& on_key_down);
+    SlowKeys& on_key_rejected(std::function<void(unsigned int)>&& on_key_rejected);
+    SlowKeys& on_key_accepted(std::function<void(unsigned int)>&& on_key_accepted);
+
+private:
+    struct Self;
+    std::shared_ptr<Self> self;
+};
+}
+
+#endif // MIRAL_SLOW_KEYS_H
---
+++ src/miral/slow_keys.cpp
@@ -0,0 +1,144 @@
+/*
+ * Copyright © Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 or 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "miral/slow_keys.h"
+#include "mir/options/option.h"
+#include "mir/server.h"
+#include "mir/shell/accessibility_manager.h"
+#include "mir/shell/slow_keys_transformer.h"
+
+#include "mir/synchronised.h"
+
+struct miral::SlowKeys::Self
+{
+    explicit Self(bool enabled) :
+        state{State{enabled}}
+    {
+    }
+
+    struct State
+    {
+        explicit State(bool enabled) :
+            enabled{enabled}
+        {
+        }
+
+        bool enabled;
+        std::chrono::milliseconds hold_delay{200};
+        std::function<void(unsigned int)> on_key_down, on_key_rejected, on_key_accepted;
+    };
+
+    mir::Synchronised<State> state;
+    std::weak_ptr<mir::shell::AccessibilityManager> accessibility_manager;
+};
+
+miral::SlowKeys::SlowKeys(bool enabled_by_default) :
+    self{std::make_shared<Self>(enabled_by_default)}
+{
+}
+
+miral::SlowKeys& miral::SlowKeys::enable()
+{
+    self->state.lock()->enabled = true;
+    if (auto const accessibility_manager = self->accessibility_manager.lock())
+        accessibility_manager->slow_keys_enabled(true);
+
+    return *this;
+}
+
+miral::SlowKeys& miral::SlowKeys::disable()
+{
+    self->state.lock()->enabled = false;
+    if (auto const accessibility_manager = self->accessibility_manager.lock())
+        accessibility_manager->slow_keys_enabled(false);
+
+    return *this;
+}
+
+miral::SlowKeys& miral::SlowKeys::hold_delay(std::chrono::milliseconds hold_delay)
+{
+    self->state.lock()->hold_delay = hold_delay;
+    if (auto const accessibility_manager = self->accessibility_manager.lock())
+        accessibility_manager->slow_keys().delay(hold_delay);
+
+    return *this;
+}
+
+miral::SlowKeys& miral::SlowKeys::on_key_down(std::function<void(unsigned int)>&& on_key_down)
+{
+    auto const state = self->state.lock();
+    state->on_key_down = std::move(on_key_down);
+    if (auto const accessibility_manager = self->accessibility_manager.lock())
+        accessibility_manager->slow_keys().on_key_down(std::move(state->on_key_down));
+
+    return *this;
+}
+
+miral::SlowKeys& miral::SlowKeys::on_key_rejected(std::function<void(unsigned int)>&& on_key_rejected)
+{
+    auto const state = self->state.lock();
+    state->on_key_rejected = std::move(on_key_rejected);
+    if (auto const accessibility_manager = self->accessibility_manager.lock())
+        accessibility_manager->slow_keys().on_key_rejected(std::move(state->on_key_rejected));
+
+    return *this;
+}
+
+miral::SlowKeys& miral::SlowKeys::on_key_accepted(std::function<void(unsigned int)>&& on_key_accepted)
+{
+    auto const state = self->state.lock();
+    state->on_key_accepted = std::move(on_key_accepted);
+    if (auto const accessibility_manager = self->accessibility_manager.lock())
+        accessibility_manager->slow_keys().on_key_accepted(std::move(state->on_key_accepted));
+
+    return *this;
+}
+
+void miral::SlowKeys::operator()(mir::Server& server)
+{
+    constexpr auto* enable_slow_keys_opt = "enable-slow-keys";
+    constexpr auto* slow_keys_hold_delay = "slow-keys-hold-delay";
+
+    {
+        auto const state = self->state.lock();
+        server.add_configuration_option(
+            enable_slow_keys_opt,
+            "Enable slow keys (presses not registering unless keys are held for a certain period)",
+            state->enabled);
+        server.add_configuration_option(
+            slow_keys_hold_delay,
+            "delay required before a press registers",
+            static_cast<int>(state->hold_delay.count()));
+    }
+
+    server.add_init_callback(
+        [&server, this]
+        {
+            self->accessibility_manager = server.the_accessibility_manager();
+
+            if (server.get_options()->get<bool>(enable_slow_keys_opt))
+                enable();
+
+            hold_delay(std::chrono::milliseconds(server.get_options()->get<int>(slow_keys_hold_delay)));
+
+            if (auto& on_key_down_ = self->state.lock()->on_key_down)
+                on_key_down(std::move(on_key_down_));
+            if (auto& on_key_rejected_ = self->state.lock()->on_key_rejected)
+                on_key_rejected(std::move(on_key_rejected_));
+            if (auto& on_key_accepted_ = self->state.lock()->on_key_accepted)
+                on_key_accepted(std::move(on_key_accepted_));
+        });
+}
