6ce46542a21d6a50735e70c22a08fb946d151d51
Tarek Ismail <tarek.ismail@canonical.com>
2025-07-10 18:49:44 +0300

    Fix some more symbols, add a bunch of symbols to the hidden symbol list

--- tools/symbols_map_generator/main.py
+++ tools/symbols_map_generator/main.py
@@ -34,6 +34,18 @@ HIDDEN_SYMBOLS = {
     "miral::CursorScale::CursorScale*;",
     "miral::InputConfiguration::InputConfiguration*;",
     "miral::OutputFilter::OutputFilter*;",
+    
+    "miral::SimulatedSecondaryClick::SimulatedSecondaryClick*;",
+    "miral::SimulatedSecondaryClick::disable*;",
+    "miral::SimulatedSecondaryClick::disabled*;",
+    "miral::SimulatedSecondaryClick::displacement_threshold*;",
+    "miral::SimulatedSecondaryClick::enable*;",
+    "miral::SimulatedSecondaryClick::enabled*;",
+    "miral::SimulatedSecondaryClick::hold_duration*;",
+    "miral::SimulatedSecondaryClick::on_hold_cancel*;",
+    "miral::SimulatedSecondaryClick::on_hold_start*;",
+    "miral::SimulatedSecondaryClick::on_secondary_click*;",
+    "miral::SimulatedSecondaryClick::operator*;",
 }
 
 class HeaderDirectory(TypedDict):
