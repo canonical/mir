501562ab51fc042d28e1fde33a418c35b9817bbb
Matt Kosarek <matt.kosarek@canonical.com>
2025-07-10 14:19:40 -0400

    feature: implement sticky keys accessibility feature

---
+++ include/miral/miral/sticky_keys.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright © Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 or 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MIRAL_STICKY_KEYS_H
+#define MIRAL_STICKY_KEYS_H
+
+#include <memory>
+
+namespace mir
+{
+class Server;
+}
+
+namespace miral
+{
+namespace live_config { class Store; }
+
+class StickyKeys
+{
+public:
+    /// Creates a 'StickyKeys' instance that is enabled by default.
+    static auto enabled() -> StickyKeys;
+
+    // Creates a 'StickyKeys' instance that is disabled by default.
+    static auto disabled() -> StickyKeys;
+
+    /// Construct a 'StickyKeys' instance with access to a live config store.
+    explicit StickyKeys(live_config::Store& config_store);
+
+    void operator()(mir::Server& server);
+
+    /// Enables sticky keys.
+    StickyKeys& enable();
+
+    /// Disables sticky keys
+    StickyKeys& disable();
+
+    /// When set to true, clicking two modifier keys are once will result
+    /// in sticky keys being temporarily disabled until all keys are released.
+    /// Defaults to true.
+    StickyKeys& should_disable_if_two_keys_are_pressed_together(bool on);
+
+private:
+    class Self;
+    explicit StickyKeys(std::shared_ptr<Self>);
+    std::shared_ptr<Self> self;
+};
+}
+
+#endif //MIRAL_STICKY_KEYS_H
