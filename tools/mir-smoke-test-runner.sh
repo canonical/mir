aa6689c42005198d464a98657edda2d22773905a
Alan Griffiths <alan.griffiths@canonical.com>
2025-08-26 10:54:17 +0100

    Tidy up

--- tools/mir-smoke-test-runner.sh
+++ tools/mir-smoke-test-runner.sh
@@ -20,8 +20,8 @@ function run_tests()
   for client in ${client_list}; do
     echo running client ${client}
     date --utc --iso-8601=seconds | xargs echo "[timestamp] Start :" ${client}
-    echo WAYLAND_DISPLAY=${wayland_display} ${root}/mir_demo_server ${options} --test-client ${client}
-    if   WAYLAND_DISPLAY=${wayland_display} ${root}/mir_demo_server ${options} --test-client ${client}
+    echo MIR_SERVER_PLATFORM_DISPLAY_LIBS="mir:$1" WAYLAND_DISPLAY=${wayland_display} ${root}/mir_demo_server ${options} --test-client ${client}
+    if   MIR_SERVER_PLATFORM_DISPLAY_LIBS="mir:$1" WAYLAND_DISPLAY=${wayland_display} ${root}/mir_demo_server ${options} --test-client ${client}
     then
       echo "I: [PASSED]" ${client}
     else
@@ -46,27 +46,27 @@ if [ -z "$XDG_RUNTIME_DIR" ]; then
 fi
 
 echo Test virtual platform
-MIR_SERVER_PLATFORM_DISPLAY_LIBS=mir:virtual MIR_SERVER_VIRTUAL_OUTPUT=1200x900 run_tests virtual
+MIR_SERVER_VIRTUAL_OUTPUT=1200x900 run_tests virtual
 
 if [ -n "${DISPLAY}" ]
 then
   echo Test X11 platform
-  MIR_SERVER_PLATFORM_DISPLAY_LIBS=mir:x11 run_tests x11
+  run_tests x11
 fi
 
 if [ -O "${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY:-wayland-0}" ]
 then
   echo Test Wayland platform
-  MIR_SERVER_PLATFORM_DISPLAY_LIBS=mir:wayland MIR_SERVER_WAYLAND_HOST="${WAYLAND_DISPLAY:-wayland-0}" run_tests wayland
+  MIR_SERVER_WAYLAND_HOST="${WAYLAND_DISPLAY:-wayland-0}" run_tests wayland
 fi
 
 if [ -z "$XDG_CURRENT_DESKTOP" ]
 then
   echo Test gbm-kms platform
-  MIR_SERVER_PLATFORM_DISPLAY_LIBS=mir:gbm-kms run_tests gbm-kms
+  run_tests gbm-kms
 
   echo Test atomic-kms platform
-  MIR_SERVER_PLATFORM_DISPLAY_LIBS=mir:atomic-kms run_tests atomic-kms
+  run_tests atomic-kms
 fi
 
 date --utc --iso-8601=seconds | xargs echo "[timestamp] End time :"
@@ -78,5 +78,5 @@ then
         echo "I:     ${client}"
     done
 fi
-echo "I: Smoke testing complete with returncode ${mir_rc}, platforms:${platforms}"
+echo "I: Smoke testing complete with returncode ${mir_rc}, platforms tested:${platforms}"
 exit ${mir_rc}
