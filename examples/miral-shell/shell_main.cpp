3735ad0f4bbe91857a38bbd40989fc38d6055149
Tarek Ismail <tarek.ismail@canonical.com>
2025-10-24 17:00:51 +0300

    Fix `LocatePointer` being invoked with extra unintended keys pressed
    Checks for other modifiers pressed / after ctrl, and keys pressed after
    ctrl.
    
    Co-authored-by: Matthew Kosarek <matt.kosarek@canonical.com>
    Co-authored-by: Alan Griffiths <alan@octopull.co.uk>

--- examples/miral-shell/shell_main.cpp
+++ examples/miral-shell/shell_main.cpp
@@ -315,23 +315,45 @@ int main(int argc, char const* argv[])
 
     auto const locate_pointer_filter = [&locate_pointer](auto const* keyboard_event)
     {
-            auto const keysym = mir_keyboard_event_keysym(keyboard_event);
-            if (keysym != XKB_KEY_Control_R && keysym != XKB_KEY_Control_L)
-                return false;
+        auto const keysym = mir_keyboard_event_keysym(keyboard_event);
+        auto const action = mir_keyboard_event_action(keyboard_event);
+        auto const modifiers = mir_keyboard_event_modifiers(keyboard_event);
 
-            switch (mir_keyboard_event_action(keyboard_event)) {
-                case mir_keyboard_action_down:
-                    locate_pointer.schedule_request();
-                    break;
-                case mir_keyboard_action_up:
-                    locate_pointer.cancel_request();
-                    break;
+        auto only_ctrl_down = [modifiers]
+        {
+            auto constexpr ctrls_mask = mir_input_event_modifier_ctrl | mir_input_event_modifier_ctrl_left |
+                                    mir_input_event_modifier_ctrl_right;
+            auto const any_ctrl_down = modifiers & ctrls_mask;
+            auto const any_other_modifier_down = modifiers & ~ctrls_mask;
 
-                default:
-                    return false;
+            return any_ctrl_down && !any_other_modifier_down;
+        }();
+
+
+        auto other_key_down = false;
+        // If ctrl is the only modifier pressed, make sure the keysym is also ctrl
+        if (only_ctrl_down)
+        {
+            switch (action)
+            {
+            case mir_keyboard_action_down:
+                other_key_down = (keysym != XKB_KEY_Control_R && keysym != XKB_KEY_Control_L);
+                break;
+            case mir_keyboard_action_up:
+                only_ctrl_down = false;
+                break;
+
+            default:
+                break;
             }
+        }
 
-            return false;
+        if (only_ctrl_down && !other_key_down)
+            locate_pointer.schedule_request();
+        else
+            locate_pointer.cancel_request();
+
+        return false;
     };
 
     return runner.run_with(
