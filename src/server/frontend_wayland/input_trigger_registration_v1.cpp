853ebd11b11c19e6f2db7f79f99d72633df1f4e2
Tarek Ismail <tarek.ismail@canonical.com>
2025-11-13 16:46:47 +0200

    Handle down and up events properly, ignore repeat events

--- src/server/frontend_wayland/input_trigger_registration_v1.cpp
+++ src/server/frontend_wayland/input_trigger_registration_v1.cpp
@@ -335,9 +335,21 @@ private:
                     return false;
                 }
 
+                if (key_event->action() == mir_keyboard_action_down)
+                {
+                    action.value().send_begin_event(bogus_time, bogus_activation_token);
+                    began = true;
+                    return true;
+                }
 
-                action.value().send_begin_event(bogus_time, bogus_activation_token);
-                began = true;
+                if (key_event->action() == mir_keyboard_action_up && began)
+                {
+                    action.value().send_end_event(bogus_time, bogus_activation_token);
+                    began = false;
+                    return true;
+                }
+
+                return false;
             }
 
             // Invalid token
