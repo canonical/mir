2e4f129b6c4f9931e7e20154566dbfa1a82b2be1
Robert Ancell <robert.ancell@canonical.com>
2025-11-17 16:27:19 +1300

    Use mkdtemp for a temp dir

--- tests/unit-tests/frontend_wayland/test_g_desktop_file_cache.cpp
+++ tests/unit-tests/frontend_wayland/test_g_desktop_file_cache.cpp
@@ -43,11 +43,8 @@ struct GDesktopFileCache : Test
     static void SetUpTestSuite()
     {
         // Establish the temporary directory
-        char tmp_file_name[] = "/tmp/mir-XXXXXX";
-        int fd = mkstemp(tmp_file_name);
-        close(fd);
-        std::filesystem::path tmp_dir_path {std::filesystem::temp_directory_path() /= tmp_file_name};
-        std::filesystem::create_directories(tmp_dir_path);
+        char tmp_dir_path[] = "/tmp/mir-XXXXXX";
+        mkdtemp(tmp_dir_path);
         std::string desktop_file_directory_name = tmp_dir_path;
         setenv("XDG_DATA_DIRS", desktop_file_directory_name.c_str(), 1);
         applications_dir = desktop_file_directory_name + "/applications";
