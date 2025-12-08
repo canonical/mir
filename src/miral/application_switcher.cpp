canonical/mir

commit 91844b9560965fc83a64ba1d91bcf631323939a2

Pull request feedback

src/miral/application_switcher.cpp
@@ -846,7 +846,7 @@ class miral::StandardApplicationSwitcher::Self
 {
 public:
     explicit Self(KeybindConfiguration const& keybind_configuration)
-        : startup_internal_client(switcher, switcher, [this] { switcher.stop(); }),
+        : startup_internal_client(switcher, switcher, [switcher=switcher] { switcher.stop(); }),
           keybind_configuration(keybind_configuration),
           keyboard_event_filter([&, is_running = false](MirKeyboardEvent const* key_event) mutable
          {

commit 8366f94c8d81ed596b53fe31ded60df4abd1d18d

Stop the app switcher only when `Self` is destroyed.
Fixes a bug introduced in 91844b9560965fc83a64ba1d91bcf631323939a2 where
the app switcher was assumed to act like a `shared_ptr`. Instead, on
destruction, it would call `self->stop()` which should have been called
in `~Self` instead.

src/miral/application_switcher.cpp
@@ -688,6 +688,11 @@ public:
             mir::log_error("Failed to create shutdown notifier");
     }
 
+    ~Self()
+    {
+        stop();
+    }
+
     void run_client(wl_display* display)
     {
         {
@@ -795,7 +800,6 @@ miral::ApplicationSwitcher::ApplicationSwitcher()
 
 miral::ApplicationSwitcher::~ApplicationSwitcher()
 {
-    self->stop();
 }
 
 miral::ApplicationSwitcher::ApplicationSwitcher(ApplicationSwitcher const&) = default;