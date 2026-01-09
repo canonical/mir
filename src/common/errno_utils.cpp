10ee7cd601fe189c5d3df968dfa0b426d0a5b87e
Ali ihsan Cengiz <aliihsan_29@hotmail.com>
2026-01-09 15:32:37 +0100

    Force POSIX compilant strerror_r

--- src/common/errno_utils.cpp
+++ src/common/errno_utils.cpp
@@ -13,35 +13,15 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#undef _GNU_SOURCE
+#define _POSIX_C_SOURCE 200112L
 
 #include <mir/errno_utils.h>
 
 #include <cstring>
 
-namespace
-{
-// Since we are normalizing between GNU/POSIX impl, one of these functions will be unused at compile time
-
-[[maybe_unused]]
-inline auto strerror_r_to_cstr(int ret, const char* buf) noexcept
-{
-    // POSIX
-    return ret ? "Unknown error" : buf;
-}
-
-[[maybe_unused]]
-inline auto strerror_r_to_cstr(char* ret, const char* /*buf*/) noexcept
-{
-    // GNU
-    return ret ? ret : "Unknown error";
-}
-
-}
-
 auto mir::errno_to_cstr(int err) noexcept -> const char*
 {
     thread_local char buf[256] = {};
-
-    auto ret = ::strerror_r(err, buf, sizeof(buf));
-    return strerror_r_to_cstr(ret, buf);
+    return ::strerror_r(err, buf, sizeof(buf)) ? "Unknown error" : buf;
 }
