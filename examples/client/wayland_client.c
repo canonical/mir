d46369d50ff97c9282f8dadb80cc27051a85d49e
Alan Griffiths <alan.griffiths@canonical.com>
2026-01-09 11:28:33 +0000

    Reduce scope of _GNU_SOURCE

--- examples/client/wayland_client.c
+++ examples/client/wayland_client.c
@@ -14,10 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
-
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
