e3d862d2c4c361b1b1e112b4b5a0363442ae26ae
Michał Sawicz <michal.sawicz@canonical.com>
2026-04-23 15:03:41 +0200

    doc: migrate to canonical.com (#4899)
    Migrate docs to canonical.com/mir/docs
    
    Check https://staging.canonical.com/mir/docs/latest (VPN required).
    Verify all paths from
    https://discourse.ubuntu.com/t/mir-documentation/27559#heading--navigation
    get a proper redirect to the new location.
    
    - [ ] Tests added and pass
    - [x] Adequate documentation added
    - [ ] (optional) Added Screenshots or videos

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -80,7 +80,7 @@ copyright = '%s, %s' % (datetime.date.today().year, author)
 # NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
 #       and is used by social media platforms; see https://ogp.me/
 
-ogp_site_url = "https://canonical-mir.readthedocs-hosted.com/"
+ogp_site_url = "https://canonical.com/mir/docs/"
 
 
 # Preview name of the documentation website
@@ -186,7 +186,7 @@ html_theme_options = {
 # TODO: If your documentation is hosted on https://docs.ubuntu.com/,
 #       uncomment and update as needed.
 
-# slug = ''
+slug = 'mir/docs'
 
 #######################
 # Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
@@ -194,11 +194,11 @@ html_theme_options = {
 
 # Use RTD canonical URL to ensure duplicate pages have a specific canonical URL
 
-html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+html_baseurl = "https://canonical.com/mir/docs/"
 
 # sphinx-sitemap uses html_baseurl to generate the full URL for each page:
 
-sitemap_url_scheme = '{link}'
+sitemap_url_scheme = f"{os.environ.get('READTHEDOCS_VERSION', '')}/{{link}}"
 
 # Include `lastmod` dates in the sitemap:
 
@@ -219,7 +219,7 @@ sitemap_excludes = [
 # Template and asset locations #
 ################################
 
-# html_static_path = ["_static"]
+html_static_path = ["_static"]
 templates_path = ["_templates"]
 
 
@@ -340,6 +340,7 @@ html_css_files = [
 # Adds custom JavaScript files, located under 'html_static_path'
 
 html_js_files = [
+    "js/overwrite_links.js",
     "https://assets.ubuntu.com/v1/287a5e8f-bundle.js",
 ]
 
@@ -398,6 +399,7 @@ if os.path.exists('./reuse/substitutions.yaml'):
 # Add configuration for intersphinx mapping
 # Map only the Sphinx documentation sets that you need to link to from your docs set.
 intersphinx_mapping = {
+    'frame': ("https://ubuntu.com/frame/docs/24", None),
     'checkbox': ("https://canonical-checkbox.readthedocs-hosted.com/latest/", None),
     'server': ('https://documentation.ubuntu.com/server/', None),
     'snapcraft': ('https://documentation.ubuntu.com/snapcraft/stable/', None),
