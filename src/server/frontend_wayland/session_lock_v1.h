96b196a12fa19b1012d989a5ddd7d796c18667e0
Matthew Kosarek <matt.kosarek@canonical.com>
2026-04-29 13:27:27 -0400

    Migrate session lock v1

--- src/server/frontend_wayland/session_lock_v1.h
+++ src/server/frontend_wayland/session_lock_v1.h
@@ -17,7 +17,8 @@
 #ifndef MIR_FRONTEND_SESSION_LOCK_V1_H
 #define MIR_FRONTEND_SESSION_LOCK_V1_H
 
-#include "ext-session-lock-v1_wrapper.h"
+#include "ext_session_lock_v1.h"
+#include "client.h"
 
 namespace mir
 {
@@ -38,11 +39,10 @@ class OutputManager;
 class SessionLockV1;
 class SurfaceRegistry;
 
-class SessionLockManagerV1 : public wayland::SessionLockManagerV1::Global
+class SessionLockManagerV1
 {
 public:
     SessionLockManagerV1(
-        wl_display* display,
         Executor& wayland_executor,
         std::shared_ptr<shell::Shell> const& shell,
         std::shared_ptr<scene::SessionLock> const& session_lock,
@@ -63,9 +63,9 @@ public:
     bool try_relinquish_locking_privilege(SessionLockV1* lock);
     bool try_unlock(SessionLockV1* lock);
     bool is_active_lock(SessionLockV1* lock);
+    auto create(std::shared_ptr<wayland_rs::Client> const& client) -> std::shared_ptr<wayland_rs::ExtSessionLockManagerV1Impl>;
 private:
     class Instance;
-    void bind(wl_resource* new_resource);
 
     SessionLockV1* active_lock = nullptr;
 };
