f64ad3289522e8e6005cf14ce8a1240f1e1e20ec
Matt Kosarek <matt.kosarek@canonical.com>
2026-08-13 08:40:41 -0400

    Remove fail_surface_for_sink_param

--- tests/unit-tests/compositor/test_basic_screen_shooter.cpp
+++ tests/unit-tests/compositor/test_basic_screen_shooter.cpp
@@ -69,10 +69,6 @@ struct BasicScreenShooter : Test
             .WillByDefault(
                 [this](mg::DisplaySink& sink, auto const&) -> std::unique_ptr<mg::gl::OutputSurface>
                 {
-                    if (fail_surface_for_sink)
-                    {
-                        BOOST_THROW_EXCEPTION((std::runtime_error{"Throw in surface_for_sink"}));
-                    }
                     if (auto cpu_provider = sink.acquire_compatible_allocator<mg::CPUAddressableDisplayAllocator>())
                     {
                         captured_sink = &sink;
@@ -129,7 +125,6 @@ struct BasicScreenShooter : Test
     }
 
     int renderers_created{0};
-    bool fail_surface_for_sink{false};
     std::vector<geom::Size> sink_sizes;
     mg::DisplaySink* captured_sink{nullptr};
     mg::CPUAddressableDisplayAllocator* captured_allocator{nullptr};
