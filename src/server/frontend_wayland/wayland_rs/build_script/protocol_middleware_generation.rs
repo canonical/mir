c67a8d169f34aa55e5570c053f5e11a1a754df35
Matthew Kosarek <matt.kosarek@canonical.com>
2026-08-25 14:15:02 -0400

    (wayland-rs) fix sending an event with a null object param leading to panic

--- src/server/frontend_wayland/wayland_rs/build_script/protocol_middleware_generation.rs
+++ src/server/frontend_wayland/wayland_rs/build_script/protocol_middleware_generation.rs
@@ -185,10 +185,45 @@ fn generate_extension_method_for_event(event: &WaylandEvent, interface_name: &st
         })
         .collect();
 
+    // Sending an event whose target or object argument has been destroyed panics
+    // wayland-backend ("Attempting to send an event with objects from wrong client"),
+    // e.g. wl_keyboard.leave sent from the focused surface's own destruction path.
+    // libwayland tolerated such events (clients ignore ids of zombie objects), so
+    // match that by dropping the event instead.
+    //
+    // TODO: If we want a more "correct" Rust version in the future, we will need
+    // to change the behavior of surface focus. The issue here is that we send out
+    // a surface "leave" event on a surface that lost focus due to its own destruction.
+    // This is not necessarily wrong (libwayland tolerates it!).
+    let object_guards: Vec<TokenStream> = event
+        .args
+        .iter()
+        .filter(|arg| matches!(arg.type_, WaylandArgType::Object))
+        .map(|arg| {
+            let name = format_ident!("{}", sanitize_identifier(&arg.name));
+            if arg.allow_null.unwrap_or(false) {
+                quote! {
+                    if let Some(__obj) = unsafe { #name.as_ref() } {
+                        if !__obj.wrapped.is_alive() {
+                            return;
+                        }
+                    }
+                }
+            } else {
+                quote! {
+                    if !#name.wrapped.is_alive() {
+                        return;
+                    }
+                }
+            }
+        })
+        .collect();
+
     // Generate the ffi code that will call into the underlying method that is defined on the object
     // The first item is the definition, and the second is the method declaration for the trait.
     quote! {
         pub fn #event_name(&mut self, #(#ffi_args),*) {
+            #(#object_guards)*
             self.wrapped.#event_name(#(#rust_args),*)
         }
     }
