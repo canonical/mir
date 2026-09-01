e5eb5ec343d4f116e134e3cb99cab7b15b353fef
Tom Anderson <tom.anderson@canonical.com>
2026-09-01 17:17:38 +1000

    revert rust changes

--- src/server/frontend_wayland/wayland_rs/build_script/main.rs
+++ src/server/frontend_wayland/wayland_rs/build_script/main.rs
@@ -45,7 +45,6 @@ fn main() {
     println!("cargo:rerun-if-changed=build_script/ffi_generation.rs");
     println!("cargo:rerun-if-changed=build_script/cpp_protocol_generation.rs");
     println!("cargo:rerun-if-changed=build_script/dispatch_generator.rs");
-    println!("cargo:rerun-if-changed=build_script/helpers.rs");
     println!("cargo:rerun-if-changed=build_script/protocol_generator.rs");
     println!("cargo:rerun-if-changed=build_script/protocol_middleware_generation.rs");
     println!("cargo:rerun-if-changed=build_script/wayland_server_generation.rs");
